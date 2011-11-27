d73d3889da67aaba58cd64457ab16a82e74acbcb
Michael Eichberg <mail@michael-eichberg.de>
2011-11-27 11:03:37 +0100

    reformatted the class level  comment for Scaladoc

--- src/main/de/tud/cs/st/bat/resolved/ElementValue.scala
+++ src/main/de/tud/cs/st/bat/resolved/ElementValue.scala
@@ -38,31 +38,29 @@ import scala.xml.Node
 /**
  * The values of an annotation.
  *
- * <p>
- * 	From the specification:
- * 	<pre>
- * element_value {
- * u1 tag;
- * union {
- * u2   const_value_index;
+ * '''From the Specification'''
+ * {{{
+ * 	element_value {
+ * 		u1 tag;
+ * 		union {
+ * 			u2   const_value_index;
  *
- * {
- * u2 type_name_index;
- * u2 const_name_index;
- * } enum_const_value;
+ * 			{
+ * 				u2 type_name_index;
+ * 				u2 const_name_index;
+ * 			} enum_const_value;
  *
- * u2 class_info_index;
+ * 			u2 class_info_index;
  *
- * annotation annotation_value;
+ * 			annotation annotation_value;
  *
- * {
- * u2    num_values;
- * element_value values[num_values];
- * } array_value;
- * } value;
- * }
- * 	</pre>
- * </p>
+ * 			{
+ * 				u2    num_values;
+ * 				element_value values[num_values];
+ * 			} array_value;
+ * 		} value;
+ * 	}
+ * }}}
  *
  * @author Michael Eichberg
  */
