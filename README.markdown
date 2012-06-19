7b74a7eeda957abf2349b25749ac9400416c775e
Michael Eichberg <mail@michael-eichberg.de>
2012-06-19 16:39:59 +0200

    adopting a standard maven directory structure

--- README.markdown
+++ README.markdown
@@ -1,29 +1,9 @@
 # Overview
-BAT is a Scala library written in Scala 2.9.x for analyzing Java Bytecode.
+BAT is library for analyzing Java Bytecode. BAT is written in Scala.
 
-BAT provides different representations of Java Bytecode to support a project's special needs.
-BAT generally supports Java 6 class files and contains preliminary support for Java 7 as of Jan. 2012.
+BAT generally supports Java 6 class files and contains preliminary support for Java 7 as of June 2012.
 
-Currently, four representations are provided/are supported:
+BAT in particular provides support for the analysis of static code dependencies. Furthermore, it 
+provides extensive support for pattern-matching on Java Bytecode.
 
-1. Native/Canonical - Basically, a one to one representation of the class file; Java Bytecode's constant pool is not resolved and instructions are not made explicit.
-
-2. Resolved - An object-oriented representation that facilitates writing analysis against the bytecode. 
-            If you want to write bytecode analyses in Scala then we recommend to use this representation. 
-            The most important property of this representation is that the constant pool is completely resolved and all instructions are explicitly represented.
-            This representation also provides functionality to extract a class file's dependencies and to create a project's class hierarchy.
-
-3. Prolog - A representation of Java Bytecode as a set of Prolog facts. This representation builds upon the resolved representation and basically represents all information that are represented by the resolved representation.
-
-4. XML - A representation of Java Bytecode as XML. Currently, instructions are not represented.
-
-If you need "your own" representation or if you miss a certain feature, just write us an email. Adding further representations is straightforward, because BAT was engineered for this particular use case.
-
-*Currently, BAT does not support manipulating existing Java class files and we have no intentions to add this feature in the near future.*
-
-**Using BAT**
-
-If you just want to use, but not otherwise extend BAT, it is recommended that you call "ant package" and use the generated jar file. At run time no further packages are required. 
-
-To compile BAT just call the corresponding ANT target. Please note, that BAT makes heavy use of code generation and some parts of the hand-written code depends on the generated code. The generated code is found under "build/src/" and if you want to edit BAT using your favorite IDE, make sure that you include the folder with the generated source files.
-
+If you miss a certain feature, just write us an email. 
---
+++ src/main/scala/de/tud/cs/st/bat/resolved/IREM.scala
@@ -0,0 +1,90 @@
+/*
+--------------------------------------------------------------------------
+
+		THIS FILE IS AUTO GENERATED - DO NOT CHANGE MANUALLY!
+		Generated:  2012-06-19T12:01:56.502+02:00
+		Source File: GenerateInstructionClasses.xsl
+
+--------------------------------------------------------------------------
+
+ License (BSD Style License):
+ Copyright (c) 2009, 2011
+ Software Technology Group
+ Department of Computer Science
+ Technische Universität Darmstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ - Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ - Neither the name of the Software Technology Group or Technische
+   Universität Darmstadt nor the names of its contributors may be used to
+   endorse or promote products derived from this software without specific
+   prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+package de.tud.cs.st.bat.resolved
+
+import de.tud.cs.st.util.ControlAbstractions.repeat
+
+import de.tud.cs.st.bat.resolved.InstructionExceptions._
+
+/**
+ * Remainder int.
+ *
+ * @version Generator: 0.10.0 (Last change: June, 16 2012)
+ */
+case object IREM
+extends Instruction {
+
+	def opcode : Int = 112
+
+	def mnemonic : String = "irem"
+
+	lazy val exceptions : List[ObjectType] =  Nil
+
+
+	def toXML(pc : Int) =
+		<irem pc={ pc.toString }>
+		</irem>
+
+
+	def toProlog[F,T,A <: T](
+		factory : PrologTermFactory[F,T,A],
+		declaringEntityKey : A,
+		pc : Int,
+		pc_to_seqNo : Array[Int]
+	) : F = {
+
+		import factory._
+
+		Fact(
+			"instr",
+			declaringEntityKey,
+			IntegerAtom(pc_to_seqNo(pc)),
+			Term(
+				"rem",
+				StringAtom("int")
+			)
+		
+		)
+	}
+}
