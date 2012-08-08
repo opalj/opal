0cfebc06ca6cea6fb290c00dfe0bfea198f443be
Michael Eichberg <mail@michael-eichberg.de>
2012-08-08 10:42:20 +0200

    a more complex example of a serializable inner class

--- demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/bugs/InnerSerializableClass.java
+++ demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/bugs/InnerSerializableClass.java
@@ -1,17 +1,23 @@
 package de.tud.cs.st.bat.resolved.analyses.bugs;
 
 /**
- * Democode for the issue: "A non-seriablizable class has a serializable inner class". This situation is
- * problematic, because the serialization of the inner class would require – due to the link to its outer
- * class – always the serialization of the outer class which will, however, fail.
+ * Democode for the issue: "A non-seriablizable class has a serializable inner class". This
+ * situation is problematic, because the serialization of the inner class would require – due to the
+ * link to its outer class – always the serialization of the outer class which will, however, fail.
  * 
  * @author Michael Eichberg
  */
-public class InnerSerializableClass {
+public class InnerSerializableClass implements java.io.Serializable {
 
-	class InnerClass implements java.io.Serializable {
+	private static final long serialVersionUID = -1182351106716239966L;
 
-		static final long serialVersionUID = 1l;
+	class SomeInnerClass {
+
+		class InnerInnerClass implements java.io.Serializable {
+
+			private static final long serialVersionUID = 1l;
+
+		}
 
 		public String toString() {
 			return "InnerSerializableClass.InnerClass";
@@ -19,4 +25,3 @@ public class InnerSerializableClass {
 	}
 
 }
- 
