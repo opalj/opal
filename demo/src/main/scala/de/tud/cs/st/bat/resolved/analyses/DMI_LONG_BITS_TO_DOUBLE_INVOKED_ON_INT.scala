384a8c5a14815e2248a7bc9dd3283c0bf389c338
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-08 13:09:43 +0200

    Fixed errors

--- demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT.scala
+++ demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT.scala
@@ -10,6 +10,7 @@ import de.tud.cs.st.bat.resolved._
  *
  */
 object DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT
+        extends Analysis
 {
 
     val doubleType = ObjectType("java/lang/Double")
@@ -17,15 +18,22 @@ object DMI_LONG_BITS_TO_DOUBLE_INVOKED_ON_INT
     val longBitsToDoubleDescriptor = MethodDescriptor(List(LongType), doubleType)
 
 
-    def analyze(classFiles: Traversable[ClassFile], classHierarchy: ClassHierarchy) = {
+    def analyze(project: Project) = {
+        val classFiles: Traversable[ClassFile] = project.classFiles
         for (classFile ← classFiles;
              method ← classFile.methods if method.body.isDefined
         ) yield {
             val calls =
-                for (i ← 1 to method.body.get.instructions.length - 1;
-                     INVOKESTATIC(`doubleType`, "longBitsToDouble", `longBitsToDoubleDescriptor`) ← method.body.get
-                             .instructions(i);
-                     I2L ← method.body.get.instructions(i - 1)
+                for (i ← 1 to method.body.get.instructions.length - 1 if (
+                        (method.body.get.instructions(i) match {
+                            case INVOKESTATIC(`doubleType`, "longBitsToDouble", `longBitsToDoubleDescriptor`) => true
+                            case _ => false
+                        }) &&
+                                (method.body.get.instructions(i - 1) match {
+                                    case I2L => true
+                                    case _ => false
+                                })
+                        )
                 ) yield i
             (classFile, method, calls)
         }
