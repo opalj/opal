f8eae51c347c3a33eb78f2185a47f5cd31d9ebe5
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-09 14:17:54 +0200

    Implemented checker for uninitialized field access from super constructor calls

--- demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/BaseAnalyses.scala
+++ demo/src/main/scala/de/tud/cs/st/bat/resolved/analyses/BaseAnalyses.scala
@@ -34,11 +34,79 @@ object BaseAnalyses
         }).toSet
     }
 
+    /**
+     * Returns true if the method is also declared in the superclass; regardless of abstract or interface methods
+     */
+    def isOverride(project: Project)(classFile: ClassFile)(method: Method): Boolean = {
+        // TODO we could also check for an @Override annotation
+        val superMethods = (for (superClass ← project.classHierarchy.superclasses(classFile.thisClass).getOrElse(Set());
+                                 (_, method) ← project
+                                         .lookupMethodDeclaration(superClass, method.name, method.descriptor)
+        ) yield {
+            method
+        })
+
+        superMethods.size > 0
+
+    }
 
     /**
-     *  returns a filtered sequence of instructions without the bytecode padding
+     * Returns the field declared in a given classFile
      */
-    def withIndex(instructions : Array[Instruction]) : Seq[(Instruction, Int)] = {
-        instructions.zipWithIndex.filter{ case (instr, _) => instr != null }
+    def findField(classFile: ClassFile)(name : String, fieldType : FieldType): Option[Field] = {
+        classFile.fields.find{
+            case Field(_, `name`, `fieldType`, _) => true
+            case _ => false
+        }
+    }
+
+    /**
+     * Returns true if classFile declares the given Field
+     */
+    def declaresField(classFile: ClassFile)(name : String, fieldType : FieldType): Boolean = {
+
+    }
+
+    /**
+     * Returns the super constructor called in the given constructor or None
+     */
+    def calledSuperConstructor(project: Project)
+                              (classFile: ClassFile,
+                               constructor: Method): Option[(ClassFile, Method)] = {
+        val superClasses = project.classHierarchy.superclasses(classFile.thisClass)
+        if (!superClasses.isDefined) {
+            return None
+        }
+        val Some((targetType, name, desc)) = constructor.body.get.instructions.collectFirst {
+            case INVOKESPECIAL(trgt, n, d)
+                if superClasses.get.contains(trgt.asInstanceOf[ObjectType]) =>
+                (trgt.asInstanceOf[ObjectType], n, d)
+
+        }
+        project.lookupMethodDeclaration(targetType, name, desc)
+    }
+
+    def calls(sourceMethod: Method, targetClass: ClassFile, targetMethod: Method): Boolean = {
+        sourceMethod.body.isDefined &&
+                sourceMethod.body.get.instructions.exists {
+                    case INVOKEINTERFACE(targetType, name, desc) => targetClass.thisClass == targetType && targetMethod
+                            .name == name && targetMethod.descriptor == desc
+                    case INVOKEVIRTUAL(targetType, name, desc) => targetClass.thisClass == targetType && targetMethod
+                            .name == name && targetMethod.descriptor == desc
+                    case INVOKESTATIC(targetType, name, desc) => targetClass.thisClass == targetType && targetMethod
+                            .name == name && targetMethod.descriptor == desc
+                    case INVOKESPECIAL(targetType, name, desc) => targetClass.thisClass == targetType && targetMethod
+                            .name == name && targetMethod.descriptor == desc
+                    case _ => false
+                }
+    }
+
+    /**
+     * Returns a filtered sequence of instructions without the bytecode padding
+     */
+    def withIndex(instructions: Array[Instruction]): Seq[(Instruction, Int)] = {
+        instructions.zipWithIndex.filter {
+            case (instr, _) => instr != null
+        }
     }
 }
