48617dfaaee2cdd20844f2dd73d81e0dd3517933
Michael Eichberg <mail@michael-eichberg.de>
2013-06-14 17:49:07 +0200

    updated the formatting

--- core/src/main/scala/de/tud/cs/st/bat/reader/LineNumberTable_attributeReader.scala
+++ core/src/main/scala/de/tud/cs/st/bat/reader/LineNumberTable_attributeReader.scala
@@ -40,14 +40,15 @@ import de.tud.cs.st.util.ControlAbstractions.repeat
 /**
  * <pre>
  * LineNumberTable_attribute {
- * 	u2 attribute_name_index;
- * 	u4 attribute_length;
- * 	u2 line_number_table_length;
- * 	{	u2 start_pc;
- * 		u2 line_number;
- * 	}	line_number_table[line_number_table_length];
+ *   u2 attribute_name_index;
+ *   u4 attribute_length;
+ *   u2 line_number_table_length;
+ *   {  u2 start_pc;
+ *      u2 line_number;
+ *   }  line_number_table[line_number_table_length];
  * }
  * </pre>
+ * 
  * @author Michael Eichberg
  */
 trait LineNumberTable_attributeReader extends AttributeReader {
