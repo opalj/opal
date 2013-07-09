caf0e2bcbf5441812be618198aac9fc8f8fd640a
Michael Eichberg <mail@michael-eichberg.de>
2013-07-10 00:30:33 +0200

    minor refactoring to improve the readability

--- core/src/test/scala/de/tud/cs/st/bat/resolved/reader/SignaturesTest.scala
+++ core/src/test/scala/de/tud/cs/st/bat/resolved/reader/SignaturesTest.scala
@@ -1,35 +1,35 @@
 /* License (BSD Style License):
-*  Copyright (c) 2009, 2011
-*  Software Technology Group
-*  Department of Computer Science
-*  Technische Universität Darmstadt
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  - Redistributions of source code must retain the above copyright notice,
-*    this list of conditions and the following disclaimer.
-*  - Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*  - Neither the name of the Software Technology Group or Technische
-*    Universität Darmstadt nor the names of its contributors may be used to
-*    endorse or promote products derived from this software without specific
-*    prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-*  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-*  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2009 - 2013
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the Software Technology Group or Technische
+ *    Universität Darmstadt nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st.bat
 package resolved
 package reader
@@ -45,10 +45,12 @@ import org.scalatest.ParallelTestExecution
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class SignaturesTest extends FunSuite with TestSupport with ParallelTestExecution {
 
+    import SignatureParser.parseClassSignature
+
     test("traversing a minimal class type signature") {
         var types: Set[Type] = Set()
         val visitor = new TypesVisitor(t ⇒ { types = types + t })
-        visitor.visit(SignatureParser.parseClassSignature("<E:Ljava/Object;>Lde/Iterator<TE;>;"))
+        visitor.visit(parseClassSignature("<E:Ljava/Object;>Lde/Iterator<TE;>;"))
 
         assert(types == Set(ObjectType("java/Object"), ObjectType("de/Iterator")))
     }
@@ -56,15 +58,22 @@ class SignaturesTest extends FunSuite with TestSupport with ParallelTestExecutio
     test("traversing a class type signature") {
         var types: Set[Type] = Set()
         val visitor = new TypesVisitor(t ⇒ { types = types + t })
-        visitor.visit(SignatureParser.parseClassSignature("LDefault;Lde/Collection<Lde/Type;>;LAnotherDefault;Lde/MyObject;"))
+        visitor.visit(parseClassSignature("LDefault;Lde/Collection<Lde/Type;>;LAnotherDefault;Lde/MyObject;"))
 
-        assert(types == Set(ObjectType("Default"), ObjectType("de/Collection"), ObjectType("de/Type"), ObjectType("AnotherDefault"), ObjectType("de/MyObject")))
+        assert(
+            types == Set(
+                ObjectType("Default"),
+                ObjectType("de/Collection"),
+                ObjectType("de/Type"),
+                ObjectType("AnotherDefault"),
+                ObjectType("de/MyObject")))
     }
 
-    private val classA = Java7Framework.ClassFile(locateTestResources("classfiles/Signatures.zip"), "signatures/A.class")
+    import Java7Framework.ClassFile
+    private val classA = ClassFile(locateTestResources("classfiles/Signatures.zip"), "signatures/A.class")
     assert(classA ne null)
 
-    private val classB = Java7Framework.ClassFile(locateTestResources("classfiles/Signatures.zip"), "signatures/B.class")
+    private val classB = ClassFile(locateTestResources("classfiles/Signatures.zip"), "signatures/B.class")
     assert(classB ne null)
 
     test("parsing the class signatures") {
