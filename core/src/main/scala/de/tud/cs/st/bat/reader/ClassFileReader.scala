368c4972650caa25b103307fbd79d980d8e17e81
Michael Eichberg <mail@michael-eichberg.de>
2013-07-19 09:38:38 +0200

    updated the scaladoc

--- core/src/main/scala/de/tud/cs/st/bat/reader/ClassFileReader.scala
+++ core/src/main/scala/de/tud/cs/st/bat/reader/ClassFileReader.scala
@@ -1,35 +1,35 @@
 /* License (BSD Style License):
-*  Copyright (c) 2009, 2011
-*  Software Technology Group
-*  Department of Computer Science
-*  Technische Universität Darmstadt
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  - Redistributions of source code must retain the above copyright notice,
-*    this list of conditions and the following disclaimer.
-*  - Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*  - Neither the name of the Software Technology Group or Technische
-*    Universität Darmstadt nor the names of its contributors may be used to
-*    endorse or promote products derived from this software without specific
-*    prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-*  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-*  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2009 - 2013
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the Software Technology Group or Technische
+ *    Universität Darmstadt nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tud.cs.st
 package bat
 package reader
@@ -39,40 +39,41 @@ import java.util.zip.{ ZipFile, ZipEntry }
 import java.net.URL
 
 /**
- * Implements several template methods to read in a Java class files.
+ * Implements the template method to read in a Java class file. Additionally,
+ * several convenience methods are defined to read in class files from various
+ * sources (Streams, Files, JAR archives).
  *
  * This library supports class files from version 45 (Java 1.1) up to
  * version 51 (Java 7).
  *
- * '''Format'''
+ * ==Notes for Implementors==
+ * Reading of the class file's major structures: the constant pool, fields, methods
+ * the set of implemented interfaces, and the attributes is
+ * delegated to special readers. This enables a very high-level of adaptability.
+ *
+ * ==Class File Structure==
  * <pre>
  * ClassFile {
- * 	  u4 magic;
- * 	  u2 minor_version;
- * 	  u2 major_version;
- * 	  u2 constant_pool_count;
- * 	  cp_info constant_pool[constant_pool_count-1];
- * 	  u2 access_flags;
- * 	  u2 this_class;
- * 	  u2 super_class;
- * 	  u2 interfaces_count;
- * 	  u2 interfaces[interfaces_count];
- *  	u2 fields_count;
- * 	  field_info fields[fields_count];
- * 	  u2 methods_count;
- * 	  method_info methods[methods_count];
+ *    u4 magic;
+ *    u2 minor_version;
+ *    u2 major_version;
+ *    u2 constant_pool_count;
+ *    cp_info constant_pool[constant_pool_count-1];
+ *    u2 access_flags;
+ *    u2 this_class;
+ *    u2 super_class;
+ *    u2 interfaces_count;
+ *    u2 interfaces[interfaces_count];
+ *    u2 fields_count;
+ *    field_info fields[fields_count];
+ *    u2 methods_count;
+ *    method_info methods[methods_count];
  *    u2 attributes_count;
  *    attribute_info attributes[attributes_count];
  * }
  * </pre>
  *
- * For details see the JVM Specification: The ClassFile Structure.
- *
- * ==Notes for Implementors==
- *
- * Reading of the class file's major structures: the constant pool, fields, methods
- * the set of implemented interfaces, and the attributes is
- * delegated to special readers. This enables a very high-level of adaptability.
+ * For further details see the JVM Specification: The ClassFile Structure.
  *
  * @author Michael Eichberg
  */
@@ -115,11 +116,12 @@ trait ClassFileReader extends Constant_PoolAbstractions {
     /**
      * Reads the constant pool using the given stream.
      *
-     * The given stream is positioned
-     * at the very beginning of the constant pool. This method is called by the
-     * template method that reads in a class file to delegate the reading of the
-     * constant pool. Only information belonging to the constant pool are allowed
-     * to be read. The stream must not be closed after reading the constant pool.
+     * When this method is called, the given stream has to be positioned at the very
+     * beginning of the constant pool. This method is called by the template method that
+     * reads in a class file to delegate the reading of the constant pool. Only
+     * information belonging to the constant pool are allowed to be read. 
+     * 
+     * The stream must not be closed after reading the constant pool.
      */
     protected def Constant_Pool(in: DataInputStream): Constant_Pool
 
@@ -205,12 +207,12 @@ trait ClassFileReader extends Constant_PoolAbstractions {
     /**
      * Template method to read in a Java class file from the given input stream.
      *
-     * @param in the DataInputStream from which the class file will be read. The
+     * @param in The DataInputStream from which the class file will be read. The
      *    stream is not closed by this method.
      */
     def ClassFile(in: DataInputStream): ClassFile = {
         // magic
-        require(CLASS_FILE_MAGIC == in.readInt, "No class file.")
+        require(CLASS_FILE_MAGIC == in.readInt, "No Java class file.")
 
         val minor_version = in.readUnsignedShort
         val major_version = in.readUnsignedShort
@@ -319,7 +321,7 @@ trait ClassFileReader extends Constant_PoolAbstractions {
     }
 
     /**
-     * Reads '''in parallel''' all class files stored in the given zip file. For each
+     * Reads '''in parallel''' all class files stored in the given jar file. For each
      * successfully read class file the function `f` is called.
      *
      * @param jarFile A valid jar file that contains `.class` files; other files
