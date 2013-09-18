639b0c422da41ebcd8374998c03f66dfa0bdc5df
Michael Eichberg <mail@michael-eichberg.de>
2013-09-18 22:13:40 +0200

    added code that compiles to tableswitch/lookupswitch statements

--- ext/ai/src/test/resources/sourcefiles/ai/MethodsWithBranches.java
+++ ext/ai/src/test/resources/sourcefiles/ai/MethodsWithBranches.java
@@ -35,90 +35,120 @@ package ai;
 /**
  * Just a very large number of methods that do contain control-flow statements.
  * 
- * <h2>NOTE</h2> This class is not meant to be (automatically) recompiled; it just serves
- * documentation purposes. The compiled class that is used by the tests is found in the
- * test-classfiles directory.
+ * <h2>NOTE</h2> This class is not meant to be (automatically) recompiled; it
+ * just serves documentation purposes. The compiled class that is used by the
+ * tests is found in the test-classfiles directory.
  * 
  * @author Michael Eichberg
  */
 public class MethodsWithBranches {
 
-    //
-    // COMPARISON
+	//
+	// COMPARISON
 
-    public static boolean nullComp(Object o) {
-        if (o == null)
-            return true;
-        else
-            return false;
-    }
+	public static boolean nullComp(Object o) {
+		if (o == null)
+			return true;
+		else
+			return false;
+	}
 
-    public static boolean nonnullComp(Object o) {
-        if (o != null)
-            return true;
-        else
-            return false;
-    }
+	public static boolean nonnullComp(Object o) {
+		if (o != null)
+			return true;
+		else
+			return false;
+	}
 
-    //
-    // MULTIPLE COMPARISONS
+	//
+	// MULTIPLE COMPARISONS
 
-    /**
-     * <pre>
-     * public static boolean multipleComp(java.lang.Object a, java.lang.Object b);
-     *      0  aload_0 [a]
-     *      1  ifnull 17
-     *      4  aload_1 [b]
-     *      5  ifnull 17
-     *      8  aload_0 [a]
-     *      9  aload_1 [b]
-     *     10  if_acmpne 15
-     *     13  iconst_1
-     *     14  ireturn
-     *     15  iconst_0
-     *     16  ireturn
-     *     17  iconst_0
-     *     18  ireturn
-     * </pre>
-     */
-    public static boolean multipleComp(Object a, Object b) {
-        if (a != null && b != null) {
-            return a == b;
-        } else {
-            return false;
-        }
+	/**
+	 * <pre>
+	 * public static boolean multipleComp(java.lang.Object a, java.lang.Object b);
+	 *      0  aload_0 [a]
+	 *      1  ifnull 17
+	 *      4  aload_1 [b]
+	 *      5  ifnull 17
+	 *      8  aload_0 [a]
+	 *      9  aload_1 [b]
+	 *     10  if_acmpne 15
+	 *     13  iconst_1
+	 *     14  ireturn
+	 *     15  iconst_0
+	 *     16  ireturn
+	 *     17  iconst_0
+	 *     18  ireturn
+	 * </pre>
+	 */
+	public static boolean multipleComp(Object a, Object b) {
+		if (a != null && b != null) {
+			return a == b;
+		} else {
+			return false;
+		}
 
-    }
+	}
 
-    // RELATIONAL OPERATORS
-    // Comparing two float/double values puts an int value on the stack which is
-    // then compared - by means of an if instruction - with the predefined value 0 and
-    // then the result is pushed onto the stack.
+	// RELATIONAL OPERATORS
+	// Comparing two float/double values puts an int value on the stack which is
+	// then compared - by means of an if instruction - with the predefined value
+	// 0 and
+	// then the result is pushed onto the stack.
 
-    /*
-     * FLOAD 0 FLOAD 1 FCMPL IFLE L1
-     */
-    public static boolean fCompFCMPL(float i, float j) {
-        return i > j;
-    }
+	/*
+	 * FLOAD 0 FLOAD 1 FCMPL IFLE L1
+	 */
+	public static boolean fCompFCMPL(float i, float j) {
+		return i > j;
+	}
 
-    public static boolean fCompFCMPG(float i, float j) {
-        return i < j;
-    }
+	public static boolean fCompFCMPG(float i, float j) {
+		return i < j;
+	}
 
-    public static boolean dCompDCMPL(double i, double j) {
-        return i > j;
-    }
+	public static boolean dCompDCMPL(double i, double j) {
+		return i > j;
+	}
 
-    public static boolean dCompDCMPG(double i, double j) {
-        return i < j;
-    }
+	public static boolean dCompDCMPG(double i, double j) {
+		return i < j;
+	}
 
-    public static boolean lCompDCMP(long i, long j) {
-        return i == j;
-    }
+	public static boolean lCompDCMP(long i, long j) {
+		return i == j;
+	}
 
-    public static boolean dCompDCMPG(int i, int j) {
-        return i < j;
-    }
+	public static boolean dCompDCMPG(int i, int j) {
+		return i < j;
+	}
+
+	public static int onValue(int value) {
+		switch (value) {
+		case 1:
+			return 100;
+		case 203:
+			return 455;
+		case 29111:
+			return 32;
+		default:
+			return -1;
+		}
+	}
+	
+	
+	public static int onValueDense(int value) {
+		switch (value) {
+		case 0:
+			return 32;
+		case 1:
+			return 100;
+		case 2:
+			return 455;
+		case 3:
+			return 32;
+		default:
+			return -1;
+		}
+	}
 }
