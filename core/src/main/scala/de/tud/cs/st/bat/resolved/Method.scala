b680936e7aa98ee3b89d04fb5fa0577698378212
Michael Eichberg <mail@michael-eichberg.de>
2014-01-10 17:31:19 +0100

    added a pattern matcher (MethodWithBody) to facilitate the matching of any method that has some body
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- core/src/main/scala/de/tud/cs/st/bat/resolved/Method.scala
+++ core/src/main/scala/de/tud/cs/st/bat/resolved/Method.scala
@@ -138,6 +138,8 @@ final class Method private (
 }
 /**
  * Defines factory and extractor methods for `Method` objects.
+ *
+ * @author Michael Eichberg
  */
 object Method {
 
@@ -173,4 +175,24 @@ object Method {
 
     def unapply(method: Method): Option[(Int, String, MethodDescriptor)] =
         Some((method.accessFlags, method.name, method.descriptor))
+}
+/**
+ * Provides pattern matching facilities for methods with bodies.
+ *
+ * @example
+ * Matching all methods that have a method body:
+ * {{{
+ * for {
+ *      classFile ← project.classFiles
+ *      method @ MethodWithBody(code) ← classFile.methods
+ * } {
+ *      // the type of method is "..resolved.Method"
+ *      // the type of code is "..resolved.Code"
+ * }
+ * }}}
+ *
+ * @author Michael Eichberg
+ */
+object MethodWithBody {
+    def unapply(method: Method): Option[Code] = method.body
 }
