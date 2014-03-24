df651fdaaea75dc8e1ed6935c2b770dbb49ebe20
Michael Eichberg <mail@michael-eichberg.de>
2014-03-24 11:53:34 +0000

    fixed the link in the comment

--- core/src/main/scala/de/tud/cs/st/bat/resolved/AnnotationTable.scala
+++ core/src/main/scala/de/tud/cs/st/bat/resolved/AnnotationTable.scala
@@ -38,9 +38,10 @@ package resolved
  * The runtime (in)visible annotations of a class, method, or field.
  *
  * @note At the JVM level, repeating annotations 
- *     [[http://docs.oracle.com/javase/tutorial/java/annotations/type_annotations.html]]
- *     have no explicit support.
- *     For further information go to: [[http://cr.openjdk.java.net/~abuckley/8misc.pdf]].
+ *    ([[http://docs.oracle.com/javase/tutorial/java/annotations/repeating.html]])
+ *    have no explicit support.
+ *    For further information about type-level annotations go to: 
+ *    [[http://cr.openjdk.java.net/~abuckley/8misc.pdf]].
  * @author Michael Eichberg
  */
 trait AnnotationTable extends Attribute {
