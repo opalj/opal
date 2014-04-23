e5b027e6437af28bbd1ee329a0fab8babae72a6e
lschulte <larsschulte@web.de>
2014-04-23 17:22:41 +0200

    edited author name

--- incubation/src/test/java/jdkBugsTest/ForwardClass.java
+++ incubation/src/test/java/jdkBugsTest/ForwardClass.java
@@ -32,8 +32,7 @@ package jdkBugsTest;
  * This is a test for the JDKBugs Class.forName() analysis. It has a call to
  * Class.forName() forwards it and returns it back.
  * 
- * @author Lars
- * 
+ * @author Lars Schulte
  */
 public class ForwardClass {
 
