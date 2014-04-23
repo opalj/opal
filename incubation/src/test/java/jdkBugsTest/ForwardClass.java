d889c6c77dd8890ccbc78e3bfe30d07e315848ec
lars_schulte <lars.h.schulte@gmail.com>
2014-04-23 13:14:43 +0000

    Clas => Class

--- incubation/src/test/java/jdkBugsTest/ForwardClass.java
+++ incubation/src/test/java/jdkBugsTest/ForwardClass.java
@@ -30,7 +30,7 @@ package jdkBugsTest;
 
 /**
  * This is a test for the JDKBugs Class.forName() analysis. It has a call to
- * Clas.forName() forwards it and returns it back.
+ * Class.forName() forwards it and returns it back.
  * 
  * @author Lars
  * 
