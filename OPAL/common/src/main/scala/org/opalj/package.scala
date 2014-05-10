6ccb49238cc318fb055eede60916908b8e67ed38
Michael Eichberg <mail@michael-eichberg.de>
2014-05-10 15:44:34 +0200

    moved all "util" functionality to the common project
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/package.scala
+++ OPAL/common/src/main/scala/org/opalj/package.scala
@@ -29,11 +29,41 @@
 package de.tud.cs
 
 /**
- * Common definitions.
+ * OPAL is a Scala-based framework for the static analysis of Java bytecode
+ * that is designed with performance, scalability and adaptability in mind.
+ *
+ * Its main components are:
+ *  - a library for reading in Java bytecode and creating arbitrary representations
+ *  - an easily customizable framework for the abstract interpretation of Java bytecode.
+ *  - a library to extract dependencies between code elements. 
+ *
+ * ==General Design Decisions==
+ *
+ * ===Thread Safety===
+ * Unless explicitly noted, '''OPAL is thread safe'''. I.e., it is possible to read
+ * and process class files concurrently without explicit synchronization on the
+ * client side.
+ *
+ * ===No `null` Values===
+ * OPAL generally does not make use of `null` values in its public interface. I.e.,
+ * fields that are accessible  will never contain `null` values and methods will never
+ * return `null`. **If a method accepts `null` as a value for a parameter or returns
+ * a `null` value it is always explicitly documented.**
+ * In general, the behavior of methods that are passed `null` values is undefined.
+ *
+ * ===No Typecasts for Collections===
+ * For efficiency reasons OPAL sometimes uses mutable data-structures during construction
+ * time. After construction time, these data-structures are generally represented using
+ * their generic type interfaces (`scala.collection.{Set,Map}`). However, a downcast
+ * (e.g., to add elements) is always forbidden as it would effectively prevent the
+ * thread-safety properties. Furthermore, the concrete data-structure is always
+ * considered an implementation detail and may always change.
  *
  * @author Michael Eichberg
  */
 package object st {
+
+	final val WEBPAGE = "http://www.opal-project.de" 
 
     /**
      * A simple type alias that can be used to communicate that the respective
