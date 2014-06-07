edd6076a81fb0619eac77b74ab24d7a2d04d4ccd
wael alkhatib <alkhatib@cdc.informatik.tu-darmstadt.de>
2014-06-06 13:41:52 -1200

    added support for the "Signature_Attriubte"

---
+++ OPAL/da/src/main/scala/org/opalj/da/Signature_attribute.scala
@@ -0,0 +1,61 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.opalj
+package da
+
+/**
+ * <pre>
+ * Signature_attribute { 
+ * 	u2 attribute_name_index; 
+ * 	u4 attribute_length; 
+ * 	u2 signature_index; 
+ * } 
+ * </pre> 
+ *
+ * @author Michael Eichberg
+ */
+case class Signature_attribute (
+        attribute_name_index: Int,
+        sourceFile_index: Int)extends Attribute {
+
+	/**
+	 * The value of the attribute_length item is fixed; it is always 4.
+	 */
+	def attribute_length = 2
+	
+	def attribute_name = Signature_attribute.name
+
+}
+
+
+object Signature_attribute {
+	
+	val name = "Signature"
+	
+}
