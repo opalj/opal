090e6b02ac01198e95481533b22bf5049423919c
Arne Lottmann <arne.lottmann@stud.tu-darmstadt.de>
2014-06-11 17:43:15 +0200

    Added actualTarget field to VirtualMethod.

--- OPAL/br/src/main/scala/org/opalj/br/Method.scala
+++ OPAL/br/src/main/scala/org/opalj/br/Method.scala
@@ -90,7 +90,7 @@ final class Method private (
     final override def asMethod = this
 
     def asVirtualMethod(declaringClassType: ObjectType): VirtualMethod =
-        VirtualMethod(declaringClassType, name, descriptor)
+        VirtualMethod(declaringClassType, name, descriptor, Some(this))
 
     def runtimeVisibleParameterAnnotations: ParameterAnnotations =
         (attributes collectFirst {
