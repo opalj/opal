7fb466f2e606d852ec7ad4724f820f1f725fb41d
Marco Jacobasch <mj@orod.de>
2014-06-23 12:01:59 +0200

    More try/catch tests for the callgraph

--- OPAL/ai/src/test/resources/sourcefiles/callgraph/misc/TryCatchFinally.java
+++ OPAL/ai/src/test/resources/sourcefiles/callgraph/misc/TryCatchFinally.java
@@ -28,10 +28,13 @@
  */
 package callgraph.misc;
 
+import callgraph.base.AlternateBase;
 import callgraph.base.Base;
 import callgraph.base.SimpleBase;
+
 import org.opalj.ai.test.invokedynamic.annotations.InvokedConstructor;
 import org.opalj.ai.test.invokedynamic.annotations.InvokedMethod;
+import org.opalj.ai.test.invokedynamic.annotations.InvokedMethods;
 
 /**
  * This class was used to create a class file with some well defined attributes. The
@@ -44,11 +47,8 @@ import org.opalj.ai.test.invokedynamic.annotations.InvokedMethod;
  * <!--
  * 
  * 
- * 
- * 
  * INTENTIONALLY LEFT EMPTY (THIS AREA CAN BE EXTENDED/REDUCED TO MAKE SURE THAT THE
  * SPECIFIED LINE NUMBERS ARE STABLE.
- * 
  * 
  * 
  * 
@@ -58,8 +58,11 @@ import org.opalj.ai.test.invokedynamic.annotations.InvokedMethod;
  */
 public class TryCatchFinally {
 
-    @InvokedConstructor(receiverType = SimpleBase.class, lineNumber = 67)
-    @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 68)
+    Base simple = new SimpleBase();
+    Base alternate = new AlternateBase();
+
+    @InvokedConstructor(receiverType = SimpleBase.class, lineNumber = 70)
+    @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 71)
     void callVirtualMethodInCatch() {
         try {
             throw new Exception();
@@ -69,7 +72,7 @@ public class TryCatchFinally {
         }
     }
 
-    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 77)
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 80)
     void callStaticMethodInCatch() {
         try {
             throw new Exception();
@@ -98,7 +101,7 @@ public class TryCatchFinally {
     }
 
     @InvokedConstructor(receiverType = SimpleBase.class, lineNumber = 108)
-    @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 109)
+    @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 112)
     void callVirtualMethodInFinally() {
         try {
             throw new Exception();
@@ -110,7 +113,7 @@ public class TryCatchFinally {
         }
     }
 
-    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 120)
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 123)
     void callStaticMethodInFinally() {
         try {
             throw new Exception();
@@ -119,6 +122,229 @@ public class TryCatchFinally {
         } finally {
             SimpleBase.staticMethod();
         }
+    }
+
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 132)
+    void callsMethodThatThrowsException() {
+        try {
+            throwsException();
+        } catch (IllegalArgumentException e) {
+            SimpleBase.staticMethod();
+        } catch (Exception e) {
+            // EMPTY
+        }
+    }
+
+    void noCallInMethodThatThrowsExceptionCatchInfiniteLoop() {
+        try {
+            throwsException();
+        } catch (IllegalArgumentException e) {
+            while (!this.toString().isEmpty()) {
+                // Infinite Loop
+            }
+            SimpleBase.staticMethod();
+        } catch (Exception e) {
+            // EMPTY
+        }
+    }
+
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 159)
+    void callsMethodThatThrowsExceptionCatchNoInfiniteLoop() {
+        try {
+            throwsException();
+        } catch (IllegalArgumentException e) {
+            while (this.toString().isEmpty()) {
+                // EMPTY
+            }
+            SimpleBase.staticMethod();
+        } catch (Exception e) {
+            // EMPTY
+        }
+    }
+
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 173)
+    void callMethodInCatchWithDoWhile() {
+        try {
+            throw new IllegalAccessError();
+        } catch (Exception e) {
+            // EMPTY
+        } catch (Error e) {
+            do {
+                SimpleBase.staticMethod();
+            } while (false);
+        }
+    }
+
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 186)
+    void callInCatchAfterDoWhileInfiniteLoop() {
+        try {
+            throw new IllegalAccessError();
+        } catch (Exception e) {
+            // EMPTY
+        } catch (Error e) {
+            do {
+                SimpleBase.staticMethod();
+            } while (this.getClass() == TryCatchFinally.class);
+            AlternateBase.staticMethod();
+        }
+    }
+
+    @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 199)
+    void throwErrorCatchExceptionOrError() {
+        try {
+            throw new IllegalAccessError();
+        } catch (Exception e) {
+            AlternateBase.staticMethod();
+        } catch (Error e) {
+            SimpleBase.staticMethod();
+        }
+    }
+
+    @InvokedMethod(receiverType = AlternateBase.class, name = "implementedMethod", lineNumber = 206)
+    void callInCatchWithBreak() {
+        try {
+            alternate.implementedMethod();
+            throw new Exception();
+        } catch (Exception e) {
+            for (int i = 0; i < 2; i++) {
+                if (i == 0) {
+                    break;
+                }
+                SimpleBase.staticMethod();
+            }
+        }
+    }
+
+    @InvokedMethod(receiverType = AlternateBase.class, name = "implementedMethod", lineNumber = 221)
+    void noCallInCatchWithLabeledBreak() {
+        try {
+            alternate.implementedMethod();
+            throw new Exception();
+        } catch (Exception e) {
+            label: for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (i == 0) {
+                        break label;
+                    }
+                    SimpleBase.staticMethod();
+                }
+
+            }
+        }
+    }
+
+    @InvokedMethod(receiverType = AlternateBase.class, name = "implementedMethod", lineNumber = 244)
+    void callInEmptyForLoop() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            for (int i = 0; i < 0; i++) {
+                simple.implementedMethod();
+            }
+            alternate.implementedMethod();
+        }
+    }
+
+    @InvokedMethods({
+            @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 256),
+            @InvokedMethod(receiverType = AlternateBase.class, name = "implementedMethod", lineNumber = 258),
+            @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 260) })
+    void callMethodBasedOnThrowableType() {
+        try {
+            throw createThrowable();
+        } catch (IllegalAccessException | RuntimeException e) {
+            simple.implementedMethod();
+        } catch (Exception e) {
+            alternate.implementedMethod();
+        } catch (Error e) {
+            SimpleBase.staticMethod();
+        } catch (Throwable e) {
+            AlternateBase.staticMethod();
+        }
+    }
+
+    @InvokedMethods({
+            @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 256),
+            @InvokedMethod(receiverType = AlternateBase.class, name = "implementedMethod", lineNumber = 258) })
+    void callMethodBasedOnThrowableTypePartly() {
+        try {
+            throwThrowablePartly();
+        } catch (IllegalAccessException | RuntimeException e) {
+            simple.implementedMethod();
+        } catch (Exception e) {
+            alternate.implementedMethod();
+        } catch (Error e) {
+            SimpleBase.staticMethod();
+        } catch (Throwable e) {
+            AlternateBase.staticMethod();
+        }
+    }
+
+    @InvokedMethods({
+            @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 290),
+            @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 291) })
+    void possibleNullPointerException(Object o) {
+        try {
+            canThrowNullPointerException(o);
+        } catch (NullPointerException e) {
+            simple.implementedMethod();
+            SimpleBase.staticMethod();
+        }
+    }
+
+    @InvokedMethods({
+            @InvokedMethod(receiverType = SimpleBase.class, name = "implementedMethod", lineNumber = 290),
+            @InvokedMethod(receiverType = SimpleBase.class, name = "staticMethod", isStatic = true, lineNumber = 291) })
+    void possibleNullPointerExceptionCatchOtherExceptions(Object o) {
+        try {
+            canThrowNullPointerException(o);
+        } catch (NullPointerException e) {
+            simple.implementedMethod();
+            SimpleBase.staticMethod();
+        } catch (Exception e) {
+            alternate.implementedMethod();
+        }
+    }
+
+    //
+    //
+    // Helper Functions after. To prevent changes of line numbers.
+    //
+    //
+    void throwsException() {
+        throw new IllegalArgumentException();
+    }
+
+    Throwable createThrowable() {
+        int mod = this.hashCode() % 4;
+        switch (mod) {
+        case 0:
+            return new IllegalArgumentException();
+        case 1:
+            return new IllegalAccessException();
+        case 2:
+            return new Exception();
+        case 3:
+            return new Error();
+        default:
+            return new Throwable();
+        }
+    }
+
+    void throwThrowable() throws Throwable {
+        throw createThrowable();
+    }
+
+    void throwThrowablePartly() throws Throwable {
+        try {
+            throw createThrowable();
+        } catch (Error e) {
+            // catch all Error
+        }
+    }
+
+    void canThrowNullPointerException(Object o) {
+        o.hashCode();
     }
 
     // TODO more precise catch
