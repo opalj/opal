3264a19e69cad4a1103a5fab16cbcb63b02fe422
Daniel Klauer <d_klauer@rbg.informatik.tu-darmstadt.de>
2014-07-06 15:47:15 +0200

    frb: Fix UnusedPrivateFields false-positives for final constant fields

--- OPAL/frb/analyses/src/test/resources/sourcefiles/analyses/UnusedPrivateFields/UsedSerialVersionUID.java
+++ OPAL/frb/analyses/src/test/resources/sourcefiles/analyses/UnusedPrivateFields/UsedSerialVersionUID.java
@@ -29,13 +29,12 @@
 package UnusedPrivateFields;
 
 /**
- * Class with an unused private serialVersionUID field. serialVersionUID is not special
- * here, because this class is not Serializable, thus it should be reported.
+ * A Serializable class with a private serialVersionUID field. The serialVersionUID field
+ * is used by Serializable, and thus it should not be reported.
  * 
  * @author Daniel Klauer
  */
-public class UnusedPrivateSerialVersionUID {
+class UsedSerialVersionUID implements java.io.Serializable {
 
-    @SuppressWarnings("unused")
     private static final long serialVersionUID = 1L;
 }
