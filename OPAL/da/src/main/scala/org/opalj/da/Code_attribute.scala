ce5156ef22f464af92607b2c370a3e1a71617053
Michael Eichberg <mail@michael-eichberg.de>
2014-08-01 11:31:19 +0200

    reformatted the code to comply with the default settings

--- OPAL/da/src/main/scala/org/opalj/da/Code_attribute.scala
+++ OPAL/da/src/main/scala/org/opalj/da/Code_attribute.scala
@@ -30,74 +30,54 @@ package org.opalj
 package da
 
 import scala.xml.Node
-//import org.opalj.br.reader.LineNumberTable_attribute
 
 /**
- * <pre>
- * Code_attribute { 
- * 	u2 attribute_name_index; 
- * 	u4 attribute_length; 
- * 	u2 max_stack; 
- * 	u2 max_locals; 
- * 	u4 code_length; 
- * 	u1 code[code_length]; 
- * 	u2 exception_table_length; 
- * 	{	u2 start_pc; 
- * 		u2 end_pc; 
- * 		u2 handler_pc; 
- * 		u2 catch_type; 
- * 	} exception_table[exception_table_length]; 
- * 	u2 attributes_count; 
- * 	attribute_info attributes[attributes_count]; 
- * } 
- * </pre>
  * @author Michael Eichberg
  * @author Wael Alkhatib
  * @author Isbel Isbel
  * @author Noorulla Sharief
  */
-case class Code_attribute (
+case class Code_attribute(
         attribute_name_index: Constant_Pool_Index,
         attribute_length: Int,
         max_stack: Int,
         max_locals: Int,
         code: Code,
         exception_handlers: IndexedSeq[ExceptionTableEntry],
-        attributes: Attributes) extends Attribute{
-        //val lineNumberTable = attributes.collectFirst{case lnt: LineNumberTable_attribute=> lnt}
-	 override def toXHTML(implicit cp: Constant_Pool): Node = {
-	  
-	    val exception_handlershtml=exception_handlersToXHTML(cp)
-	    val codeattributes=attributesToXHTML(cp)
+        attributes: Attributes) extends Attribute {
+
+    override def toXHTML(implicit cp: Constant_Pool): Node = {
+
+        val methodBodyHeader =
+            s"Method Body (Size: ${code.instructions.size} bytes, Max Stack: $max_stack, Max Locals: $max_locals)"
+
         <div id="sourcecode">
             <details>
-                <summary>code</summary>
-	                max_stack :{max_stack}, max_locals:{max_locals}
-                    {code.toXHTML(cp,attributes,exception_handlers)}                    
-                    {exception_handlershtml}
-                    {codeattributes}
+                <summary>{ methodBodyHeader }</summary>
+                { code.toXHTML(attributes, exception_handlers) }
+                { exception_handlersAsXHTML }
+                { attributesAsXHTML }
             </details>
         </div>
     }
-	
-	def attributesToXHTML(implicit cp: Constant_Pool) = {
-        { for (attribute ← attributes) yield attribute.toXHTML(cp) }
+
+    def attributesAsXHTML(implicit cp: Constant_Pool) = {
+        for (attribute ← attributes) yield attribute.toXHTML(cp)
     }
-	def exception_handlersToXHTML(implicit cp: Constant_Pool):Node = {
-	  if(exception_handlers.length>0)
-	    <div>
-	      <details>
-	      <summary>Exception Table:</summary>
-	          { for (exception ← exception_handlers) yield exception.toXHTML(cp,code) }
-	      </details>
-	    </div>
-	  else
-	    <div></div>
+
+    def exception_handlersAsXHTML(implicit cp: Constant_Pool): Node = {
+        if (exception_handlers.length > 0)
+            <div>
+                <details>
+                    <summary>Exception Table:</summary>
+                    { for (exception ← exception_handlers) yield exception.toXHTML(cp, code) }
+                </details>
+            </div>
+        else
+            <div></div>
     }
 }
-object Code_attribute{
-	val name = "Code"
-}
+
 
 
 
