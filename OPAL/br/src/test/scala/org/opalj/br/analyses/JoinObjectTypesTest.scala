41bedf144694ae324cf607bba09c79117c6fe1cf
Michael Eichberg <mail@michael-eichberg.de>
2014-10-29 14:42:37 +0100

    added copyright and author information; fixed the formatting

--- OPAL/br/src/test/scala/org/opalj/br/analyses/JoinObjectTypesTest.scala
+++ OPAL/br/src/test/scala/org/opalj/br/analyses/JoinObjectTypesTest.scala
@@ -1,3 +1,31 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.opalj
 package br
 package analyses
@@ -11,10 +39,14 @@ import org.opalj.graphs.toDot
 import org.opalj.collection.immutable.UIDSet
 import scala.language.implicitConversions
 
+/**
+ * @author Tobias Becker
+ */
 @RunWith(classOf[JUnitRunner])
 class JoinObjectTypesTest
         extends FunSpec
         with Matchers {
+
     val classhierachy = ClassHierarchy(
         Traversable.empty,
         List(() ⇒ this.getClass.getResourceAsStream("ClassHierachyUpperBounds.ths"))
@@ -28,16 +60,30 @@ class JoinObjectTypesTest
         param.toSeq.map(_.toJava).mkString("{", ",", "}")
     }
 
-    def testJoinOfTwoObjectTypes(param1: ObjectType, param2: ObjectType, reflexive: Boolean, expected: UIDSet[ObjectType]) = {
+    def testJoinOfTwoObjectTypes(
+        param1: ObjectType,
+        param2: ObjectType,
+        reflexive: Boolean,
+        expected: UIDSet[ObjectType]) = {
         val result = classhierachy.joinObjectTypes(param1, param2, reflexive)
         if (result != expected)
-            fail(s"${param1} join${if (reflexive) "(reflexive)" else ""} ${param2} is ${mkString(result)}; expected ${mkString(expected)}")
+            fail(
+                s"${param1} join${if (reflexive) "(reflexive)" else ""}"+
+                    s" ${param2} is ${mkString(result)};"+
+                    s" expected ${mkString(expected)}")
     }
 
-    def testJoinOfObjectTypesWithUpperBound(param1: ObjectType, param2: UIDSet[ObjectType], reflexive: Boolean, expected: UIDSet[ObjectType]) = {
+    def testJoinOfObjectTypesWithUpperBound(
+        param1: ObjectType,
+        param2: UIDSet[ObjectType],
+        reflexive: Boolean,
+        expected: UIDSet[ObjectType]) = {
         val result = classhierachy.joinObjectTypes(param1, param2, reflexive)
         if (result != expected)
-            fail(s"${param1} join${if (reflexive) "(reflexive)" else ""} ${mkString(param2)} is ${mkString(result)}; expected ${mkString(expected)}")
+            fail(
+                s"${param1} join${if (reflexive) "(reflexive)" else ""}"+
+                    s" ${mkString(param2)} is ${mkString(result)};"+
+                    s" expected ${mkString(expected)}")
     }
 
     describe("the behavior of the method joinObjectTypes(ObjectType,ObjectType) of ClassHierachy") {
