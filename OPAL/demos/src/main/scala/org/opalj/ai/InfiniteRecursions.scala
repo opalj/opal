c88687cf367accabdc3304a56563b0b23e197e35
Michael Eichberg <mail@michael-eichberg.de>
2015-01-02 19:09:16 +0100

    tweaked the InfiniteRecursions analysis to improve the precision

--- OPAL/demos/src/main/scala/org/opalj/ai/InfiniteRecursions.scala
+++ OPAL/demos/src/main/scala/org/opalj/ai/InfiniteRecursions.scala
@@ -30,189 +30,224 @@ package org.opalj
 package ai
 
 import java.net.URL
-import org.opalj.collection.immutable.{ UIDSet, UIDSet1 }
-import org.opalj.br.analyses.{ Analysis, AnalysisExecutor, BasicReport, Project, SomeProject }
-import org.opalj.br.{ ClassFile, Method }
-import org.opalj.br.{ ReferenceType }
+import scala.Console.BLUE
+import scala.Console.BOLD
+import scala.Console.RESET
+import scala.annotation.elidable
+import scala.annotation.elidable.ASSERTION
+import scala.language.existentials
+import org.opalj.br.ClassFile
+import org.opalj.br.Method
+import org.opalj.br.MethodWithBody
+import org.opalj.br.analyses.AnalysisExecutor
+import org.opalj.br.analyses.BasicReport
+import org.opalj.br.analyses.OneStepAnalysis
+import org.opalj.br.analyses.Project
+import org.opalj.br.analyses.SomeProject
+import org.opalj.br.instructions.INVOKEINTERFACE
+import org.opalj.br.instructions.INVOKESPECIAL
 import org.opalj.br.instructions.INVOKESTATIC
 import org.opalj.br.instructions.INVOKEVIRTUAL
-import org.opalj.ai.domain._
-import org.opalj.br.MethodWithBody
-import org.opalj.br.instructions._
-import org.opalj.ai.domain.l2.PerformInvocations
-import org.opalj.br.Code
-import org.opalj.br.analyses.Analysis
-import org.opalj.br.analyses.AnalysisExecutor
-import org.opalj.br.analyses.Project
-import org.opalj.br.analyses.ProgressManagement
-import scala.annotation.tailrec
+import org.opalj.br.instructions.NEW
 
 /**
- * A shallow analysis that tries find infinite recursions.
+ * An analysis that finds self-recursive calls with unchanged parameters.
  *
  * @author Marco Jacobasch
+ * @author Michael Eichberg
  */
-object InfiniteRecursions extends AnalysisExecutor {
+object InfiniteRecursions extends AnalysisExecutor with OneStepAnalysis[URL, BasicReport] {
 
-    val analysis = new Analysis[URL, BasicReport] {
+    val analysis = this
 
-        val iterationDepth = 5
-        val minCountOfConsecutivelyUnchangedOperands = 3
+    override def title: String =
+        "infinite recursions analysis"
 
-        override def title: String =
-            "Infinite Recursions"
+    override def description: String =
+        "identifies method which calls themselves using infinite recursion"
 
-        override def description: String =
-            "Identifies methods which causes infinite recursions."
+    override def doAnalyze(
+        project: Project[URL],
+        parameters: Seq[String] = List.empty,
+        isInterrupted: () ⇒ Boolean) = {
 
-        override def analyze(
-            theProject: Project[URL],
-            parameters: Seq[String] = List.empty,
-            initProgressManagement: (Int) ⇒ ProgressManagement) = {
+        // TODO read from parameter
+        val maxRecursionDepth = 3
 
-            // Select every method which invokes itself (static, virtual, special or interface)
-            val result = for {
-                classFile ← theProject.classFiles
-                if !classFile.isAnnotationDeclaration
+        val result =
+            // for every method that calls itself ...
+            for {
+                classFile ← project.classFiles.par
                 method @ MethodWithBody(body) ← classFile.methods
-                objecType = classFile.thisType
+                name = method.name
+                descriptor = method.descriptor
+                if descriptor.parameterTypes.forall { t ⇒
+                    // we don't have (as of Jan 1st 2015) a domain that enables a meaningful
+                    // tracking of Float and Double values
+                    t.isReferenceType || t.isLongType || t.isIntegerType
+                }
+                classType = classFile.thisType
                 pcs = body.collectWithIndex {
-                    case (pc, INVOKEVIRTUAL(`objecType`, method.name, method.descriptor))   ⇒ pc
-                    case (pc, INVOKESTATIC(`objecType`, method.name, method.descriptor))    ⇒ pc
-                    case (pc, INVOKESPECIAL(`objecType`, method.name, method.descriptor))   ⇒ pc
-                    case (pc, INVOKEINTERFACE(`objecType`, method.name, method.descriptor)) ⇒ pc
+                    case (pc, INVOKEVIRTUAL(`classType`, `name`, `descriptor`))   ⇒ pc
+                    case (pc, INVOKESTATIC(`classType`, `name`, `descriptor`))    ⇒ pc
+                    case (pc, INVOKESPECIAL(`classType`, `name`, `descriptor`))   ⇒ pc
+                    case (pc, INVOKEINTERFACE(`classType`, `name`, `descriptor`)) ⇒ pc
                 }
                 if pcs.nonEmpty
-            } yield {
-                val domain = new InfiniteRecursionsDomain(theProject, method)
+                result ← inifiniteRecursions(
+                    maxRecursionDepth,
+                    project, classFile, method,
+                    pcs)
+            } yield { result }
 
-//                println("\n------------------------------------------------")
-//                println("%s %s PCs(%s)" format (classFile.thisType, method.toJava, pcs.mkString(", ")))
-//                println("---")
-
-                val result = findInfiniteRecursion(iterationDepth, minCountOfConsecutivelyUnchangedOperands, classFile, method, pcs, domain, body)
-
-                val resultAsString = result.collect { case Some(infiniteRecursion) ⇒ infiniteRecursion }.mkString("\n")
-//                println(resultAsString)
-
-                resultAsString
-            }
-
-            BasicReport(result.mkString("\n"))
-        }
-
-        /**
-         * Find possible infinite recursions of the passed method for every passed PC invoking a call to the same method and methodescriptor
-         *
-         * The depth of the DFS is limited by the value of iterations.
-         * minCountOfUnchangedArguments is the amount of needed consecutive steps without a change in the operands to be considered a infinite recursion.
-         */
-        def findInfiniteRecursion(iterations: Int, minCountOfUnchangedArguments: Int, classFile: ClassFile, method: Method, pcs: Seq[PC], domain: Domain, body: Code): Seq[Option[InfiniteRecursion]] = {
-
-            /**
-             * Perform a single step AI for each possible PC and analyze all previous AI results of the current taken path for an occurence of a possible infinite recursion.
-             */
-            def analyzeRecursionStep(iterations: Int, aiResults: Seq[(PC, AIResult)] = Seq()): Seq[Option[InfiniteRecursion]] = {
-                if (iterations <= 0)
-                    return Seq(None)
-
-                val intermediateStepResult = for {
-                    pc ← pcs
-                    (lastPC, lastAiResult) = aiResults.head
-                    if (lastAiResult.operandsArray(pc) != null)
-                } yield {
-                    // Abstract Interpretation
-                    val params = PerformInvocations mapOperandsToParameters (lastAiResult.operandsArray(pc), method, domain)
-                    val aiResult = BaseAI.perform(body, domain)(Nil, params)
-                    val combinedAIResults = (pc, aiResult) +: aiResults
-
-                    // Analyse Result
-                    val operandsOfInterpretedBranches = combinedAIResults filter {
-                        case (localPC, localAiResult) ⇒ localAiResult.operandsArray(localPC) != null
-                    }
-
-                    val adaptedOperands = operandsOfInterpretedBranches map {
-                        case (localPC, localAiResult) ⇒
-                            val operandsToAdapt = localAiResult.operandsArray(localPC)
-                            PerformInvocations.mapOperandsToParameters(operandsToAdapt, method, domain)
-                    }
-
-                    val operandsComparedForEquality =
-                        for {
-                            Seq(currentOperands, followingOperands) ← adaptedOperands.take(3).sliding(2)
-                        } yield {
-                            currentOperands == followingOperands
-                        }
-
-                    val isInfiniteRecursion = operandsComparedForEquality.fold(adaptedOperands.length >= minCountOfUnchangedArguments)(_ && _)
-                    if (isInfiniteRecursion) {
-                        Seq(Some(InfiniteRecursion(classFile, method, pc, adaptedOperands)))
-                    } else {
-                        analyzeRecursionStep(iterations - 1, combinedAIResults)
-                    }
-
-                }
-                intermediateStepResult.flatten
-            }
-
-            val perform = BaseAI(classFile, method, domain)
-            val result = for {
-                pc ← pcs
-            } yield {
-                analyzeRecursionStep(
-                    iterations,
-                    Seq((pc, BaseAI(classFile, method, domain))))
-            }
-            result.flatten
-        }
-
+        BasicReport(result.map(_.toString).mkString("\n"))
     }
+
+    /**
+     * Perform an abstract interpretation and check if (after some stabilization time)
+     * the parameters to the recursive call are unchanged.
+     *
+     * `maxRecursionDepth` determines after how many non-recursive calls the analysis
+     * is aborted.
+     */
+    def inifiniteRecursions(
+        maxRecursionDepth: Int,
+        project: SomeProject,
+        classFile: ClassFile,
+        method: Method,
+        pcs: Seq[PC]): Option[InfiniteRecursion] = {
+
+        assert(maxRecursionDepth > 1)
+        assert(pcs.toSet.size == pcs.size, s"the seq $pcs contains duplicates")
+
+        val body = method.body.get
+        val parametersCount =
+            method.descriptor.parametersCount + (if (method.isStatic) 0 else 1)
+
+        // we are always analyzing the same method, hence, we can reuse the same domain
+        // for all "Abstract Interpretations"
+        val domain = new InfiniteRecursionsDomain(project, method)
+        import domain.Operands
+
+        var previousCallOperandsList: Seq[Operands] = Seq.empty
+
+        def reduceCallOperands(operandsArray: domain.OperandsArray): Seq[Operands] = {
+            var callOperandsList: List[Operands] = List.empty
+            for {
+                pc ← pcs
+                if operandsArray(pc) ne null
+                nextCallOperands: domain.Operands = operandsArray(pc).take(parametersCount)
+            } {
+                // IntegerRangeValues and ReferenceValues have useable equals semantics
+                if (!callOperandsList.exists { _ == nextCallOperands })
+                    callOperandsList = nextCallOperands :: callOperandsList
+            }
+            callOperandsList
+        }
+
+        // initialize callOperandsList by doing a first abstract interpretation
+        val initialOperandsArray = BaseAI(classFile, method, domain).operandsArray
+        previousCallOperandsList = reduceCallOperands(initialOperandsArray)
+
+        def analyze(depth: Int, callOperands: Operands): Option[InfiniteRecursion] = {
+            if (depth > maxRecursionDepth)
+                return None;
+
+            val parameters = mapOperandsToParameters(callOperands, method, domain)
+            val aiResult = BaseAI.perform(body, domain)(List.empty, parameters)
+            val operandsArray = aiResult.operandsArray
+            val localsArray = aiResult.localsArray
+            val callOperandsList =
+                reduceCallOperands(operandsArray) filter { callOperands ⇒
+                    if (previousCallOperandsList.contains(callOperands)) {
+
+                        // let's check if we have a potential recursive call...
+                        // i.e., if we can track back the operands to parameters
+                        // concrete (fixed) values or values that are always created
+                        // in the same manner; the idea is to reduce false positives
+                        // due to non-infinite recursions due to side effects 
+                        if (callOperands.forall {
+                            case domain.DomainSingleOriginReferenceValue(v) ⇒
+                                if (v.origin < 0 /* === the value is a parameter*/ ||
+                                    // the value is always created anew (no sideeffect)
+                                    body.instructions(v.origin).opcode == NEW.opcode)
+                                    true
+                                else
+                                    false
+                            case v: domain.AnIntegerValue ⇒
+                                if (localsArray(0).exists(_ eq v))
+                                    true // the value is parameter
+                                else
+                                    false
+                            case v: domain.ALongValue ⇒
+                                if (localsArray(0).exists(_ eq v))
+                                    true // the value is parameter
+                                else
+                                    false
+                            case _: domain.LongSet      ⇒ true
+                            case _: domain.IntegerRange ⇒ true
+                            case _                      ⇒ false
+                        })
+                            return Some(InfiniteRecursion(classFile, method, callOperands));
+
+                        // these operands are not relevant...
+                        false
+                    } else {
+                        true
+                    }
+                }
+
+            callOperandsList foreach { callOperands ⇒
+                val result = analyze(depth + 1, callOperands)
+                if (result.nonEmpty)
+                    return result;
+            }
+            None
+        }
+
+        previousCallOperandsList foreach { callOperands ⇒
+            val result = analyze(0, callOperands)
+            if (result.nonEmpty)
+                return result;
+        }
+        // no recursion...
+        None
+    }
+
 }
 
-class InfiniteRecursionsDomain(
-    override val project: Project[java.net.URL],
-    override val method: Method,
-    override val maxCardinalityOfIntegerRanges: Long = 16l)
-        extends Domain
-        with domain.DefaultDomainValueBinding
-        with domain.ThrowAllPotentialExceptionsConfiguration
-        with domain.l0.DefaultTypeLevelFloatValues
-        with domain.l0.DefaultTypeLevelDoubleValues
-        with domain.l0.TypeLevelFieldAccessInstructions
-        with domain.l0.TypeLevelInvokeInstructions
-        with domain.l1.DefaultReferenceValuesBinding
-        with domain.l1.DefaultIntegerRangeValues
-        with domain.l1.MaxArrayLengthRefinement
-        with domain.l1.ConstraintsBetweenIntegerValues
-        //with domain.l1.DefaultIntegerSetValues
-        with domain.l1.DefaultLongValues
-        with domain.l1.LongValuesShiftOperators
-        with domain.l1.DefaultConcretePrimitiveValuesConversions
-        with domain.DefaultHandlingOfMethodResults
-        with domain.IgnoreSynchronization
-        with domain.TheProject[java.net.URL]
-        with domain.TheMethod
-        with domain.ProjectBasedClassHierarchy
+class InfiniteRecursionsDomain(val project: SomeProject, val method: Method)
+    extends Domain
+    with domain.DefaultDomainValueBinding
+    with domain.ThrowAllPotentialExceptionsConfiguration
+    with domain.l0.DefaultTypeLevelFloatValues
+    with domain.l0.DefaultTypeLevelDoubleValues
+    with domain.l0.TypeLevelFieldAccessInstructions
+    with domain.l0.TypeLevelInvokeInstructions
+    with domain.l1.DefaultReferenceValuesBinding
+    with domain.l1.DefaultIntegerRangeValues
+    with domain.l1.MaxArrayLengthRefinement
+    with domain.l1.ConstraintsBetweenIntegerValues
+    //with domain.l1.DefaultIntegerSetValues
+    with domain.l1.DefaultLongSetValues
+    with domain.l1.LongSetValuesShiftOperators
+    with domain.l1.ConcretePrimitiveValuesConversions
+    with domain.DefaultHandlingOfMethodResults
+    with domain.IgnoreSynchronization
+    with domain.TheProject
+    with domain.TheMethod
 
 case class InfiniteRecursion(
         classFile: ClassFile,
         method: Method,
-        infinitRecursionPC: PC,
-        operands: Seq[_]) {
+        operands: Iterable[_]) {
 
-    def infinitRecursionLineNumber: Option[PC] =
-        method.body.get.lineNumber(infinitRecursionPC)
-
-    def message: String = ""
-
-    override def toString = {
-        import Console._
+    override def toString: String = {
         val declaringClassOfMethod = classFile.thisType.toJava
 
-        "Infinite recursion in "+BOLD + BLUE +
-            declaringClassOfMethod+"{ "+method.toJava+"{ "+
-            GREEN+"PC: "+infinitRecursionPC+" Line: "+infinitRecursionLineNumber.getOrElse(-1) +
-            RESET+" "+operands.mkString(", ")+" }}"
+        "infinite recursion in "+BOLD + BLUE +
+            declaringClassOfMethod + RESET+
+            "{ "+method.toJava+"{ "+operands.mkString(", ")+" }}"
     }
 }
 
