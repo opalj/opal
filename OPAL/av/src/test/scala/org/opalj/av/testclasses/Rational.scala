a9ca10d36a0f9583a55c31c9a7f564c59d47397f
Marco Torsello <marco.torsello@stud.tu-darmstadt.de>
2015-01-05 01:11:01 +0100

    Added tests for all currently supported constraints in the architecture validation framework.

---
+++ OPAL/av/src/test/scala/org/opalj/av/testclasses/Rational.scala
@@ -0,0 +1,58 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.opalj.av.testclasses
+
+/**
+ * @author Samuel Beracasa
+ */
+class Rational(val numer: Number, val denom: Number) extends Operations {
+
+    //Rational operator Number = Rational
+    def +(other: Number) =
+        new Rational(((other / this.denom) + this.numer), this.denom)
+    def -(other: Number) =
+        new Rational((other / this.denom) - this.numer, this.denom)
+    def *(other: Number) =
+        new Rational(other * this.numer, this.denom)
+    def /(other: Number) =
+        new Rational(this.numer, this.denom / other)
+
+    //Rational operator Rational = Rational
+    def +(other: Rational) =
+        new Rational((this.numer / other.denom) + (other.numer / this.denom), other.denom * this.denom)
+    def -(other: Rational) =
+        new Rational((this.numer / other.denom) - (other.numer / this.denom), other.denom * this.denom)
+    def *(other: Rational) =
+        new Rational((this.numer * other.numer), (other.denom / this.denom))
+    def /(other: Rational) =
+        new Rational((this.numer * other.denom), (other.numer * this.denom))
+
+    //Print
+    override def toString() = numer.toString+"/"+denom.toString
+}
