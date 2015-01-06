5108d5f70a064456c2c7c226b68c3419bb076f17
Michael Eichberg <mail@michael-eichberg.de>
2015-01-06 14:07:25 +0100

    moved the type "LocalCallGraphInformation" to the companion object

--- OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/CallGraphExtractor.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/CallGraphExtractor.scala
@@ -13,7 +13,7 @@
  *  - Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -22,7 +22,7 @@
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
@@ -50,9 +50,12 @@ import org.opalj.br.MethodSignature
  */
 trait CallGraphExtractor { extractor ⇒
 
-    type LocalCallGraphInformation = (( /*Caller*/ Method, Map[PC, /*Callees*/ Set[Method]]), List[UnresolvedMethodCall])
+    import CallGraphExtractor.LocalCallGraphInformation
 
-    def extract(project: SomeProject, classFile: ClassFile, method: Method): LocalCallGraphInformation
+    def extract(
+        project: SomeProject,
+        classFile: ClassFile,
+        method: Method): LocalCallGraphInformation
 
     def cache: CallGraphCache[MethodSignature, Set[Method]]
 
@@ -115,5 +118,10 @@ trait CallGraphExtractor { extractor ⇒
             }
         }
     }
+}
+object CallGraphExtractor {
+
+    type LocalCallGraphInformation = (( /*Caller*/ Method, Map[PC, /*Callees*/ Set[Method]]), List[UnresolvedMethodCall])
+
 }
 
