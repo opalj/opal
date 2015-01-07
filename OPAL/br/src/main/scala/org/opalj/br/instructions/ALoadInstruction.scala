e8d4479b8362c34b078c5b9795138b1365eea319
Michael Eichberg <mail@michael-eichberg.de>
2015-01-07 15:54:37 +0100

    added additional support for pattern matching on XLoad and XStore instructions

--- OPAL/br/src/main/scala/org/opalj/br/instructions/ALoadInstruction.scala
+++ OPAL/br/src/main/scala/org/opalj/br/instructions/ALoadInstruction.scala
@@ -31,16 +31,15 @@ package br
 package instructions
 
 /**
- * Load double from local variable with index 2.
+ * Load reference from local variable.
  *
  * @author Michael Eichberg
  */
-case object DLOAD_2 extends DLoadInstruction with ImplicitLocalVariableIndex {
+trait ALoadInstruction extends LoadLocalVariableInstruction {
 
-    final val lvIndex = 2
+    final def computationalType: ComputationalType = ComputationalTypeReference
 
-    final val opcode = 40
-
-    final val mnemonic = "dload_2"
+}
+object ALoadInstruction {
 
 }
