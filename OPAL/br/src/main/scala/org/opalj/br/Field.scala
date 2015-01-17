3f31aafaf9858d0dbd812ac92f26ccd31d084750
Michael Eichberg <mail@michael-eichberg.de>
2015-01-17 10:30:35 +0100

    added convenience methods to create VirtualSourceElements

--- OPAL/br/src/main/scala/org/opalj/br/Field.scala
+++ OPAL/br/src/main/scala/org/opalj/br/Field.scala
@@ -13,7 +13,7 @@
  *  - Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -22,7 +22,7 @@
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
@@ -74,6 +74,9 @@ final class Field private (
     final override def isField = true
 
     final override def asField = this
+
+    final def asVirtualField(declaringClassFile: ClassFile): VirtualField =
+        asVirtualField(declaringClassFile.thisType)
 
     def asVirtualField(declaringClassType: ObjectType): VirtualField =
         VirtualField(declaringClassType, name, fieldType)
