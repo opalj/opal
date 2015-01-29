199581448bb3874b1cbc16dfd6087b23a6cdb9ae
Michael Eichberg <mail@michael-eichberg.de>
2015-01-29 09:37:35 +0100

    improved the documentation

--- OPAL/ai/src/main/scala/org/opalj/ai/CoreDomainFunctionality.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/CoreDomainFunctionality.scala
@@ -255,9 +255,9 @@ trait CoreDomainFunctionality extends ValuesDomain {
     /**
      * ''Called by the framework after performing a computation''. That is, after
      * evaluating the effect of the instruction with `currentPC` on the current stack and
-     * register and joining the updated stack and registers with the stack and registers
-     * associated with the instruction `successorPC`. (Hence, this method is NOT called
-     * for `return` instructions.)
+     * register and (if necessary) joining the updated stack and registers with the stack
+     * and registers associated with the instruction `successorPC`. (Hence, this method
+     * is NOT called for `return` instructions.)
      * This function basically informs the domain about the instruction that
      * ''may be'' evaluated next. The flow function is called for ''every possible
      * successor'' of the instruction with `currentPC`. This includes all branch
