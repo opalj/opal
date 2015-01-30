4650d390746d77e4e4160b901f1dce3e1d024e10
Michael Eichberg <mail@michael-eichberg.de>
2015-01-30 16:56:22 +0100

    the call graph now keeps a reference to the function that calculates the set of entry methods
    We do not store the set of entry methods, because – in general – it is not needed.

--- OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/ComputedCallGraph.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/ComputedCallGraph.scala
@@ -32,22 +32,43 @@ package analyses
 package cg
 
 import org.opalj.br.analyses.SomeProject
+import org.opalj.br.Method
 
 /**
  * Representation of a computed call graph.
  *
+ * @param entryPoints A function to get the entry points that were used to start
+ *      the calculation of the call graph.
+ *
  * @author Michael Eichberg
  */
-case class ComputedCallGraph(
-    callGraph: CallGraph,
-    unresolvedMethodCalls: List[UnresolvedMethodCall],
-    constructionExceptions: List[CallGraphConstructionException])
+class ComputedCallGraph(
+    val callGraph: CallGraph,
+    val entryPoints: () ⇒ Iterable[Method],
+    val unresolvedMethodCalls: List[UnresolvedMethodCall],
+    val constructionExceptions: List[CallGraphConstructionException])
 
 object ComputedCallGraph {
 
+    def apply(
+        callGraph: CallGraph,
+        entryPoints: () ⇒ Iterable[Method],
+        unresolvedMethodCalls: List[UnresolvedMethodCall],
+        constructionExceptions: List[CallGraphConstructionException]) =
+        new ComputedCallGraph(
+            callGraph,
+            entryPoints,
+            unresolvedMethodCalls,
+            constructionExceptions)
+
+    def unapply(
+        cg: ComputedCallGraph): Some[(CallGraph, List[UnresolvedMethodCall], List[CallGraphConstructionException])] =
+        Some((cg.callGraph, cg.unresolvedMethodCalls, cg.constructionExceptions))
+
     def empty(project: SomeProject) =
-        ComputedCallGraph(
+        apply(
             new CallGraph(project, Map.empty, Map.empty),
+            () ⇒ List.empty,
             List.empty,
             List.empty)
 }
