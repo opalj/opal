8f1d085cfd08c45882e2d64ee4b812d71be17c3f
Michael Eichberg <mail@michael-eichberg.de>
2015-03-18 17:26:27 +0100

    fixes for Java 1.8.0_40

--- TOOLS/bp/ui/src/main/scala/org/opalj/bugpicker/ui/dialogs/DialogStage.scala
+++ TOOLS/bp/ui/src/main/scala/org/opalj/bugpicker/ui/dialogs/DialogStage.scala
@@ -13,7 +13,7 @@
  *  - Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -22,7 +22,7 @@
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
@@ -105,7 +105,7 @@ object DialogStage {
                         margin = Insets(20)
                     }
                     bottom = new HBox {
-                        content = Seq(
+                        children = Seq(
                             new Button {
                                 text = buttonNegativeMessage
                                 defaultButton = true
@@ -122,7 +122,7 @@ object DialogStage {
                                     theStage.close()
                                 }
                             })
-                        alignment = Pos.CENTER
+                        alignment = Pos.Center
                     }
                 }
                 stylesheets += BugPicker.defaultAppCSSURL
