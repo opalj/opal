fa92106824c893bf358dce10b7acdf7c821bb9da
Michael Eichberg <mail@michael-eichberg.de>
2015-05-04 07:40:09 +0200

    fixed some typos
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/ai/src/main/scala/org/opalj/ai/domain/AsDomainValue.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/domain/AsDomainValue.scala
@@ -35,7 +35,7 @@ package ai
 package domain
 
 /**
- * Mixed in by domain's that support the conversation of a Java Object into a
+ * Mixed in by domains that support the conversation of a Java Object into a
  * `DomainValue` into.
  *
  * @see [[AsJavaObject]] for further information on limitations.
