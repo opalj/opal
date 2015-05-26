5ef4bc7e419adea975c0ede942764a871b6dd718
Michael Eichberg <mail@michael-eichberg.de>
2015-05-26 12:53:42 +0200

    renaming EventType => ProgressEvents
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/br/analyses/ProgressEvents.scala
+++ OPAL/br/src/main/scala/org/opalj/br/analyses/ProgressEvents.scala
@@ -31,40 +31,30 @@ package br
 package analyses
 
 /**
- * An analysis that performs all computations in one step. Only very short-running
- * analyses should use this interface!
+ * Characterizes the type of an event related to a running analysis.
+ *
+ * @see [[ProgressManagement]] for further details.
  *
  * @author Michael Eichberg
  */
-trait OneStepAnalysis[Source, +AnalysisResult] extends Analysis[Source, AnalysisResult] {
+object ProgressEvents extends Enumeration {
 
-    def doAnalyze(
-        project: Project[Source],
-        parameters: Seq[String] = List.empty,
-        isInterrupted: () ⇒ Boolean): AnalysisResult
+    /**
+     * Used to signal the start of a (longer-running) computation.
+     * Each computation that signals a start must also signal an end of the computation.
+     */
+    val Start = Value("start")
 
-    final def analyze(
-        project: Project[Source],
-        parameters: Seq[String] = List.empty,
-        initProgressManagement: (Int) ⇒ ProgressManagement = ProgressManagement.None): AnalysisResult = {
+    /**
+     * Used to signal the end of a computation.
+     */
+    val End = Value("end")
 
-        val pm = initProgressManagement(1)
-        pm.progress(1, ProgressEvents.Start, Some(title))
-        var wasKilled = false
-        def isInterrupted(): Boolean = {
-            wasKilled = pm.isInterrupted()
-            wasKilled
-        }
-
-        val result = doAnalyze(project, parameters, isInterrupted)
-
-        if (wasKilled)
-            pm.progress(-1, ProgressEvents.Killed, None)
-        else
-            pm.progress(1, ProgressEvents.End, None)
-
-        result
-    }
-
+    /**
+     * Use to signal that a computation was killed.
+     *
+     * '''After signaling a `Killed` event the underlying computation is not
+     * allowed to signal any further events.'''
+     */
+    val Killed = Value("killed")
 }
-
