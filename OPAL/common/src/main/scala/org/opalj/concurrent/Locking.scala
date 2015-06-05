8e4378a54b180dd6c9e05ace4358827daa3119f7
Michael Eichberg <mail@michael-eichberg.de>
2015-06-05 09:14:01 +0200

    we are now using lock (instead of lockInterruptibly) again (the decision might change again!)
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/concurrent/Locking.scala
+++ OPAL/common/src/main/scala/org/opalj/concurrent/Locking.scala
@@ -64,18 +64,13 @@ trait Locking {
 object Locking {
 
     /**
-     * Creates a new reentrant read-write lock.
-     */
-    def apply(): Locking = new Locking {}
-
-    /**
      * Acquires the write lock associated with this instance and then executes
      * the function `f`. Afterwards, the lock is released.
      */
-    final def withWriteLock[B](rwLock: ReentrantReadWriteLock)(f: ⇒ B): B = {
+    @inline final def withWriteLock[B](rwLock: ReentrantReadWriteLock)(f: ⇒ B): B = {
         var isLocked = false
         try {
-            rwLock.writeLock().lockInterruptibly()
+            rwLock.writeLock().lock()
             isLocked = true
             f
         } finally {
@@ -87,7 +82,7 @@ object Locking {
      * Acquires the read lock and then executes
      * the function `f`. Afterwards, the lock is released.
      */
-    final def withReadLock[B](rwLock: ReentrantReadWriteLock)(f: ⇒ B): B = {
+    @inline final def withReadLock[B](rwLock: ReentrantReadWriteLock)(f: ⇒ B): B = {
         try {
             rwLock.readLock().lock()
             f
@@ -103,7 +98,7 @@ object Locking {
     final def tryWithReadLock[B](rwLock: ReentrantReadWriteLock)(f: ⇒ B): Option[B] = {
         var isLocked = false
         try {
-            isLocked = rwLock.readLock().tryLock(100l, TimeUnit.NANOSECONDS)
+            isLocked = rwLock.readLock().tryLock(100l, TimeUnit.MILLISECONDS)
             if (isLocked)
                 Some(f)
             else
