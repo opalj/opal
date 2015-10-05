3e24424da7b0bc16ebf6671d5d367a7a39e91cd2
mireif <reif@st.informatik.tu-darmstadt.de>
2015-10-05 10:41:39 +0200

    added a first sketch of the overriding analysis

---
+++ OPAL/fpa/src/main/scala/org/opalj/br/analyses/OverridingAnalysis.scala
@@ -0,0 +1,111 @@
+package org.opalj.br.analyses
+
+import org.opalj.fpa.AssumptionBasedFixpointAnalysis
+import org.opalj.br.analyses.fp.ProjectAccessibility
+import org.opalj.fp.Result
+import org.opalj.br.analyses.fp.PackageLocal
+import java.net.URL
+import org.opalj.fp.PropertyStore
+import org.opalj.fp.PropertyComputationResult
+import org.opalj.br.Method
+import org.opalj.fp.Entity
+import org.opalj.fpa.FilterEntities
+import org.opalj.fp.Property
+import org.opalj.fp.PropertyKey
+import org.opalj.br.ClassFile
+
+sealed trait Overridden extends Property {
+    final def key = Overridden.Key
+}
+
+object Overridden {
+    final val Key = PropertyKey.create("Overridden", NonOverridden)
+}
+
+case object IsOverridden extends Overridden { final val isRefineable = false }
+
+case object PotentiallyOverridden extends Overridden { final val isRefineable = false }
+
+case object NonOverridden extends Overridden { final val isRefineable = false }
+
+/**
+ * @author Michael Reif
+ */
+object OverridingAnalysis
+        extends AssumptionBasedFixpointAnalysis
+        with FilterEntities[Method] {
+
+    val propertyKey = ProjectAccessibility.Key
+
+    /**
+     * Determines the [[ProjectAccessibility]] property of static methods considering shadowing of methods
+     * provided by super classes. It is tailored to entry point set computation where we have to consider different kind
+     * of assumption depending on the analyzed program.
+     *
+     * Computational differences regarding static methods are :
+     *  - private methods can be handled equal in every context
+     *  - if OPA is met, all package visible classes are visible which implies that all non-private methods are
+     *    visible too
+     *  - if CPA is met, methods in package visible classes are not visible by default.
+     *
+     */
+    def determineProperty(
+        method: Method)(
+            implicit project: Project[URL],
+            store: PropertyStore): PropertyComputationResult = {
+
+        if (method.isPrivate || method.isFinal)
+            return Result(method, NonOverridden)
+
+        // this would be way more efficient when caluclating entry points
+        // but if you want to have a dedicated looking at the isOverridden property,
+        // it will lead to incorrect results
+        /*if (isOpenPackagesAssumption)
+            return Result(method, Global)
+            method.toJava()*/
+
+        val declaringClass = project.classFile(method)
+
+        if (declaringClass.isFinal)
+            return Result(method, NonOverridden)
+
+        val methodDescriptor = method.descriptor
+        val methodName = method.name
+
+        val subtypes = project.classHierarchy.allSubtypes(declaringClass.thisType, false)
+
+        val visibleClass: ClassFile ⇒ Boolean = _.isPublic || isOpenPackagesAssumption
+
+        if (subtypes.size == 0 && visibleClass(declaringClass))
+            return Result(method, PotentiallyOverridden)
+
+        subtypes foreach { subtype ⇒
+            project.classFile(subtype) map { classFile ⇒
+                val potentialMethod = classFile.findMethod(methodName, methodDescriptor)
+                val couldInheritMethod = potentialMethod.isEmpty || !potentialMethod.map { curMethod ⇒
+                    curMethod.visibilityModifier.equals(method.visibilityModifier)
+                }.getOrElse(false)
+
+                if (couldInheritMethod) {
+                    val superuperTypes = project.classHierarchy.allSupertypes(subtype, false)
+
+                    val inheritsMethod = !superuperTypes.exists { supertype ⇒
+                        project.classFile(supertype).map { supClassFile ⇒
+                            supClassFile.findMethod(methodName, methodDescriptor).nonEmpty
+                        }.getOrElse(true) && (supertype ne declaringClass.thisType)
+                    }
+
+                    if (inheritsMethod)
+                        return Result(method, NonOverridden)
+                }
+            }
+        }
+
+        // If no subtype is found, the method is not accessible
+        Result(method, IsOverridden)
+    }
+
+    val entitySelector: PartialFunction[Entity, Method] = {
+        case m: Method if !m.isStatic && m.body.isDefined ⇒ m
+    }
+}
