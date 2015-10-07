60f60e7f3e8c8551048a1d52dfbf78b1f756f436
mireif <reif@st.informatik.tu-darmstadt.de>
2015-10-07 13:16:18 +0200

    added a test for the OverridingAnalysis. This commit contains a test , an annotation and annotated java test classes as source and compiled to a jar.

---
+++ OPAL/fpa/src/test/resources/sourcefiles/overriding/ASuperclass.java
@@ -0,0 +1,63 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package overriding;
+
+import org.opalj.fpa.test.annotations.OverriddenKeys;
+import org.opalj.fpa.test.annotations.OverriddenProperty;
+
+public abstract class ASuperclass {
+	
+	@OverriddenProperty(OverriddenKeys.CantNotBeOverridden)
+	private void privateMethod(){
+		System.out.println("private");
+	}
+	
+	@OverriddenProperty(OverriddenKeys.IsOverridden)
+	public void publicMethod(){
+		privateMethod();
+		protectedMethod();
+		publicFinalMethod();
+		System.out.println("public");
+	}
+	
+	@OverriddenProperty(OverriddenKeys.IsOverridden)
+	protected void protectedMethod(){
+		System.out.println("protected");
+	}
+	
+	@OverriddenProperty(OverriddenKeys.IsOverridden)
+	void packagePrivateMethod(){
+		System.out.println("package private");
+	}
+	
+	@OverriddenProperty(OverriddenKeys.CantNotBeOverridden)
+	public final void publicFinalMethod(){
+		System.out.println("public and final");
+	}
+}
