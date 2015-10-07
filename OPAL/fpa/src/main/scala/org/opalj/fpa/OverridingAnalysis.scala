6f5e39061d950f353e828f18947bf46d8909dd3c
mireif <reif@st.informatik.tu-darmstadt.de>
2015-10-07 13:08:35 +0200

    Moved the analysis to another package and adopted it

--- OPAL/fpa/src/main/scala/org/opalj/fpa/OverridingAnalysis.scala
+++ OPAL/fpa/src/main/scala/org/opalj/fpa/OverridingAnalysis.scala
@@ -1,18 +1,46 @@
-package org.opalj.br.analyses
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-import org.opalj.fpa.AssumptionBasedFixpointAnalysis
+package org.opalj.fpa
+
 import org.opalj.br.analyses.fp.ProjectAccessibility
 import org.opalj.fp.Result
-import org.opalj.br.analyses.fp.PackageLocal
 import java.net.URL
 import org.opalj.fp.PropertyStore
 import org.opalj.fp.PropertyComputationResult
 import org.opalj.br.Method
 import org.opalj.fp.Entity
-import org.opalj.fpa.FilterEntities
 import org.opalj.fp.Property
 import org.opalj.fp.PropertyKey
 import org.opalj.br.ClassFile
+import org.opalj.br.analyses.Project
+import org.opalj.br.ObjectType
 
 sealed trait Overridden extends Property {
     final def key = Overridden.Key
@@ -28,6 +56,8 @@ case object PotentiallyOverridden extends Overridden { final val isRefineable = 
 
 case object NonOverridden extends Overridden { final val isRefineable = false }
 
+case object CantNotBeOverridden extends Overridden { final val isRefineable = false }
+
 /**
  * @author Michael Reif
  *
@@ -39,7 +69,9 @@ object OverridingAnalysis
         with FilterEntities[Method] {
 
     val propertyKey = ProjectAccessibility.Key
-
+    
+    val objectType = ObjectType.Object
+    
     /**
      * Determines the [[Overridden]] property of static methods considering shadowing of methods
      * provided by super classes. It is tailored to entry point set computation where we have to consider different kind
@@ -58,7 +90,7 @@ object OverridingAnalysis
             store: PropertyStore): PropertyComputationResult = {
 
         if (method.isPrivate || method.isFinal)
-            return Result(method, NonOverridden)
+            return Result(method, CantNotBeOverridden)
 
         // this would be way more efficient when caluclating entry points
         // but if you want to have a dedicated looking at the isOverridden property,
@@ -69,15 +101,18 @@ object OverridingAnalysis
         val declaringClass = project.classFile(method)
 
         if (declaringClass.isFinal)
-            return Result(method, NonOverridden)
-
+            return Result(method, CantNotBeOverridden)
+            
+        val declClassType = declaringClass.thisType    
         val methodDescriptor = method.descriptor
         val methodName = method.name
-
-        val subtypes = project.classHierarchy.allSubtypes(declaringClass.thisType, false)
-
-        val visibleClass: ClassFile ⇒ Boolean = _.isPublic || isOpenPackagesAssumption
-
+        val packageVisible = method.isPackagePrivate
+        
+        val subtypes = project.classHierarchy.allSubtypes(declClassType, false)
+        
+        if(subtypes.size == 0)
+            return Result(method, NonOverridden)
+            
         subtypes foreach { subtype ⇒
             project.classFile(subtype) map { classFile ⇒
                 val potentialMethod = classFile.findMethod(methodName, methodDescriptor)
@@ -85,16 +120,19 @@ object OverridingAnalysis
                     curMethod.visibilityModifier.equals(method.visibilityModifier)
                 }.getOrElse(false)
 
-                if (couldInheritMethod) {
+                val packagePrivateCondition = 
+                    if(packageVisible) subtype.packageName == declClassType.packageName
+                    else true
+                
+                if (couldInheritMethod && packagePrivateCondition) {
                     val superuperTypes = project.classHierarchy.allSupertypes(subtype, false)
 
-                    // the check, if the superclass is visible, leads to wrong results in terms
-                    // of the Overridden property
                     val inheritsMethod = !superuperTypes.exists { supertype ⇒
+                        (supertype ne declClassType) && 
+                        (supertype ne objectType) && 
                         project.classFile(supertype).map { supClassFile ⇒
-                            supClassFile.findMethod(methodName, methodDescriptor).nonEmpty &&
-                                visibleClass(supClassFile)
-                        }.getOrElse(true) && (supertype ne declaringClass.thisType)
+                            supClassFile.findMethod(methodName, methodDescriptor).nonEmpty
+                        }.getOrElse(true)
                     }
 
                     if (inheritsMethod)
@@ -103,8 +141,8 @@ object OverridingAnalysis
             }
         }
 
-        if (visibleClass(declaringClass))
-            return Result(method, PotentiallyOverridden)
+        //        if (visibleClass(declaringClass))
+        //      return Result(method, PotentiallyOverridden)
 
         // If no subtype is found, the method is not accessible
         Result(method, IsOverridden)
