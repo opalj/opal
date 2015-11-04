fb5de06b4e865e5235db1a767d7342e71f148998
mireif <reif@st.informatik.tu-darmstadt.de>
2015-11-04 10:33:43 +0100

    added a shortcut to get the id of the property key

--- OPAL/common/src/main/scala/org/opalj/fpcf/Property.scala
+++ OPAL/common/src/main/scala/org/opalj/fpcf/Property.scala
@@ -50,6 +50,12 @@ trait Property {
     def key: PropertyKey
 
     /**
+     * The id uniquely identifies this property's category. All property objects of the
+     * same kind have to use the same id which is guaranteed since they share the same `PropertyKey`
+     */
+    def id: Int = key.id
+    
+    /**
      * Returns `true` if the current property may be refined in the future and, hence,
      * it is meaningful to register for update events.
      */
