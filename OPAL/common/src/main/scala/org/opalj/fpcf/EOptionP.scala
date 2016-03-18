a1ffa14da729a405b40fd89e8d23b0e3a88f8cf4
Michael Eichberg <mail@michael-eichberg.de>
2016-03-18 22:13:10 +0100

    merged all EOptionP classes in the same file to make it possible to make EOptionP sealed
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/fpcf/EOptionP.scala
+++ OPAL/common/src/main/scala/org/opalj/fpcf/EOptionP.scala
@@ -33,7 +33,7 @@ package org.opalj.fpcf
  *
  * @author Michael Eichberg
  */
-trait EOptionP[+E <: Entity, +P <: Property] {
+sealed trait EOptionP[+E <: Entity, +P <: Property] {
 
     /**
      * The entity.
@@ -51,6 +51,8 @@ trait EOptionP[+E <: Entity, +P <: Property] {
      * @return `true` if the entity is associated with a property.
      */
     def hasProperty: Boolean
+
+    final def hasNoProperty: Boolean = !hasProperty
 
     /**
      * Returns the property if it is available otherwise an `UnsupportedOperationException` is
@@ -73,5 +75,105 @@ object EOptionP {
             case Some(p) ⇒ EP(e, p)
             case None    ⇒ EPK(e, pk)
         }
+    }
+}
+
+/**
+ * A pairing of an [[Entity]] and an associated [[Property]].
+ *
+ * Compared to a standard `Tuple2` the equality of two `EP` objects is based on
+ * comparing the entities using reference equality.
+ *
+ * @author Michael Eichberg
+ */
+final class EP[+E <: Entity, +P <: Property](val e: E, val p: P)
+        extends EOptionP[E, P]
+        with Product2[E, P] {
+
+    override def _1: E = e
+    override def _2: P = p
+
+    def hasProperty = true
+
+    override def equals(other: Any): Boolean = {
+        other match {
+            case that: EP[_, _] ⇒ (that.e eq this.e) && this.p == that.p
+            case _              ⇒ false
+        }
+    }
+
+    override def canEqual(that: Any): Boolean = that.isInstanceOf[EP[_, _]]
+
+    def pk: PropertyKey[P] = p.key.asInstanceOf[PropertyKey[P]]
+
+    def toEPK: EPK[E, P] = EPK(e, pk)
+
+    override def hashCode: Int = e.hashCode() * 727 + p.hashCode()
+
+    override def toString: String = s"EP($e,$p)"
+}
+
+/**
+ * Provides a factory and an extractor for [[EP]] objects.
+ *
+ * @author Michael Eichberg
+ */
+object EP {
+
+    def apply[E <: Entity, P <: Property](e: E, p: P): EP[E, P] = new EP(e, p)
+
+    def unapply[E <: Entity, P <: Property](ep: EP[E, P]): Option[(E, P)] = Some((ep.e, ep.p))
+
+}
+
+/**
+ * A simple pair consisting of an [[Entity]] and a [[PropertyKey]].
+ *
+ * Compared to a standard `Tuple2` the entities are compared using reference comparison
+ * and not equality based on `equals` checks.
+ *
+ * @author Michael Eichberg
+ */
+final class EPK[+E <: Entity, +P <: Property](
+    val e:  E,
+    val pk: PropertyKey[P]
+)
+        extends EOptionP[E, P]
+        with Product2[E, PropertyKey[P]] {
+
+    override def _1 = e
+    override def _2 = pk
+
+    def hasProperty: Boolean = false
+
+    def p: Nothing = throw new UnsupportedOperationException()
+
+    def toEPK: this.type = this
+
+    override def equals(other: Any): Boolean = {
+        other match {
+            case that: EPK[_, _] ⇒ (that.e eq this.e) && this.pk == that.pk
+            case _               ⇒ false
+        }
+    }
+
+    override def canEqual(that: Any): Boolean = that.isInstanceOf[EPK[_, _]]
+
+    override def hashCode: Int = e.hashCode() * 511 + pk.id
+
+    override def toString: String = s"EPK($e,name=${PropertyKey.name(pk.id)},id=${pk.id})"
+}
+
+/**
+ * Factory and extractor for [[EPK]] objects.
+ *
+ * @author Michael Eichberg
+ */
+object EPK {
+
+    def apply[E <: Entity, P <: Property](e: E, pk: PropertyKey[P]): EPK[E, P] = new EPK(e, pk)
+
+    def unapply[E <: Entity, P <: Property](epk: EPK[E, P]): Option[(E, PropertyKey[P])] = {
+        Some((epk.e, epk.pk))
     }
 }
