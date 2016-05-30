b0566fbf1f87c51a3794ee9012492fcba422eaef
Michael Eichberg <mail@michael-eichberg.de>
2016-05-30 14:59:12 +0200

    added another test related to arrays handling

--- OPAL/ai/src/test/resources/sourcefiles/ai/MethodsWithArrays.java
+++ OPAL/ai/src/test/resources/sourcefiles/ai/MethodsWithArrays.java
@@ -442,4 +442,15 @@ public class MethodsWithArrays {
         processIt(is); // we "just" know that is's first dimension is "2"
     }
 
+	public static Object[] arrayStoreException(boolean v) {
+		Object[] array = new Cloneable[1];// arrays are covariant, but this is not relevant here...
+
+		if (v) {
+			array[0] = new java.io.Serializable() {
+				private static final long serialVersionUID = 1L;
+			};
+		}
+
+		return array;
+	}
 }
