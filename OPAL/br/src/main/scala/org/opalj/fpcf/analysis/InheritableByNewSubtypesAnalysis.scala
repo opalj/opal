61e501c191a8fc82ca9983430110a3ddcbd528ba
mireif <reif@st.informatik.tu-darmstadt.de>
2016-06-15 10:17:24 +0200

    removed invaled scaladoc link

--- OPAL/br/src/main/scala/org/opalj/fpcf/analysis/InheritableByNewSubtypesAnalysis.scala
+++ OPAL/br/src/main/scala/org/opalj/fpcf/analysis/InheritableByNewSubtypesAnalysis.scala
@@ -1,3 +1,31 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2016
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.opalj
 package fpcf
 package analysis
@@ -10,7 +38,7 @@ import org.opalj.fpcf.properties.IsInheritableByNewTypes
 import scala.collection.mutable
 
 /**
- * This analysis computes the [[InheritableByNewTypes]] property.* I.e., it determines whether a method can directly be inherited by a future subtype.
+ * This analysis computes the InheritableByNewTypes property.* I.e., it determines whether a method can directly be inherited by a future subtype.
  * This is in particular important when analyzing libraries.
  *
  * The analysis assumes that packages that start with "java." are closed, i.e., that no client can put a class into these specific packages.
