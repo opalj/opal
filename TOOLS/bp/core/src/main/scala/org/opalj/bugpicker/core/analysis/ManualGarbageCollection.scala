0bfdd4fd04e214c48f81d462b51a98df82aa4a12
Michael Eichberg <mail@michael-eichberg.de>
2016-06-22 16:41:35 +0200

    fixed pattern matchers

--- TOOLS/bp/core/src/main/scala/org/opalj/bugpicker/core/analysis/ManualGarbageCollection.scala
+++ TOOLS/bp/core/src/main/scala/org/opalj/bugpicker/core/analysis/ManualGarbageCollection.scala
@@ -79,7 +79,7 @@ object ManualGarbageCollection {
         for {
             method @ MethodWithBody(body) ← classFile.methods
             (pc, gcCall) ← body.collectWithIndex {
-                case (pc, INVOKESTATIC(ObjectType.System, "gc", NoArgsAndReturnVoid)) ⇒
+                case (pc, INVOKESTATIC(ObjectType.System, false, "gc", NoArgsAndReturnVoid)) ⇒
                     (pc, "System.gc()")
                 case (pc, INVOKEVIRTUAL(Runtime, "gc", NoArgsAndReturnVoid)) ⇒
                     (pc, "Runtime.gc()")
