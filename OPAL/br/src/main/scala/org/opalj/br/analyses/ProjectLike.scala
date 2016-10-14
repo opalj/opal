990322d82aff9734f88bfb00757454f14aae401f
Michael Eichberg <mail@michael-eichberg.de>
2016-10-14 11:20:25 +0200

    started adding support for Java 8 Method resolution
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/br/analyses/ProjectLike.scala
+++ OPAL/br/src/main/scala/org/opalj/br/analyses/ProjectLike.scala
@@ -37,12 +37,26 @@ import scala.collection.mutable
 //import org.opalj.log.LogContext
 import org.opalj.br.instructions.FieldAccess
 import org.opalj.br.instructions.MethodInvocationInstruction
-//import org.opalj.collection.immutable.UIDSet
-//import org.opalj.collection.immutable.UIDSet0
-//import org.opalj.collection.immutable.UIDSet1
+import org.opalj.collection.immutable.Result
+import org.opalj.collection.immutable.Failure
+import org.opalj.collection.immutable.Success
+import org.opalj.collection.immutable.Empty
+import org.opalj.collection.immutable.UIDSet
+import org.opalj.collection.immutable.UIDSet0
+import org.opalj.br.instructions.INVOKESTATIC
 
 /**
- * Enables project wide lookups.
+ * Enables project wide lookups of methods and fields.
+ *
+ * @note 	The current implementation is based on the '''correct project assumption''';
+ * 			i.e., if the bytecode as a whole is not valid, the result is generally undefined.
+ * 			One example would be, if we have two interfaces which define a non-abstract method
+ * 			with the same signature and both interfaces are implemented by a third interface
+ * 			which does not override these methods. In this case the result of a
+ * 			[[resolveMethodCall]] is not defined, because the code base as a whole is not
+ * 			valid.
+ *
+ * @note
  *
  * @author Michael Eichberg
  */
@@ -50,10 +64,15 @@ trait ProjectLike extends ClassFileRepository { project ⇒
 
     implicit def classHierarchy: ClassHierarchy
 
+    /**
+     * Returns the class file of `java.lang.Object` if available.
+     */
+    val ObjectClassFile: Option[ClassFile]
+
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     //
     //
-    // RESOLVING FIELD AND METHOD REFERENCES
+    // RESOLVING FIELD REFERENCES
     //
     //
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -116,11 +135,50 @@ trait ProjectLike extends ClassFileRepository { project ⇒
         resolveFieldReference(fieldAccess.declaringClass, fieldAccess.name, fieldAccess.fieldType)
     }
 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    //
+    //
+    // RESOLVING METHOD REFERENCES / DETERMING THE INVOKED METHOD(S)
+    //
+    //
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    /* GENERAL NOTES
+     *
+     * (Accessibilty checks are done by the JVM when the method is resolved; this is, however not
+     * done by the following methods as it does not affect the search for the correct
+     * methods.)
+     *
+     * Invokestatic 	=> 	the resolved method is called (if it is accessible, static etc...)
+     * Invokespecial 	=>	the resolved (interface) method is called if it is a constructor
+     * 						call, or the resolved method belongs to the calling class (calls of
+     * 						private methods). Otherwise, the calling class' supertypes are
+     * 						searched for a method with the same signature up until the resolved
+     * 						method. When we search the type hierarchy upwards, we first search
+     * 						for a method defined by a superclass (unless the current class defines
+     * 						an interface type) before we search the interfaces. In the later
+     * 						case we compute the set of the maximally specific matching methods
+     * 						and select THE concrete one (it is an error if multiple concrete ones
+     * 						exist!)
+     * Invokevirtual	=>	the resolved method is called, if the resolved method is signature
+     * 						polymorphic; in this case the concrete type of the method receiver
+     * 						is not relevant. Otherwise, the type of the receiver is used to
+     * 						start searching for the method that is to be invoked. That method
+     * 						however, has to override the resolved method (which is generally not
+     * 						the case if the method is private; and is only the case if the resolved
+     * 						method and the current type belong to the same package in case of
+     * 						a method with default visibility).
+     * Invokeinterface 	=>	the resolved interface method just defines an upper bound; the
+     * 						called method is determined as in case of invokevirtual; but
+     * 						signature polymorphic calls are not relevant
+     *
+     */
+
     /**
      * Tries to resolve a method reference as specified by the JVM specification.
      * I.e., the algorithm tries to find the class that actually declares the referenced
      * method. Resolution of '''signature polymorphic''' method calls is also
-     * supported; for details see `lookupMethodDefinition`).
+     * supported.
      *
      * This method is the basis for the implementation of the semantics
      * of the `invokeXXX` instructions. However, it does not check whether the resolved
@@ -130,32 +188,270 @@ trait ProjectLike extends ClassFileRepository { project ⇒
      * in case of `invokevirtual` and `invokeinterface` instructions. I.e.,
      * additional processing is necessary on the client side.
      *
-     * @note Generally, if the type of the receiver is not precise the receiver object's
-     *    subtypes should also be searched for method implementations (at least those
-     *    classes should be taken into consideration that may be instantiated).
      * @note This method just resolves a method reference. Additional checks,
      *    such as whether the resolved method is accessible, may be necessary.
+     *
      * @param receiverType The type of the object that receives the method call. The
      *      type must be a class type and must not be an interface type.
+     *      I.e., no check w.r.t. a potential `IncompatibleClassChangeError` is done
+     *      by this method.
      * @return The resolved method `Some(`'''METHOD'''`)` or `None`.
      *      To get the defining class file use the project's respective method.
      */
     def resolveMethodReference(
-        receiverType:     ObjectType,
-        methodName:       String,
-        methodDescriptor: MethodDescriptor
+        receiverType: ObjectType,
+        name:         String,
+        descriptor:   MethodDescriptor
     ): Option[Method] = {
-
+        /*
         project.classFile(receiverType) flatMap { classFile ⇒
 
             {
-                lookupMethodDefinition(receiverType, methodName, methodDescriptor)
+                lookupMethodDefinition(receiverType, name, descriptor)
             } orElse {
-                lookupMethodInSuperinterfaces(classFile, methodName, methodDescriptor)
+                lookupMethodInSuperinterfaces(classFile, name, descriptor)
+            }
+        }
+        */
+        resolveClassMethodReference(receiverType, name, descriptor) match {
+            case Success(method) ⇒ Some(method)
+            case Failure         ⇒ None
+            case Empty ⇒
+                val superinterfaceTypes = classHierarchy.superinterfaceTypes(receiverType).get
+                val (_, methods) =
+                    findMaximallySpecificSuperinterfaceMethods(
+                        superinterfaceTypes, name, descriptor,
+                        analyzedSuperinterfaceTypes = UIDSet0
+                    )
+                methods.headOption // either it is THE max. specific method or some ...
+        }
+    }
+
+    def resolveInterfaceMethodReference(
+        receiverType: ObjectType,
+        name:         String,
+        descriptor:   MethodDescriptor
+    ): Option[Method] = {
+
+        def lookupInObject(): Option[Method] = {
+            ObjectClassFile flatMap { classFile ⇒
+                classFile.findMethod(name, descriptor) filter (m ⇒ m.isPublic && !m.isStatic)
+            }
+        }
+
+        project.classFile(receiverType) flatMap { classFile ⇒
+            assert(classFile.isInterfaceDeclaration)
+            classFile.findMethod(name, descriptor) orElse {
+                lookupInObject() orElse {
+                    classHierarchy.superinterfaceTypes(receiverType) flatMap { superinterfaceTypes ⇒
+                        val (_, methods) = findMaximallySpecificSuperinterfaceMethods(
+                            superinterfaceTypes, name, descriptor, UIDSet0
+                        )
+                        methods.headOption
+                    }
+                }
             }
         }
     }
 
+    /**
+     * Computes the set of maximally specific superinterface methods with the
+     * given name and descriptor.
+     *
+     * @note 	'''This method does not consider methods defined by `java.lang.Object`'''!
+     * 			Those methods have precedence over respective methods defined by
+     * 			superinterfaces! A corresponding check needs to be done before calling
+     * 			this method.
+     */
+    def findMaximallySpecificSuperinterfaceMethods(
+        superinterfaceType:          ObjectType,
+        name:                        String,
+        descriptor:                  MethodDescriptor,
+        analyzedSuperinterfaceTypes: UIDSet[ObjectType] = UIDSet0
+    ): ( /*analyzed types*/ UIDSet[ObjectType], Set[Method]) = {
+
+        val newAnalyzedSuperinterfaceTypes = analyzedSuperinterfaceTypes + superinterfaceType
+
+        // the superinterfaceTypes in which it is potentially relevant to search for methods
+        val superinterfaceTypes: UIDSet[ObjectType] =
+            classHierarchy.superinterfaceTypes(superinterfaceType).getOrElse(UIDSet0) --
+                analyzedSuperinterfaceTypes
+
+        project.classFile(superinterfaceType) match {
+            case Some(classFile) ⇒
+                assert(classFile.isInterfaceDeclaration)
+
+                classFile.findMethod(name, descriptor) match {
+                    case Some(method) if !method.isPrivate && !method.isStatic ⇒
+                        val analyzedTypes = newAnalyzedSuperinterfaceTypes ++ superinterfaceTypes
+                        (analyzedTypes, Set(method))
+
+                    case None ⇒
+                        if (superinterfaceTypes.isEmpty) {
+                            (newAnalyzedSuperinterfaceTypes, Set.empty)
+                        } else if (superinterfaceTypes.isSingletonSet) {
+                            findMaximallySpecificSuperinterfaceMethods(
+                                superinterfaceTypes.head,
+                                name, descriptor,
+                                newAnalyzedSuperinterfaceTypes
+                            )
+                        } else {
+                            findMaximallySpecificSuperinterfaceMethods(
+                                superinterfaceTypes,
+                                name, descriptor,
+                                newAnalyzedSuperinterfaceTypes
+                            )
+                        }
+                }
+
+            case None ⇒
+                (analyzedSuperinterfaceTypes ++ superinterfaceTypes + superinterfaceType, Set.empty)
+        }
+
+    }
+
+    /**
+     * Computes the maximally specific superinterface method with the given name
+     * and descriptor
+     *
+     * @param interfaceTypes A set of interfaces which potentially declare a method
+     * 			with the given name and descriptor.
+     */
+    def findMaximallySpecificSuperinterfaceMethods(
+        superinterfaceTypes:         UIDSet[ObjectType],
+        name:                        String,
+        descriptor:                  MethodDescriptor,
+        analyzedSuperinterfaceTypes: UIDSet[ObjectType]
+    ): ( /*analyzed types*/ UIDSet[ObjectType], Set[Method]) = {
+
+        val anchor = ((analyzedSuperinterfaceTypes, Set.empty[Method]))
+        superinterfaceTypes.foldLeft(anchor) { (currentResult, interfaceType) ⇒
+            val (currentAnalyzedSuperinterfaceTypes, currentMethods) = currentResult
+            val (analyzedSuperinterfaceTypes, methods) =
+                findMaximallySpecificSuperinterfaceMethods(
+                    interfaceType, name, descriptor,
+                    currentAnalyzedSuperinterfaceTypes
+                )
+
+            val allMethods = currentMethods ++ methods
+            if (allMethods.isEmpty || allMethods.size == 1) {
+                (analyzedSuperinterfaceTypes, allMethods /*empty or singleton set*/ )
+            } else {
+                // When we reach this point, we may have a situation such as:
+                //     intf A { default void foo(){} }
+                //     intf B extends A { default void foo(){} }
+                //     intf C extends A { }
+                //     intf D extends B { }
+                // and we started the analysis with the set {C,D} and
+                // first selected C (hence, first found A.foo).
+                //
+                // We now have to determine the maximally specific method.
+
+                // both, the set of `currentMethods` and also the set of `methods`
+                // each only contains maximally specific methods w.r.t. their
+                // set
+                var currentMaximallySpecificMethods = currentMethods
+                var additionalMaximallySpecificMethods = Set.empty[Method]
+                methods.view.filter(!currentMethods.contains(_)) foreach { method ⇒
+                    val newMethodDeclaringClassType = project.classFile(method).thisType
+                    var addNewMethod = true
+                    currentMaximallySpecificMethods = currentMaximallySpecificMethods.filter { method ⇒
+                        val specificMethodDeclaringClassType = project.classFile(method).thisType
+                        if (classHierarchy.isSubtypeOf(specificMethodDeclaringClassType, newMethodDeclaringClassType).isYes) {
+                            addNewMethod = false
+                            true
+                        } else if (classHierarchy.isSubtypeOf(newMethodDeclaringClassType, specificMethodDeclaringClassType).isYes) {
+                            false
+                        } else {
+                            //... we have an incomplete class hierarchy; let's keep both methods
+                            true
+                        }
+                    }
+                    if (addNewMethod)
+                        additionalMaximallySpecificMethods += method
+                }
+                currentMaximallySpecificMethods ++= additionalMaximallySpecificMethods
+
+                val concreteMaximallySpecificMethods = currentMaximallySpecificMethods.filter(!_.isAbstract)
+                if (concreteMaximallySpecificMethods.isEmpty)
+                    // We have not yet found any method or we may have multiple abstract methods...
+                    (analyzedSuperinterfaceTypes, currentMaximallySpecificMethods)
+                else
+                    (analyzedSuperinterfaceTypes, concreteMaximallySpecificMethods)
+            }
+        }
+    }
+
+    def resolveClassMethodReference(
+        receiverType: ObjectType,
+        name:         String,
+        descriptor:   MethodDescriptor
+    ): Result[Method] = {
+
+        project.classFile(receiverType) match {
+            case Some(classFile) ⇒
+                assert(!classFile.isInterfaceDeclaration)
+
+                def resolveSuperclassMethodReference(): Result[Method] = {
+                    classFile.superclassType match {
+                        case Some(superclassType) ⇒
+                            resolveClassMethodReference(superclassType, name, descriptor)
+                        case None ⇒ Empty //the current type is already java.lang.Object
+                    }
+                }
+
+                // [FROM THE SPECIFICATION]]
+                // A method is signature polymorphic if all of the following conditions hold :
+                // - It is declared in the java.lang.invoke.MethodHandle class.
+                // - It has a single formal parameter of type Object[].
+                // - It has a return type of Object.
+                // - It has the ACC_VARARGS and ACC_NATIVE flags set.
+                val isPotentiallySignaturePolymorphicCall =
+                    (receiverType eq ObjectType.MethodHandle) &&
+                        (descriptor == MethodDescriptor.SignaturePolymorphicMethod)
+
+                if (isPotentiallySignaturePolymorphicCall) {
+                    val methods = classFile.findMethod(name)
+                    if (methods.isSingletonList) {
+                        val method = methods.head
+                        if (method.isNativeAndVarargs)
+                            Success(method) // the resolved method is signature polymorphic
+                        else if (method.descriptor == descriptor)
+                            Success(method) // "normal" resolution of a method
+                        else
+                            resolveSuperclassMethodReference()
+                    } else {
+                        methods.find(method ⇒ method.descriptor == descriptor) match {
+                            case None                 ⇒ resolveSuperclassMethodReference()
+                            case Some(resolvedMethod) ⇒ Success(resolvedMethod)
+                        }
+                    }
+                } else {
+                    classFile.findMethod(name, descriptor) match {
+                        case None                 ⇒ resolveSuperclassMethodReference()
+                        case Some(resolvedMethod) ⇒ Success(resolvedMethod)
+                    }
+                }
+
+            case None ⇒ Failure
+        }
+    }
+
+//    invokestaticTarget(i : INVOKESTATIC) : Option[Method] = {
+//        declaringClass:   ObjectType, // a class type to be precise
+//        isInterface:      Boolean,
+//        name:             String,
+//        methodDescriptor: MethodDescriptor
+//    }
+
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+    /////////// OLD OLD OLD OLD OLD OLD //////////
+
+    /*
     def resolveInterfaceMethodReference(
         receiverType:     ObjectType,
         methodName:       String,
@@ -172,6 +468,7 @@ trait ProjectLike extends ClassFileRepository { project ⇒
             }
         }
     }
+    */
 
     def lookupMethodInInterface(
         classFile:        ClassFile,
@@ -188,7 +485,6 @@ trait ProjectLike extends ClassFileRepository { project ⇒
         methodDescriptor: MethodDescriptor
     ): Option[Method] = {
 
-        // IMPROVE [PERFORMANCE] Get rid of inner "return"
         classFile.interfaceTypes foreach { superinterface: ObjectType ⇒
             project.classFile(superinterface) foreach { superclass ⇒
                 val result = lookupMethodInInterface(superclass, methodName, methodDescriptor)
