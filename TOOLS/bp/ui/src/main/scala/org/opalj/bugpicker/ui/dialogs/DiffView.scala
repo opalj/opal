e923503f92386ac9b83fde6b70cd2bb425a777d0
Michael Eichberg <mail@michael-eichberg.de>
2016-10-20 11:59:47 +0200

    reformatting
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- TOOLS/bp/ui/src/main/scala/org/opalj/bugpicker/ui/dialogs/DiffView.scala
+++ TOOLS/bp/ui/src/main/scala/org/opalj/bugpicker/ui/dialogs/DiffView.scala
@@ -76,7 +76,12 @@ import scalafx.stage.Stage
 import scalafx.stage.StageStyle
 
 //FIXME currentParamters are not used anymore
-class DiffView(currentName: String, currentIssues: Iterable[Node], currentParameters: Seq[String] = Seq.empty, oldAnalysis: StoredAnalysis) extends Stage {
+class DiffView(
+    currentName: String,
+     currentIssues: Iterable[Node],
+     currentParameters: Seq[String] = Seq.empty,
+     oldAnalysis: StoredAnalysis
+ ) extends Stage {
     self ⇒
 
     showing.onChange((_, _, newShow) ⇒ {
@@ -233,7 +238,7 @@ class DiffView(currentName: String, currentIssues: Iterable[Node], currentParame
                 while (!found && x < oIssues.size) {
                     if (isSameIssue(i, x)) { // found the same issue in both, but old one later than expected
                         commonIssues = commonIssues :+ c1 // add common issue
-                        for (s ← j until x) { // everything between add as missing issues 
+                        for (s ← j until x) { // everything between add as missing issues
                             missingIssues = missingIssues :+ oIssues(s)
                         }
                         j = x + 1
