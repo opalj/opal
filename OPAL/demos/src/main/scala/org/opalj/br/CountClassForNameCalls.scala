89e59a739d632eeb95800a87c1f109f642cb05ba
Michael Eichberg <mail@michael-eichberg.de>
2016-10-25 15:12:49 +0200

    fixed a race condition and improved the output
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/demos/src/main/scala/org/opalj/br/CountClassForNameCalls.scala
+++ OPAL/demos/src/main/scala/org/opalj/br/CountClassForNameCalls.scala
@@ -41,14 +41,9 @@ import org.opalj.br.analyses.DefaultOneStepAnalysis
  */
 object CountClassForNameCalls extends DefaultOneStepAnalysis {
 
-    override def description: String =         "Counts the number of times Class.forName is called."
+    override def description: String = "Counts the number of times Class.forName is called."
 
-    def doAnalyze(
-        project:       Project[URL],
-        parameters:    Seq[String],
-        isInterrupted: () ⇒ Boolean
-    ) = {
-        var classForNameCount = 0
+    def doAnalyze(project: Project[URL], params: Seq[String], isInterrupted: () ⇒ Boolean) = {
 
         import ObjectType.{String, Class}
         // Next, we create a descriptor of a method that takes a single parameter of
@@ -66,15 +61,10 @@ object CountClassForNameCalls extends DefaultOneStepAnalysis {
                 // Class.forName(String) : Class<?>
                 (pc, INVOKESTATIC(Class, _, "forName", `descriptor`)) ← code
             } yield {
-                classForNameCount += 1
-                s"${method.toJava(classFile)}// pc=$pc" 
+                method.toJava(classFile, s"pc=$pc")
             }
-        val uniqueInvokes = invokes.seq.toSet
-
-        BasicReport(
-            "Class.forName(String) was called: "+classForNameCount+" times.\n\t"+
-                uniqueInvokes.mkString("\n\t")
-        )
+        val header = s"found ${invokes.size} calls of Class.forName(String)\n\t"
+        BasicReport(invokes.seq.toList.sorted.mkString(header, "\n\t", "\n"))
     }
 
 }
