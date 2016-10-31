54e4d4e5d516a0c0bf755b458492503a2662bf7a
Michael Eichberg <mail@michael-eichberg.de>
2016-10-31 13:40:29 +0100

    extended Result to make it easier to use
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/collection/immutable/Result.scala
+++ OPAL/common/src/main/scala/org/opalj/collection/immutable/Result.scala
@@ -32,27 +32,44 @@ package immutable
 
 /**
  * Represents the result of some computation that either (a) succeeded and encapsulates some value,
- * (b) succeeded, but has no value - because it was not possible to compute a value using the given
+ * (b) finished, but has no value - because it was not possible to compute a value using the given
  * information - or (c) that failed.
  *
- * @author Michael Eichberg
+ * @note    Depending on the context, it may be useful to distinguish between a success that returns
+ *          an empty collection and a success that has no further information.
+ *
+ * @author  Michael Eichberg
  */
 sealed trait Result[@specialized(Int) +T] extends Serializable {
+    def hasValue: Boolean
+    def value: T
     def map[B](f: T ⇒ B): Result[B]
     def flatMap[B](f: T ⇒ Result[B]): Result[B]
     def foreach[U](f: (T) ⇒ U): Unit
     def withFilter(q: (T) ⇒ Boolean): Result[T]
 }
 
+/**
+ * The computation '''succeeded''' and produced a result. In general
+ */
 case class Success[@specialized(Int) +T](value: T) extends Result[T] {
 
     class FilteredSuccess(p: (T) ⇒ Boolean) extends Result[T] {
+        def hasValue: Boolean = p(value)
+        def value: T = {
+            if (hasValue) {
+                value
+            } else {
+                throw new UnsupportedOperationException("the result is filtered")
+            }
+        }
         def foreach[U](f: (T) ⇒ U): Unit = if (p(value)) f(value)
         def map[B](f: (T) ⇒ B): Result[B] = if (p(value)) Success(f(value)) else Empty
         def flatMap[B](f: (T) ⇒ Result[B]): Result[B] = if (p(value)) f(value) else Empty
         def withFilter(p: (T) ⇒ Boolean): Result[T] = new FilteredSuccess((t: T) ⇒ p(t) && this.p(t))
     }
 
+    def hasValue: Boolean = true
     def foreach[U](f: (T) ⇒ U): Unit = f(value)
     def map[B](f: (T) ⇒ B): Success[B] = Success(f(value))
     def flatMap[B](f: (T) ⇒ Result[B]): Result[B] = f(value)
@@ -60,11 +77,26 @@ case class Success[@specialized(Int) +T](value: T) extends Result[T] {
 }
 
 sealed trait NoResult extends Result[Nothing] {
+    def hasValue: Boolean = false
+    def value: Nothing = throw new UnsupportedOperationException("this result has no value")
     def foreach[U](f: (Nothing) ⇒ U): Unit = {}
     def map[B](f: (Nothing) ⇒ B): this.type = this
     def flatMap[B](f: (Nothing) ⇒ Result[B]): this.type = this
     def withFilter(q: (Nothing) ⇒ Boolean): this.type = this
 }
 
+/**
+ * The computation '''finished''', but did no produce any results or the result was filtered.
+ *
+ * @note    The precise semantics of ''succeeded without results'' is dependent on the semantics
+ *          of the concrete computation and needs to be defined per use case.
+ */
 case object Empty extends NoResult
+
+/**
+ * The computation '''failed''' because of missing/incomplete information.
+ *
+ * @note    The precise semantics of the ''computation failed'' is dependent on the semantics
+ *          of the concrete computation and needs to be defined per use case.
+ */
 case object Failure extends NoResult
