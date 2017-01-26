88c62e1529976d785b81a986e2d2df5803691d2d
Michael Eichberg <mail@michael-eichberg.de>
2017-01-26 16:10:43 +0100

    refined the signature of jumpOffsets to avoid the useless creation of intermediate data structures
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/br/instructions/CompoundConditionalBranchInstruction.scala
+++ OPAL/br/src/main/scala/org/opalj/br/instructions/CompoundConditionalBranchInstruction.scala
@@ -40,9 +40,9 @@ import org.opalj.collection.immutable.Chain
  */
 trait CompoundConditionalBranchInstructionLike extends ConditionalBranchInstructionLike {
 
-    def operandCount: Int = 1
+    override def operandCount: Int = 1
 
-    final def stackSlotsChange: Int = -1
+    override final def stackSlotsChange: Int = -1
 
     /**
      * Returns all case values that are '''not related to the default branch'''.
@@ -56,7 +56,7 @@ trait CompoundConditionalBranchInstruction
 
     def defaultOffset: Int
 
-    def jumpOffsets: Seq[Int]
+    def jumpOffsets: Iterable[Int]
 
     /**
      * Returns the case value(s) that are associated with the given `jumpOffset`.
