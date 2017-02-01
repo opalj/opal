49ebb8ed4331828336c12690d307aad046270172
Michael Eichberg <mail@michael-eichberg.de>
2017-02-01 14:46:45 +0100

    [Refactoring] updated the visibility to make its intended usage clear
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/collection/mutable/IntQueue.scala
+++ OPAL/common/src/main/scala/org/opalj/collection/mutable/IntQueue.scala
@@ -31,23 +31,25 @@ package collection
 package mutable
 
 /**
- * A linked-list based implementation of a mutable queue of int values.
+ * An efficient, lightweight linked-list based implementation of a mutable queue of int values.
+ *
+ * @note   This data-structure is only intended to be used by OPAL.
  *
  * @author Michael Eichberg
  */
-final class IntQueue private (
+protected[opalj] final class IntQueue private (
         private var first: QueueNode = null,
         private var last:  QueueNode = null
 ) extends Serializable { queue ⇒
 
     def this(value: Int) = {
-        this(new QueueNode(value))
+        this(new QueueNode(value,null))
     }
 
     def isEmpty: Boolean = first eq null
+
     def nonEmpty: Boolean = first ne null
-    def head: Int = first.value
-    def tail: this.type = { first = first.next; this }
+
     def size: Int = {
         var size = 0
         var c = first
@@ -57,6 +59,10 @@ final class IntQueue private (
         }
         size
     }
+
+        def head: Int = first.value
+
+    def tail: this.type = { first = first.next; this }
 
     def foreach[U](f: Int ⇒ U): Unit = {
         var c = first
@@ -68,10 +74,10 @@ final class IntQueue private (
 
     def enqueue(value: Int): this.type = {
         if (last eq null) {
-            last = QueueNode(value, null)
+            last = new QueueNode(value, null)
             first = last
         } else {
-            last.next = QueueNode(value, null)
+            last.next = new QueueNode(value, null)
             last = last.next
         }
         this
@@ -102,7 +108,7 @@ final class IntQueue private (
     }
 }
 
-private case class QueueNode(value: Int, var next: QueueNode = null)
+private[mutable] class QueueNode(val value: Int, var next: QueueNode)
 
 /**
  * Factory to create [[IntArrayStack]]s.
