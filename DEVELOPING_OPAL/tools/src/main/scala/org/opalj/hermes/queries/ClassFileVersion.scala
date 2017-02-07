bf78d37cebada97aa6c1e3371c1954fc0c4d2132
Michael Eichberg <mail@michael-eichberg.de>
2017-02-07 16:33:48 +0100

    initial release of Hermes
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

---
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/ClassFileVersion.scala
@@ -0,0 +1,93 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2017
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.opalj
+package hermes
+package queries
+
+import org.opalj.bi.Java9MajorVersion
+import org.opalj.bi.Java1MajorVersion
+import org.opalj.bi.jdkVersion
+import org.opalj.br.analyses.Project
+import org.opalj.collection.mutable.ArrayMap
+import org.opalj.collection.immutable.Naught
+import org.opalj.collection.immutable.Chain
+import scalafx.application.Platform
+
+//import org.opalj.br.analyses.ProgressManagement
+
+/**
+ * Counts the number of class files per class file version.
+ *
+ * @author Michael Eichberg
+ */
+object ClassFileVersion extends FeatureExtractor {
+
+    def featureId(majorVersion: Int) = s"Class File\n${jdkVersion(majorVersion)}"
+
+    override def featureIDs: Seq[String] = {
+        for (majorVersion ← (Java1MajorVersion to Java9MajorVersion)) yield featureId(majorVersion)
+    }
+
+    override def apply[S](
+        projectConfiguration: ProjectConfiguration,
+        project:              Project[S],
+        rawClassFiles:        Traversable[(da.ClassFile, S)],
+        features:             Map[String, Feature[S]]
+    ): Unit = {
+
+        val data = ArrayMap[Chain[ClassFileLocation[S]]](Java9MajorVersion)
+
+        for {
+            (classFile, source) ← project.projectClassFilesWithSources
+            if !isInterrupted()
+        } {
+            val version = classFile.majorVersion
+            val locations = data(version)
+            val location = ClassFileLocation(source, classFile.thisType.fqn)
+            data(version) =
+                if (locations eq null)
+                    location :&: Naught
+                else
+                    location :&: locations
+        }
+
+        if (!isInterrupted) { // we want to avoid that we return partial results
+            for (majorVersion ← (Java1MajorVersion to Java9MajorVersion)) yield {
+                val feature = features(featureId(majorVersion))
+                val extensions = data(majorVersion)
+                if (extensions ne null) {
+                    Platform.runLater {
+                        feature.count.value = extensions.size
+                        feature.extensions ++= extensions
+                    }
+                }
+            }
+        }
+    }
+}
