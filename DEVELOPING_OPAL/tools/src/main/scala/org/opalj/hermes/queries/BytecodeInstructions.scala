f6cbc830c25ad0b2dc8f9220dfee904ecad6ac95
Michael Eichberg <mail@michael-eichberg.de>
2017-02-15 19:19:36 +0100

    added a new query which analyzes which bytecode instructions are used

---
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/BytecodeInstructions.scala
@@ -0,0 +1,79 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2017
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.opalj
+package hermes
+package queries
+
+import java.net.URL
+
+import org.opalj.bytecode.JVMInstructions
+import org.opalj.bytecode.JVMOpcodes
+import org.opalj.br.analyses.Project
+import org.opalj.br.MethodWithBody
+
+/**
+ * Counts the number of occurrences of each bytecode instruction.
+ *
+ * @author Michael Eichberg
+ */
+object BytecodeInstructions extends FeatureQuery {
+
+    val jvmInstructions = JVMInstructions
+    val jvmOpcodes = JVMOpcodes
+
+    override def htmlDescription: Either[String, URL] = {
+        Right(new URL("http://www.opal-project.de/bi/JVMInstructions.xml"))
+    }
+
+    override def featureIDs: IndexedSeq[String] = {
+        jvmInstructions.map { i ⇒
+            val (opcode, mnemonic) = i; s"$mnemonic (opcode:$opcode)"
+        }.toIndexedSeq
+    }
+
+    override def apply[S](
+        projectConfiguration: ProjectConfiguration,
+        project:              Project[S],
+        rawClassFiles:        Traversable[(da.ClassFile, S)]
+    ): TraversableOnce[Feature[S]] = {
+        val instructions = Array.fill[LocationsContainer[S]](256)(new LocationsContainer[S])
+
+        for {
+            (classFile, source) ← project.projectClassFilesWithSources
+            classFileLocation = ClassFileLocation(source, classFile)
+            method @ MethodWithBody(body) ← classFile.methods
+            methodLocation = MethodLocation(classFileLocation, method)
+            (pc, i) ← body
+        } {
+            instructions(i.opcode) += InstructionLocation(methodLocation, pc)
+        }
+
+        for { (locations, opcode) ← instructions.view.zipWithIndex } yield Feature[S](featureIDs(opcode), locations)
+    }
+}
