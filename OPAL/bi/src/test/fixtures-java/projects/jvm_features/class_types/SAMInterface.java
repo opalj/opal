33a1e60263b806a88cffc990c5b20f2f212c5d8c
Michael Eichberg <mail@michael-eichberg.de>
2017-02-19 09:18:27 +0100

    improved the examples related to functional interfaces

--- OPAL/bi/src/test/fixtures-java/projects/jvm_features/class_types/SAMInterface.java
+++ OPAL/bi/src/test/fixtures-java/projects/jvm_features/class_types/SAMInterface.java
@@ -37,6 +37,14 @@ interface GenericSAMInterface extends MarkerInterface, Cloneable {
 
 }
 
+@FunctionalInterface
+@SuppressWarnings("all")
+interface SignatureCompatibleGenericSAMInterface extends MarkerInterface, Cloneable {
+
+    boolean apply(List o);
+
+}
+
 /**
  * Defines a functional interface/a single abstract method interface as used in the combination
  * with lambda expressions. A functional interface can define arbitrary constants and static
@@ -68,8 +76,19 @@ public interface SAMInterface extends GenericSAMInterface {
 @FunctionalInterface
 interface ExtSAMInterface extends SAMInterface {
 
-        default void printExtended(){System.out.println("Extended!");}
+    default void printExtended(){System.out.println("Extended!");}
+
 }
+
+@FunctionalInterface
+interface ExtExtSAMInterface extends ExtSAMInterface, SignatureCompatibleGenericSAMInterface {
+
+    static int max(int a,int b){ return a>b ? a : b; }
+
+    boolean apply(List o);
+
+}
+
 
 // THIS IS NOT A SAMInterface anymore, though it it defines a single abstract method.
 // @FunctionalInterface
@@ -77,23 +96,23 @@ interface SomeInterface extends SAMInterface {
 
     boolean call(Object o);
 
+    static void printIt(String s){ System.out.println(s); }
 }
 
 @SuppressWarnings("all")
 class SAMInterfaceDemo {
 
- public SAMInterfaceDemo(SAMInterface i) {
-     System.out.println(i.apply(null));
- }
+    public SAMInterfaceDemo(SAMInterface i) {
+        System.out.println(i.apply(null));
+    }
 
- public static SAMInterfaceDemo factory() {
-     SAMInterface i =  (List o) -> { return o == null; };
-     return new SAMInterfaceDemo(i);
- }
+    public static SAMInterfaceDemo factory() {
+        SAMInterface i =  (List o) -> { return o == null; };
+        return new SAMInterfaceDemo(i);
+    }
 
- public static SAMInterfaceDemo factory(Object test) {
-     return new SAMInterfaceDemo((List o) -> { return o == test; });
- }
-
+    public static SAMInterfaceDemo factory(Object test) {
+        return new SAMInterfaceDemo((List o) -> { return o == test; });
+    }
 
 }
