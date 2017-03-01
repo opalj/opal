74ca3397892914e569239931a3f7b3b895a9fc31
Michael Eichberg <mail@michael-eichberg.de>
2017-03-01 08:47:17 +0100

    improved documentation
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/fpcf/analysis/PurityAnalysis.scala
+++ OPAL/br/src/main/scala/org/opalj/fpcf/analysis/PurityAnalysis.scala
@@ -100,19 +100,20 @@ import org.opalj.log.OPALLogger
  *      while program execution proceeds or between different executions of the program, nor can it
  *      depend on any external input from I/O devices.
  *
- *      '''[Hence, using true constants (e.g., Math.e) is not a problem as well as creating
- *      intermediate
- *      (mutable) data structures. Furthermore, instance method based calls can also be pure if
- *      the receiving object is (effectively final).'''
+ *      '''Hence, using true constants (e.g., Math.e) is not a problem as well as creating
+ *      intermediate (mutable) data structures. 
+ *      Furthermore, instance method based calls can also be pure if
+ *      the receiving object is (effectively final) or was created as part of the evaluation 
+ *      of the method.'''
  *
  *  -   Evaluation of the result does not cause any semantically observable side effect or output,
  *      such as mutation of mutable objects or output to I/O devices.
  *      The result value need not depend on all (or any) of the argument values. However, it must
  *      depend on nothing other than the argument values. The function may return multiple result
  *      values and these conditions must apply to all returned values for the function to be
- *      considered pure. If an argument is call by reference, any parameter mutation will alter
+ *      considered pure. If an argument is "call-by-reference", any parameter mutation will alter
  *      the value of the argument outside the function, which will render the function impure.
- *      '''However, if the referenced object is immutable it is object.'''
+ *      '''However, if the referenced object is immutable it is ok.'''
  *
  * @author Michael Eichberg
  */
@@ -279,7 +280,7 @@ class PurityAnalysis private ( final val project: SomeProject) extends FPCFAnaly
     def determinePurity(method: Method): PropertyComputationResult = {
 
         // Due to a lack of knowledge, we classify all native methods or methods that
-        // have no body because they are loaded using a library class file loader as Impure.
+        // have no body - because they are loaded using a library class file loader - as Impure.
         if (method.body.isEmpty /*HERE: method.isNative || "isLibraryMethod(method)"*/ )
             return ImmediateResult(method, Impure);
 
@@ -288,6 +289,7 @@ class PurityAnalysis private ( final val project: SomeProject) extends FPCFAnaly
         if (!referenceTypeParameters.forall { p ⇒ propertyStore.isKnown(p) })
             return ImmediateResult(method, Impure);
 
+            // TODO Currently we are not able to interleave the purity analysis and the immutability analysis... which is, however, required!
         propertyStore.allHaveProperty(
             method, Purity.key, referenceTypeParameters, ImmutableType
         ) { areImmutable ⇒
