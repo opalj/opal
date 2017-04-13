8a9b2afba5ce8b41ad66897a8053bf6d59145c7e
Michael Eichberg <mail@michael-eichberg.de>
2017-04-13 08:24:55 +0200

    applied standard Scalariform formatting
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/ThreadAPIUsage.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/ThreadAPIUsage.scala
@@ -33,67 +33,63 @@ import org.opalj.collection.immutable.Chain
 import org.opalj.hermes.queries.util._
 
 /**
-  * Captures the usage of Thread-related API usage.
-  *
-  * @author Ben Hermann
-  */
-object ThreadAPIUsage extends APIFeatureQuery{
-  override val apiFeatures: Chain[APIFeature] = {
+ * Captures the usage of Thread-related API usage.
+ *
+ * @author Ben Hermann
+ */
+object ThreadAPIUsage extends APIFeatureQuery {
+    override val apiFeatures: Chain[APIFeature] = {
 
-    val Thread = ObjectType("java/lang/Thread")
-    val ThreadGroup = ObjectType("java/lang/ThreadGroup")
-    val Object = ObjectType("java/lang/Object")
+        val Thread = ObjectType("java/lang/Thread")
+        val ThreadGroup = ObjectType("java/lang/ThreadGroup")
+        val Object = ObjectType("java/lang/Object")
 
+        val constructor = "<init>"
 
-    val constructor = "<init>"
-
-
-    Chain(
-
-      // PROCESS
-
-      APIFeatureGroup(
         Chain(
-          InstanceAPIMethod(Object, "notify"),
-          InstanceAPIMethod(Object, "notifyAll"),
-          InstanceAPIMethod(Object, "wait")
-        ),
-        "Object-based Thread Notification"
-      ),
 
-      APIFeatureGroup(
-        Chain(
-          InstanceAPIMethod(Thread, constructor),
-          InstanceAPIMethod(Thread, "interrupt"),
-          InstanceAPIMethod(Thread, "join"),
-          InstanceAPIMethod(Thread, "run"),
-          StaticAPIMethod(Thread, "sleep"),
-          InstanceAPIMethod(Thread, "start"),
-          // Deprecated members
-          InstanceAPIMethod(Thread, "destroy"),
-          InstanceAPIMethod(Thread, "resume"),
-          InstanceAPIMethod(Thread, "stop"),
-          InstanceAPIMethod(Thread, "suspend")
-        ),
-        "Usage of Thread API"
-      ),
+            // PROCESS
 
-      APIFeatureGroup(
-        Chain(
-          InstanceAPIMethod(ThreadGroup, constructor),
-          InstanceAPIMethod(ThreadGroup, "destroy"),
-          InstanceAPIMethod(ThreadGroup, "interrupt"),
-          // Deprecated members
-          InstanceAPIMethod(ThreadGroup, "resume"),
-          InstanceAPIMethod(ThreadGroup, "stop"),
-          InstanceAPIMethod(ThreadGroup, "suspend")
-        ),
-        "Usage of ThreadGroup API"
-      )
+            APIFeatureGroup(
+                Chain(
+                    InstanceAPIMethod(Object, "notify"),
+                    InstanceAPIMethod(Object, "notifyAll"),
+                    InstanceAPIMethod(Object, "wait")
+                ),
+                "Object-based Thread Notification"
+            ),
 
+            APIFeatureGroup(
+                Chain(
+                    InstanceAPIMethod(Thread, constructor),
+                    InstanceAPIMethod(Thread, "interrupt"),
+                    InstanceAPIMethod(Thread, "join"),
+                    InstanceAPIMethod(Thread, "run"),
+                    StaticAPIMethod(Thread, "sleep"),
+                    InstanceAPIMethod(Thread, "start"),
+                    // Deprecated members
+                    InstanceAPIMethod(Thread, "destroy"),
+                    InstanceAPIMethod(Thread, "resume"),
+                    InstanceAPIMethod(Thread, "stop"),
+                    InstanceAPIMethod(Thread, "suspend")
+                ),
+                "Usage of Thread API"
+            ),
 
-    )
-  }
+            APIFeatureGroup(
+                Chain(
+                    InstanceAPIMethod(ThreadGroup, constructor),
+                    InstanceAPIMethod(ThreadGroup, "destroy"),
+                    InstanceAPIMethod(ThreadGroup, "interrupt"),
+                    // Deprecated members
+                    InstanceAPIMethod(ThreadGroup, "resume"),
+                    InstanceAPIMethod(ThreadGroup, "stop"),
+                    InstanceAPIMethod(ThreadGroup, "suspend")
+                ),
+                "Usage of ThreadGroup API"
+            )
 
+        )
+    }
 
 }
