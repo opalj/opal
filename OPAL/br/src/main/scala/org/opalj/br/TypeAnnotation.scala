6497f1edf30964e404417ebd0a2b48101e81e4db
Michael Eichberg <mail@michael-eichberg.de>
2017-05-18 12:41:55 +0200

    added the typeIds/kindIds to facilitate fast switch statements
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/br/TypeAnnotation.scala
+++ OPAL/br/src/main/scala/org/opalj/br/TypeAnnotation.scala
@@ -34,7 +34,10 @@ package br
  *
  * @author Michael Eichberg
  */
-sealed abstract class TypeAnnotationTarget
+sealed abstract class TypeAnnotationTarget {
+
+    def typeId: Int
+}
 
 /**
  * The path that describes which type is actually annotated using a [[TypeAnnotation]].
@@ -49,7 +52,15 @@ sealed abstract class TypeAnnotationPath
  *
  * @author Michael Eichberg
  */
-sealed abstract class TypeAnnotationPathElement
+sealed abstract class TypeAnnotationPathElement {
+    /**
+     * A value in the range [0..3] which identifies the `path kind` as specified by the JVM
+     * specification.
+     *
+     * @note This enables efficient identificaion – e.g., in a switch – of the type path kind.
+     */
+    def kindId: Int
+}
 
 /**
  * A type annotation (*TA*).
@@ -70,83 +81,127 @@ case class TypeAnnotation(
 )
 
 case class TAOfCastExpression(
-    offset:              Int,
-    type_argument_index: Int
-) extends TypeAnnotationTarget
+        offset:              Int,
+        type_argument_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x47
+}
 
-case class TAOfCatch(exception_table_index: Int) extends TypeAnnotationTarget
+case class TAOfCatch(exception_table_index: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x42
+}
 
 case class TAOfConstructorInMethodReferenceExpression(
-    offset:              Int,
-    type_argument_index: Int
-) extends TypeAnnotationTarget
+        offset:              Int,
+        type_argument_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x4A
+}
 
 case class TAOfConstructorInvocation(
-    offset:              Int,
-    type_argument_index: Int
-) extends TypeAnnotationTarget
+        offset:              Int,
+        type_argument_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x48
+}
 
-case object TAOfFieldDeclaration extends TypeAnnotationTarget
+case object TAOfFieldDeclaration extends TypeAnnotationTarget {
+    def typeId: Int = 0x13
+}
 
-case class TAOfFormalParameter(formal_parameter_index: Int) extends TypeAnnotationTarget
+case class TAOfFormalParameter(formal_parameter_index: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x16
+}
 
-case class TAOfInstanceOf(offset: Int) extends TypeAnnotationTarget
+case class TAOfInstanceOf(offset: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x43
+}
 
 case class TAOfLocalvarDecl(
-    localVarTable: IndexedSeq[LocalvarTableEntry]
-) extends TypeAnnotationTarget
+        localVarTable: IndexedSeq[LocalvarTableEntry]
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x40
+}
 
 case class TAOfResourcevarDecl(
-    localVarTable: IndexedSeq[LocalvarTableEntry]
-) extends TypeAnnotationTarget
+        localVarTable: IndexedSeq[LocalvarTableEntry]
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x41
+}
 
 case class LocalvarTableEntry(
-    start_pc:                   Int,
-    length:                     Int,
-    local_variable_table_index: Int
+    startPC: Int,
+    length:  Int,
+    index:   Int
 )
 
 case class TAOfMethodInMethodReferenceExpression(
-    offset:              Int,
-    type_argument_index: Int
-) extends TypeAnnotationTarget
+        offset:              Int,
+        type_argument_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x4B
+}
 
 case class TAOfMethodInvocation(
-    offset:              Int,
-    type_argument_index: Int
-) extends TypeAnnotationTarget
+        offset:              Int,
+        type_argument_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x49
+}
 
-case class TAOfMethodReferenceExpressionIdentifier(offset: Int) extends TypeAnnotationTarget
+case class TAOfMethodReferenceExpressionIdentifier(offset: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x46
+}
 
-case class TAOfMethodReferenceExpressionNew(offset: Int) extends TypeAnnotationTarget
+case class TAOfMethodReferenceExpressionNew(offset: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x45
+}
 
-case class TAOfNew(offset: Int) extends TypeAnnotationTarget
+case class TAOfNew(offset: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x44
+}
 
 case class TAOfParameterDeclarationOfClassOrInterface(
-    type_parameter_index: Int
-) extends TypeAnnotationTarget
+        type_parameter_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x00
+}
 
 case class TAOfParameterDeclarationOfMethodOrConstructor(
-    type_parameter_index: Int
-) extends TypeAnnotationTarget
+        type_parameter_index: Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x01
+}
 
-case object TAOfReceiverType extends TypeAnnotationTarget
+case object TAOfReceiverType extends TypeAnnotationTarget {
+    def typeId: Int = 0x15
+}
 
-case object TAOfReturnType extends TypeAnnotationTarget
+case object TAOfReturnType extends TypeAnnotationTarget {
+    def typeId: Int = 0x14
+}
 
-case class TAOfSupertype(supertype_index: Int) extends TypeAnnotationTarget
+case class TAOfSupertype(supertype_index: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x10
+}
 
-case class TAOfThrows(throws_type_index: Int) extends TypeAnnotationTarget
+case class TAOfThrows(throws_type_index: Int) extends TypeAnnotationTarget {
+    def typeId: Int = 0x17
+}
 
 case class TAOfTypeBoundOfParameterDeclarationOfClassOrInterface(
-    type_parameter_index: Int,
-    bound_index:          Int
-) extends TypeAnnotationTarget
+        type_parameter_index: Int,
+        bound_index:          Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x11
+}
 
 case class TAOfTypeBoundOfParameterDeclarationOfMethodOrConstructor(
-    type_parameter_index: Int,
-    bound_index:          Int
-) extends TypeAnnotationTarget
+        type_parameter_index: Int,
+        bound_index:          Int
+) extends TypeAnnotationTarget {
+    def typeId: Int = 0x12
+}
 
 //
 //
@@ -154,14 +209,30 @@ case class TAOfTypeBoundOfParameterDeclarationOfMethodOrConstructor(
 //
 //
 
+// path length == 0
 case object TADirectlyOnType extends TypeAnnotationPath
 
+// path length > 0
 case class TAOnNestedType(path: IndexedSeq[TypeAnnotationPathElement]) extends TypeAnnotationPath
 
-case object TADeeperInArrayType extends TypeAnnotationPathElement
+case object TADeeperInArrayType extends TypeAnnotationPathElement {
+    final override def kindId: Int = KindId
+    final val KindId = 0
+}
 
-case object TADeeperInNestedType extends TypeAnnotationPathElement
+case object TADeeperInNestedType extends TypeAnnotationPathElement {
+    final override def kindId: Int = KindId
+    final val KindId = 1
+}
 
-case object TAOnBoundOfWildcardType extends TypeAnnotationPathElement
+case object TAOnBoundOfWildcardType extends TypeAnnotationPathElement {
+    final override def kindId: Int = KindId
+    final val KindId = 2
+}
 
-case class TAOnTypeArgument(type_argument_index: Int) extends TypeAnnotationPathElement
+case class TAOnTypeArgument(index: Int) extends TypeAnnotationPathElement {
+    final override def kindId: Int = TAOnTypeArgument.KindId
+}
+object TAOnTypeArgument {
+    final val KindId = 3
+}
