bdcb16ff363b52e260e22ca8dbd1bbafb69fbf4a
Michael Eichberg <mail@michael-eichberg.de>
2017-05-20 09:28:43 +0200

    solved formatting issues

--- OPAL/ba/src/main/scala/org/opalj/ba/AccessModifier.scala
+++ OPAL/ba/src/main/scala/org/opalj/ba/AccessModifier.scala
@@ -32,7 +32,7 @@ package ba
 /**
  * Represents the access flags of a class, method or field declaration.
  *
- * All standard access flags are predefined in this package [[org.opalj.ba]].
+ * All standard access flags are predefined.
  *
  * @author Malte Limmeroth
  * @author Michael Eichberg
@@ -40,22 +40,59 @@ package ba
 final class AccessModifier(private[ba] val accessFlags: Int) extends AnyVal {
 
     final def PUBLIC: AccessModifier = new AccessModifier(this.accessFlags | ba.PUBLIC.accessFlags)
+
     final def FINAL: AccessModifier = new AccessModifier(this.accessFlags | ba.FINAL.accessFlags)
+
     final def SUPER: AccessModifier = new AccessModifier(this.accessFlags | ba.SUPER.accessFlags)
-    final def INTERFACE: AccessModifier = new AccessModifier(this.accessFlags | ba.INTERFACE.accessFlags)
-    final def ABSTRACT: AccessModifier = new AccessModifier(this.accessFlags | ba.ABSTRACT.accessFlags)
-    final def SYNTHETIC: AccessModifier = new AccessModifier(this.accessFlags | ba.SYNTHETIC.accessFlags)
-    final def ANNOTATION: AccessModifier = new AccessModifier(this.accessFlags | ba.ANNOTATION.accessFlags)
+
+    final def INTERFACE: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.INTERFACE.accessFlags)
+    }
+
+    final def ABSTRACT: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.ABSTRACT.accessFlags)
+    }
+
+    final def SYNTHETIC: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.SYNTHETIC.accessFlags)
+    }
+
+    final def ANNOTATION: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.ANNOTATION.accessFlags)
+    }
+
     final def ENUM: AccessModifier = new AccessModifier(this.accessFlags | ba.ENUM.accessFlags)
-    final def PRIVATE: AccessModifier = new AccessModifier(this.accessFlags | ba.PRIVATE.accessFlags)
-    final def PROTECTED: AccessModifier = new AccessModifier(this.accessFlags | ba.PROTECTED.accessFlags)
+
+    final def PRIVATE: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.PRIVATE.accessFlags)
+    }
+
+    final def PROTECTED: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.PROTECTED.accessFlags)
+    }
+
     final def STATIC: AccessModifier = new AccessModifier(this.accessFlags | ba.STATIC.accessFlags)
-    final def SYNCHRONIZED: AccessModifier = new AccessModifier(this.accessFlags | ba.SYNCHRONIZED.accessFlags)
+
+    final def SYNCHRONIZED: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.SYNCHRONIZED.accessFlags)
+    }
+
     final def BRIDGE: AccessModifier = new AccessModifier(this.accessFlags | ba.BRIDGE.accessFlags)
-    final def VARARGS: AccessModifier = new AccessModifier(this.accessFlags | ba.VARARGS.accessFlags)
+
+    final def VARARGS: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.VARARGS.accessFlags)
+    }
+
     final def NATIVE: AccessModifier = new AccessModifier(this.accessFlags | ba.NATIVE.accessFlags)
+
     final def STRICT: AccessModifier = new AccessModifier(this.accessFlags | ba.STRICT.accessFlags)
-    final def VOLATILE: AccessModifier = new AccessModifier(this.accessFlags | ba.VOLATILE.accessFlags)
-    final def TRANSIENT: AccessModifier = new AccessModifier(this.accessFlags | ba.TRANSIENT.accessFlags)
+
+    final def VOLATILE: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.VOLATILE.accessFlags)
+    }
+
+    final def TRANSIENT: AccessModifier = {
+        new AccessModifier(this.accessFlags | ba.TRANSIENT.accessFlags)
+    }
 
 }
