12c65e54761772ce45e16302565e25214d08c3a3
Michael Eichberg <mail@michael-eichberg.de>
2017-05-20 09:28:36 +0200

    added a very first docu

--- OPAL/ba/src/main/scala/org/opalj/ba/EXCEPTIONS.scala
+++ OPAL/ba/src/main/scala/org/opalj/ba/EXCEPTIONS.scala
@@ -29,6 +29,11 @@
 package org.opalj
 package ba
 
+/**
+ * Builder for the [[org.opalj.br.ExceptionTable]] attribute.
+ *
+ * @author Michael Eichberg
+ */
 case class EXCEPTIONS(exceptionTypes: String*) extends br.MethodAttributeBuilder {
 
     def apply(
