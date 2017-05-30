48ad96eb85bd7edb159335360743225b61d079ee
Michael Eichberg <mail@michael-eichberg.de>
2017-05-30 12:30:28 +0200

    fixed visibility issues
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/ba/src/main/scala/org/opalj/ba/CodeElement.scala
+++ OPAL/ba/src/main/scala/org/opalj/ba/CodeElement.scala
@@ -52,7 +52,8 @@ trait CodeElement[+T]
 object CodeElement {
 
     /**
-     * Converts [[org.opalj.br.instructions.InstructionLike]]s to [[InstructionElement]].
+     * Converts [[org.opalj.br.instructions.InstructionLike]]s to
+     * [org.opalj.ba.[InstructionElement]].
      */
     implicit def instructionToInstructionElement(
         instruction: InstructionLike
@@ -62,7 +63,7 @@ object CodeElement {
 
     /**
      * Converts a tuple of [[org.opalj.br.instructions.InstructionLike]] and `scala.AnyRef`
-     * (an annotated instruction) to [[AnnotatedInstructionElement]].
+     * (an annotated instruction) to [[org.opalj.ba.AnnotatedInstructionElement]].
      */
     implicit def annotatedInstructionToAnnotatedInstructionElement[T](
         ai: (InstructionLike, T)
@@ -71,7 +72,7 @@ object CodeElement {
     }
 
     /**
-     * Converts a `Symbol` (label) to [[LabelElement]].
+     * Converts a `Symbol` (label) to [[org.opalj.ba.LabelElement]].
      */
     implicit def symbolToLabelElement(label: Symbol): LabelElement = new LabelElement(label)
 }
