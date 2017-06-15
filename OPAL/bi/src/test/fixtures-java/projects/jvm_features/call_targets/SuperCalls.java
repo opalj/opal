4ae843ef10cd729c4d40f8297fe684df5cf58b47
Michael Eichberg <mail@michael-eichberg.de>
2017-06-15 11:47:03 +0200

    added new test case related to indirect super calls

---
+++ OPAL/bi/src/test/fixtures-java/projects/jvm_features/call_targets/SuperCalls.java
@@ -0,0 +1,61 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2017
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package call_targets;
+
+/**
+ * Defines an annotation which basically covers all features an annotation can have.
+ *
+ * NOTE<br />
+ * This class is only meant to be (automatically) compiled by OPAL's build script.
+ *
+ * @author Michael Eichberg
+ */
+public class SuperCalls {
+    protected String[] stringValues() { return new String[]{ "String 1", "String2" }; }
+}
+
+class SubSuperCalls extends SuperCalls {
+
+    @Override public String toString() {
+        return "SubSuperClass";
+    }
+}
+
+class SubSubSuperCalls extends SubSuperCalls {
+
+    @Override protected String[] stringValues() {
+        String[] sv = super.stringValues();
+        sv[0] = "<EMPTY>";
+        return sv;
+    }
+
+    @Override public String toString() {
+        return "SubSuperClass";
+    }
+}
