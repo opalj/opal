9783fd59b15c776c5be994401a009d71787dfca2
Florian Kübler <florian_vincent.kuebler@stud.tu-darmstadt.de>
2017-06-27 12:44:07 +0200

    Changed test to use type parameter annotations

--- OPAL/ai/src/test/scala/org/opalj/fpcf/analysis/SimpleEscapeAnalysisTest.scala
+++ OPAL/ai/src/test/scala/org/opalj/fpcf/analysis/SimpleEscapeAnalysisTest.scala
@@ -28,12 +28,15 @@
  */
 package org.opalj.fpcf.analysis
 
-
-import org.opalj.br.ObjectType
+import org.opalj.br._
+import org.opalj.br.analyses.{PropertyStoreKey, SomeProject}
+import org.opalj.br.instructions.NEW
+import org.opalj.collection.immutable.Chain
 import org.opalj.fpcf.PropertyKey
 import org.opalj.fpcf.properties.{EscapeProperty, NoEscape}
 
 class SimpleEscapeAnalysisTest extends AbstractFixpointAnalysisTest {
+
     def analysisName = "SimpleEscapeAnalysis"
 
     override def testFileName = "escape-1.8-g-parameters-genericsignature.jar"
@@ -48,5 +51,103 @@ class SimpleEscapeAnalysisTest extends AbstractFixpointAnalysisTest {
         ObjectType("annotations/target/EscapeProperty")
     }
 
+    /**
+      * Add all AllocationsSites found in the project to the entities in the property
+      * stores created with the PropertyStoreKey.
+      */
+    override def init(): Unit = {
+        PropertyStoreKey.addEntityDerivationFunction[Map[Method, Map[PC, AllocationSite]]](
+            (p: SomeProject) ⇒ {
+                var allAs: List[Chain[AllocationSite]] = Nil
+
+                // Associate every new instruction in a method with an allocation site object
+                val as = p.allMethods.flatMap { m ⇒
+                    m.body match {
+                        case None ⇒ Nil
+                        case Some(code) ⇒
+                            val as = code.collectWithIndex {
+                                case (pc, instructions.NEW(_)) ⇒ new AllocationSite(m, pc)
+                            }
+                            if (as.nonEmpty) allAs ::= as
+                            as
+                    }
+                }
+
+                // In the context we store a map which makes the set of allocation sites
+                // easily accessible
+                val mToPCToAs = allAs.map { asPerMethod ⇒
+                    val pcToAs = asPerMethod.map(as ⇒ as.pc → as).toMap
+                    val m = pcToAs.head._2.method
+                    m → pcToAs
+                }.toMap
+
+                (as, mToPCToAs)
+            }
+        )
+    }
+
     def defaultValue = NoEscape.toString
+
+    def propertyExtraction(annotation: TypeAnnotation): Option[String] = {
+        annotation.elementValuePairs collectFirst { case ElementValuePair("value", EnumValue(_, property)) ⇒ property }
+    }
+
+    def validateProperyByTypeAnnotation(method: Method, annotation: TypeAnnotation): Unit = {
+        val instructions = method.body.get.instructions
+        val annotatedOProperty = propertyExtraction(annotation)
+        val annotatedProperty = annotatedOProperty getOrElse defaultValue
+
+        assert(method ne null, "method is empty")
+
+        val expr = annotation.target match {
+            case TAOfNew(pc) ⇒ {
+                instructions(pc) match {
+                    case NEW(_) ⇒ Some(AllocationSite(method, pc))
+                }
+            }
+            case TAOfFormalParameter(index) ⇒
+                throw new RuntimeException("Not yet implemented")
+            case _ ⇒ None
+        }
+
+        expr.foreach(entity => {
+            val computedOProperty = propertyStore(entity, propertyKey)
+
+            if (computedOProperty.hasNoProperty) {
+                val className = project.classFile(method).fqn
+                val message =
+                    "Entity has no property: " + s"$className $method $entity  for: $propertyKey;" +
+                        s"\nexpected property: $annotatedProperty"
+                fail(message)
+            }
+
+            val computedProperty = computedOProperty.p.toString
+
+            if (computedProperty != annotatedProperty) {
+                val className = project.classFile(method).fqn
+                val message =
+                    "Wrong property computed: " +
+                        s"$className $method $entity" +
+                        s"has the property $computedProperty for $propertyKey;" +
+                        s"\n\tactual property:   $computedProperty" +
+                        s"\n\texpected property: $annotatedProperty"
+                fail(message)
+            }
+
+        })
+    }
+
+    // TEST
+
+    for {
+        classFile ← project.allClassFiles
+        method@MethodWithBody(code) ← classFile.methods
+        annotation ← code.runtimeVisibleTypeAnnotations
+        if annotation.annotationType == propertyAnnotation
+    } {
+        analysisName should ("correctly calculate the property of the expression " + annotation.target + "in method " + method + " in class " + classFile.fqn) in {
+            validateProperyByTypeAnnotation(method, annotation)
+        }
+    }
+
 }
