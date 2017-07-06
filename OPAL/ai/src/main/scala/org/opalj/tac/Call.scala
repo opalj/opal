121c74c00a2bc0ad10cbf0d9364dfafb8ceec2b2
Michael Eichberg <mail@michael-eichberg.de>
2017-07-06 23:39:28 +0200

    Added a convenience extractor to extract method call parameters

--- OPAL/ai/src/main/scala/org/opalj/tac/Call.scala
+++ OPAL/ai/src/main/scala/org/opalj/tac/Call.scala
@@ -51,3 +51,15 @@ object Call {
         Some((call.declaringClass, call.isInterface, call.name, call.descriptor))
     }
 }
+
+object MethodCallParameters {
+
+    def unapply[V <: Var[V]](astNode: ASTNode[V]): Option[Seq[Expr[V]]] = {
+        astNode match {
+            case c: Call[V @unchecked]                   ⇒ Some(c.params)
+            case Assignment(_, _, c: Call[V @unchecked]) ⇒ Some(c.params)
+            case _                                       ⇒ None
+        }
+    }
+
+}
