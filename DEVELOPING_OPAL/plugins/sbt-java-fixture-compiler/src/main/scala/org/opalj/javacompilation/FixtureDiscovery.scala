498e468cac3e02e6b0a7ef23d11ed255fb129bd4
Michael Eichberg <mail@michael-eichberg.de>
2017-07-20 21:03:59 +0200

    applied OPAL’s standard Scala formatting

--- DEVELOPING_OPAL/plugins/sbt-java-fixture-compiler/src/main/scala/org/opalj/javacompilation/FixtureDiscovery.scala
+++ DEVELOPING_OPAL/plugins/sbt-java-fixture-compiler/src/main/scala/org/opalj/javacompilation/FixtureDiscovery.scala
@@ -26,122 +26,118 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+package org.opalj.javacompilation
 
-  package org.opalj.javacompilation
+import sbt._
+import sbt.Keys.TaskStreams
+import java.io.File
+import java.io.Writer
+import java.io.PrintWriter
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.Path
+import java.nio.file.Files
+import java.nio.file.FileVisitResult
+import java.nio.file.attribute.FileTime
+import java.nio.file.attribute.BasicFileAttributes
+import scala.io.Source.fromFile
 
-  import sbt._
-  import sbt.Keys.TaskStreams
-  import java.io.File
-  import java.io.Writer
-  import java.io.PrintWriter
-  import java.nio.file.SimpleFileVisitor
-  import java.nio.file.Path
-  import java.nio.file.Files
-  import java.nio.file.FileVisitResult
-  import java.nio.file.attribute.FileTime
-  import java.nio.file.attribute.BasicFileAttributes
-  import scala.io.Source.fromFile
+import org.opalj.javacompilation.FixtureCompileSpec._
 
-  import org.opalj.javacompilation.FixtureCompileSpec._
+/**
+ * Defines methods to find and compile test fixtures.
+ *
+ * E.g., some test fixtures are found in the OPAL/bi project (OPAL/bi/src/test/fixtures-java/Readme.md)
+ *
+ * @author Simon Leischnig
+ */
+object FixtureDiscovery {
 
-
- /**
-  * This class discovers and compiles test fixtures in the OPAL project.
-  * See: Test fixtures as seen in the OPAL/bi project (OPAL/bi/src/test/fixtures-java/Readme.md)
-  *
-  * @author Simon Leischnig
-  */
- object FixtureDiscovery {
-
-   /** Returns compilation tasks for test fixtures that were discovered in the
-     * OPAL project.
+    /**
+     * Returns compilation tasks for test fixtures that were found in the configured folders.
      */
-   class OPALTestFixtureDiscovery (
-     projectsDir: File,
-     supportDir: File,
-     targetFolder: File, // where the subfolders for the discovered projects will be created
-     compiler: TestFixtureCompiler
-   ) {
+    class OPALTestFixtureDiscovery(
+            projectsDir:  File,
+            supportDir:   File,
+            targetFolder: File, // where the subfolders for the discovered projects will be created
+            compiler:     TestFixtureCompiler
+    ) {
 
-     /** Finds and returns all test fixtures in the project. */
-     def discoverFixtureTasks(): Seq[JavaFixtureCompilationTask] = {
-       for {
-           sourceFolder ← projectsDir.listFiles
-           if sourceFolder.isDirectory
-           configFile = sourceFolder.getAbsoluteFile / "compiler.config"
-           (supportLibraries, defaultConfigurationOptions) = parseConfigFile(configFile)
-           configurationOptions ← defaultConfigurationOptions
-       } yield {
+        /** Finds and returns all test fixtures in the project. */
+        def discoverFixtureTasks(): Seq[JavaFixtureCompilationTask] = {
+            for {
+                sourceFolder ← projectsDir.listFiles
+                if sourceFolder.isDirectory
+                configFile = sourceFolder.getAbsoluteFile / "compiler.config"
+                (supportLibraries, defaultConfigurationOptions) = parseConfigFile(configFile)
+                configurationOptions ← defaultConfigurationOptions
+            } yield {
 
-           val fixture = TestFixture(sourceFolder)
-           val outputFolder = obtainTargetFolder(configFile, sourceFolder, configurationOptions)
+                val fixture = TestFixture(sourceFolder)
+                val outputFolder = obtainTargetFolder(configFile, sourceFolder, configurationOptions)
 
-           JavaFixtureCompilationTask(
-             fixture,
-             outputFolder,
-             configurationOptions,
-             supportLibraries,
-             compiler
-           )
-       }
-     }
+                JavaFixtureCompilationTask(
+                    fixture,
+                    outputFolder,
+                    configurationOptions,
+                    supportLibraries,
+                    compiler
+                )
+            }
+        }
 
-   /** Returns 'require' specifications, and configuration options for a test
-     * fixture as can be (optionally) specified in a "compiler.config" file in the fixture.
-     *
-     * This involves checking if a config file exists (default values if not),
-     * filtering comments out, and partitioning by the 'requires' keyword.
-     *
-     * @param configFile configuration  file of the test fixture (may or
-     * may not exist)
-     * @return: Returns a pair of 'requires' specs and config options for the compiler
-     */
-     def parseConfigFile(configFile: File) = {
-       if (configFile.exists) {
-           val (requires, configurationOptions) = fromFile(configFile).getLines.
-               map(_.trim).
-               filter(l ⇒ l.nonEmpty && !l.startsWith("#")).toList.
-               partition(_.startsWith("requires"))
+        /**
+         * Returns ''require'' specifications, and configuration options for a test
+         * fixture that are (optionally) specified in a "compiler.config" file in the fixture.
+         *
+         * This involves checking if a config file exists (default values if not),
+         * filtering comments out, and partitioning by the 'requires' keyword.
+         *
+         * @param configFile configuration file of the test fixture (may or may not exist)
+         * @return Returns a pair of 'requires' specs and config options for the compiler
+         */
+        def parseConfigFile(configFile: File) = {
+            if (configFile.exists) {
+                val (requires, configurationOptions) = fromFile(configFile).getLines.
+                    map(_.trim).
+                    filter(l ⇒ l.nonEmpty && !l.startsWith("#")).toList.
+                    partition(_.startsWith("requires"))
 
-           ( // return value: pair of mapped requires specs and config options (rest)
-               requires.
-               map(librarySpec ⇒ librarySpec.substring(librarySpec.indexOf('=') + 1)). /* support library name */
-               map(libraryName ⇒ supportDir / libraryName). /* support library folder */
-               mkString(" "),
+                ( // return value: pair of mapped requires specs and config options (rest)
+                    requires.
+                    map(librarySpec ⇒ librarySpec.substring(librarySpec.indexOf('=') + 1)). /* support library name */
+                    map(libraryName ⇒ supportDir / libraryName). /* support library folder */
+                    mkString(" "),
+                    configurationOptions
+                )
+            } else {
+                ("", Seq("-g -8 -parameters -genericsignature"))
+            }
+        }
 
-               configurationOptions
-           )
-       } else {
-           ("", Seq("-g -8 -parameters -genericsignature"))
-       }
-     }
+        /** Returns the name for the target folder from the configuration options. */
+        def obtainTargetFolder(
+            configFile:           File, // compiler.config file object
+            sourceFolder:         File, // source folder object
+            configurationOptions: String // configuration options
+        ) = {
 
-    /** Returns the name for the target folder from the configuration options. */
-     def obtainTargetFolder(
-           configFile: File, // compiler.config file object
-           sourceFolder: File, // source folder object
-           configurationOptions: String // configuration options
-         ) = {
+            val selectedOptionsIdentification =
+                if (configFile.exists)
+                    configurationOptions.replace(" ", "").replace(':', '=')
+                else
+                    ""
 
-       val selectedOptionsIdentification =
-           if (configFile.exists)
-               configurationOptions.replace(" ", "").replace(':', '=')
-           else
-               ""
+            targetFolder.getAbsoluteFile / (sourceFolder.getName + selectedOptionsIdentification)
+        }
 
-           targetFolder.getAbsoluteFile / (
-               sourceFolder.getName + selectedOptionsIdentification
-           )
-     }
+    }
 
-   }
+    class LogWriter(println: String ⇒ Unit) extends Writer {
+        override def flush(): Unit = {}
+        override def close(): Unit = ??? // not expected to be called
+        override def write(chars: Array[Char], offset: Int, length: Int): Unit = {
+            println(new String(chars, offset, length))
+        }
+    }
 
-   class LogWriter(println: String ⇒ Unit) extends Writer {
-       override def flush(): Unit = {}
-       override def close(): Unit = ??? // not expected to be called
-       override def write(chars: Array[Char], offset: Int, length: Int): Unit = {
-           println(new String(chars, offset, length))
-       }
-   }
-
- }
+}
