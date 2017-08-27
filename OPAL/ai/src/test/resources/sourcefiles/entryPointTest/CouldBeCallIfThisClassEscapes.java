3f3af1409bf660a0fb6aa1565b83bec9cef84304
Michael Eichberg <mail@michael-eichberg.de>
2017-08-27 16:57:22 +0200

    fixed copyrights

--- OPAL/ai/src/test/resources/sourcefiles/entryPointTest/CouldBeCallIfThisClassEscapes.java
+++ OPAL/ai/src/test/resources/sourcefiles/entryPointTest/CouldBeCallIfThisClassEscapes.java
@@ -1,6 +1,6 @@
 
 /* BSD 2-Clause License:
- * Copyright (c) 2009 - 2015
+ * Copyright (c) 2009 - 2017
  * Software Technology Group
  * Department of Computer Science
  * Technische Universität Darmstadt
