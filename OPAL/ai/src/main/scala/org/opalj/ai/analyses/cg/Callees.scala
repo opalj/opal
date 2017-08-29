6d624fe5ce15cd892c5329fc7a69f0fc12a484db
Michael Eichberg <mail@michael-eichberg.de>
2017-08-29 09:48:26 +0200

    removed legacy call graph code
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/Callees.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/analyses/cg/Callees.scala
@@ -51,35 +51,58 @@ trait Callees {
     def cache: CallGraphCache[MethodSignature, Set[Method]]
 
     @inline def callees(
+        caller:             Method,
         declaringClassType: ObjectType,
+        isInterface:        Boolean,
         name:               String,
         descriptor:         MethodDescriptor
     ): Set[Method] = {
 
-        // FIXME Only correct if the code that we are analyzing is an app
-        val classesFilter = (t: ObjectType) ⇒ !instantiableClasses.isNotInstantiable(t)
+        assert(
+            classHierarchy.isInterface(declaringClassType) == Answer(isInterface),
+            s"callees - inconsistent isInterface information for $declaringClassType"
+        )
+
+        def classesFilter(m: Method) = !instantiableClasses.isNotInstantiable(m.classFile.thisType)
+
+        def callTargets() = {
+            if (isInterface)
+                project.
+                    interfaceCall(declaringClassType, name, descriptor).
+                    filter(classesFilter)
+            else {
+                val callerPackage = caller.classFile.thisType.packageName
+                project.
+                    virtualCall(callerPackage, declaringClassType, name, descriptor).
+                    filter(classesFilter)
+            }
+        }
+
+        if (!isInterface) {
+            // Only if the method is public, we can cache the result to avoid that we cache
+            // wrong results...
+            project.resolveClassMethodReference(declaringClassType, name, descriptor) match {
+                case Success(resolvedMethod) ⇒
+                    if (!resolvedMethod.isPublic)
+                        return callTargets();
+                // else ... go on with the analysis and try to cache the results.
+                case _ ⇒
+                    // no caching...
+                    callTargets()
+            }
+        }
 
         classHierarchy.hasSubtypes(declaringClassType) match {
-
             case Yes ⇒
                 val methodSignature = new MethodSignature(name, descriptor)
                 cache.getOrElseUpdate(declaringClassType, methodSignature)(
-                    {
-                        project.lookupImplementingMethods(
-                            declaringClassType, name, descriptor, classesFilter
-                        )
-                    },
+                    callTargets(),
                     syncOnEvaluation = true //false
                 )
 
-            case No ⇒
-                val result = project.lookupImplementingMethods(
-                    declaringClassType, name, descriptor, classesFilter
-                )
-                result
+            case /* no caching; not worth the effort... */ No ⇒ callTargets()
 
-            case /*Unknown <=> the type is unknown */ _ ⇒
-                Set.empty
+            case /*Unknown <=> the type is unknown */ _       ⇒ Set.empty
         }
     }
 
