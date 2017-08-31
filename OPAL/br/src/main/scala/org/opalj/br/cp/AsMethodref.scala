00be8161c5e0bc7d4341475031c42991145cb960
Michael Eichberg <mail@michael-eichberg.de>
2017-08-31 22:48:43 +0200

    added volatile annotation

--- OPAL/br/src/main/scala/org/opalj/br/cp/AsMethodref.scala
+++ OPAL/br/src/main/scala/org/opalj/br/cp/AsMethodref.scala
@@ -30,6 +30,8 @@ package org.opalj
 package br
 package cp
 
+import scala.annotation.volatile
+
 /**
  * Constant pool entry that represents method refs.
  *
@@ -47,7 +49,7 @@ trait AsMethodref extends Constant_Pool_Entry {
     def isInterfaceMethodRef: Boolean
 
     // to cache the result
-    private[this] var methodref: (ReferenceType, Boolean /*InterfaceMethodRef*/ , String, MethodDescriptor) = null
+    private[this] @volatile var methodref: (ReferenceType, Boolean /*InterfaceMethodRef*/ , String, MethodDescriptor) = null
     override def asMethodref(cp: Constant_Pool): (ReferenceType, Boolean, String, MethodDescriptor) = {
         // The following solution is sufficiently thread safe; i.e.,
         // it may happen that two or more methodref instances
