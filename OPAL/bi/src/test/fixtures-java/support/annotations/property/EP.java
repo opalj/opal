aa2bceabf4b97d0b00b64270b7fd208ddd44a8c2
Michael Eichberg <mail@michael-eichberg.de>
2017-09-01 12:58:28 +0200

    improved documentation

--- OPAL/bi/src/test/fixtures-java/support/annotations/property/EP.java
+++ OPAL/bi/src/test/fixtures-java/support/annotations/property/EP.java
@@ -43,25 +43,32 @@ public @interface EP {
     /**
      * The entity that should have a specific property value.
      *
-     * Entities accepted currently are classes, fields and methods.
-     * Classes are specified by fully qualified name, e.g. java/lang/Math
-     * Fields are specified by className . fieldName, e.g. java/lang/Long.value
-     * Methods are specified by className . methodName methodDescriptor, e.g. java/lang/Math.abs(I)I
+     * Supported entities are:
+     * <ul>
+     *  <li>Classes are specified using the fully qualified name, e.g.
+     *      <ode>java/lang/Math</code></li>
+     *  <li>Fields are specified by &lt;className&gt;.&lt;fieldName&gt;, e.g.
+     *      <code>java/lang/Long.value</code></li>
+     *  <li>Methods are specified by &lt;className&gt;.&lt;methodName&gt;&lt;methodDescriptor&gt;,
+     *      e.g. <code>java/lang/Math.abs(I)I</code>
+     * </ul>
      */
     String e();
 
     /**
-     * The property key for the property required, empty to use the property key of the test.
+     * The name of the property key of the required property.
      *
-     * By convention, use the simple name of the property class for this, e.g. pk="Purity".
+     * By convention, it is the simple name of the property class, e.g. <code>pk="Purity"</code>.
      * The actual mapping to the property key is up to the test, though.
      */
     String pk() default "";
 
     /**
-     * The property value that the entity should have, empty if the property should not exist.
+     * The expected entity's property value; should be left empty if the entity should not have
+     * the respective property.
      *
-     * @note This can't be a specific enum as it must be able to hold any property.
+     * <i>Implementation note: This can't be a specific enum as it must be able to hold any
+     * property.</i>
      */
     String p() default "";
 }
