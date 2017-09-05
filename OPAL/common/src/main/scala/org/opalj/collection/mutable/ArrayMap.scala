8ddf9e647a836eb15e4576506978e7e61ad7eada
Michael Eichberg <mail@michael-eichberg.de>
2017-09-05 11:19:02 +0200

    refined the documentation
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/common/src/main/scala/org/opalj/collection/mutable/ArrayMap.scala
+++ OPAL/common/src/main/scala/org/opalj/collection/mutable/ArrayMap.scala
@@ -35,7 +35,7 @@ import java.util.Arrays
 
 /**
  * Conceptually, a map where the keys are positive `Int` values and the values are
- * non-`null`. (`null` values are not permitted!)
+ * non-`null`; `null` values are not permitted!
  * The key values always have to be larger than or equal to 0 and are ideally continues
  * (0,1,2,3,...). The values are stored in a plain array to enable true O(1) retrieval.
  * Furthermore, the array is only as large as it has to be to keep the value associated
