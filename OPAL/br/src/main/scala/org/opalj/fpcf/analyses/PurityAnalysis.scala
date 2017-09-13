40846ca65d66d219993111935e02aa12526d257b
Michael Eichberg <mail@michael-eichberg.de>
2017-09-13 15:20:49 +0200

    synchronized methods are impure
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/br/src/main/scala/org/opalj/fpcf/analyses/PurityAnalysis.scala
+++ OPAL/br/src/main/scala/org/opalj/fpcf/analyses/PurityAnalysis.scala
@@ -243,6 +243,8 @@ class PurityAnalysis private ( final val project: SomeProject) extends FPCFAnaly
      * Determines the purity of the given method.
      */
     def determinePurity(method: Method): PropertyComputationResult = {
+        if (method.isSynchronized)
+            return ImmediateResult(method, Impure);
 
         // Due to a lack of knowledge, we classify all native methods or methods that
         // have no body - because they are loaded using a library class file loader - as Impure.
