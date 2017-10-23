f7538b71e1ab235c6e1d7fad9da12d6c44932176
Florian Kübler <florian_vincent.kuebler@stud.tu-darmstadt.de>
2017-10-23 13:01:40 +0200

    more tests

---
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/escape/Circle.java
@@ -0,0 +1,21 @@
+package org.opalj.fpcf.fixtures.escape;
+
+public class Circle {
+    public static int pi = 3;
+    public int radius;
+    public int area;
+
+    public Circle(int radius) {
+        this.radius = radius;
+        this.area = pi * radius * radius;
+    }
+
+    public Circle() {
+        this(1);
+    }
+
+    @Override
+    public String toString() {
+        return "r: "+radius;
+    }
+}
