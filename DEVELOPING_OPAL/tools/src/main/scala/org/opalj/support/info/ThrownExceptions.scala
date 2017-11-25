7d7e7200b366e98fca3979670d8761c4f472be97
Michael Eichberg <mail@michael-eichberg.de>
2017-11-25 15:41:30 +0100

    reactivated “aggregator” analysis
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/ThrownExceptions.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/ThrownExceptions.scala
@@ -72,7 +72,7 @@ object ThrownExceptions extends DefaultOneStepAnalysis {
         if (parameters.contains(L1TEParameter)) {
             L1ThrownExceptionsAnalysis.start(project, ps)
             // IMPROVE ThrownExceptionsByOverridingMethods.startLazily(project, ps)
-            //ThrownExceptionsByOverridingMethods.start(project, ps)
+            ThrownExceptionsByOverridingMethods.start(project, ps)
         } else {
             val fallbackAnalysis = new ThrownExceptionsFallbackAnalysis(ps)
             ps.scheduleForEntities(project.allMethods)(fallbackAnalysis)
