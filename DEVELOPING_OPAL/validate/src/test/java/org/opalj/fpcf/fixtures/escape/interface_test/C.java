7fa73a75f6422a216b2c75c81c0c2449b702c231
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-03-02 09:49:25 +0100

    added tests for interface handling

---
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/escape/interface_test/C.java
@@ -0,0 +1,14 @@
+package org.opalj.fpcf.fixtures.escape.interface_test;
+
+public final class C extends B {
+
+    @Override
+    public void foo(Object param) {
+
+    }
+
+    @Override
+    public void bar(Object param) {
+
+    }
+}
