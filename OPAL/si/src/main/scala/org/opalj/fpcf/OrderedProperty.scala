a7f1991241e2a7bd2aa94eaa1b11ca5e92ba903d
Michael Eichberg <mail@michael-eichberg.de>
2018-04-18 12:51:43 +0200

    improved documentation of OrderedProperties
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/si/src/main/scala/org/opalj/fpcf/OrderedProperty.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/OrderedProperty.scala
@@ -32,19 +32,20 @@ package org.opalj.fpcf
  * Ordered properties makes the order between all properties regarding a respective kind explicit;
  * all properties that are of the same kind have to inherit from ordered property or none.
  *
- * This information is used by the property store when debugging is turned on to test if an
- * analysis, which derives a new property, always derives a more precise property. These tests
- * are only executed in-phase!
+ * This information is used by the property store when assertions/debugging is turned on to test
+ * if an analysis, which derives a new property, always derives a more precise property.
  *
  * @author Michael Eichberg
  */
 trait OrderedProperty extends Property {
 
+    override type Self <: OrderedProperty
+
     /**
-     * Tests if this property is more precise than the given one (more precise means, a value
-     * more to the top in the underlying lattice.)
+     * Tests if this property is equal or better than the given one (better means that the
+     * value is above the given value in the underlying lattice.)
      */
     @throws[IllegalArgumentException]("if this property is not more precise than the given one")
-    def checkIsMoreOrEquallyPreciseThan(other: Property): Unit
+    def checkIsEqualOrBetterThan(other: Self): Unit
 
 }
