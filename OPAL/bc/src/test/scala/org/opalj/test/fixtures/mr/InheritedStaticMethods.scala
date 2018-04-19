9547006e4ae787b5f833b0eaa92b645b8f7c0f51
Fabian Kaiser <fabian.kaiser@stud.tu-darmstadt.de>
2018-04-19 03:43:08 +0000

    fixed

--- OPAL/bc/src/test/scala/org/opalj/test/fixtures/mr/InheritedStaticMethods.scala
+++ OPAL/bc/src/test/scala/org/opalj/test/fixtures/mr/InheritedStaticMethods.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2009 - 2017
  * Software Technology Group
  * Department of Computer Science
- * Technische Universit‰t Darmstadt
+ * Technische Universit√§t Darmstadt
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
