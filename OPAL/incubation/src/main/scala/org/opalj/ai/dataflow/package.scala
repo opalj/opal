65e626e098ddde874367fb3c031e9f94f15e916d
Fabian Kaiser <fabian.kaiser@stud.tu-darmstadt.de>
2018-05-03 05:55:11 +0000

    restored numbering

--- OPAL/incubation/src/main/scala/org/opalj/ai/dataflow/package.scala
+++ OPAL/incubation/src/main/scala/org/opalj/ai/dataflow/package.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2009 - 2017
  * Software Technology Group
  * Department of Computer Science
- * Technische UniversitÃ¤t Darmstadt
+ * Technische Universität Darmstadt
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -53,22 +53,22 @@ import br.Method
  *  1. Select sources
  *      1. Sources are parameters passed to methods (e.g., doPost(session : Session)
  *      (This covers the main method as well as typical callback methods.)
- *      2. Values returned by methods (e.g., System.in.read) (here, we identify the call site)
- *  2. Select sinks
+ *      1. Values returned by methods (e.g., System.in.read) (here, we identify the call site)
+ *  1. Select sinks
  *      1. A sink is either a field (in which the value is stored)
- *      2. a method (parameter) which is passed the value
- *  3. Filtering (Terminating) data-flows
+ *      1. a method (parameter) which is passed the value
+ *  1. Filtering (Terminating) data-flows
  *      1. If a specific operation was performed, e.g.,
- *      2. If a comparison (e.g., against null, > 0 , ...)
- *      3. An instanceOf/a checkcast
- *      4. A mathematical operation (e.g. +.-,...)
- *      5. [OPTIMIZATION] If the value was passed to a specific method (e.g., check(x : X) - throws Exception if the check fails)
- *      6. [OPTIMIZATION] If the value was returned by a well-identified method (e.g., String sanitized = s.replace(...,...))
+ *      1. If a comparison (e.g., against null, > 0 , ...)
+ *      1. An instanceOf/a checkcast
+ *      1. A mathematical operation (e.g. +.-,...)
+ *      1. [OPTIMIZATION] If the value was passed to a specific method (e.g., check(x : X) - throws Exception if the check fails)
+ *      1. [OPTIMIZATION] If the value was returned by a well-identified method (e.g., String sanitized = s.replace(...,...))
  *
  *  4. Extending data-flows (Side Channels)
  *      - OPEN:   What would be the general strategy if a value influences another value?
  *      - [SIDE CHANNELS?] What happens if the value is stored in a field of an object and that object is used?
- *      - [SIDE CHANNELS?] What happens if the value is used during the computation, but does not (directly) influence the output?
+ *      - [SIDE CHANNELS?] What happens if the value is used during the computation, but does not (directly) influence the output.
  *          (e.g., if(x == 0) 1; else 2;
  *
  * Furthermore, the framework will automatically handle taint propagation and aliasing.
