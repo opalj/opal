e1a8966b62e9975e9833631437e007dd67408c9b
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-05-14 13:45:02 +0200

    some documentation

--- OPAL/ai/src/main/scala/org/opalj/ai/DefinitionSiteLike.scala
+++ OPAL/ai/src/main/scala/org/opalj/ai/DefinitionSiteLike.scala
@@ -32,6 +32,19 @@ package ai
 import org.opalj.br.Method
 import org.opalj.collection.immutable.IntTrieSet
 
+/**
+ * Identifies a definition site object in a method in the bytecode using its program counter and
+ * the corresponding use-sites.
+ * It corresponds to the [[org.opalj.fpcf.properties.EscapeProperty]] and the computing
+ * analyses.
+ * Concrete definition sites are usually associated with [[DefinitionSite]].
+ * However, to compute the [[org.opalj.fpcf.properties.FieldLocality]] it is necessary to represent
+ * definition site objects that have a reduced usedBy set (PutField's are removed here).
+ * Therefore, this trait acts as a common interface for such objects.
+ *
+ * @author Dominik Helm
+ * @author Florian Kuebler
+ */
 trait DefinitionSiteLike {
 
     val pc: Int
