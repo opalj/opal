5405fd12c47f30c0fe63e3d2500026a78e2381d5
Michael Eichberg <mail@michael-eichberg.de>
2018-05-22 08:25:42 +0200

    updated the documentation
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/package.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/package.scala
@@ -29,12 +29,12 @@
 package org.opalj.support
 
 /**
- * Contains standalone tools that provide general, coarse-grained information about
+ * Contains standalone tools/analyses that provide general, coarse-grained information about
  * Java projects and the current runtime environment.
- * The tools range from very trivial and fast to complex analyses.
+ * The tools range from very trivial and fast to (more) complex analyses.
  *
  * The tools in this package primarily serve the comprehension of a given code base;
- * tools that primarily serve the debugging of OPAL are stored in the [[org.opalj.support.debug]]
- * package.
+ * tools that primarily serve the debugging of OPAL or analyses developed on top of OPAL
+ * are stored in the [[org.opalj.support.debug]] package.
  */
 package object info
