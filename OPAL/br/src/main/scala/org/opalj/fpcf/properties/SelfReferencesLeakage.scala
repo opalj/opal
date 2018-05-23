9abb9e72eeb5fc33194931ff537df243c6ad97fc
Fabian Kaiser <fabian.kaiser@stud.tu-darmstadt.de>
2018-05-23 03:31:23 +0000

    solved conflict

--- OPAL/br/src/main/scala/org/opalj/fpcf/properties/SelfReferencesLeakage.scala
+++ OPAL/br/src/main/scala/org/opalj/fpcf/properties/SelfReferencesLeakage.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2009 - 2017
  * Software Technology Group
  * Department of Computer Science
- * Technische Universit√§t Darmstadt
+ * Technische Universit‰t Darmstadt
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,12 +30,23 @@ package org.opalj
 package fpcf
 package properties
 
-import org.opalj.fpcf.Property
-import org.opalj.fpcf.PropertyKey
+import org.opalj.br.ObjectType
 
 /**
- * This property determines if an object leaks its self reference (`this`) by passing
- * it to methods or assigning it to fields.
+ * Determines if an object potentially leaks its self reference (`this`) by passing
+ * assigning it to fields or passing it to methods which assign it to fields.
+ * Hence, it computes a special escape information.
+ *
+ * Here, the self-reference escapes the scope of a class if:
+ *  - ... it is assigned to a (static) field,
+ *  - ... it is passed to a method that assigns it to a field,
+ *  - ... it is stored in an array,
+ *  - ... it is returned (recall that constructors have a void return type),
+ *  - ... if a superclass leaks the self reference.
+ *
+ * This property can be used as a foundation for an analysis that determines whether
+ * all instances created for a specific class never escape their creating methods and,
+ * hence, respective types cannot occur outside the respective contexts.
  */
 sealed trait SelfReferenceLeakage extends Property {
 
@@ -44,15 +55,21 @@ sealed trait SelfReferenceLeakage extends Property {
     final def key = SelfReferenceLeakage.Key
 }
 
+/**
+ * Models the top of the self-references leakage lattice.
+ */
+case object DoesNotLeakSelfReference extends SelfReferenceLeakage
+
+/**
+ * Models the bottom of the lattice.
+ */
+case object LeaksSelfReference extends SelfReferenceLeakage
+
 object SelfReferenceLeakage {
-    final val Key = PropertyKey.create[SelfReferenceLeakage](
-        "SelfReferenceLeakage",
-        LeaksSelfReference
+
+    final val Key = PropertyKey.create[ObjectType, SelfReferenceLeakage](
+        name = "org.opalj.SelfReferenceLeakage",
+        fallbackProperty = LeaksSelfReference
     )
+
 }
-
-case object LeaksSelfReference extends SelfReferenceLeakage { final val isRefinable = false }
-
-case object DoesNotLeakSelfReference extends SelfReferenceLeakage { final val isRefinable = false }
-
-case object MayNotLeakSelfReference extends SelfReferenceLeakage { final val isRefinable = true }
