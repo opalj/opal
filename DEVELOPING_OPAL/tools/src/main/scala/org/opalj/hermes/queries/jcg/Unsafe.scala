9f7c2e74154ab2b25a87968b203b5478dc45c5a3
Michael Reif <>
2018-06-06 17:29:52 +0200

    updated the copyright information

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/jcg/Unsafe.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/jcg/Unsafe.scala
@@ -1,5 +1,5 @@
 /* BSD 2-Clause License:
- * Copyright (c) 2009 - 2017
+ * Copyright (c) 2009 - 2018
  * Software Technology Group
  * Department of Computer Science
  * Technische Universität Darmstadt
