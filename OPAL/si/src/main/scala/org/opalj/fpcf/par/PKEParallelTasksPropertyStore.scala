20b0558798fd580936a7e479551bc6ab2e42fd18
Michael Eichberg <mail@michael-eichberg.de>
2018-07-02 13:30:18 +0200

    formatting fix
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- OPAL/si/src/main/scala/org/opalj/fpcf/par/PKEParallelTasksPropertyStore.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/par/PKEParallelTasksPropertyStore.scala
@@ -1383,11 +1383,11 @@ case object NoRelevantUpdate extends UpdateAndNotifyState {
     override def isNotificationRequired: Boolean = false
     override def areDependersNotified: Boolean = false
 }
- case object RelevantUpdateButNoNotification extends UpdateAndNotifyState {
+case object RelevantUpdateButNoNotification extends UpdateAndNotifyState {
     override def isNotificationRequired: Boolean = true
     override def areDependersNotified: Boolean = false
 }
- case object RelevantUpdateAndNotification extends UpdateAndNotifyState {
+case object RelevantUpdateAndNotification extends UpdateAndNotifyState {
     override def isNotificationRequired: Boolean = false
     override def areDependersNotified: Boolean = true
 }
