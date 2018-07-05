0958f7426f0dbe68a1aaef564275dd22c80e300a
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-07-05 15:50:00 +0200

    restructured analysis

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/cg/LoadedClassesAnalysis.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/cg/LoadedClassesAnalysis.scala
@@ -40,33 +40,11 @@ import org.opalj.collection.immutable.UIDSet
 import org.opalj.fpcf.properties.CallersProperty
 import org.opalj.fpcf.properties.LoadedClasses
 import org.opalj.fpcf.properties.NoCallers
+import org.opalj.fpcf.properties.OnlyCallersWithUnknownContext
 import org.opalj.tac.Assignment
 import org.opalj.tac.GetField
 import org.opalj.tac.PutStatic
 import org.opalj.tac.SimpleTACAIKey
-
-class LoadedClassesState(
-        private[this] var _loadedClasses: UIDSet[ObjectType],
-        private[this] var _dependees:     Set[EOptionP[DeclaredMethod, CallersProperty]]
-) {
-    def loadedClasses: UIDSet[ObjectType] = _loadedClasses
-
-    def addLoadedType(objectType: ObjectType): Unit = _loadedClasses += objectType
-
-    def dependees: Traversable[EOptionP[DeclaredMethod, CallersProperty]] = _dependees
-
-    def addDependee(dependee: EOptionP[DeclaredMethod, CallersProperty]): Unit = {
-        _dependees += dependee
-    }
-    def removeDependee(epk: EPK[DeclaredMethod, CallersProperty]): Unit = {
-        assert(_dependees.count(dependee ⇒ (dependee.e eq epk.e) && dependee.pk == epk.pk) <= 1)
-        _dependees = _dependees.filter(dependee ⇒ (dependee.e ne epk.e) || dependee.pk != epk.pk)
-    }
-    def updateDependee(newEPS: EPS[DeclaredMethod, CallersProperty]): Unit = {
-        removeDependee(newEPS.toEPK)
-        addDependee(newEPS)
-    }
-}
 
 /**
  * Computes the set of classes that are being loaded by the VM during the execution of the
@@ -74,8 +52,7 @@ class LoadedClassesState(
  * @author Florian Kuebler
  */
 class LoadedClassesAnalysis(
-        val project:                         SomeProject,
-        private[this] val callGraphAnalysis: CallGraphAnalysis
+        val project: SomeProject
 ) extends FPCFAnalysis {
     private val tacaiProvider = project.get(SimpleTACAIKey)
     private val declaredMethods = project.get(DeclaredMethodsKey)
@@ -91,20 +68,13 @@ class LoadedClassesAnalysis(
      *
      */
     def doAnalyze(project: SomeProject): PropertyComputationResult = {
-        // todo use a better data structure here
-        var newReachableMethods = List.empty[DeclaredMethod]
-        val state = new LoadedClassesState(UIDSet.empty, Set.empty)
 
-        // todo: here we should depend on all updates
-        for (m ← project.allMethods) {
-            val dm = declaredMethods(m)
-            // check if the method is reachable and handle it correspondingly
-            newReachableMethods ++= handleCaller(state, propertyStore(dm, CallersProperty.key))
-        }
+        propertyStore.registerTriggeredComputation(CallersProperty.key, handleCaller)
 
-        // if there are open dependencies left, return an intermediate result
-        // force call graph computation for new reachable methods
-        returnResult(newReachableMethods.iterator, state)
+        PartialResult[SomeProject, LoadedClasses](project, LoadedClasses.key, {
+            case EPK(p, _) ⇒ Some(EPS(p, null /*TODO LB*/ , new LoadedClasses(UIDSet.empty)))
+            case _         ⇒ None
+        })
     }
 
     /**
@@ -121,29 +91,49 @@ class LoadedClassesAnalysis(
      *         analysis and became reachable here.
      */
     def handleCaller(
-        state: LoadedClassesState, callersOfMethod: EOptionP[DeclaredMethod, CallersProperty]
-    ): Iterator[DeclaredMethod] = {
-        var reachableMethods = List.empty[DeclaredMethod]
+        declaredMethod: DeclaredMethod
+    ): PropertyComputationResult = {
+        val callersOfMethod = propertyStore(declaredMethod, CallersProperty.key)
         callersOfMethod match {
             case FinalEP(_, NoCallers) ⇒
-                // the method was not called at all
-                state.removeDependee(callersOfMethod.toEPK)
-            case _: EPK[DeclaredMethod, CallersProperty] ⇒
-                // there is no result for this method available yet.
-                // here we assume that the dependency was not yet registered or a set is used
-                // as underlying data structure
-                state.addDependee(callersOfMethod)
-            case eps @ EPS(_, _, NoCallers) ⇒
-                // the method may have no callers, or may be reachable.
-                // we do not know, so keep the dependency alive (up to date)
-                state.updateDependee(eps)
+                // nothing to do, since there is no caller
+                NoResult
+            case EPK(_, _) ⇒
+                throw new IllegalStateException("unexpected state")
+            case EPS(_, _, NoCallers) ⇒
+                // we can not create a dependency here, so the analysis is not allowed to create
+                // such a result
+                throw new IllegalStateException("illegal immediate result for callers")
             case eps @ EPS(dm: DeclaredMethod, _, _) ⇒
-                // the method has callers. we have to analyze it and the dependency can be removed
-                state.removeDependee(eps.toEPK)
-                val newReachableMethods = handleNewReachableMethod(dm, state)
-                reachableMethods ++= newReachableMethods
+                // the method has callers. we have to analyze it
+                val (newCLInits, newLoadedClasses) = handleNewReachableMethod(dm)
+
+                if (newLoadedClasses.nonEmpty) {
+                    PartialResult[SomeProject, LoadedClasses](project, LoadedClasses.key, {
+                        case EPK(p, _) ⇒
+                            throw new IllegalStateException("there should be already a result")
+                        case EPS(_, _: LoadedClasses, ub: LoadedClasses) if newLoadedClasses.subsetOf(ub.classes) ⇒
+                            None
+                        case EPS(p, lb: LoadedClasses, ub: LoadedClasses) ⇒
+                            Some(EPS(p, lb, new LoadedClasses(ub.classes ++ newLoadedClasses)))
+
+                    })
+
+                    newCLInits map { clinit ⇒
+                        PartialResult[DeclaredMethod, CallersProperty](clinit, CallersProperty.key, {
+                            case EPK(clInit, _) ⇒
+                                Some(EPS(clInit, CallersProperty.fallback(dm, p), OnlyCallersWithUnknownContext)) // todo use here VMLevel
+                            case EPS(clInit, lb, ub) if !ub.hasCallersWithUnknownContext ⇒
+                                Some(EPS(clInit, lb, ub.updateWithUnknownContext))
+                            case _ ⇒ None
+                        })
+                    }
+                } else {
+                    NoResult
+                }
+
+                NoResult
         }
-        reachableMethods.iterator
     }
 
     /**
@@ -157,34 +147,33 @@ class LoadedClassesAnalysis(
      *
      */
     def handleNewReachableMethod(
-        dm: DeclaredMethod, state: LoadedClassesState
-    ): List[DeclaredMethod] = {
+        dm: DeclaredMethod
+    ): (Set[DeclaredMethod], UIDSet[ObjectType]) = {
         assert(dm.hasSingleDefinedMethod)
         val method = dm.definedMethod
         val methodDCT = method.classFile.thisType
         assert(dm.declaringClassType eq methodDCT)
 
-        var newReachableMethods = List.empty[DeclaredMethod]
+        var newCLInits = Set.empty[DeclaredMethod]
+        var newLoadedClasses = UIDSet.empty[ObjectType]
 
         // whenever a method is called the first time, its declaring class gets loaded
-        if (!state.loadedClasses.contains(method.classFile.thisType)) {
-            handleType(method.classFile.thisType).foreach(newReachableMethods +:= _)
-            state.addLoadedType(methodDCT)
-        }
+        handleType(method.classFile.thisType).foreach(newCLInits += _)
+        newLoadedClasses += methodDCT
 
         for (stmt ← tacaiProvider(method).stmts) {
             stmt match {
-                case PutStatic(_, dct, _, _, _) if !state.loadedClasses.contains(dct) ⇒
-                    handleType(dct).foreach(newReachableMethods +:= _)
-                    state.addLoadedType(dct)
-                case Assignment(_, _, GetField(_, dct, _, _, _)) if !state.loadedClasses.contains(dct) ⇒
-                    handleType(dct).foreach(newReachableMethods +:= _)
-                    state.addLoadedType(dct)
+                case PutStatic(_, dc, _, _, _) if !newLoadedClasses.contains(dc) ⇒
+                    handleType(dc).foreach(newCLInits += _)
+                    newLoadedClasses += dc
+                case Assignment(_, _, GetField(_, dc, _, _, _)) if !newLoadedClasses.contains(dc) ⇒
+                    handleType(dc).foreach(newCLInits += _)
+                    newLoadedClasses += dc
                 case _ ⇒
             }
         }
 
-        newReachableMethods
+        (newCLInits, newLoadedClasses)
     }
 
     /**
@@ -200,50 +189,16 @@ class LoadedClassesAnalysis(
                 // only if registerMethodToProcess returns true, we have Some(<clinit>)
                 // i.e. the call graph analysis has not already processed this method
                 // (after a successful registration, a second call will return false)
-                Some(clInitDM).filter(callGraphAnalysis.registerMethodToProcess)
+                Some(clInitDM)
             }
         }
-    }
-
-    /**
-     * Returns an [[IncrementalResult]], which let the newly reachable methods being analyzed
-     * by the call graph analysis. This result contains the result for the loaded classes
-     * (intermediate if there are open dependencies)
-     */
-    def returnResult(
-        reachableMethods: Iterator[DeclaredMethod], state: LoadedClassesState
-    ): PropertyComputationResult = {
-        val loadedClassesUB = new LoadedClasses(state.loadedClasses)
-
-        val loadedClassesResult = if (state.dependees.isEmpty) {
-            Result(project, new LoadedClasses(state.loadedClasses))
-        } else {
-            IntermediateResult(
-                project, null /*TODO LB */ , loadedClassesUB, state.dependees, continuation(state)
-            )
-        }
-
-        IncrementalResult(
-            loadedClassesResult,
-            reachableMethods.map(m ⇒ (callGraphAnalysis.processMethod(isEntryPoint = true) _, m))
-        )
-    }
-
-    /**
-     * Just handles the update according to the `handleCaller` method and `returnResult`
-     */
-    def continuation(state: LoadedClassesState)(eps: SomeEPS): PropertyComputationResult = {
-        val reachableMethods =
-            handleCaller(state, eps.asInstanceOf[EPS[DeclaredMethod, CallersProperty]])
-
-        returnResult(reachableMethods, state)
     }
 }
 
 object EagerLoadedClassesAnalysis extends FPCFEagerAnalysisScheduler {
 
     override def start(project: SomeProject, propertyStore: PropertyStore): FPCFAnalysis = {
-        val analysis = new LoadedClassesAnalysis(project, null)
+        val analysis = new LoadedClassesAnalysis(project)
         propertyStore.scheduleEagerComputationsForEntities(List(project))(analysis.doAnalyze)
         analysis
     }
