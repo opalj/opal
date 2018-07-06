ce8b384fe9bf9651d05ceb6d9167f3fc7c8c8dda
Michael Eichberg <mail@michael-eichberg.de>
2018-07-06 10:32:44 +0200

    fixed a problem that prevented sbt from running all architecture tests; refined the tests along the way
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/bi/BIArchitectureConsistencyTest.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/bi/BIArchitectureConsistencyTest.scala
@@ -27,66 +27,54 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 package org.opalj
-package de
+package bi
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.Matchers
-import org.opalj.av.checking._
-import org.opalj.br.reader.LambdaExpressionsRewriting
+
+import org.opalj.av.checking.Specification
 
 /**
- * Tests that the implemented architecture of the dependency extraction
- * library is consistent with its specification/with the intended
- * architecture.
+ * Tests that the implemented architecture of the infrastructure project
+ * is consistent with its specification/with the intended architecture.
  *
  * @author Michael Eichberg
  */
 @RunWith(classOf[JUnitRunner])
-class ArchitectureConsistencyTest extends FlatSpec with Matchers with BeforeAndAfterAll {
+class BIArchitectureConsistencyTest extends FlatSpec with Matchers with BeforeAndAfterAll {
 
-    behavior of "the Dependency Extraction Library's implemented architecture"
+    behavior of "the Infrastructure Project's implemented architecture"
 
-    it should "be well modularized in the sense that a superpackage does not depend on a subpackage" in {
-        val deTargetClasses = Specification
-            .ProjectDirectory("OPAL/de/target/scala-2.12/classes")
-            .filterNot { cfSrc ⇒
-                // Ignore the rewritten lambda expressions
-                val (cf, _) = cfSrc
-                cf.thisType.toJava.matches(LambdaExpressionsRewriting.LambdaNameRegEx)
-            }
+    it should "be consistent with the specified architecture" in {
         val expected =
             new Specification(
-                deTargetClasses,
+                Specification.ProjectDirectory("OPAL/bi/target/scala-2.12/classes"),
                 useAnsiColors = true
             ) {
 
-                val DependencyExtractorElements: SourceElementsMatcher =
-                    "org.opalj.de.DependencyExtractor*"
-
-                val DependencyTypeElements: SourceElementsMatcher =
-                    "org.opalj.de.DependencyType*"
-
-                val DependencyProcessorElements: SourceElementsMatcher =
-                    "org.opalj.de.DependencyProcessor*"
-
-                ensemble('DependencyExtractorCore) {
-                    DependencyExtractorElements and
-                        DependencyTypeElements and
-                        DependencyProcessorElements
+                ensemble('Bi) {
+                    "org.opalj.bi.*" except
+                        classes("""org\.opalj\.bi\..+Test.*""".r)
                 }
 
-                ensemble('DependencyExtractionSupport) {
-                    "org.opalj.de.*" except DependencyExtractorElements
+                ensemble('Reader) {
+                    "org.opalj.bi.reader.*" except
+                        classes("""org\.opalj\.bi\.reader\..+Test.*""".r)
                 }
 
-                'DependencyExtractorCore is_only_allowed_to (USE, empty)
+                'Bi is_only_allowed_to (USE, empty)
+
+                // 'Reader is allowed to use everything
+
             }
 
         val result = expected.analyze()
-        result.map(_.toString(useAnsiColors = true)).mkString("\n") should be("")
+        if (result.nonEmpty) {
+            println("Violations:\n\t"+result.map(_.toString(useAnsiColors = true)).mkString("\n\t"))
+            fail("The implemented and the specified architecture are not consistent (see the console for details).")
+        }
     }
-
 }
