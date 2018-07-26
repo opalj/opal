c20613babbd8d7fc9b1bf7f6fabfb8e9e51859c3
Michael Eichberg <mail@michael-eichberg.de>
2018-07-26 14:41:12 +0200

    comprehensive tests relates to the simple TACAI analysis
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: b703e4b5111ac6b21b2a2d7c4e823d4be3e0721b

--- OPAL/ai/src/it/scala/org/opalj/tac/fpcf/analyses/TACAIAnalaysisIntegrationTest.scala
+++ OPAL/ai/src/it/scala/org/opalj/tac/fpcf/analyses/TACAIAnalaysisIntegrationTest.scala
@@ -1,23 +1,23 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
-package org.opalj.tac.fpcf.analyses
+package org.opalj
+package tac
+package fpcf
+package analyses
 
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.junit.runner.RunWith
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
-import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.opalj.util.PerformanceEvaluation.time
-import org.opalj.br.Method
 import org.opalj.br.analyses.SomeProject
 import org.opalj.br.TestSupport
-import org.opalj.ai.Domain
-import org.opalj.ai.BaseAI
-import org.opalj.ai.domain.RecordDefUse
-import org.opalj.ai.domain.l0.PrimitiveTACAIDomain
-import org.opalj.ai.domain.l1.DefaultDomainWithCFGAndDefUse
-import org.opalj.ai.domain.l2.DefaultPerformInvocationsDomainWithCFGAndDefUse
-import org.opalj.br.analyses.MethodInfo
-import org.opalj.fpcf.PropertyStoreKey
+import org.opalj.ai.fpcf.properties.BaseAIResult
+import org.opalj.concurrent.ConcurrentExceptions
 import org.opalj.fpcf.FPCFAnalysesManagerKey
+import org.opalj.fpcf.PropertyStoreKey
+import org.opalj.tac.fpcf.properties.TACAI
+import org.opalj.util.PerformanceEvaluation.time
 
 /**
  * Tests that all methods of OPAL's test projects + the JDK can be converted to the ai-based
@@ -26,109 +26,80 @@ import org.opalj.fpcf.FPCFAnalysesManagerKey
  * @author Michael Eichberg
   */
 @RunWith(classOf[JUnitRunner])
-class TACAIAnalaysisIntegrationTest extends FunSpec with Matchers {
+class TACAIAnalysisIntegrationTest extends FunSpec with Matchers {
 
     def analyzeProject(
-        project:       SomeProject
+        theProject:       SomeProject
     ): Unit = {
         if (Thread.currentThread().isInterrupted) return;
+        time {            // 1. Test eager analysis...
+                val p = theProject
+                val counter = new AtomicInteger()
+                val fpcfManager = p.get(FPCFAnalysesManagerKey)
+                val ps = fpcfManager.runAll(EagerL0TACAIAnalysis)
+                p.parForeachMethodWithBody() { mi =>
+                    val m = mi.method
+                    val aiResultProperty = ps(m, BaseAIResult.key)
+                    val Some(aiResult) = aiResultProperty.asFinal.p.aiResult
+                    // ... smoke test...
+                    val tacaiProperty = ps(m, TACAI.key)
+                    val Some(tac) = tacaiProperty.asFinal.p.tac
+                    // ... smoke test...
+                    counter.incrementAndGet()
+                }
+                ps.shutdown()
+                info(s"eagerly performed AI and generated TAC for ${counter.get} method(s)")
+            } {t => info(s"eager analysis took ${t.toSeconds}")}
 
-        val fpcfManager = project.get(FPCFAnalysesManagerKey)
-        val ps = fpcfManager.runAll(EagerL0TACAIAnalysis)
-        project.parForeachMethodWithBody { mi =>
-            val m = mi.method
-            ps.apply(m,BaseAI.key)
-            ps.app
-        }
-
-
-/*
-        var errors: List[(String, Throwable)] = Nil
-        val successfullyCompleted = new java.util.concurrent.atomic.AtomicInteger(0)
-        val ch = project.classHierarchy
-        for {
-            cf ← project.allProjectClassFiles.par
-            if !Thread.currentThread().isInterrupted
-            m ← cf.methods
-            body ← m.body
-        } {
-            val aiResult = BaseAI(m, domainFactory(project, m))
-            try {
-
-                val TACode(params, tacAICode, _, cfg, _, _) = TACAI(m, ch, aiResult)(List.empty)
-                ToTxt(params, tacAICode, cfg, false, true, true)
-
-                // Some additional consistency tests...
-
-                tacAICode.iterator.zipWithIndex foreach { stmtIndex ⇒
-                    val (stmt, index) = stmtIndex
-                    val bb = cfg.bb(index)
-                    if (bb.endPC == index && bb.mayThrowException && stmt.isSideEffectFree) {
-                        fail(
-                            s"$stmt is side effect free "+
-                                "but the basic block has a catch node/abnormal exit node as a successor"
-                        )
+           time { // 2. Test lazy analysis...
+                val p = theProject.recreate()
+                val counter = new AtomicInteger()
+                val ps = p.get(PropertyStoreKey)
+                ps.setupPhase(Set( BaseAIResult, TACAI),Set.empty)
+                LazyL0TACAIAnalysis.init(ps)
+                LazyL0TACAIAnalysis.schedule(ps,null)
+                try {p.parForeachMethodWithBody() { mi =>
+                    counter.incrementAndGet() % 3 match {
+                        case 0 => ps.force(mi.method, BaseAIResult.key)
+                        case 1 => ps.force(mi.method, TACAI.key)
+                        case 2 =>
+                            ps.force(mi.method, BaseAIResult.key)
+                            ps.force(mi.method, TACAI.key)
                     }
                 }
-
-                successfullyCompleted.incrementAndGet()
-            } catch {
-                case e: Throwable ⇒ this.synchronized {
-                    val methodSignature = m.toJava
-
-                    println(methodSignature+" - size: "+body.instructions.length)
-                    e.printStackTrace(Console.out)
-                    if (e.getCause != null) {
-                        println("\tcause:")
-                        e.getCause.printStackTrace(Console.out)
-                    }
-                    val instrWithIndex = body.instructions.zipWithIndex.filter(_._1 != null)
-                    println(
-                        instrWithIndex.map(_.swap).mkString("Instructions:\n\t", "\n\t", "\n")
-                    )
-                    println(
-                        body.exceptionHandlers.mkString("Exception Handlers:\n\t", "\n\t", "\n")
-                    )
-                    errors ::= ((project.source(cf)+":"+methodSignature, e))
+                } catch {
+                    case t : ConcurrentExceptions =>
+                        t.getSuppressed.foreach(_.printStackTrace())
+                        throw t;
                 }
-            }
-        }
-        if (errors.nonEmpty) {
-            val summary =
-                s"successfully transformed ${successfullyCompleted.get} methods: "+
-                    "; failed methods: "+errors.size+"\n"
-            val message = errors.map(_.toString()).mkString("Errors thrown:\n", "\n\n", summary)
-            fail(message)
-        }
-        */
-    }
-
-    protected def domainFactories = {
-        Seq[(String, (SomeProject, Method) ⇒ Domain with RecordDefUse)](
-            (
-                "l0.PrimitiveTACAIDomain",
-                (p: SomeProject, m: Method) ⇒ new PrimitiveTACAIDomain(p.classHierarchy, m)
-            ),
-            (
-                "l1.DefaultDomainWithCFGAndDefUse",
-                (p: SomeProject, m: Method) ⇒ new DefaultDomainWithCFGAndDefUse(p, m)
-            ),
-            (
-                "l2.DefaultPerformInvocationsDomainWithCFGAndDefUse",
-                (p: SomeProject, m: Method) ⇒ {
-                    new DefaultPerformInvocationsDomainWithCFGAndDefUse(p, m)
+                ps.waitOnPhaseCompletion()
+                p.parForeachMethodWithBody() { mi =>
+                    val m = mi.method
+                    val aiResultProperty = ps(m, BaseAIResult.key)
+                    val Some(aiResult) = aiResultProperty.asFinal.p.aiResult
+                    // ... smoke test...
+                    val tacaiProperty = ps(m, TACAI.key)
+                    val Some(tac) = tacaiProperty.asFinal.p.tac
+                    // ... smoke test...
+                    counter.incrementAndGet()
                 }
-            )
-        )
+                ps.shutdown()
+                info(s"lazily performed AI and generated TAC for ${counter.get/2} method(s)")
+            } {t => info(s"lazy analysis took ${t.toSeconds}")}
+
+
+
     }
 
     // TESTS
 
     describe(s"creating the 3-address code using the PropertyStore") {
 
-        TestSupport.allBITestProjects() foreach { biProject ⇒
+        TestSupport.allManagedBITestProjects() foreach { biProject ⇒
             val (name, projectFactory) = biProject
-            it(s"for $name") {                analyzeProject(projectFactory())            }
+            it(s"for $name") {
+                analyzeProject(projectFactory())
+            }
         }
     }
 }
