33642fdeea3f8017269548e1e54485c665b71467
Michael Reif <>
2018-08-16 17:57:12 +0200

    almost finished library test case query
    Former-commit-id: e2d5a0cb49d6acc4bd2495267b3cd63bb375283e

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/jcg/Library.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/hermes/queries/jcg/Library.scala
@@ -6,10 +6,18 @@ package jcg
 
 import org.opalj.br.MethodWithBody
 import org.opalj.br.Field
+import org.opalj.br.ObjectType
+import org.opalj.br.MethodDescriptor
+import org.opalj.br.Method
 import org.opalj.br.analyses.Project
+import org.opalj.br.analyses.SomeProject
+import org.opalj.br.analyses.ProjectIndex
+import org.opalj.br.analyses.ProjectIndexKey
 import org.opalj.br.instructions.INVOKEVIRTUAL
 import org.opalj.br.instructions.INVOKEINTERFACE
 import org.opalj.da.ClassFile
+
+import scala.collection.mutable
 
 /**
  * Groups test case features that test the support for libraries/partial programs. All test cases
@@ -37,6 +45,9 @@ class Library(implicit hermes: HermesConfig) extends DefaultFeatureQuery {
         rawClassFiles:        Traversable[(ClassFile, S)]
     ): IndexedSeq[LocationsContainer[S]] = {
         val instructionLocations = Array.fill(featureIDs.size)(new LocationsContainer[S])
+
+        val projectIndex = project.get(ProjectIndexKey)
+        var cbsCache = Map.empty[String, Set[Method]]
 
         for {
             (classFile, source) ← project.projectClassFilesWithSources
@@ -67,27 +78,154 @@ class Library(implicit hermes: HermesConfig) extends DefaultFeatureQuery {
                 instructionLocations(1) += l
             }
 
-            //            val ai = new InterruptableAI[Domain]
-            //
-            //            if (true) {
-            //                val domain = new DefaultDomainWithCFGAndDefUse(project, method)
-            //                val result = ai(method, domain)
-            //
-            //                //                invokeKind match {
-            //                //                    case INVOKEVIRTUAL(declClass, _, _) if declClass.isObjectType ⇒ {
-            //                //                        val ot = declClass.asObjectType
-            //                //                        if (fieldTypes.contains(ot))
-            //                //
-            //                //
-            //                //                    }
-            //                //                    case INVOKEINTERFACE.opcode ⇒ {
-            //                //                        ???
-            //                //                    }
-            //                //                }
-            //            }
+            val isParameterType = paramTypes.contains(otID)
+            if (isParameterType) {
+                if (invokeKind.opcode == INVOKEINTERFACE.opcode) {
+                    val INVOKEINTERFACE(declClass, name, descriptor) = invokeKind
+                    val cfO = project.classFile(declClass)
+                    if(cfO.nonEmpty) {
+                        val cf = cfO.get
+                        val cacheKey = s"${cf.thisType.id}-$name-${descriptor.toJVMDescriptor}"
+                        val targets = cbsCache.get(cacheKey) match {
+                            case None => {
+                                val newTargets = cf.findMethod(name, descriptor)
+                                    .map(getCBSTargets(projectIndex,project,_))
+                                    .getOrElse(Set.empty[Method])
+                                cbsCache = cbsCache + ((cacheKey, newTargets))
+                                newTargets
+                            }
+                            case Some(result) => result
+                        }
+
+                        var publicTarget = false
+                        var packagePrivateTarget = false
+//                        var subclassTarget = false
+
+                        val itr = targets.iterator
+                        while(itr.hasNext
+                            && !(publicTarget && packagePrivateTarget && subclassTarget)){
+
+                            val target = itr.next()
+                            val declClass = target.classFile
+
+                            publicTarget |= declClass.isPublic
+                            packagePrivateTarget |= declClass.isPackageVisible
+//                            subclassTarget |= declClass.superclassType.map{ ot =>
+//                                val cf = project.classFile(ot).map{ cf =>
+//
+//                            }
+//                                ot..find(target.name, target.descriptor)
+//                                false
+//                            }.getOrElse(false)
+                        }
+
+                        if(publicTarget)
+                            instructionLocations(2) += l
+
+                        if(packagePrivateTarget)
+                            instructionLocations(3) += l
+
+                        if(subclassTarget)
+                            instructionLocations(4) += l
+                    }
+                }
+
+                instructionLocations(0) += l
+            }
+
         }
 
         instructionLocations;
 
+    }
+
+    def getCBSTargets(
+           projectIndex:      ProjectIndex,
+           project: SomeProject,
+           method: Method
+       ): Set[Method] = {
+
+        val methodName = method.name
+        val methodDescriptor = method.descriptor
+
+        import projectIndex.findMethods
+
+        val interfaceClassFile = method.classFile
+        val interfaceType = interfaceClassFile.thisType
+
+        val cbsTargets = mutable.Set.empty[Method]
+
+        def analyzePotentialCBSTarget(cbsCallee: Method): Unit = {
+            if (!cbsCallee.isPublic)
+                return ;
+
+            if (cbsCallee.isAbstract)
+                return ;
+
+            if (!isInheritableMethod(cbsCallee))
+                return ;
+
+            val cbsCalleeDeclaringClass = cbsCallee.classFile
+
+            if (!cbsCalleeDeclaringClass.isClassDeclaration)
+                return ;
+
+            if (cbsCalleeDeclaringClass.isEffectivelyFinal)
+                return ;
+
+            val cbsCalleeDeclaringType = cbsCalleeDeclaringClass.thisType
+
+            if (cbsCalleeDeclaringType eq ObjectType.Object)
+                return ;
+
+            if (project.classHierarchy.isSubtypeOf(
+                cbsCalleeDeclaringType, interfaceType))
+                return ;
+
+            if (hasSubclassWhichInheritsFromInterface(cbsCalleeDeclaringType, interfaceType, methodName, methodDescriptor,project).isYes)
+                return ;
+
+            cbsTargets += cbsCallee
+        }
+
+        findMethods(methodName, methodDescriptor) foreach analyzePotentialCBSTarget
+
+        cbsTargets.toSet
+    }
+
+    @inline private[this] def isInheritableMethod(method: Method): Boolean = {
+
+        !method.isPrivate
+    }
+
+    private[this] def hasSubclassWhichInheritsFromInterface(
+               classType:        ObjectType,
+               interfaceType:    ObjectType,
+               methodName:       String,
+               methodDescriptor: MethodDescriptor,
+               project: SomeProject,
+           ): Answer = {
+
+        val classHierarchy = project.classHierarchy
+
+        val itr = classHierarchy.allSubclassTypes(classType, reflexive = false)
+        var isUnknown = false
+
+        while (itr.hasNext) {
+            val subtype = itr.next
+            project.classFile(subtype) match {
+                case Some(subclassFile) ⇒
+                    if (subclassFile.findMethod(methodName, methodDescriptor).isEmpty
+                        && classHierarchy.isSubtypeOf(subtype, interfaceType))
+                        return Yes;
+                case None ⇒
+                    isUnknown = false
+            }
+        }
+
+        if (isUnknown)
+            Unknown
+        else
+            No
     }
 }
