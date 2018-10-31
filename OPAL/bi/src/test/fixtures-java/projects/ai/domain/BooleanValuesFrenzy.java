1e50c210b7d998eda454bd078fe17d49571e686f
Michael Eichberg <mail@michael-eichberg.de>
2018-10-31 11:34:41 +0100

    added a very simple example to demonstrate the challenge of analyzing “!<BooleanValue>”
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: 1e302ff2deaecb39e856ebc68f2909fb329f80ae

---
+++ OPAL/bi/src/test/fixtures-java/projects/ai/domain/BooleanValuesFrenzy.java
@@ -0,0 +1,12 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package ai.domain;
+
+/**
+ * @author Michael Eichberg
+ */
+public class BooleanValuesFrenzy {
+
+  public boolean not(boolean a) {
+      return !a;
+  }
+}
