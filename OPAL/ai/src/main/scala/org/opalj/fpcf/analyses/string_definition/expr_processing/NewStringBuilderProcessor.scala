9c3f1055390eac611239f9f506160354cc39d68b
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-11-11 16:19:34 +0100

    Extended the string definition analysis in a way that it can now correctly process the new test case 'ifElseInLoopWithAppendAfterwards'. For this, a little bug in StringTree#groupRepetitionElementsAcc had to be fixed.
    Former-commit-id: 34d43a98918bde28d90d690d482814c2ea34770d

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/expr_processing/NewStringBuilderProcessor.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/expr_processing/NewStringBuilderProcessor.scala
@@ -42,7 +42,7 @@ class NewStringBuilderProcessor(
                 val uses = assignment.targetVar.usedBy.filter(!ignore.contains(_)).toArray.sorted
                 val (inits, nonInits) = getInitsAndNonInits(uses, stmts, cfg)
                 val initTreeNodes = ListBuffer[StringTree]()
-                val nonInitTreeNodes = ListBuffer[StringTree]()
+                val nonInitTreeNodes = mutable.Map[Int, ListBuffer[StringTree]]()
 
                 inits.foreach { next ⇒
                     val toProcess = stmts(next) match {
@@ -79,7 +79,12 @@ class NewStringBuilderProcessor(
                 nonInits.foreach { next ⇒
                     val subtree = exprHandler.concatDefSites(next)
                     if (subtree.isDefined) {
-                        nonInitTreeNodes.append(subtree.get)
+                        val key = next.min
+                        if (nonInitTreeNodes.contains(key)) {
+                            nonInitTreeNodes(key).append(subtree.get)
+                        } else {
+                            nonInitTreeNodes(key) = ListBuffer(subtree.get)
+                        }
                     }
                 }
 
@@ -90,8 +95,10 @@ class NewStringBuilderProcessor(
                 // Append nonInitTreeNodes to initTreeNodes (as children)
                 if (nonInitTreeNodes.nonEmpty) {
                     val toAppend = nonInitTreeNodes.size match {
-                        case 1 ⇒ nonInitTreeNodes.head
-                        case _ ⇒ StringTreeOr(nonInitTreeNodes)
+                        // If there is only one element in the map use this
+                        case 1 ⇒ nonInitTreeNodes.head._2.head
+                        // Otherwise, we need to build the proper tree, considering dominators
+                        case _ ⇒ orderNonInitNodes(nonInitTreeNodes, cfg)
                     }
                     if (initTreeNodes.isEmpty) {
                         initTreeNodes.append(toAppend)
@@ -181,6 +188,114 @@ class NewStringBuilderProcessor(
         // Make the list unique (as described above) and sort it in ascending order
         val uniqueLists = blocks.values.toList.distinct.reverse
         (inits.toList.sorted, uniqueLists.map(_.toList))
+    }
+
+    /**
+     * The relation of nodes is not obvious, i.e., one cannot generally say that two nodes, which,
+     * e.g., modify a [[StringBuilder]] object are concatenations or  are to be mapped to a
+     * [[StringTreeOr]].
+     * <p>
+     * This function uses the following algorithm to determine the relation of the given nodes:
+     * <ol>
+     *   <li>
+     *       For each given node, compute the dominators (and store it in lists called
+     *       ''dominatorLists'').</li>
+     *   <li>
+     *       Take all ''dominatorLists'' and union them (without duplicates) and sort this list in
+     *       descending order. This list, called ''uniqueDomList'', is then used in the next step.
+     *   </li>
+     *   <li>
+     *       Traverse ''uniqueDomList''. Here, we call the next element in that list ''nextDom''.
+     *       One of the following cases will occur:
+     *       <ol>
+     *           <li>
+     *               Only one [[StringTree]] element in ''treeNodes'' has ''nextDom'' as a
+     *               dominator. In this case, nothing is done as we cannot say anything about the
+     *               relation to other elements in ''treeNodes''.
+     *           </li>
+     *           <li>
+     *               At least two elements in ''treeNodes'' have ''nextDom'' as a dominator. In this
+     *               case, these nodes are in a 'only-one-node-is-evaluated-relation' as it happens
+     *               when the dominator of two nodes is an if condition, for example. Thus, these
+     *               elements are put into a [[StringTreeOr]] element and then no longer considered
+     *               for the computation.
+     *           </li>
+     *       </ol>
+     *   </li>
+     *   <li>
+     *       It might be that not all elements in ''treeNodes'' were processed by the second case of
+     *       the previous step (normally, this should be only one element. These elements represent
+     *       a concatenation relation. Thus, all [[StringTreeOr]] elements of the last step are then
+     *       put into a [[StringTreeConcat]] element with the ones not processed yet. They are
+     *       ordered in ascending order according to their index in the statement list to preserve
+     *       the correct order.
+     *   </li>
+     * </ol>
+     *
+     * @param treeNodes The nodes which are to be ordered. The key of the map refers to the index in
+     *                  the statement list. It might be that some operation (e.g., append) have two
+     *                  definition sites; In this case, pass the minimum index. The values of the
+     *                  map correspond to [[StringTree]] elements that resulted from the evaluation
+     *                  of the definition site(s).
+     * @param cfg The control flow graph.
+     * @return This function computes the correct relation of the given [[StringTree]] elements
+     *         and returns this as a single tree element.
+     */
+    private def orderNonInitNodes(
+        treeNodes: mutable.Map[Int, ListBuffer[StringTree]],
+        cfg:       CFG[Stmt[V], TACStmts[V]]
+    ): StringTree = {
+        // TODO: Put this function in a different place (like DominatorTreeUtils) and generalize the procedure.
+        val domTree = cfg.dominatorTree
+        // For each list of nodes, get the dominators
+        val rootIndex = cfg.startBlock.startPC
+        val dominatorLists = mutable.Map[Int, List[Int]]()
+        for ((key, _) ← treeNodes) {
+            val dominators = ListBuffer[Int]()
+            var next = domTree.immediateDominators(key)
+            while (next != rootIndex) {
+                dominators.prepend(next)
+                next = domTree.immediateDominators(next)
+            }
+            dominators.prepend(rootIndex)
+            dominatorLists(key) = dominators.toList
+        }
+
+        // Build a unique union of the dominators that is in descending order
+        var uniqueDomList = ListBuffer[Int]()
+        for ((_, value) ← dominatorLists) {
+            uniqueDomList.append(value: _*)
+        }
+        uniqueDomList = uniqueDomList.distinct.sorted.reverse
+
+        val newChildren = ListBuffer[StringTree]()
+        uniqueDomList.foreach { nextDom ⇒
+            // Find elements with the same dominator
+            val indicesWithSameDom = ListBuffer[Int]()
+            for ((key, value) ← dominatorLists) {
+                if (value.contains(nextDom)) {
+                    indicesWithSameDom.append(key)
+                }
+            }
+            if (indicesWithSameDom.size > 1) {
+                val newOrElement = ListBuffer[StringTree]()
+                // Sort in order to have the correct order
+                indicesWithSameDom.sorted.foreach { nextIndex ⇒
+                    newOrElement.append(treeNodes(nextIndex).head)
+                    dominatorLists.remove(nextIndex)
+                }
+                newChildren.append(StringTreeOr(newOrElement))
+            }
+        }
+
+        // If there are elements left, add them as well (they represent concatenations)
+        for ((key, _) ← dominatorLists) { newChildren.append(treeNodes(key).head) }
+
+        if (newChildren.size == 1) {
+            newChildren.head
+        } else {
+            StringTreeConcat(newChildren)
+        }
     }
 
 }
