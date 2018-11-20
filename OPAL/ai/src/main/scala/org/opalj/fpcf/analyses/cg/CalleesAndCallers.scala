f21cf8ca4919d6ee60244e2609302795baf3128d
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-11-20 11:38:32 +0100

    unused implicits
    Former-commit-id: 9399a13cd06883b0a4fbe27e1972098fccc1fe32

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/cg/CalleesAndCallers.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/cg/CalleesAndCallers.scala
@@ -38,7 +38,7 @@ private[cg] class CalleesAndCallers(
 
     private[cg] def updateWithCall(
         caller: DeclaredMethod, callee: DeclaredMethod, pc: Int
-    )(implicit declaredMethods: DeclaredMethods): Unit = {
+    ): Unit = {
         val calleeId = callee.id
         if (!_callees.contains(pc) || !_callees(pc).contains(calleeId)) {
             _callees = _callees.updated(pc, _callees.getOrElse(pc, IntTrieSet.empty) + calleeId)
@@ -72,7 +72,7 @@ private[cg] class CalleesAndCallers(
 
     private[this] def createPartialResultForCaller(
         caller: DeclaredMethod, callee: DeclaredMethod, pc: Int
-    )(implicit declaredMethods: DeclaredMethods): PartialResult[DeclaredMethod, CallersProperty] = {
+    ): PartialResult[DeclaredMethod, CallersProperty] = {
         PartialResult[DeclaredMethod, CallersProperty](callee, CallersProperty.key, {
             case IntermediateESimpleP(_, ub) ⇒
                 val newCallers = ub.updated(caller, pc)
@@ -104,7 +104,7 @@ private[cg] class IndirectCalleesAndCallers(
 
     private[cg] override def updateWithCall(
         caller: DeclaredMethod, callee: DeclaredMethod, pc: Int
-    )(implicit declaredMethods: DeclaredMethods): Unit = {
+    ): Unit = {
         throw new UnsupportedOperationException("Use updateWithIndirectCall instead!")
     }
 
@@ -113,7 +113,7 @@ private[cg] class IndirectCalleesAndCallers(
         callee:     DeclaredMethod,
         pc:         Int,
         parameters: Seq[Option[(ValueInformation, IntTrieSet)]]
-    )(implicit declaredMethods: DeclaredMethods): Unit = {
+    ): Unit = {
         super.updateWithCall(caller, callee, pc)
         _parameters = _parameters.updated(
             pc,
