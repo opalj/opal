8fab2d9e616e0bde671fc0a838b92779d9859c3c
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-11-21 12:19:48 +0100

    Implemented a way to find all paths in a CFG from a set of given starting points.
    Former-commit-id: 1728aa76c9ee6e10645380b36f9d3204bafe1e80

---
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/preprocessing/AbstractPathFinder.scala
@@ -0,0 +1,121 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.fpcf.analyses.string_definition.preprocessing
+
+import org.opalj.br.cfg.CFG
+import org.opalj.br.cfg.CFGNode
+import org.opalj.fpcf.analyses.string_definition.Path
+import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.tac.Stmt
+import org.opalj.tac.TACStmts
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+/**
+ * [[SubPath]] represents the general item that forms a [[Path]].
+ */
+sealed class SubPath()
+
+/**
+ * A flat element, e.g., for representing a single statement. The statement is identified by
+ * `element`.
+ */
+case class FlatPathElement(element: Int) extends SubPath
+
+/**
+ * A nested path element, that is, items can be used to form arbitrary structures / hierarchies.
+ * `element` holds all child elements.
+ */
+case class NestedPathElement(element: ListBuffer[SubPath]) extends SubPath
+
+/**
+ * [[AbstractPathFinder]] provides a scaffolding for finding all relevant paths in a CFG in the
+ * scope of string definition analyses.
+ *
+ * @author Patrick Mell
+ */
+trait AbstractPathFinder {
+
+    /**
+     * Generates a new [[NestedPathElement]] with a given number of inner [[NestedPathElement]]s.
+     */
+    protected def generateNestPathElement(numInnerElements: Int): NestedPathElement = {
+        val outerNested = NestedPathElement(ListBuffer())
+        for (_ ← 0.until(numInnerElements)) {
+            outerNested.element.append(NestedPathElement(ListBuffer()))
+        }
+        outerNested
+    }
+
+    /**
+     * Determines whether a given `site` is the head of a loop by comparing it to a set of loops
+     * (here a list of lists). This function returns ''true'', if `site` is the head of one of the
+     * inner lists.
+     */
+    protected def isHeadOfLoop(site: Int, loops: List[List[Int]]): Boolean =
+        loops.foldLeft(false)((old: Boolean, nextLoop: List[Int]) ⇒ old || nextLoop.head == site)
+
+    /**
+     * Determines whether a given `site` is the end of a loop by comparing it to a set of loops
+     * (here a list of lists). This function returns ''true'', if `site` is the last element of one
+     * of the inner lists.
+     */
+    protected def isEndOfLoop(site: Int, loops: List[List[Int]]): Boolean =
+        loops.foldLeft(false)((old: Boolean, nextLoop: List[Int]) ⇒ old || nextLoop.last == site)
+
+    /**
+     * This function checks if a branching corresponds to an if (or if-elseif) structure that has no
+     * else block.
+     * Currently, this function is implemented to check whether the very last element of
+     * `successors` is a path past the if (or if-elseif) paths.
+     *
+     * @param successors The successors of a branching.
+     * @param cfg The control flow graph underlying the successors.
+     * @return Returns ''true'', if the very last element of `successors` is a child of one of the
+     *         other successors. If this is the case, the branching corresponds to one without an
+     *         ''else'' branch.
+     */
+    def isCondWithoutElse(successors: List[Int], cfg: CFG[Stmt[V], TACStmts[V]]): Boolean = {
+        // Separate the last element from all previous ones
+        val branches = successors.reverse.tail.reverse
+        val lastEle = successors.last
+
+        // For every successor (except the very last one), execute a DFS to check whether the very
+        // last element is a successor. If so, this represents a path past the if (or if-elseif).
+        branches.count { next ⇒
+            val seenNodes = ListBuffer[CFGNode](cfg.bb(next))
+            val toVisitStack = mutable.Stack[CFGNode](cfg.bb(next).successors.toArray: _*)
+            while (toVisitStack.nonEmpty) {
+                val from = toVisitStack.pop()
+                val to = from.successors
+                if (to.contains(cfg.bb(lastEle))) {
+                    return true
+                }
+                seenNodes.append(from)
+                toVisitStack.pushAll(to.filter(!seenNodes.contains(_)))
+            }
+            return false
+        } > 1
+    }
+
+    /**
+     * Implementations of this function find all paths starting from the sites, given by
+     * `startSites`, within the provided control flow graph, `cfg`. As this is executed within the
+     * context of a string definition analysis, implementations are free to decide whether they
+     * include only statements that work on [[StringBuffer]] / [[StringBuilder]] or include all
+     * statements in the paths.
+     *
+     * @param startSites A list of possible start sites, that is, initializations. Several start
+     *                  sites denote that an object is initialized within a conditional.
+     * @param endSite An end site which an implementation might use to early-stop the procedure.
+     *                This site can be the read operation of interest, for instance.
+     * @param cfg The underlying control flow graph which servers as the basis to find the paths.
+     * @return Returns all found paths as a [[Path]] object. That means, the return object is a flat
+     *         structure, however, captures all hierarchies and (nested) flows. Note that a
+     *         [[NestedPathElement]] with only one child can either refer to a loop or an ''if''
+     *         that has no ''else'' block (from a high-level perspective). It is the job of the the
+     *         procedure processing the return object to further identify that (if necessary).
+     */
+    def findPaths(startSites: List[Int], endSite: Int, cfg: CFG[Stmt[V], TACStmts[V]]): Path
+
+}
---
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/preprocessing/DefaultPathFinder.scala
@@ -0,0 +1,137 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.fpcf.analyses.string_definition.preprocessing
+
+import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.tac.Stmt
+import org.opalj.tac.TACStmts
+import org.opalj.br.cfg.CFG
+import org.opalj.br.cfg.ExitNode
+import org.opalj.fpcf.analyses.string_definition.Path
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ * An approach based on an a naive / intuitive traversing of the control flow graph.
+ *
+ * @author Patrick Mell
+ */
+class DefaultPathFinder extends AbstractPathFinder {
+
+    /**
+     * This implementation finds all paths based on an a naive / intuitive traversing of the `cfg`
+     * and, based on that, determines in what relation a statement / instruction is with its
+     * predecessors / successors.
+     * The paths contain all instructions, not only those that modify a [[StringBuilder]] /
+     * [[StringBuffer]] object.
+     * For this implementation, `endSite` is not required, thus passing any value is fine.
+     *
+     * @see [[AbstractPathFinder.findPaths]]
+     */
+    override def findPaths(
+        startSites: List[Int], endSite: Int, cfg: CFG[Stmt[V], TACStmts[V]]
+    ): Path = {
+        // path will accumulate all paths
+        val path = ListBuffer[SubPath]()
+        val stack = ListBuffer[Int](startSites: _*)
+        val seenElements = ListBuffer[Int]()
+        // numSplits serves a queue that stores the number of possible branches (or successors)
+        val numSplits = ListBuffer[Int]()
+        // Also a queue that stores the indices of which branch of a conditional to take next
+        val currSplitIndex = ListBuffer[Int]()
+        // Used to quickly find the element at which to insert a sub path
+        val nestedElementsRef = ListBuffer[NestedPathElement]()
+        val natLoops = cfg.findNaturalLoops()
+
+        // Multiple start sites => We start within a conditional => Prepare for that
+        if (startSites.size > 1) {
+            val outerNested = generateNestPathElement(startSites.size)
+            numSplits.append(startSites.size)
+            currSplitIndex.append(0)
+            nestedElementsRef.append(outerNested)
+            path.append(outerNested)
+        }
+
+        while (stack.nonEmpty) {
+            val popped = stack.head
+            stack.remove(0)
+            val bb = cfg.bb(popped)
+            val isLoopHeader = isHeadOfLoop(popped, natLoops)
+            var isLoopEnding = false
+            var belongsToLoopHeader = false
+
+            // Append everything of the current basic block to the path
+            for (i ← bb.startPC.to(bb.endPC)) {
+                seenElements.append(i)
+                val toAppend = FlatPathElement(i)
+
+                if (!isLoopEnding) {
+                    isLoopEnding = isEndOfLoop(i, natLoops)
+                }
+
+                // For loop headers, insert a new nested element (and thus, do the housekeeping)
+                if (isHeadOfLoop(i, natLoops)) {
+                    numSplits.prepend(1)
+                    currSplitIndex.prepend(0)
+
+                    val outer = generateNestPathElement(1)
+                    outer.element.head.asInstanceOf[NestedPathElement].element.append(toAppend)
+                    nestedElementsRef.prepend(outer)
+                    path.append(outer)
+
+                    belongsToLoopHeader = true
+                } // The instructions belonging to a loop header are stored in a flat structure
+                else if (!belongsToLoopHeader && (numSplits.isEmpty || bb.predecessors.size > 1)) {
+                    path.append(toAppend)
+                } // Within a nested structure => append to an inner element
+                else {
+                    val relevantRef = nestedElementsRef.head.element(currSplitIndex.head)
+                    relevantRef.asInstanceOf[NestedPathElement].element.append(toAppend)
+                }
+            }
+
+            val successors = bb.successors.filter {
+                !_.isInstanceOf[ExitNode]
+            }.map(_.nodeId).toList.sorted
+            val successorsToAdd = successors.filter { next ⇒
+                !seenElements.contains(next) && !stack.contains(next)
+            }
+            val hasSeenSuccessor = successors.foldLeft(false) {
+                (old: Boolean, next: Int) ⇒ old || seenElements.contains(next)
+            }
+
+            // At the join point of a branching, do some housekeeping
+            if (currSplitIndex.nonEmpty &&
+                ((bb.predecessors.size > 1 && !isLoopHeader) || hasSeenSuccessor)) {
+                currSplitIndex(0) += 1
+                if (currSplitIndex.head == numSplits.head) {
+                    numSplits.remove(0)
+                    currSplitIndex.remove(0)
+                    nestedElementsRef.remove(0)
+                }
+            }
+
+            // Within a conditional, prepend in order to keep the correct order
+            if (numSplits.nonEmpty && (bb.predecessors.size == 1)) {
+                stack.prependAll(successorsToAdd)
+            } else {
+                stack.appendAll(successorsToAdd)
+            }
+            // On a split point, prepare the next (nested) element
+            if (successors.length > 1 && !isLoopHeader) {
+                val appendSite = if (numSplits.isEmpty) path else
+                    nestedElementsRef(currSplitIndex.head).element
+                val relevantNumSuccessors = if (isCondWithoutElse(successors, cfg))
+                    successors.size - 1 else successors.size
+                val outerNested = generateNestPathElement(relevantNumSuccessors)
+
+                numSplits.prepend(relevantNumSuccessors)
+                currSplitIndex.prepend(0)
+                nestedElementsRef.prepend(outerNested)
+                appendSite.append(outerNested)
+            }
+        }
+
+        path.toList
+    }
+
+}
