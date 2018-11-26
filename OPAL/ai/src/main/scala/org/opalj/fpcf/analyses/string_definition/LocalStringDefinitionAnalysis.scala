8f5068d3e46c17ec7d4c88a3c55311f79b185cc4
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-11-26 17:03:43 +0100

    Updated the LocalStringDefinitionAnalysis to work with the new interpretation approach.
    Former-commit-id: aaf0a0a4d1eb3cd240f82f672324921406a85b48

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/LocalStringDefinitionAnalysis.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/LocalStringDefinitionAnalysis.scala
@@ -6,21 +6,21 @@ import org.opalj.fpcf.FPCFAnalysis
 import org.opalj.fpcf.PropertyComputationResult
 import org.opalj.fpcf.PropertyKind
 import org.opalj.fpcf.PropertyStore
-import org.opalj.fpcf.Result
 import org.opalj.fpcf.properties.StringConstancyProperty
 import org.opalj.fpcf.FPCFLazyAnalysisScheduler
 import org.opalj.fpcf.ComputationSpecification
-import org.opalj.fpcf.analyses.string_definition.expr_processing.ExprHandler
-import org.opalj.fpcf.string_definition.properties.StringTree
-import org.opalj.fpcf.string_definition.properties.StringTreeCond
+import org.opalj.fpcf.NoResult
+import org.opalj.fpcf.analyses.string_definition.interpretation.NewStringBuilderProcessor
+import org.opalj.fpcf.analyses.string_definition.preprocessing.AbstractPathFinder
+import org.opalj.fpcf.analyses.string_definition.preprocessing.DefaultPathFinder
+import org.opalj.fpcf.analyses.string_definition.preprocessing.PathTransformer
+import org.opalj.fpcf.Result
+import org.opalj.fpcf.analyses.string_definition.interpretation.ExprHandler
 import org.opalj.tac.SimpleTACAIKey
 import org.opalj.tac.Stmt
 
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.ListBuffer
-
 class StringTrackingAnalysisContext(
-    val stmts: Array[Stmt[V]]
+        val stmts: Array[Stmt[V]]
 )
 
 /**
@@ -44,30 +44,46 @@ class LocalStringDefinitionAnalysis(
     def analyze(data: P): PropertyComputationResult = {
         val tacProvider = p.get(SimpleTACAIKey)
         val stmts = tacProvider(data._2).stmts
+        val cfg = tacProvider(data._2).cfg
 
-        val exprHandler = ExprHandler(p, data._2)
         val defSites = data._1.definedBy.toArray.sorted
-        if (ExprHandler.isStringBuilderToStringCall(stmts(defSites.head).asAssignment.expr)) {
-            val subtrees = ArrayBuffer[StringTree]()
-            defSites.foreach { nextDefSite ⇒
-                val treeElements = ExprHandler.getDefSitesOfToStringReceiver(
-                    stmts(nextDefSite).asAssignment.expr
-                ).map { exprHandler.processDefSite }.filter(_.isDefined).map { _.get }
-                if (treeElements.length == 1) {
-                    subtrees.append(treeElements.head)
-                } else {
-                    subtrees.append(StringTreeCond(treeElements.to[ListBuffer]))
-                }
+        val expr = stmts(defSites.head).asAssignment.expr
+        val pathFinder: AbstractPathFinder = new DefaultPathFinder()
+        if (ExprHandler.isStringBuilderToStringCall(expr)) {
+            val initDefSites = NewStringBuilderProcessor.findDefSiteOfInit(
+                expr.asVirtualFunctionCall, stmts
+            )
+            if (initDefSites.isEmpty) {
+                throw new IllegalStateException("did not find any initializations!")
             }
 
-            val finalTree = if (subtrees.size == 1) subtrees.head else
-                StringTreeCond(subtrees.to[ListBuffer])
-            Result(data, StringConstancyProperty(finalTree))
-        } // If not a call to StringBuilder.toString, then we deal with pure strings
+            val paths = pathFinder.findPaths(initDefSites, data._1.definedBy.head, cfg)
+            val leanPaths = paths.makeLeanPath(data._1, stmts)
+            // The following case should only occur if an object is queried that does not occur at
+            // all within the CFG
+            if (leanPaths.isEmpty) {
+                return NoResult
+            }
+
+            val tree = new PathTransformer(cfg).pathToStringTree(leanPaths.get)
+            if (tree.isDefined) {
+                Result(data, StringConstancyProperty(tree.get))
+            } else {
+                NoResult
+            }
+        } // If not a call to String{Builder, Buffer}.toString, then we deal with pure strings
         else {
-            Result(data, StringConstancyProperty(
-                exprHandler.processDefSites(data._1.definedBy.toArray).get
-            ))
+            val paths = pathFinder.findPaths(defSites.toList, data._1.definedBy.head, cfg)
+            if (paths.elements.isEmpty) {
+                NoResult
+            } else {
+                val tree = new PathTransformer(cfg).pathToStringTree(paths)
+                if (tree.isDefined) {
+                    Result(data, StringConstancyProperty(tree.get))
+                } else {
+                    NoResult
+                }
+            }
         }
     }
 
@@ -95,8 +111,8 @@ sealed trait LocalStringDefinitionAnalysisScheduler extends ComputationSpecifica
  * Executor for the lazy analysis.
  */
 object LazyStringDefinitionAnalysis
-        extends LocalStringDefinitionAnalysisScheduler
-        with FPCFLazyAnalysisScheduler {
+    extends LocalStringDefinitionAnalysisScheduler
+    with FPCFLazyAnalysisScheduler {
 
     final override def startLazily(
         p: SomeProject, ps: PropertyStore, unused: Null
