4b07c41ec8c45113838bf1952e380182de71adb8
Michael Eichberg <mail@michael-eichberg.de>
2018-11-27 21:26:53 +0100

    removed useless final modifiers
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: 2bbd0d819e1b81b27a6e8a200edf1b80e8a6d01d

--- OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
@@ -10,11 +10,12 @@ sealed trait PropertyComputationHint
 /**
  * A standard property computation.
  */
-final case object DefaultPropertyComputation extends PropertyComputationHint
+case object DefaultPropertyComputation extends PropertyComputationHint
 
 /**
  * The property computation is extremely cheap. Therefore, the computation can/should be
  * processed in the current thread, it is extremely unlikely that we will gain anything
- * from parallelization.
+ * from parallelization. Here, `cheap` means that the computation requires at most a few
+ * hundred bytecode instructions overall!
  */
-final case object CheapPropertyComputation extends PropertyComputationHint
+case object CheapPropertyComputation extends PropertyComputationHint
