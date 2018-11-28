3b7ff0d859b94c9ee7609325696de555db732814
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-11-28 13:58:11 +0100

    Reformatted and changed a comment.
    Former-commit-id: 6b6d02f124d2451adc41f798608d9c235417b356

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/interpretation/AbstractStringInterpreter.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/interpretation/AbstractStringInterpreter.scala
@@ -15,8 +15,8 @@ import org.opalj.tac.TACStmts
  * @author Patrick Mell
  */
 abstract class AbstractStringInterpreter(
-                                            protected val cfg: CFG[Stmt[V], TACStmts[V]],
-                                            protected val exprHandler: InterpretationHandler,
+        protected val cfg: CFG[Stmt[V], TACStmts[V]],
+        protected val exprHandler: InterpretationHandler,
 ) {
 
     type T <: Any
@@ -26,11 +26,11 @@ abstract class AbstractStringInterpreter(
      * @param instr The instruction that is to be interpreted. It is the responsibility of
      *              implementations to make sure that an instruction is properly and comprehensively
      *              evaluated.
-     * @return The interpreted instruction. An empty list that an instruction was not / could not be
-     *         interpreted (e.g., because it is not supported or it was processed before). A list
-     *         with more than one element indicates an option (only one out of the values is
-     *         possible during runtime of the program); thus, all concats must already happen within
-     *         the interpretation.
+     * @return The interpreted instruction. An empty list indicates that an instruction was not /
+     *         could not be interpreted (e.g., because it is not supported or it was processed
+     *         before). A list with more than one element indicates an option (only one out of the
+     *         values is possible during runtime of the program); thus, some concatenations must
+     *         already happen within the interpretation.
      */
     def interpret(instr: T): List[StringConstancyInformation]
 
