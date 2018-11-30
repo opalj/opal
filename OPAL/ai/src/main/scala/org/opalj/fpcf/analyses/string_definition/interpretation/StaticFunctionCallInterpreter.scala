8decebe3269d05254d1841233f09d0c9974d83e4
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-11-30 20:01:56 +0100

    1) Added support for static function calls. 2) Extended the valueOfAppendCall procedure as it did not always capture all possible values.
    Former-commit-id: b7653ac99d77bc1447336ed7814875ae6f3d1191

---
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/interpretation/StaticFunctionCallInterpreter.scala
@@ -0,0 +1,39 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.fpcf.analyses.string_definition.interpretation
+
+import org.opalj.tac.Stmt
+import org.opalj.tac.TACStmts
+import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.br.cfg.CFG
+import org.opalj.fpcf.string_definition.properties.StringConstancyInformation
+import org.opalj.fpcf.string_definition.properties.StringConstancyLevel
+import org.opalj.tac.StaticFunctionCall
+
+/**
+ * The `StaticFunctionCallInterpreter` is responsible for processing [[StaticFunctionCall]]s.
+ * For supported method calls, see the documentation of the `interpret` function.
+ *
+ * @see [[AbstractStringInterpreter]]
+ *
+ * @author Patrick Mell
+ */
+class StaticFunctionCallInterpreter(
+        cfg:         CFG[Stmt[V], TACStmts[V]],
+        exprHandler: InterpretationHandler
+) extends AbstractStringInterpreter(cfg, exprHandler) {
+
+    override type T = StaticFunctionCall[V]
+
+    /**
+     * Currently, [[StaticFunctionCall]]s are not supported. Thus, this function always returns a
+     * list with a single element consisting of [[StringConstancyLevel.DYNAMIC]] and
+     * [[StringConstancyInformation.UnknownWordSymbol]].
+     *
+     * @see [[AbstractStringInterpreter.interpret]]
+     */
+    override def interpret(instr: T): List[StringConstancyInformation] =
+        List(StringConstancyInformation(
+            StringConstancyLevel.DYNAMIC, StringConstancyInformation.UnknownWordSymbol
+        ))
+
+}
