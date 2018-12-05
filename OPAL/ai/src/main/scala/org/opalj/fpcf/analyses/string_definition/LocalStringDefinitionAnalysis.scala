0ffa8e9c698b0023c5854906732b64851260ff2e
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-12-05 16:12:12 +0100

    Reducing a StringTree now allows to pre-process the tree.
    Former-commit-id: fdc098ef9186303b136e4e5759237f08005b4ac0

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/LocalStringDefinitionAnalysis.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/LocalStringDefinitionAnalysis.scala
@@ -64,7 +64,7 @@ class LocalStringDefinitionAnalysis(
                 val paths = pathFinder.findPaths(initDefSites, cfg)
                 val leanPaths = paths.makeLeanPath(nextUVar, stmts)
                 val tree = new PathTransformer(cfg).pathToStringTree(leanPaths.get)
-                scis.append(tree.simplify().groupRepetitionElements().reduce())
+                scis.append(tree.reduce(true))
             } // If not a call to String{Builder, Buffer}.toString, then we deal with pure strings
             else {
                 val interHandler = InterpretationHandler(cfg)
