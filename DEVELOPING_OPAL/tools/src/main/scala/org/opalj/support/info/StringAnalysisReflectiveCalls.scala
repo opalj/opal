467bab62ee999492029e7a1312f65dd5f278fc36
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-12-14 12:21:14 +0100

    Improved the analysis runner (it captures more relevant calls (now it should capture all relevant calls) and is much faster).
    Former-commit-id: eeffe56b7a9c2bf993cf0d35bf77d603304e7fa7

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/StringAnalysisReflectiveCalls.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/StringAnalysisReflectiveCalls.scala
@@ -7,20 +7,28 @@ import org.opalj.br.analyses.BasicReport
 import org.opalj.br.analyses.DefaultOneStepAnalysis
 import org.opalj.br.analyses.Project
 import org.opalj.br.analyses.ReportableAnalysisResult
-import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.br.instructions.Instruction
+import org.opalj.br.instructions.INVOKESTATIC
+import org.opalj.br.MethodDescriptor
+import org.opalj.br.ReferenceType
+import org.opalj.br.instructions.INVOKEVIRTUAL
+import org.opalj.br.Method
 import org.opalj.fpcf.FPCFAnalysesManagerKey
 import org.opalj.fpcf.analyses.string_definition.LazyStringDefinitionAnalysis
 import org.opalj.fpcf.string_definition.properties.StringConstancyInformation
-import org.opalj.fpcf.FinalEP
-import org.opalj.fpcf.properties.StringConstancyProperty
 import org.opalj.fpcf.PropertyStore
 import org.opalj.fpcf.PropertyStoreKey
+import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.fpcf.FinalEP
+import org.opalj.fpcf.properties.StringConstancyProperty
 import org.opalj.tac.Assignment
+import org.opalj.tac.Call
 import org.opalj.tac.ExprStmt
 import org.opalj.tac.SimpleTACAIKey
 import org.opalj.tac.StaticFunctionCall
 import org.opalj.tac.VirtualFunctionCall
 
+import scala.annotation.switch
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
@@ -30,17 +38,28 @@ import scala.collection.mutable.ListBuffer
  * <p>
  * Currently, this runner supports / handles the following reflective calls:
  * <ul>
- *     <li>`Class.forName(string)`</li>
- *     <li>`Class.forName(string, boolean, classLoader)`</li>
- *     <li>`Class.getField(string)`</li>
- *     <li>`Class.getDeclaredField(string)`</li>
- *     <li>`Class.getMethod(String, Class[])`</li>
- *     <li>`Class.getDeclaredMethod(String, Class[])`</li>
+ * <li>`Class.forName(string)`</li>
+ * <li>`Class.forName(string, boolean, classLoader)`</li>
+ * <li>`Class.getField(string)`</li>
+ * <li>`Class.getDeclaredField(string)`</li>
+ * <li>`Class.getMethod(String, Class[])`</li>
+ * <li>`Class.getDeclaredMethod(String, Class[])`</li>
  * </ul>
  *
  * @author Patrick Mell
  */
 object StringAnalysisReflectiveCalls extends DefaultOneStepAnalysis {
+
+    private type ResultMapType = mutable.Map[String, ListBuffer[StringConstancyInformation]]
+
+    /**
+     * Stores all relevant method names of the Java Reflection API, i.e., those methods from the
+     * Reflection API that have at least one string argument and shall be considered by this
+     * analysis.
+     */
+    private val relevantMethodNames = List(
+        "forName", "getField", "getDeclaredField", "getMethod", "getDeclaredMethod"
+    )
 
     override def title: String = "String Analysis for Reflective Calls"
 
@@ -49,60 +68,95 @@ object StringAnalysisReflectiveCalls extends DefaultOneStepAnalysis {
             "string values"
     }
 
+    /**
+     * Taking the `declaringClass`, the `methodName` as well as the `methodDescriptor` into
+     * consideration, this function checks whether a method is relevant for this analysis.
+     *
+     * @note Internally, this method makes use of [[relevantMethodNames]]. A method can only be
+     *       relevant if its name occurs in [[relevantMethodNames]].
+     */
+    private def isRelevantMethod(
+        declaringClass: ReferenceType, methodName: String, methodDescriptor: MethodDescriptor
+    ): Boolean =
+        relevantMethodNames.contains(methodName) && (declaringClass.toJava == "java.lang.Class" ||
+            methodDescriptor.returnType.toJava.contains("java.lang.reflect."))
+
+    /**
+     * Helper function that checks whether an array of [[Instruction]]s contains at least one
+     * relevant method that is to be processed by `doAnalyze`.
+     */
+    private def instructionsContainRelevantMethod(instructions: Array[Instruction]): Boolean = {
+        instructions.filter(_ != null).foldLeft(false) { (previous, nextInstr) ⇒
+            previous || ((nextInstr.opcode: @switch) match {
+                case INVOKESTATIC.opcode ⇒
+                    val INVOKESTATIC(declClass, _, methodName, methodDescr) = nextInstr
+                    isRelevantMethod(declClass, methodName, methodDescr)
+                case INVOKEVIRTUAL.opcode ⇒
+                    val INVOKEVIRTUAL(declClass, methodName, methodDescr) = nextInstr
+                    isRelevantMethod(declClass, methodName, methodDescr)
+                case _ ⇒ false
+            })
+        }
+    }
+
+    /**
+     * This function is a wrapper function for processing a method. It checks whether the given
+     * `method`, is relevant at all, and if so uses the given function `call` to call the
+     * analysis using the property store, `ps`, to finally store it in the given `resultMap`.
+     */
+    private def processFunctionCall(
+        ps: PropertyStore, method: Method, call: Call[V], resultMap: ResultMapType
+    ): Unit = {
+        if (isRelevantMethod(call.declaringClass, call.name, call.descriptor)) {
+            val duvar = call.params.head.asVar
+            ps((List(duvar), method), StringConstancyProperty.key) match {
+                case FinalEP(_, prop) ⇒
+                    resultMap(call.name).appendAll(prop.stringConstancyInformation)
+                case _ ⇒
+            }
+        }
+    }
+
     override def doAnalyze(
         project: Project[URL], parameters: Seq[String], isInterrupted: () ⇒ Boolean
     ): ReportableAnalysisResult = {
         implicit val propertyStore: PropertyStore = project.get(PropertyStoreKey)
         project.get(FPCFAnalysesManagerKey).runAll(LazyStringDefinitionAnalysis)
+        val tacProvider = project.get(SimpleTACAIKey)
 
         // Stores the obtained results for each supported reflective operation
-        val countMap = mutable.Map[String, ListBuffer[StringConstancyInformation]](
-            "forName" → ListBuffer(),
-            "getField" → ListBuffer(),
-            "getDeclaredField" → ListBuffer(),
-            "getMethod" → ListBuffer(),
-            "getDeclaredMethod" → ListBuffer()
-        )
+        val resultMap: ResultMapType = mutable.Map[String, ListBuffer[StringConstancyInformation]]()
+        relevantMethodNames.foreach { resultMap(_) = ListBuffer() }
 
-        val tacProvider = project.get(SimpleTACAIKey)
+        identity(propertyStore)
+
         project.allMethodsWithBody.foreach { m ⇒
-            val stmts = tacProvider(m).stmts
-            stmts.foreach {
-                // Capture the Class.forName calls
-                case Assignment(_, _, expr) if expr.isInstanceOf[StaticFunctionCall[V]] ⇒
-                    val sfc = expr.asInstanceOf[StaticFunctionCall[V]]
-                    val fqClassName = sfc.declaringClass.toJava
-                    if (countMap.contains(sfc.name) && fqClassName == "java.lang.Class") {
-                        val duvar = sfc.params.head.asVar
-                        propertyStore((List(duvar), m), StringConstancyProperty.key) match {
-                            case FinalEP(_, prop) ⇒
-                                countMap(sfc.name).appendAll(prop.stringConstancyInformation)
+            // To dramatically reduce the work of the tacProvider, quickly check if a method is
+            // relevant at all
+            if (instructionsContainRelevantMethod(m.body.get.instructions)) {
+                val stmts = tacProvider(m).stmts
+                stmts.foreach { stmt ⇒
+                    // Use the following switch to speed-up the whole process
+                    (stmt.astID: @switch) match {
+                        case Assignment.ASTID ⇒ stmt match {
+                            case Assignment(_, _, c: StaticFunctionCall[V]) ⇒
+                                processFunctionCall(propertyStore, m, c, resultMap)
+                            case Assignment(_, _, c: VirtualFunctionCall[V]) ⇒
+                                processFunctionCall(propertyStore, m, c, resultMap)
                             case _ ⇒
                         }
-                    }
-                // Capture all other reflective calls
-                case ExprStmt(_, expr) ⇒
-                    expr match {
-                        case vfc: VirtualFunctionCall[V] ⇒
-                            // Make sure we really deal with a call from the reflection API
-                            if (countMap.contains(vfc.name) &&
-                                vfc.descriptor.returnType.toJava.contains("java.lang.reflect.")) {
-                                // String argument is always the first one
-                                val duvar = vfc.params.head.asVar
-                                propertyStore((List(duvar), m), StringConstancyProperty.key) match {
-                                    case FinalEP(_, prop) ⇒
-                                        countMap(vfc.name).appendAll(
-                                            prop.stringConstancyInformation
-                                        )
-                                    case _ ⇒
-                                }
-                            }
+                        case ExprStmt.ASTID ⇒ stmt match {
+                            case ExprStmt(_, c: StaticFunctionCall[V]) ⇒
+                                processFunctionCall(propertyStore, m, c, resultMap)
+                            case ExprStmt(_, c: VirtualFunctionCall[V]) ⇒
+                                processFunctionCall(propertyStore, m, c, resultMap)
+                            case _ ⇒
+                        }
                         case _ ⇒
                     }
-                case _ ⇒
+                }
             }
         }
-
         val report = ListBuffer[String]("Results:")
         // TODO: Define what the report shall look like
         BasicReport(report)
