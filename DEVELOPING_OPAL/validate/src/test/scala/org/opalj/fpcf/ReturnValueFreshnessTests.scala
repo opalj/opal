c92548d5c718ee329304f84d75f0771d4ee43df5
Michael Eichberg <mail@michael-eichberg.de>
2018-12-18 09:31:04 +0100

    the test is now using the TACAITransformer
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: db9c61df42f446928205f3dc6bb5e819668be8c7

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/ReturnValueFreshnessTests.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/ReturnValueFreshnessTests.scala
@@ -7,19 +7,21 @@ import org.opalj.fpcf.analyses.LazyFieldLocalityAnalysis
 import org.opalj.fpcf.analyses.LazyVirtualCallAggregatingEscapeAnalysis
 import org.opalj.fpcf.analyses.LazyVirtualReturnValueFreshnessAnalysis
 import org.opalj.fpcf.analyses.escape.LazyInterProceduralEscapeAnalysis
-import org.opalj.tac.fpcf.analyses.LazyL0TACAIAnalysis
+import org.opalj.ai.fpcf.analyses.LazyL0BaseAIResultAnalysis
+import org.opalj.tac.fpcf.analyses.TACAITransformer
 
 /**
  *  Tests if the return value freshness properties specified in the test project (the classes in the
  *  (sub-)package of org.opalj.fpcf.fixture) and the computed ones match. The actual matching is
  *  delegated to PropertyMatchers to facilitate matching arbitrary complex property specifications.
  *
- * @author Florian Kuebler
+ * @author Florian Kübler
  */
 class ReturnValueFreshnessTests extends PropertiesTest {
 
-    val lazyAnalysisSchedulers = Set[FPCFLazyAnalysisScheduler](
-        LazyL0TACAIAnalysis,
+    val lazyAnalysisSchedulers = Set[FPCFLazyLikeAnalysisScheduler](
+        LazyL0BaseAIResultAnalysis,
+        TACAITransformer, // LazyL0TACAIAnalysis,
         LazyInterProceduralEscapeAnalysis,
         LazyVirtualCallAggregatingEscapeAnalysis,
         LazyVirtualReturnValueFreshnessAnalysis,
@@ -27,10 +29,7 @@ class ReturnValueFreshnessTests extends PropertiesTest {
     )
 
     describe("return value freshness analysis is executed") {
-        val as = executeAnalyses(
-            Set(EagerReturnValueFreshnessAnalysis),
-            lazyAnalysisSchedulers
-        )
+        val as = executeAnalyses(Set(EagerReturnValueFreshnessAnalysis), lazyAnalysisSchedulers)
         as.propertyStore.shutdown()
         validateProperties(
             as,
