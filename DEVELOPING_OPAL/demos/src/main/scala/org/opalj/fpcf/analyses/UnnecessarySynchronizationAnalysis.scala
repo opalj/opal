014129ba4a8431695c0e26aaf25f4eb6141950ea
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-12-27 15:47:31 +0100

    fixes #172
    Former-commit-id: 5beb527992762f09cb5da41dd9c94535e6d3998a

--- DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/UnnecessarySynchronizationAnalysis.scala
+++ DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/UnnecessarySynchronizationAnalysis.scala
@@ -3,27 +3,42 @@ package org.opalj
 package fpcf
 package analyses
 
-import org.opalj.br.analyses.Project
 import java.net.URL
 
 import org.opalj.ai.common.DefinitionSitesKey
 import org.opalj.ai.common.SimpleAIKey
 import org.opalj.ai.domain.l2.DefaultPerformInvocationsDomainWithCFGAndDefUse
+import org.opalj.ai.fpcf.analyses.LazyL0BaseAIAnalysis
 import org.opalj.br.Method
-import org.opalj.tac.DefaultTACAIKey
-import org.opalj.br.analyses.DefaultOneStepAnalysis
 import org.opalj.br.analyses.BasicReport
+import org.opalj.br.analyses.DefaultOneStepAnalysis
+import org.opalj.br.analyses.Project
+import org.opalj.fpcf.analyses.cg.LazyCalleesAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredConfiguredNativeMethodsAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredFinalizerAnalysisScheduler
+import org.opalj.fpcf.analyses.cg.TriggeredInstantiatedTypesAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredLoadedClassesAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredRTACallGraphAnalysisScheduler
+import org.opalj.fpcf.analyses.cg.TriggeredSerializationRelatedCallsAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredStaticInitializerAnalysis
+import org.opalj.fpcf.analyses.cg.TriggeredThreadRelatedCallsAnalysis
+import org.opalj.fpcf.analyses.cg.reflection.TriggeredReflectionRelatedCallsAnalysis
 import org.opalj.fpcf.analyses.escape.EagerInterProceduralEscapeAnalysis
+import org.opalj.fpcf.cg.properties.ReflectionRelatedCallees
+import org.opalj.fpcf.cg.properties.SerializationRelatedCallees
+import org.opalj.fpcf.cg.properties.StandardInvokeCallees
 import org.opalj.fpcf.properties.EscapeProperty
 import org.opalj.fpcf.properties.EscapeViaNormalAndAbnormalReturn
 import org.opalj.log.LogContext
-import org.opalj.util.PerformanceEvaluation.time
 import org.opalj.log.OPALLogger.info
-import org.opalj.tac.DVar
-import org.opalj.tac.New
 import org.opalj.tac.Assignment
-import org.opalj.tac.NewArray
+import org.opalj.tac.DVar
 import org.opalj.tac.MonitorEnter
+import org.opalj.tac.New
+import org.opalj.tac.NewArray
+import org.opalj.tac.fpcf.analyses.TACAITransformer
+import org.opalj.tac.fpcf.properties.TACAI
+import org.opalj.util.PerformanceEvaluation.time
 
 /**
  * Finds object references in monitorenter instructions that do not escape their thread.
@@ -53,14 +68,26 @@ object UnnecessarySynchronizationAnalysis extends DefaultOneStepAnalysis {
             project.get(PropertyStoreKey)
         } { t ⇒ info("progress", s"initialization of property store took ${t.toSeconds}") }
 
-        val tacai = time {
-            val tacai = project.get(DefaultTACAIKey)
-            tacai
-        } { t ⇒ info("progress", s"generating 3-address code took ${t.toSeconds}") }
-
         time {
-            EagerInterProceduralEscapeAnalysis.start(project, null)
-            propertyStore.waitOnPhaseCompletion()
+            val manager = project.get(FPCFAnalysesManagerKey)
+            manager.runAll(
+                TriggeredRTACallGraphAnalysisScheduler,
+                TriggeredStaticInitializerAnalysis,
+                TriggeredLoadedClassesAnalysis,
+                TriggeredFinalizerAnalysisScheduler,
+                TriggeredThreadRelatedCallsAnalysis,
+                TriggeredSerializationRelatedCallsAnalysis,
+                TriggeredReflectionRelatedCallsAnalysis,
+                TriggeredInstantiatedTypesAnalysis,
+                TriggeredConfiguredNativeMethodsAnalysis,
+                TriggeredSystemPropertiesAnalysis,
+                new LazyCalleesAnalysis(
+                    Set(StandardInvokeCallees, SerializationRelatedCallees, ReflectionRelatedCallees)
+                ),
+                LazyL0BaseAIAnalysis,
+                TACAITransformer,
+                EagerInterProceduralEscapeAnalysis
+            )
         } { t ⇒ info("progress", s"escape analysis took ${t.toSeconds}") }
 
         val allocationSites = project.get(DefinitionSitesKey).getAllocationSites
@@ -70,7 +97,8 @@ object UnnecessarySynchronizationAnalysis extends DefaultOneStepAnalysis {
                 method = as.method
                 FinalP(escape) = propertyStore(as, EscapeProperty.key)
                 if EscapeViaNormalAndAbnormalReturn lessOrEqualRestrictive escape
-                code = tacai(method).stmts
+                FinalP(tacai) = propertyStore(as.method, TACAI.key)
+                code = tacai.tac.get.stmts
                 defSite = code indexWhere (stmt ⇒ stmt.pc == as.pc)
                 if defSite != -1
                 stmt = code(defSite)
