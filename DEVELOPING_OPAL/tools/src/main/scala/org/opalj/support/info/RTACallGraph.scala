1821642dc7bebbea28e4fbea4a4db526f21b8bd0
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2019-01-03 16:12:30 +0100

    rta runner can also print callees/callers
    Former-commit-id: e10afbb07a0342ef3a42b81eb6597cf31d1ac4b8

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/RTACallGraph.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/RTACallGraph.scala
@@ -10,7 +10,11 @@ import org.opalj.br.analyses.BasicReport
 import org.opalj.br.analyses.DeclaredMethodsKey
 import org.opalj.br.analyses.DefaultOneStepAnalysis
 import org.opalj.br.analyses.Project
+import org.opalj.collection.immutable.Chain
 import org.opalj.fpcf.FPCFAnalysesManagerKey
+import org.opalj.fpcf.ComputationSpecification
+import org.opalj.fpcf.FPCFAnalysis
+import org.opalj.fpcf.PropertyStoreKey
 import org.opalj.fpcf.analyses.TriggeredSystemPropertiesAnalysis
 import org.opalj.fpcf.analyses.cg.EagerLibraryEntryPointsAnalysis
 import org.opalj.fpcf.analyses.cg.LazyCalleesAnalysis
@@ -28,10 +32,14 @@ import org.opalj.fpcf.cg.properties.NoCallers
 import org.opalj.fpcf.cg.properties.ReflectionRelatedCallees
 import org.opalj.fpcf.cg.properties.SerializationRelatedCallees
 import org.opalj.fpcf.cg.properties.StandardInvokeCallees
+import org.opalj.fpcf.cg.properties.Callees
 import org.opalj.tac.fpcf.analyses.TACAITransformer
 
 /**
  * Computes a RTA based call graph and reports its size.
+ * Furthermore, it can be used to print the callees or callers of specific methods.
+ * To do so, add -callers=m, where m is the method name/signature using Java notation, as parameter
+ * (for callees use -callees=m).
  *
  * @author Florian Kuebler
  */
@@ -43,36 +51,57 @@ object RTACallGraph extends DefaultOneStepAnalysis {
         "Provides the number of reachable methods and call edges in the give project."
     }
 
+    override def analysisSpecificParametersDescription: String = {
+        "[-callers=method]"+"[-callees=method]"
+    }
+
+    override def checkAnalysisSpecificParameters(parameters: Seq[String]): Traversable[String] = {
+        val remainingParameters =
+            parameters.filter { p ⇒
+                !p.startsWith("-callers=") && !p.startsWith("-callees=")
+            }
+        super.checkAnalysisSpecificParameters(remainingParameters)
+    }
+
     // todo: we would like to print the edges for a given method
     override def doAnalyze(
         project:       Project[URL],
         parameters:    Seq[String],
         isInterrupted: () ⇒ Boolean
     ): BasicReport = {
+        implicit val declaredMethods = project.get(DeclaredMethodsKey)
+        val allMethods = declaredMethods.declaredMethods.toTraversable
 
-        val (ps, _ /*executed analyses*/ ) = project.get(FPCFAnalysesManagerKey).runAll(
-            LazyL0BaseAIAnalysis,
-            TACAITransformer,
-            /* Call Graph Analyses */
-            RTACallGraphAnalysisScheduler,
-            TriggeredStaticInitializerAnalysis,
-            TriggeredLoadedClassesAnalysis,
-            TriggeredFinalizerAnalysisScheduler,
-            TriggeredThreadRelatedCallsAnalysis,
-            TriggeredSerializationRelatedCallsAnalysis,
-            TriggeredReflectionRelatedCallsAnalysis,
-            TriggeredInstantiatedTypesAnalysis,
-            TriggeredConfiguredNativeMethodsAnalysis,
-            TriggeredSystemPropertiesAnalysis,
-            EagerLibraryEntryPointsAnalysis,
-            LazyCalleesAnalysis(
-                Set(StandardInvokeCallees, SerializationRelatedCallees, ReflectionRelatedCallees)
-            )
+        implicit val ps = project.get(PropertyStoreKey)
+
+        val calleesAnalysis = LazyCalleesAnalysis(
+            Set(StandardInvokeCallees, SerializationRelatedCallees, ReflectionRelatedCallees)
         )
 
-        implicit val declaredMethods = project.get(DeclaredMethodsKey)
-
-        val allMethods = declaredMethods.declaredMethods.toTraversable
+        project.get(FPCFAnalysesManagerKey).runAll(
+            List(
+                LazyL0BaseAIAnalysis,
+                TACAITransformer,
+                /* Call Graph Analyses */
+                RTACallGraphAnalysisScheduler,
+                TriggeredStaticInitializerAnalysis,
+                TriggeredLoadedClassesAnalysis,
+                TriggeredFinalizerAnalysisScheduler,
+                TriggeredThreadRelatedCallsAnalysis,
+                TriggeredSerializationRelatedCallsAnalysis,
+                TriggeredReflectionRelatedCallsAnalysis,
+                TriggeredInstantiatedTypesAnalysis,
+                TriggeredConfiguredNativeMethodsAnalysis,
+                TriggeredSystemPropertiesAnalysis,
+                EagerLibraryEntryPointsAnalysis,
+                calleesAnalysis
+            ),
+            { css: Chain[ComputationSpecification[FPCFAnalysis]] ⇒
+                if (css.contains(calleesAnalysis)) {
+                    allMethods.foreach { dm ⇒ ps.force(dm, fpcf.cg.properties.Callees.key) }
+                }
+            }
+        )
 
         val callersProperties = ps(allMethods, CallersProperty.key)
         assert(callersProperties.forall(_.isFinal))
@@ -81,6 +110,40 @@ object RTACallGraph extends DefaultOneStepAnalysis {
 
         val numEdges = reachableMethods.foldLeft(0) { (accEdges, callersProperty) ⇒
             callersProperty.callers.size + accEdges
+        }
+
+        var calleesSigs: List[String] = Nil
+        var callersSigs: List[String] = Nil
+
+        val callersRegex = "-callers=(.*)".r
+        val calleesRegex = "-callees=(.*)".r
+        parameters.foreach {
+            case callersRegex(methodSig) ⇒ callersSigs ::= methodSig
+            case calleesRegex(methodSig) ⇒ calleesSigs ::= methodSig
+        }
+
+        println(calleesSigs.mkString("\n"))
+        println(callersSigs.mkString("\n"))
+
+        for (m ← allMethods) {
+            val mSig = m.descriptor.toJava(m.name)
+
+            for (methodSignature ← calleesSigs) {
+                if (mSig.contains(methodSignature)) {
+                    println(s"Callees of ${m.toJava}:")
+                    println(ps(m, Callees.key).ub.callSites().map {
+                        case (pc, callees) ⇒ pc → callees.map(_.toJava).mkString(", ")
+                    }.mkString("\t", "\n\t", "\n"))
+                }
+            }
+            for (methodSignature ← callersSigs) {
+                if (mSig.contains(methodSignature)) {
+                    println(s"Callers of ${m.toJava}:")
+                    println(ps(m, CallersProperty.key).ub.callers.map {
+                        case (caller, pc) ⇒ caller.toJava → pc
+                    }.mkString("\t", "\n\t", "\n"))
+                }
+            }
         }
 
         val message =
