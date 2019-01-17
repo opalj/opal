0ba471844ce18c52fc77d07b83d9c6d6ea35e237
Patrick <patrick.mell@stud.tu-darmstadt.de>
2019-01-17 17:28:14 +0100

    Started with the new algorithm.
    Former-commit-id: d021cbca2f7dc7216bed05deb14ed3ad526c4b78

--- OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/preprocessing/DefaultPathFinder.scala
+++ OPAL/ai/src/main/scala/org/opalj/fpcf/analyses/string_definition/preprocessing/DefaultPathFinder.scala
@@ -4,11 +4,12 @@ package org.opalj.fpcf.analyses.string_definition.preprocessing
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-import org.opalj.collection.mutable.IntArrayStack
 import org.opalj.fpcf.analyses.string_definition.V
-import org.opalj.br.cfg.CatchNode
+import org.opalj.br.cfg.BasicBlock
 import org.opalj.br.cfg.CFG
-import org.opalj.br.cfg.ExitNode
+import org.opalj.br.cfg.CFGNode
+import org.opalj.tac.Goto
+import org.opalj.tac.If
 import org.opalj.tac.Stmt
 import org.opalj.tac.TACStmts
 
@@ -20,6 +21,180 @@ import org.opalj.tac.TACStmts
 class DefaultPathFinder extends AbstractPathFinder {
 
     /**
+     * CSInfo stores information regarding control structures (CS) in the form: Index of the start
+     * statement of that CS, index of the end statement of that CS and the type.
+     */
+    private type CSInfo = (Int, Int, NestedPathType.Value)
+    private type CFGType = CFG[Stmt[V], TACStmts[V]]
+
+    private def determineTypeOfIf(cfg: CFGType, stmtIndex: Int): NestedPathType.Value = {
+        // Is the first condition enough to identify loops?
+        if (isHeadOfLoop(stmtIndex, cfg.findNaturalLoops(), cfg)) {
+            NestedPathType.Repetition
+        } else if (isCondWithoutElse(stmtIndex, cfg)) {
+            NestedPathType.CondWithoutAlternative
+        } else {
+            NestedPathType.CondWithAlternative
+        }
+    }
+
+    private def getStartAndEndIndexOfLoop(headIndex: Int, cfg: CFGType): (Int, Int) = {
+        var startIndex = -1
+        var endIndex = -1
+        val relevantLoop = cfg.findNaturalLoops().filter(_ ⇒
+            isHeadOfLoop(headIndex, cfg.findNaturalLoops(), cfg))
+        if (relevantLoop.nonEmpty) {
+            startIndex = relevantLoop.head.head
+            endIndex = relevantLoop.head.last
+        }
+        (startIndex, endIndex)
+    }
+
+    private def getStartAndEndIndexOfCondWithAlternative(
+        branchingSite: Int, cfg: CFGType, processedIfs: mutable.Map[Int, Unit.type]
+    ): (Int, Int) = {
+        processedIfs(branchingSite) = Unit
+
+        var endSite = -1
+        val stack = mutable.Stack[Int](branchingSite)
+        while (stack.nonEmpty) {
+            val popped = stack.pop()
+            val nextBlock = cfg.bb(popped).successors.map {
+                case bb: BasicBlock ⇒ bb.startPC
+                // Handle Catch Nodes?
+                case _              ⇒ -1
+            }.max
+            var containsIf = false
+            for (i ← cfg.bb(nextBlock).startPC.to(cfg.bb(nextBlock).endPC)) {
+                if (cfg.code.instructions(i).isInstanceOf[If[V]]) {
+                    processedIfs(i) = Unit
+                    containsIf = true
+                }
+            }
+
+            if (containsIf) {
+                stack.push(nextBlock)
+            } else {
+                // Find the goto that points after the "else" part (the assumption is that this
+                // goto is the very last element of the current branch
+                endSite = cfg.code.instructions(nextBlock - 1).asGoto.targetStmt - 1
+            }
+        }
+
+        (branchingSite, endSite)
+    }
+
+    private def getStartAndEndIndexOfCondWithoutAlternative(
+        branchingSite: Int, cfg: CFGType, processedIfs: mutable.Map[Int, Unit.type]
+    ): (Int, Int) = {
+        // Find the index of very last element in the if block (here: The goto element; is it always
+        // present?)
+        val ifTarget = cfg.code.instructions(branchingSite).asInstanceOf[If[V]].targetStmt
+        var endIndex = ifTarget
+        do {
+            endIndex -= 1
+        } while (cfg.code.instructions(branchingSite).isInstanceOf[Goto])
+
+        // It is now necessary to collect all ifs that belong to the whole if condition in the
+        // high-level construct
+        cfg.bb(ifTarget).predecessors.foreach {
+            case pred: BasicBlock ⇒
+                for (i ← pred.startPC.to(pred.endPC)) {
+                    if (cfg.code.instructions(i).isInstanceOf[If[V]]) {
+                        processedIfs(i) = Unit
+                    }
+                }
+            // How about CatchNodes?
+            case cn ⇒ println(cn)
+        }
+
+        (branchingSite, endIndex)
+    }
+
+    private def getTryCatchFinallyInfo(cfg: CFGType): List[CSInfo] = {
+        // Stores the startPC as key and the index of the end of a catch (or finally if it is
+        // present); a map is used for faster accesses
+        val tryInfo = mutable.Map[Int, Int]()
+
+        cfg.catchNodes.foreach { cn ⇒
+            if (!tryInfo.contains(cn.startPC)) {
+                val cnWithSameStartPC = cfg.catchNodes.filter(_.startPC == cn.startPC)
+                // If there is only one CatchNode for a startPC, i.e., no finally, no other catches,
+                // the end index can be directly derived from the successors
+                if (cnWithSameStartPC.tail.isEmpty) {
+                    tryInfo(cn.startPC) = cfg.bb(cn.endPC).successors.map {
+                        case bb: BasicBlock ⇒ bb.startPC - 1
+                        case _              ⇒ -1
+                    }.max
+                } // Otherwise, the largest handlerPC marks the end index
+                else {
+                    tryInfo(cn.startPC) = cnWithSameStartPC.map(_.handlerPC).max
+                }
+            }
+        }
+
+        tryInfo.map {
+            case (key, value) ⇒ (key, value, NestedPathType.TryCatchFinally)
+        }.toList
+    }
+
+    private def processBasicBlock(
+        cfg: CFGType, stmt: Int, processedIfs: mutable.Map[Int, Unit.type]
+    ): CSInfo = {
+        val csType = determineTypeOfIf(cfg, stmt)
+        val (startIndex, endIndex) = csType match {
+            case NestedPathType.Repetition ⇒
+                processedIfs(stmt) = Unit
+                getStartAndEndIndexOfLoop(stmt, cfg)
+            case NestedPathType.CondWithoutAlternative ⇒
+                getStartAndEndIndexOfCondWithoutAlternative(stmt, cfg, processedIfs)
+            // _ covers CondWithAlternative and TryCatchFinally, however, the latter one should
+            // never be present as the element referring to stmts is / should be an If
+            case _ ⇒
+                getStartAndEndIndexOfCondWithAlternative(stmt, cfg, processedIfs)
+        }
+        (startIndex, endIndex, csType)
+    }
+
+    private def findControlStructures(cfg: CFGType): List[CSInfo] = {
+        // foundCS stores all found control structures as a triple in the form (start, end, type)
+        val foundCS = ListBuffer[CSInfo]()
+        // For a fast loop-up which if statements have already been processed
+        val processedIfs = mutable.Map[Int, Unit.type]()
+        val startBlock = cfg.startBlock
+        val stack = mutable.Stack[CFGNode](startBlock)
+        val seenCFGNodes = mutable.Map[CFGNode, Unit.type]()
+        seenCFGNodes(startBlock) = Unit
+
+        while (stack.nonEmpty) {
+            val next = stack.pop()
+            seenCFGNodes(next) = Unit
+
+            next match {
+                case bb: BasicBlock ⇒
+                    for (i ← bb.startPC.to(bb.endPC)) {
+                        if (cfg.code.instructions(i).isInstanceOf[If[V]] &&
+                            !processedIfs.contains(i)) {
+                            foundCS.append(processBasicBlock(cfg, i, processedIfs))
+                            processedIfs(i) = Unit
+                        }
+                    }
+                case cn: CFGNode ⇒
+                    println(cn)
+                case _ ⇒
+            }
+
+            // Add unseen successors
+            next.successors.filter(!seenCFGNodes.contains(_)).foreach(stack.push)
+        }
+
+        // Add try-catch information, sort everything in ascending order in terms of the startPC and
+        // return
+        foundCS.appendAll(getTryCatchFinallyInfo(cfg))
+        foundCS.sortBy { case (start, _, _) ⇒ start }.toList
+    }
+
+    /**
      * This implementation finds all paths based on an a naive / intuitive traversing of the `cfg`
      * and, based on that, determines in what relation a statement / instruction is with its
      * predecessors / successors.
@@ -29,217 +204,13 @@ class DefaultPathFinder extends AbstractPathFinder {
      *
      * @see [[AbstractPathFinder.findPaths]]
      */
-    override def findPaths(
-        startSites: List[Int], endSite: Int, cfg: CFG[Stmt[V], TACStmts[V]]
-    ): Path = {
-        // path will accumulate all paths
-        val path = ListBuffer[SubPath]()
-        var stack = IntArrayStack.fromSeq(startSites.reverse)
-        val seenElements = ListBuffer[Int]()
-        // For storing the node IDs of all seen catch nodes (they are to be used only once, thus
-        // this store)
-        val seenCatchNodes = mutable.Map[Int, Unit.type]()
-        // numSplits serves a queue that stores the number of possible branches (or successors)
-        val numSplits = ListBuffer[Int]()
-        // Also a queue that stores the indices of which branch of a conditional to take next
-        val currSplitIndex = ListBuffer[Int]()
-        val numBackedgesLoop = ListBuffer[Int]()
-        val backedgeLoopCounter = ListBuffer[Int]()
-        // Used to quickly find the element at which to insert a sub path
-        val nestedElementsRef = ListBuffer[NestedPathElement]()
-        val natLoops = cfg.findNaturalLoops()
+    override def findPaths(startSites: List[Int], endSite: Int, cfg: CFGType): Path = {
+        val startPC = cfg.startBlock.startPC
+        identity(startPC)
+        val csInfo = findControlStructures(cfg)
+        identity(csInfo)
 
-        // Multiple start sites => We start within a conditional => Prepare for that
-        if (startSites.size > 1) {
-            val outerNested =
-                generateNestPathElement(startSites.size, NestedPathType.CondWithAlternative)
-            numSplits.append(startSites.size)
-            currSplitIndex.append(0)
-            outerNested.element.reverse.foreach { next ⇒
-                nestedElementsRef.prepend(next.asInstanceOf[NestedPathElement])
-            }
-            nestedElementsRef.append(outerNested)
-            path.append(outerNested)
-        } else {
-            // Is the definition site within a try? If so, we can ignore that try block as this
-            // scope cannot be left anyway
-            val defSite = startSites.head
-            cfg.bb(defSite).successors.filter(_.isInstanceOf[CatchNode]).foreach {
-                case cn: CatchNode if cn.startPC <= defSite ⇒
-                    seenCatchNodes(cn.nodeId) = Unit
-                    seenElements.append(cn.handlerPC)
-                case _ ⇒
-            }
-        }
-
-        while (stack.nonEmpty) {
-            val popped = stack.pop()
-            val bb = cfg.bb(popped)
-            val isLoopHeader = isHeadOfLoop(popped, natLoops, cfg)
-            var isLoopEnding = false
-            var loopEndingIndex = -1
-            var belongsToLoopEnding = false
-            var belongsToLoopHeader = false
-            // In some cases, this element will be used later on to append nested path elements of
-            // deeply-nested structures
-            var refToAppend: Option[NestedPathElement] = None
-
-            // Append everything of the current basic block to the path
-            for (i ← bb.startPC.to(bb.endPC)) {
-                seenElements.append(i)
-                val toAppend = FlatPathElement(i)
-
-                if (!isLoopEnding) {
-                    isLoopEnding = isEndOfLoop(cfg.bb(i).endPC, natLoops)
-                }
-
-                // For loop headers, insert a new nested element (and thus, do the housekeeping)
-                if (!belongsToLoopHeader && isHeadOfLoop(i, natLoops, cfg)) {
-                    numSplits.prepend(1)
-                    currSplitIndex.prepend(0)
-                    numBackedgesLoop.prepend(bb.predecessors.size - 1)
-                    backedgeLoopCounter.prepend(0)
-
-                    val outer = generateNestPathElement(0, NestedPathType.Repetition)
-                    outer.element.append(toAppend)
-                    nestedElementsRef.prepend(outer)
-                    path.append(outer)
-
-                    belongsToLoopHeader = true
-                } // For loop ending, find the top-most loop from the stack and add to that element
-                else if (isLoopEnding) {
-                    val loopElement = nestedElementsRef.find {
-                        _.elementType match {
-                            case Some(et) ⇒ et == NestedPathType.Repetition
-                            case _        ⇒ false
-                        }
-                    }
-                    if (loopElement.isDefined) {
-                        if (!belongsToLoopEnding) {
-                            backedgeLoopCounter(0) += 1
-                            if (backedgeLoopCounter.head == numBackedgesLoop.head) {
-                                loopEndingIndex = nestedElementsRef.indexOf(loopElement.get)
-                            }
-                        }
-                        loopElement.get.element.append(toAppend)
-                    }
-                    belongsToLoopEnding = true
-                } // The instructions belonging to a loop header are stored in a flat structure
-                else if (!belongsToLoopHeader && (numSplits.isEmpty || bb.predecessors.size > 1)) {
-                    path.append(toAppend)
-                } // Within a nested structure => append to an inner element
-                else {
-                    // For loops
-                    refToAppend = Some(nestedElementsRef(currSplitIndex.head))
-                    // Refine for conditionals and try-catch(-finally)
-                    refToAppend.get.elementType match {
-                        case Some(t) if t == NestedPathType.CondWithAlternative ||
-                            t == NestedPathType.TryCatchFinally ⇒
-                            refToAppend = Some(refToAppend.get.element(
-                                currSplitIndex.head
-                            ).asInstanceOf[NestedPathElement])
-                        case _ ⇒
-                    }
-                    refToAppend.get.element.append(toAppend)
-                }
-            }
-
-            val successors = bb.successors.filter {
-                case _: ExitNode ⇒ false
-                case _           ⇒ true
-            }.map {
-                case cn: CatchNode ⇒ cn.handlerPC
-                case s             ⇒ s.nodeId
-            }.toList.sorted
-            val catchSuccessors = bb.successors.filter { s ⇒
-                s.isInstanceOf[CatchNode] && !seenCatchNodes.contains(s.nodeId)
-            }
-            val successorsToAdd = successors.filter { next ⇒
-                !seenElements.contains(next) && !stack.contains(next)
-            }
-            val hasSeenSuccessor = successors.foldLeft(false) {
-                (old: Boolean, next: Int) ⇒ old || seenElements.contains(next)
-            }
-
-            // Clean a loop from the stacks if the end of a loop was reached
-            if (loopEndingIndex != -1) {
-                // For finding the corresponding element ref, we can use the loopEndingIndex;
-                // however, for numSplits and currSplitIndex we need to find the correct position in
-                // the array first; we do this by finding the first element with only one branch
-                // which will correspond to the loop
-                val deletePos = numSplits.indexOf(1)
-                numSplits.remove(deletePos)
-                currSplitIndex.remove(deletePos)
-                nestedElementsRef.remove(loopEndingIndex)
-                numBackedgesLoop.remove(0)
-                backedgeLoopCounter.remove(0)
-            } // For join points of branchings, do some housekeeping (explicitly excluding loops)
-            else if (currSplitIndex.nonEmpty &&
-                (hasSuccessorWithAtLeastNPredecessors(bb) || hasSeenSuccessor)) {
-                if (nestedElementsRef.head.elementType.getOrElse(NestedPathType.TryCatchFinally) !=
-                    NestedPathType.Repetition) {
-                    currSplitIndex(0) += 1
-                    if (currSplitIndex.head == numSplits.head) {
-                        numSplits.remove(0)
-                        currSplitIndex.remove(0)
-                        nestedElementsRef.remove(0)
-                    }
-                }
-            }
-
-            if ((numSplits.nonEmpty || backedgeLoopCounter.nonEmpty) && (bb.predecessors.size == 1)) {
-                // Within a conditional, prepend in order to keep the correct order
-                val newStack = IntArrayStack.fromSeq(stack.reverse)
-                newStack.push(IntArrayStack.fromSeq(successorsToAdd.reverse))
-                stack = newStack
-            } else {
-                // Otherwise, append (also retain the correct order)
-                val newStack = IntArrayStack.fromSeq(successorsToAdd.reverse)
-                newStack.push(IntArrayStack.fromSeq(stack.reverse))
-                stack = newStack
-            }
-            // On a split point, prepare the next (nested) element (however, not for loop headers),
-            // this includes if a node has a catch node as successor
-            if ((successorsToAdd.length > 1 && !isLoopHeader) || catchSuccessors.nonEmpty) {
-                seenCatchNodes ++= catchSuccessors.map(n ⇒ (n.nodeId, Unit))
-                // Simply append to path if no nested structure is available, otherwise append to
-                // the correct nested path element
-                val appendSite = if (numSplits.isEmpty) path
-                else if (refToAppend.isDefined) refToAppend.get.element
-                else nestedElementsRef(currSplitIndex.head).element
-                var relevantNumSuccessors = successors.size
-
-                var ifWithElse = true
-                if (isCondWithoutElse(popped, cfg)) {
-                    // If there are catch node successors, the number of relevant successor equals
-                    // the number of successors (because catch node are excluded here)
-                    if (catchSuccessors.isEmpty) {
-                        relevantNumSuccessors -= 1
-                    }
-                    ifWithElse = false
-                }
-
-                val outerNestedType = if (catchSuccessors.nonEmpty) NestedPathType.TryCatchFinally
-                else if (ifWithElse) NestedPathType.CondWithAlternative
-                else NestedPathType.CondWithoutAlternative
-                val outerNested = generateNestPathElement(relevantNumSuccessors, outerNestedType)
-
-                numSplits.prepend(relevantNumSuccessors)
-                currSplitIndex.prepend(0)
-                outerNested.element.reverse.foreach { next ⇒
-                    nestedElementsRef.prepend(next.asInstanceOf[NestedPathElement])
-                }
-                appendSite.append(outerNested)
-            }
-
-            // We can stop once endSite was processed and there is no more path to endSite
-            if (popped == endSite &&
-                !stack.map(doesPathExistTo(_, endSite, cfg, seenElements.toList)).reduce(_ || _)) {
-                return Path(path.toList)
-            }
-        }
-
-        Path(path.toList)
+        Path(List(FlatPathElement(0), FlatPathElement(1)))
     }
 
 }
