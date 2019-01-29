06eb30fd9ad6f3826e63399ccb566ebbad58d78b
Patrick <patrick.mell@stud.tu-darmstadt.de>
2019-01-29 20:28:17 +0100

    Added the interprocedural counterpart.
    Former-commit-id: 34a961f6e651671c5ecdaf71884670b8c17326aa

---
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string_analysis/interpretation/InterproceduralGetStaticInterpreter.scala
@@ -0,0 +1,37 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.tac.fpcf.analyses.string_analysis.interpretation
+
+import org.opalj.fpcf.ProperPropertyComputationResult
+import org.opalj.fpcf.Result
+import org.opalj.br.cfg.CFG
+import org.opalj.br.fpcf.properties.StringConstancyProperty
+import org.opalj.tac.GetStatic
+import org.opalj.tac.Stmt
+import org.opalj.tac.TACStmts
+import org.opalj.tac.fpcf.analyses.string_analysis.V
+
+/**
+ * The `InterproceduralGetStaticInterpreter` is responsible for processing
+ * [[org.opalj.tac.GetStatic]]s in an interprocedural fashion.
+ *
+ * @see [[AbstractStringInterpreter]]
+ *
+ * @author Patrick Mell
+ */
+class InterproceduralGetStaticInterpreter(
+        cfg:         CFG[Stmt[V], TACStmts[V]],
+        exprHandler: IntraproceduralInterpretationHandler
+) extends AbstractStringInterpreter(cfg, exprHandler) {
+
+    override type T = GetStatic
+
+    /**
+     * Currently, this type is not interpreted. Thus, this function always returns a result
+     * containing [[StringConstancyProperty.lowerBound]].
+     *
+     * @see [[AbstractStringInterpreter.interpret]]
+     */
+    override def interpret(instr: T): ProperPropertyComputationResult =
+        Result(instr, StringConstancyProperty.lowerBound)
+
+}
