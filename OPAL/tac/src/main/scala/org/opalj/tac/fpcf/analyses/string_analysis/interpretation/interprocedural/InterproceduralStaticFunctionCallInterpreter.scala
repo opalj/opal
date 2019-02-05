bde585bd3a2cb7dd0726c76c305dcdc35a2c1930
Patrick <patrick.mell@stud.tu-darmstadt.de>
2019-02-05 09:13:55 +0100

    Refined the "getDeclaredMethod" to include the declaring class in the search for a method as well.
    Former-commit-id: d2870c6f30fe387f0d0010146571282298890937

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string_analysis/interpretation/interprocedural/InterproceduralStaticFunctionCallInterpreter.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string_analysis/interpretation/interprocedural/InterproceduralStaticFunctionCallInterpreter.scala
@@ -53,7 +53,7 @@ class InterproceduralStaticFunctionCallInterpreter(
      * @see [[AbstractStringInterpreter.interpret]]
      */
     override def interpret(instr: T, defSite: Int): ProperPropertyComputationResult = {
-        val methodOption = getDeclaredMethod(declaredMethods, instr.name)
+        val methodOption = getDeclaredMethod(declaredMethods, instr.declaringClass, instr.name)
 
         if (methodOption.isEmpty) {
             val e: Integer = defSite
