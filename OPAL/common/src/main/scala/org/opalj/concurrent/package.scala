4c86c34e31b4154e7330907acb3f038741a09c97
Michael Eichberg <mail@michael-eichberg.de>
2019-04-03 17:04:35 +0200

    removed useless import
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: be402f9818db23312ecc1c575f2d4cea0301750e

--- OPAL/common/src/main/scala/org/opalj/concurrent/package.scala
+++ OPAL/common/src/main/scala/org/opalj/concurrent/package.scala
@@ -6,7 +6,6 @@ import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.ThreadFactory
 
 import scala.collection.parallel.ExecutionContextTaskSupport
 import scala.util.control.ControlThrowable
