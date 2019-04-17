b8ec8ac40cab4dec9d3686d70ea239322877c8ec
Mario Trageser <mario.trageser@cofinpro.de>
2019-04-17 13:15:30 +0200

    TaintAnalysisTests uses the call graph.
    Former-commit-id: b4eb0f1f1120fcb9628bac6f9b7f6421505d4615

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/TaintAnalysisTest.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/TaintAnalysisTest.scala
@@ -7,24 +7,15 @@ import org.opalj.ai.domain.l2
 import org.opalj.ai.fpcf.properties.AIDomainFactoryKey
 import org.opalj.br.analyses.{DeclaredMethodsKey, Project}
 import org.opalj.br.fpcf.FPCFAnalysisScheduler
-import org.opalj.br.fpcf.cg.properties.ReflectionRelatedCallees
-import org.opalj.br.fpcf.cg.properties.SerializationRelatedCallees
 import org.opalj.br.fpcf.cg.properties.StandardInvokeCallees
-import org.opalj.br.fpcf.cg.properties.ThreadRelatedIncompleteCallSites
 import org.opalj.ai.fpcf.analyses.LazyL0BaseAIAnalysis
 import org.opalj.tac.fpcf.analyses.TaintAnalysis
 import org.opalj.tac.fpcf.analyses.NullFact
 import org.opalj.tac.fpcf.analyses.cg.LazyCalleesAnalysis
 import org.opalj.tac.fpcf.analyses.cg.RTACallGraphAnalysisScheduler
-import org.opalj.tac.fpcf.analyses.cg.reflection.TriggeredReflectionRelatedCallsAnalysis
-import org.opalj.tac.fpcf.analyses.cg.TriggeredLoadedClassesAnalysis
-import org.opalj.tac.fpcf.analyses.cg.TriggeredStaticInitializerAnalysis
 import org.opalj.tac.fpcf.analyses.TACAITransformer
-import org.opalj.tac.fpcf.analyses.TriggeredSystemPropertiesAnalysis
 import org.opalj.tac.fpcf.analyses.cg.EagerLibraryEntryPointsAnalysis
-import org.opalj.tac.fpcf.analyses.cg.TriggeredFinalizerAnalysisScheduler
-import org.opalj.tac.fpcf.analyses.cg.TriggeredSerializationRelatedCallsAnalysis
-import org.opalj.tac.fpcf.analyses.cg.TriggeredThreadRelatedCallsAnalysis
+import org.opalj.tac.fpcf.analyses.cg.TriggeredInstantiatedTypesAnalysis
 
 /**
  * @author Mario Trageser
@@ -34,21 +25,10 @@ class TaintAnalysisTest extends PropertiesTest {
     val cgRelatedAnalysisSchedulers: Set[FPCFAnalysisScheduler] = Set[FPCFAnalysisScheduler](
         RTACallGraphAnalysisScheduler,
         EagerLibraryEntryPointsAnalysis,
-        TriggeredStaticInitializerAnalysis,
-        TriggeredLoadedClassesAnalysis,
-        TriggeredFinalizerAnalysisScheduler,
-        TriggeredThreadRelatedCallsAnalysis,
-        TriggeredSerializationRelatedCallsAnalysis,
-        TriggeredReflectionRelatedCallsAnalysis,
-        TriggeredSystemPropertiesAnalysis,
         LazyL0BaseAIAnalysis,
         TACAITransformer,
-        LazyCalleesAnalysis(Set(
-            StandardInvokeCallees,
-            SerializationRelatedCallees,
-            ReflectionRelatedCallees,
-            ThreadRelatedIncompleteCallSites
-        ))
+        TriggeredInstantiatedTypesAnalysis,
+        LazyCalleesAnalysis(Set(StandardInvokeCallees))
     )
 
     override def init(p: Project[URL]): Unit = {
