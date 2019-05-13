6f907af8fb5e6c6b5421acf39c2eeb6a702d82b0
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2019-05-13 15:55:19 +0200

    let it compile again….
    Former-commit-id: 5578dc29dee6ebfaee5e3f4d983ef7da4fa6caae

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/VTATest.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/VTATest.scala
@@ -6,29 +6,13 @@ import java.net.URL
 import org.opalj.fpcf.properties.vta.ExpectedType
 import org.opalj.br.analyses.DeclaredMethodsKey
 import org.opalj.br.analyses.Project
-import org.opalj.br.fpcf.FPCFAnalysisScheduler
-import org.opalj.br.fpcf.cg.properties.StandardInvokeCallees
 import org.opalj.ai.domain.l2
-import org.opalj.ai.fpcf.analyses.LazyL0BaseAIAnalysis
 import org.opalj.ai.fpcf.properties.AIDomainFactoryKey
-import org.opalj.tac.fpcf.analyses.cg.EagerLibraryEntryPointsAnalysis
-import org.opalj.tac.fpcf.analyses.cg.TriggeredInstantiatedTypesAnalysis
-import org.opalj.tac.fpcf.analyses.TACAITransformer
-import org.opalj.tac.fpcf.analyses.cg.LazyCalleesAnalysis
-import org.opalj.tac.fpcf.analyses.cg.RTACallGraphAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.IFDSBasedVariableTypeAnalysis
 import org.opalj.tac.fpcf.analyses.VTANullFact
 
 class VTATest extends PropertiesTest {
 
-    val cgRelatedAnalysisSchedulers: Set[FPCFAnalysisScheduler] = Set[FPCFAnalysisScheduler](
-        RTACallGraphAnalysisScheduler,
-        EagerLibraryEntryPointsAnalysis,
-        LazyL0BaseAIAnalysis,
-        TACAITransformer,
-        TriggeredInstantiatedTypesAnalysis,
-        LazyCalleesAnalysis(Set(StandardInvokeCallees))
-    )
 
     override def init(p: Project[URL]): Unit = {
         p.updateProjectInformationKeyInitializationData(
@@ -40,7 +24,7 @@ class VTATest extends PropertiesTest {
     }
 
     describe("Test the ExpectedType annotations") {
-        val testContext = executeAnalyses(cgRelatedAnalysisSchedulers + IFDSBasedVariableTypeAnalysis)
+        val testContext = executeAnalyses(IFDSBasedVariableTypeAnalysis)
         val project = testContext.project
         val declaredMethods = project.get(DeclaredMethodsKey)
         val eas = methodsWithAnnotations(project).map {
