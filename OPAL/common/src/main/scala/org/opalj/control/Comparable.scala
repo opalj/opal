d9755b97c1e0e58665015f4e612ed5d7ddf03161
Michael Eichberg <mail@michael-eichberg.de>
2019-05-20 10:16:24 +0200

    removed a legacy class which duplicated functionality defined by the JDK
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: edba4845a5184e1055976cb617f2ae67902439ba

---
+++ OPAL/common/src/main/scala/org/opalj/control/Comparable.scala
@@ -0,0 +1,17 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj
+package control
+
+/**
+ * Factory for creating a `Comparable` based on a function that enables the
+ * (one-way) comparison with value of a specific type.
+ *
+ * @author Michael Eichberg
+ */
+object Comparable {
+
+    def apply[T](f: T ⇒ Int): Comparable[T] = {
+        new Comparable[T] { def compareTo(t: T): Int = f(t) }
+    }
+
+}
