ee46c865cccb46ef37854476a1d36dfa3b386188
Michael Eichberg <mail@michael-eichberg.de>
2019-05-22 16:57:15 +0200

    improved the documentation of PropertyComputationHint
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: 989af124fcafdd21edefcf3443c3ee92a94c115c

--- OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
@@ -18,10 +18,12 @@ object PropertyComputationHint {
 case object DefaultPropertyComputation extends PropertyComputationHint
 
 /**
- * The property computation is extremely cheap. Therefore, the computation can/should be
- * processed right away, because it is extremely unlikely that we will gain anything
- * from parallelization or postponing the computation.
- * Here, `cheap` means that the computation requires ''at most a few hundred bytecode instructions''
- * overall and that the computation in particular '''will not query further properties'''.
+ * The property computation is extremely cheap (at most a few dozen bytecode operations). 
+ * Therefore, the property store may process the computation right away, because it is 
+ * extremely unlikely that we will gain anythingfrom parallelization or postponing 
+ * the computation. 
+ * 
+ * Please note, that even if a computation is very cheap dependees may not be triggered
+ * immediately by the property store. 
  */
 case object CheapPropertyComputation extends PropertyComputationHint
