9c9192d071ba1045b52d37eb916129f10d939a21
Michael Eichberg <mail@michael-eichberg.de>
2019-05-27 09:46:58 +0200

    added missing author information
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: f8e0df6c6d28d3efc1dcf43898b94c94d2c35388

--- OPAL/common/src/main/scala/org/opalj/log/DevNullLogger.scala
+++ OPAL/common/src/main/scala/org/opalj/log/DevNullLogger.scala
@@ -3,8 +3,15 @@ package org.opalj
 package log
 
 /**
-  * An instance of [[OPALLogger]] that does not perform any logging.
-  */
+ * An instance of [[OPALLogger]] that does not perform _any logging_ .
+ *
+ * In general, it is HIGHLY recommended to use the `ConsoleOPALLogger` and
+ * to the set the minimium log level to `Error`.
+ *
+ * @author Florian Kübler
+ */
 object DevNullLogger extends OPALLogger {
+
     override def log(message: LogMessage)(implicit ctx: LogContext): Unit = {}
+
 }
