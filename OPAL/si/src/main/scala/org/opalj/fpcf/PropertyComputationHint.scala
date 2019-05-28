e09fb668a3f0607b2aed4abf4b1bf2ba13041156
Michael Eichberg <mail@michael-eichberg.de>
2019-05-28 14:10:51 +0200

    fixed typo
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>
    
    Former-commit-id: 0e1e78ba4745ab86e6c3324a2f26ab8af3f86be0

--- OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/PropertyComputationHint.scala
@@ -20,7 +20,7 @@ case object DefaultPropertyComputation extends PropertyComputationHint
 /**
  * The property computation is extremely cheap (at most a few dozen bytecode operations).
  * Therefore, the property store may process the computation right away, because it is
- * extremely unlikely that we will gain anythingfrom parallelization or postponing
+ * extremely unlikely that we will gain anything from parallelization or postponing
  * the computation.
  *
  * Please note, that even if a computation is very cheap dependees may not be triggered
