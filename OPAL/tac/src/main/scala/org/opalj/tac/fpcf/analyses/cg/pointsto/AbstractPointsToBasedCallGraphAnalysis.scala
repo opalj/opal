d090e0fe66ff4eac63bf99f592aef87639d253cb
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2019-06-14 16:45:50 +0200

    temporarily repaired points-to based call graph analysis
    Former-commit-id: fe850149b1fc814fb281588575aff876a111c56c

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/pointsto/AbstractPointsToBasedCallGraphAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/pointsto/AbstractPointsToBasedCallGraphAnalysis.scala
@@ -7,7 +7,11 @@ package cg
 package pointsto
 
 import org.opalj.collection.ForeachRefIterator
+import org.opalj.collection.immutable.IntTrieSet
+import org.opalj.fpcf.Entity
+import org.opalj.fpcf.EOptionP
 import org.opalj.fpcf.EPS
+import org.opalj.fpcf.EUBPS
 import org.opalj.fpcf.ProperPropertyComputationResult
 import org.opalj.fpcf.PropertyBounds
 import org.opalj.fpcf.PropertyKey
@@ -17,12 +21,17 @@ import org.opalj.br.DefinedMethod
 import org.opalj.br.Method
 import org.opalj.br.ObjectType
 import org.opalj.br.ReferenceType
+import org.opalj.br.analyses.VirtualFormalParameters
+import org.opalj.br.analyses.VirtualFormalParametersKey
 import org.opalj.br.fpcf.properties.pointsto.AllocationSitePointsToSet
 import org.opalj.br.fpcf.properties.pointsto.NoAllocationSites
 import org.opalj.br.fpcf.properties.pointsto.NoTypes
 import org.opalj.br.fpcf.properties.pointsto.PointsToSetLike
 import org.opalj.br.fpcf.properties.pointsto.TypeBasedPointsToSet
+import org.opalj.tac.common.DefinitionSites
+import org.opalj.tac.common.DefinitionSitesKey
 import org.opalj.tac.fpcf.analyses.pointsto.AbstractPointsToBasedAnalysis
+import org.opalj.tac.fpcf.analyses.pointsto.toEntity
 import org.opalj.tac.fpcf.properties.TACAI
 
 /**
@@ -30,13 +39,22 @@ import org.opalj.tac.fpcf.properties.TACAI
  * [[org.opalj.tac.common.DefinitionSite]] and[[org.opalj.br.analyses.VirtualFormalParameter]]s
  * in order to determine the targets of virtual method calls.
  *
+ * TODO: This analysis is currently copy&paste and should be refactored
+ *
  * @author Florian Kuebler
  */
 trait AbstractPointsToBasedCallGraphAnalysis[PointsToSet <: PointsToSetLike[_, _, PointsToSet]]
     extends AbstractCallGraphAnalysis
     with AbstractPointsToBasedAnalysis[CallSiteT, PointsToSet] {
 
-    type State = PointsToBasedCGState[PointsToSet]
+    protected[this] implicit val formalParameters: VirtualFormalParameters = {
+        p.get(VirtualFormalParametersKey)
+    }
+    protected[this] implicit val definitionSites: DefinitionSites = {
+        p.get(DefinitionSitesKey)
+    }
+
+    override type State = PointsToBasedCGState[PointsToSet]
 
     /**
      * Computes the calls of the given `method` including the known effect of the `call` and
@@ -52,16 +70,16 @@ trait AbstractPointsToBasedCallGraphAnalysis[PointsToSet <: PointsToSetLike[_, _
         potentialTargets:              ForeachRefIterator[ObjectType],
         calleesAndCallers:             DirectCalls
     )(implicit state: State): Unit = {
-        /*val callerType = caller.definedMethod.classFile.thisType
+        val callerType = caller.definedMethod.classFile.thisType
         val callSite = (pc, call.name, call.descriptor, call.declaringClass)
 
         // get the upper bound of the pointsToSet and creates a dependency if needed
-        val pointsToSet = currentPointsTo(callSite, call.receiver.asVar.definedBy)
+        val pointsToSet: PointsToSet = currentPointsTo(callSite, call.receiver.asVar.definedBy)
 
         var types = IntTrieSet.empty
 
         for (newType ← potentialTargets) {
-            if (pointsToSet.contains(newType)) {
+            if (pointsToSet.types.contains(newType)) {
                 val tgtR = project.instanceCall(
                     callerType,
                     newType,
@@ -81,23 +99,72 @@ trait AbstractPointsToBasedCallGraphAnalysis[PointsToSet <: PointsToSetLike[_, _
                 types += newType.id
             }
         }
-        state.setPotentialTypesOfCallSite(callSite, types)*/
-        ???
+        state.setPotentialTypesOfCallSite(callSite, types)
+    }
 
+    @inline protected[this] def currentPointsTo(
+        depender: CallSiteT, dependeeDefSite: Int
+    )(implicit state: State): PointsToSet = {
+        if (ai.isMethodExternalExceptionOrigin(dependeeDefSite)) {
+            // FIXME ask what exception has been thrown
+            emptyPointsToSet
+        } else if (ai.isImmediateVMException(dependeeDefSite)) {
+            // FIXME -  we need to get the actual exception type here
+            emptyPointsToSet
+        } else {
+            currentPointsTo(depender, toEntity(dependeeDefSite, state.method, state.tac.stmts))
+        }
+    }
+
+    @inline protected[this] def currentPointsTo(
+        depender: CallSiteT, dependee: Entity
+    )(implicit state: State): PointsToSet = {
+        if (state.hasPointsToDependee(dependee)) {
+            val p2s = state.getPointsToProperty(dependee)
+
+            // It might be the case that there a dependency for that points-to state in the state
+            // from another depender.
+            if (!state.hasPointsToDependency(depender, dependee)) {
+                state.addPointsToDependency(depender, p2s)
+            }
+            pointsToUB(p2s)
+        } else {
+            val p2s = propertyStore(dependee, pointsToPropertyKey)
+            if (p2s.isRefinable) {
+                state.addPointsToDependency(depender, p2s)
+            }
+            pointsToUB(p2s)
+        }
+    }
+
+    @inline protected[this] def currentPointsTo(
+        depender: CallSiteT,
+        defSites: IntTrieSet
+    )(
+        implicit
+        state: State
+    ): Iterator[PointsToSet] = {
+        defSites.iterator.map[PointsToSet](currentPointsTo(depender, _))
+    }
+
+    @inline private[this] def pointsToUB(eOptP: EOptionP[Entity, PointsToSet]): PointsToSet = {
+        if (eOptP.hasUBP)
+            eOptP.ub
+        else
+            emptyPointsToSet
     }
 
     override def c(
         state: State
     )(eps: SomeEPS): ProperPropertyComputationResult = {
-        /*
         eps match {
             case EUBPS(e, ub: PointsToSetLike[_, _, _], isFinal) ⇒
-                val relevantCallSites = state.dependersOf(e)
+                val relevantCallSites: TraversableOnce[CallSiteT] = state.dependersOf(e)
 
                 // ensures, that we only add new calls
                 val calls = new DirectCalls()
 
-                val oldEOptP = state.getPointsToProperty(eps.e)
+                val oldEOptP: EOptionP[Entity, PointsToSet] = state.getPointsToProperty(eps.e)
                 val seenElements = if (oldEOptP.hasUBP) oldEOptP.ub.numTypes else 0
 
                 // perform the update for the new types
@@ -133,8 +200,7 @@ trait AbstractPointsToBasedCallGraphAnalysis[PointsToSet <: PointsToSetLike[_, _
                 returnResult(calls)(state)
 
             case _ ⇒ super.c(state)(eps)
-        }*/
-        ???
+        }
     }
 
     override def createInitialState(
--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/pointsto/PointsToBasedCGState.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/pointsto/PointsToBasedCGState.scala
@@ -9,7 +9,10 @@ package pointsto
 import scala.collection.mutable
 
 import org.opalj.collection.immutable.IntTrieSet
+import org.opalj.fpcf.Entity
 import org.opalj.fpcf.EOptionP
+import org.opalj.fpcf.EPS
+import org.opalj.fpcf.SomeEOptionP
 import org.opalj.br.DefinedMethod
 import org.opalj.br.Method
 import org.opalj.br.ObjectType
@@ -17,14 +20,146 @@ import org.opalj.br.fpcf.properties.pointsto.PointsToSetLike
 import org.opalj.tac.fpcf.properties.TACAI
 
 /**
- * Represents the state of a points-to based call graph analysis, while analyzing a certain method.
+ * TODO: That state object is currently copy&paste and should be refactored.
  *
  * @author Florian Kuebler
  */
 class PointsToBasedCGState[PointsToSet <: PointsToSetLike[_, _, PointsToSet]](
         override val method:                       DefinedMethod,
         override protected[this] var _tacDependee: EOptionP[Method, TACAI]
-) extends CGState {
+) extends CGState with TACAIBasedAnalysisState {
+
+    // maps a defsite to its result in the property store for the points-to set
+    private[this] val _pointsToDependees: mutable.Map[Entity, EOptionP[Entity, PointsToSet]] = {
+        mutable.Map.empty
+    }
+
+    // We organize the dependencies to points-to states within a bijective mapping of
+    // dependers (the use sites) and their dependees (the respective definition sites).
+    // For each dependee (def-site) we store the corresponding EOptionP, such that we can
+    // efficiently perform updates here.
+    // If we get an update for a dependee, we have to update all points-to sets for the
+    // its dependers (_dependeeToDependers(dependee).
+    private[this] val _dependeeToDependers: mutable.Map[Entity, mutable.Set[CallSiteT]] = {
+        mutable.Map.empty
+    }
+
+    val _dependeeCount: mutable.Map[Entity, Int] = mutable.Map.empty
+
+    private[this] val _dependerToDependees: mutable.Map[CallSiteT, mutable.Set[Entity]] = {
+        mutable.Map.empty
+    }
+
+    final def addPointsToDependency(
+        depender: CallSiteT,
+        dependee: EOptionP[Entity, PointsToSet]
+    ): Unit = {
+        assert(
+            !_dependeeToDependers.contains(dependee.e) ||
+                !_dependeeToDependers(dependee.e).contains(depender)
+        )
+        _dependeeToDependers.getOrElseUpdate(dependee.e, mutable.Set.empty).add(depender)
+
+        assert(
+            !_dependerToDependees.contains(depender) ||
+                !_dependerToDependees(depender).contains(dependee.e)
+        )
+        _dependerToDependees.getOrElseUpdate(depender, mutable.Set.empty).add(dependee.e)
+
+        if (!_pointsToDependees.contains(dependee.e))
+            _pointsToDependees(dependee.e) = dependee
+    }
+
+    final def removePointsToDependee(dependee: Entity): Unit = {
+        // delete the points-to set of the dependee
+        if (_pointsToDependees.remove(dependee).isEmpty)
+            throw new RuntimeException(s"failed to remove dependee: $dependee")
+
+        // for every depender we have to remove the dependee of the set of dependees
+        val dependers = _dependeeToDependers(dependee)
+        for (depender ← dependers) {
+            val dependees = _dependerToDependees(depender)
+            if (!dependees.remove(dependee))
+                throw new RuntimeException(s"failed to remove dependee: $dependee")
+
+            // if there is no dependee left, we remove the entry from the map
+            if (dependees.isEmpty && _dependerToDependees.remove(depender).isEmpty) {
+                throw new RuntimeException(s"failed to remove depender: $depender")
+            }
+        }
+
+        // finally, we also remove the dependee from the map
+        if (_dependeeToDependers.remove(dependee).isEmpty)
+            throw new RuntimeException(s"failed to remove dependee: $dependee")
+    }
+
+    final def removePointsToDepender(depender: CallSiteT): Unit = {
+        // for every dependee of the given depender:
+        // we have to remove the depender from the set of their dependers
+        for (dependee ← _dependerToDependees(depender)) {
+            if (!_dependeeToDependers(dependee).remove(depender)) {
+                throw new RuntimeException(s"failed to remove depender: $depender")
+            }
+            // if there is no depender left for this dependee, we remove the entry from the map
+            if (_dependeeToDependers(dependee).isEmpty) {
+                removePointsToDependee(dependee)
+            }
+        }
+
+        // now we can delete the depender
+        if (_dependerToDependees.remove(depender).isEmpty) {
+            throw new RuntimeException(s"failed to remove depender: $depender")
+        }
+    }
+
+    final def hasPointsToDependee(dependee: Entity): Boolean = {
+        _pointsToDependees.contains(dependee)
+    }
+
+    final def hasPointsToDependency(depender: CallSiteT, dependee: Entity): Boolean = {
+        _dependerToDependees.contains(depender) && _dependerToDependees(depender).contains(dependee)
+    }
+
+    final def hasPointsToDependency(depender: CallSiteT): Boolean = {
+        assert(!_dependerToDependees.contains(depender) || _dependerToDependees(depender).nonEmpty)
+        _dependerToDependees.contains(depender)
+    }
+
+    final def getPointsToProperty(dependee: Entity): EOptionP[Entity, PointsToSet] = {
+        _pointsToDependees(dependee)
+    }
+
+    final def updatePointsToDependency(eps: EPS[Entity, PointsToSet]): Unit = {
+        assert(_pointsToDependees.contains(eps.e))
+        _pointsToDependees(eps.e) = eps
+    }
+
+    // IMPROVE: In order to be thread-safe, we return an immutable copy of the set.
+    //  However, this is very inefficient!
+    final def dependersOf(dependee: Entity): Set[CallSiteT] = {
+        _dependeeToDependers(dependee).toSet
+    }
+
+    final def hasPointsToDependees: Boolean = {
+        assert(
+            (_pointsToDependees.isEmpty == _dependeeToDependers.isEmpty) &&
+                (_dependeeToDependers.isEmpty == _dependerToDependees.isEmpty)
+        )
+        _pointsToDependees.nonEmpty
+    }
+
+    override def dependees: List[SomeEOptionP] = {
+        // IMPROVE: make it more efficient (maybe use immutable map and join traversables)
+        var allDependees = super.dependees
+
+        _pointsToDependees.valuesIterator.foreach(d ⇒ allDependees ::= d)
+
+        allDependees
+    }
+
+    override def hasOpenDependencies: Boolean = {
+        hasPointsToDependees || super.hasOpenDependencies
+    }
 
     // maps a definition site to the ids of the potential (not yet resolved) objecttypes
     private[this] val _virtualCallSites: mutable.Map[CallSiteT, IntTrieSet] = mutable.Map.empty
@@ -45,7 +180,7 @@ class PointsToBasedCGState[PointsToSet <: PointsToSetLike[_, _, PointsToSet]](
         val typesLeft = _virtualCallSites(callSite) - instantiatedType.id
         if (typesLeft.isEmpty) {
             _virtualCallSites -= callSite
-            ??? // removePointsToDepender(callSite)
+            removePointsToDepender(callSite)
         } else {
             _virtualCallSites(callSite) = typesLeft
         }
