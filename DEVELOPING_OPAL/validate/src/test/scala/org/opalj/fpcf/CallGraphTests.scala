ccf1a3a311601ad31b82e660d0d4f8812daa1086
Andreas Bauer <a10r@mailbox.org>
2019-08-31 12:41:02 +0200

    Added InstantiatedTypesAnalysis which recognizes reflective instantiations. (Based on the RTA variant.)
    Former-commit-id: 81891b9972bcdfb90aa9ef8f3a0e8b72c15aa349

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/CallGraphTests.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/CallGraphTests.scala
@@ -6,14 +6,18 @@ import com.typesafe.config.Config
 import com.typesafe.config.ConfigValueFactory
 import org.opalj.br.analyses.cg.InitialEntryPointsKey
 import org.opalj.br.analyses.cg.InitialInstantiatedTypesKey
+import org.opalj.br.fpcf.FPCFAnalysis
 import org.opalj.fpcf.properties.callgraph.TypePropagationVariant
 import org.opalj.tac.fpcf.analyses.cg.CHACallGraphAnalysisScheduler
+import org.opalj.tac.fpcf.analyses.cg.reflection.ReflectionRelatedCallsAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.rta.InstantiatedTypesAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.rta.RTACallGraphAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.xta.CTASetEntitySelector
 import org.opalj.tac.fpcf.analyses.cg.xta.FTASetEntitySelector
+import org.opalj.tac.fpcf.analyses.cg.xta.InstantiatedTypesAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.xta.MTASetEntitySelector
 import org.opalj.tac.fpcf.analyses.cg.xta.PropagationBasedCallGraphAnalysisScheduler
+import org.opalj.tac.fpcf.analyses.cg.xta.SetEntitySelector
 import org.opalj.tac.fpcf.analyses.cg.xta.SimpleInstantiatedTypesAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.xta.TypePropagationAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.xta.XTASetEntitySelector
@@ -26,8 +30,6 @@ import scala.collection.JavaConverters._
  * @author Andreas Bauer
  */
 class CallGraphTests extends PropertiesTest {
-
-    //override val withRT = true
 
     override def createConfig(): Config = {
         val baseConfig = super.createConfig()
@@ -84,14 +86,23 @@ class CallGraphTests extends PropertiesTest {
         )
     }
 
-    describe("the XTA call graph analysis is executed") {
-        val as = executeAnalyses(
-            Set(
-                new SimpleInstantiatedTypesAnalysisScheduler(XTASetEntitySelector),
-                new PropagationBasedCallGraphAnalysisScheduler(XTASetEntitySelector),
-                new TypePropagationAnalysisScheduler(XTASetEntitySelector)
-            )
+    def schedulersForPropagationBasedAlgorithms(selector: SetEntitySelector): Set[ComputationSpecification[FPCFAnalysis]] = {
+        Set(
+            // Still needed for arrays...
+            new SimpleInstantiatedTypesAnalysisScheduler(selector),
+            // Handles type instantiations.
+            new InstantiatedTypesAnalysisScheduler(selector),
+            // Creates callers/callees based on locally available types.
+            new PropagationBasedCallGraphAnalysisScheduler(selector),
+            // Handles type propagation.
+            new TypePropagationAnalysisScheduler(selector),
+            // Handles reflection based calls (especially: instantiations).
+            ReflectionRelatedCallsAnalysisScheduler
         )
+    }
+
+    describe("the XTA call graph analysis is executed") {
+        val as = executeAnalyses(schedulersForPropagationBasedAlgorithms(XTASetEntitySelector))
         as.propertyStore.shutdown()
         // We need to manually store which variant was executed. Otherwise, there is no good way
         // to get this information in the property matcher.
@@ -109,13 +120,8 @@ class CallGraphTests extends PropertiesTest {
     }
 
     describe("the MTA call graph analysis is executed") {
-        val as = executeAnalyses(
-            Set(
-                new SimpleInstantiatedTypesAnalysisScheduler(MTASetEntitySelector),
-                new PropagationBasedCallGraphAnalysisScheduler(MTASetEntitySelector),
-                new TypePropagationAnalysisScheduler(MTASetEntitySelector)
-            )
-        )
+        val as = executeAnalyses(schedulersForPropagationBasedAlgorithms(MTASetEntitySelector))
+
         as.propertyStore.shutdown()
         as.propertyStore.getOrCreateInformation(
             TypePropagationVariant.tag, TypePropagationVariant.MTA
@@ -131,13 +137,8 @@ class CallGraphTests extends PropertiesTest {
     }
 
     describe("the FTA call graph analysis is executed") {
-        val as = executeAnalyses(
-            Set(
-                new SimpleInstantiatedTypesAnalysisScheduler(FTASetEntitySelector),
-                new PropagationBasedCallGraphAnalysisScheduler(FTASetEntitySelector),
-                new TypePropagationAnalysisScheduler(FTASetEntitySelector)
-            )
-        )
+        val as = executeAnalyses(schedulersForPropagationBasedAlgorithms(FTASetEntitySelector))
+
         as.propertyStore.shutdown()
         as.propertyStore.getOrCreateInformation(
             TypePropagationVariant.tag, TypePropagationVariant.FTA
@@ -153,13 +154,8 @@ class CallGraphTests extends PropertiesTest {
     }
 
     describe("the CTA call graph analysis is executed") {
-        val as = executeAnalyses(
-            Set(
-                new SimpleInstantiatedTypesAnalysisScheduler(CTASetEntitySelector),
-                new PropagationBasedCallGraphAnalysisScheduler(CTASetEntitySelector),
-                new TypePropagationAnalysisScheduler(CTASetEntitySelector)
-            )
-        )
+        val as = executeAnalyses(schedulersForPropagationBasedAlgorithms(CTASetEntitySelector))
+
         as.propertyStore.shutdown()
         as.propertyStore.getOrCreateInformation(
             TypePropagationVariant.tag, TypePropagationVariant.CTA
---
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/xta/InstantiatedTypesAnalysis.scala
@@ -0,0 +1,310 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj
+package tac
+package fpcf
+package analyses
+package cg
+package xta
+
+import org.opalj.br.DeclaredMethod
+import org.opalj.br.ObjectType
+import org.opalj.br.PCAndInstruction
+import org.opalj.br.ReferenceType
+import org.opalj.br.analyses.DeclaredMethods
+import org.opalj.br.analyses.DeclaredMethodsKey
+import org.opalj.br.analyses.SomeProject
+import org.opalj.br.fpcf.FPCFAnalysis
+import org.opalj.br.fpcf.FPCFTriggeredAnalysisScheduler
+import org.opalj.br.fpcf.properties.cg.Callers
+import org.opalj.br.fpcf.properties.cg.InstantiatedTypes
+import org.opalj.br.fpcf.properties.cg.NoCallers
+import org.opalj.br.instructions.INVOKESPECIAL
+import org.opalj.br.instructions.NEW
+import org.opalj.collection.immutable.UIDSet
+import org.opalj.fpcf.EOptionP
+import org.opalj.fpcf.EPK
+import org.opalj.fpcf.EPS
+import org.opalj.fpcf.Entity
+import org.opalj.fpcf.FinalP
+import org.opalj.fpcf.InterimEP
+import org.opalj.fpcf.InterimEUBP
+import org.opalj.fpcf.InterimPartialResult
+import org.opalj.fpcf.InterimUBP
+import org.opalj.fpcf.NoResult
+import org.opalj.fpcf.PartialResult
+import org.opalj.fpcf.PropertyBounds
+import org.opalj.fpcf.PropertyComputationResult
+import org.opalj.fpcf.PropertyKey
+import org.opalj.fpcf.PropertyStore
+import org.opalj.fpcf.SomeEPS
+
+//import scala.language.existentials
+
+/**
+ * Marks types as instantiated if their constructor is invoked. Constructors invoked by subclass
+ * constructors do not result in additional instantiated types.
+ *
+ * This analysis is adapted from the RTA version. Instead of adding the instantiations to the type
+ * set of the Project, they are added to the type set of the calling method. Which entity the type
+ * set is attached to depends on the call graph variant used.
+ *
+ * @author Florian Kuebler
+ * @author Andreas Bauer
+ */
+class InstantiatedTypesAnalysis private[analyses] (
+        final val project:     SomeProject,
+        val setEntitySelector: SetEntitySelector
+) extends FPCFAnalysis {
+    implicit private val declaredMethods: DeclaredMethods = project.get(DeclaredMethodsKey)
+
+    def analyze(declaredMethod: DeclaredMethod): PropertyComputationResult = {
+
+        // only constructors may initialize a class
+        if (declaredMethod.name != "<init>")
+            return NoResult;
+
+        val callersEOptP = propertyStore(declaredMethod, Callers.key)
+
+        val callersUB: Callers = (callersEOptP: @unchecked) match {
+            case FinalP(NoCallers) ⇒
+                // nothing to do, since there is no caller
+                return NoResult;
+
+            case eps: EPS[_, _] ⇒
+                if (eps.ub eq NoCallers) {
+                    // we can not create a dependency here, so the analysis is not allowed to create
+                    // such a result
+                    throw new IllegalStateException("illegal immediate result for callers")
+                } else {
+                    eps.ub
+                }
+            // the method is reachable, so we analyze it!
+        }
+
+        // the set of types that are definitely initialized at this point in time
+        //val instantiatedTypesEOptP = propertyStore(project, InstantiatedTypes.key)
+        //val instantiatedTypesUB: UIDSet[ReferenceType] = getInstantiatedTypesUB(instantiatedTypesEOptP)
+
+        val declaredType = declaredMethod.declaringClassType.asObjectType
+
+        // if the current type is already instantiated, no work is left
+        //if (instantiatedTypesUB.contains(declaredType))
+        //    return NoResult;
+
+        val cfOpt = project.classFile(declaredType)
+
+        // abstract classes can never be instantiated
+        cfOpt.foreach { cf ⇒
+            if (cf.isAbstract)
+                return NoResult;
+        }
+
+        processCallers(declaredMethod, declaredType, callersEOptP, callersUB, Set.empty)
+    }
+
+    private[this] def processCallers(
+        declaredMethod:   DeclaredMethod,
+        declaredType:     ObjectType,
+        callersEOptP:     EOptionP[DeclaredMethod, Callers],
+        callersUB:        Callers,
+        seenSuperCallers: Set[DeclaredMethod]
+    ): PropertyComputationResult = {
+        var newSeenSuperCallers = seenSuperCallers
+        for {
+            (caller, _, _) ← callersUB.callers
+            // if we already analyzed the caller, we do not need to do it twice
+            // note, that this is only needed for the continuation
+            if !newSeenSuperCallers.contains(caller)
+        } {
+            if (caller.name != "<init>") {
+                return partialResult(caller, declaredType);
+            }
+
+            // the constructor is called from another constructor. it is only an new instantiated
+            // type if it was no super call. Thus the caller must be a subtype
+            if (!classHierarchy.isSubtypeOf(caller.declaringClassType, declaredType))
+                return partialResult(caller, declaredType);
+
+            // actually it must be the direct subtype! -- we did the first check to return early
+            project.classFile(caller.declaringClassType.asObjectType).foreach { cf ⇒
+                cf.superclassType.foreach { supertype ⇒
+                    if (supertype != declaredType)
+                        return partialResult(caller, declaredType);
+                }
+            }
+
+            // if the caller is not available, we have to assume that it was no super call
+            if (!caller.hasSingleDefinedMethod) {
+                return partialResult(caller, declaredType);
+            }
+
+            val callerMethod = caller.definedMethod
+
+            // if the caller has no body, we have to assume that it was no super call
+            if (callerMethod.body.isEmpty)
+                return partialResult(caller, declaredType);
+
+            val supercall = INVOKESPECIAL(
+                declaredType,
+                isInterface = false,
+                "<init>",
+                declaredMethod.descriptor
+            )
+
+            val pcsOfSuperCalls = callerMethod.body.get.collectInstructionsWithPC {
+                case pcAndInstr @ PCAndInstruction(_, `supercall`) ⇒ pcAndInstr
+            }
+
+            assert(pcsOfSuperCalls.nonEmpty)
+
+            // there can be only one super call, so there must be an explicit call
+            if (pcsOfSuperCalls.size > 1)
+                return partialResult(caller, declaredType);
+
+            // there is exactly the current call as potential super call, it still might no super
+            // call if the class has another constructor that calls the super. In that case
+            // there must either be a new of the `declaredType`
+            val newInstr = NEW(declaredType)
+            val hasNew = callerMethod.body.get.exists {
+                case (_, i) ⇒ i == newInstr
+            }
+            if (hasNew)
+                return partialResult(caller, declaredType);
+
+            // to call is a super call, we should remember the call, in order to not evaluate it
+            // again, if there are new callers!
+            newSeenSuperCallers += caller
+        }
+
+        if (callersEOptP.isFinal) {
+            NoResult
+        } else {
+            InterimPartialResult(
+                Some(callersEOptP),
+                continuation(declaredMethod, declaredType, newSeenSuperCallers)
+            )
+        }
+    }
+
+    private[this] def continuation(
+        declaredMethod:   DeclaredMethod,
+        declaredType:     ObjectType,
+        seenSuperCallers: Set[DeclaredMethod]
+    )(someEPS: SomeEPS): PropertyComputationResult = {
+        val eps = someEPS.asInstanceOf[EPS[DeclaredMethod, Callers]]
+        processCallers(declaredMethod, declaredType, eps, eps.ub, seenSuperCallers)
+
+    }
+
+    private[this] def partialResult(
+        caller:       DeclaredMethod,
+        declaredType: ObjectType
+    ): PartialResult[SetEntity, InstantiatedTypes] = {
+        val setEntity = setEntitySelector(caller)
+        PartialResult[SetEntity, InstantiatedTypes](
+            setEntity,
+            InstantiatedTypes.key,
+            InstantiatedTypesAnalysis.update(setEntity, UIDSet(declaredType))
+        )
+    }
+
+    def getInstantiatedTypesUB(
+        instantiatedTypesEOptP: EOptionP[SomeProject, InstantiatedTypes]
+    ): UIDSet[ReferenceType] = {
+        instantiatedTypesEOptP match {
+            case eps: EPS[_, _] ⇒ eps.ub.types
+            case _              ⇒ UIDSet.empty
+        }
+    }
+}
+
+object InstantiatedTypesAnalysis {
+    def update2(
+        p:                    SomeProject,
+        newInstantiatedTypes: UIDSet[ObjectType]
+    )(
+        eop: EOptionP[SomeProject, InstantiatedTypes]
+    ): Option[InterimEP[SomeProject, InstantiatedTypes]] = eop match {
+        case InterimUBP(ub: InstantiatedTypes) ⇒
+            val newUB = ub.updated(newInstantiatedTypes)
+            if (newUB.types.size > ub.types.size)
+                Some(InterimEUBP(p, newUB))
+            else
+                None
+
+        case _: EPK[_, _] ⇒
+            throw new IllegalStateException(
+                "the instantiated types property should be pre initialized"
+            )
+
+        case r ⇒ throw new IllegalStateException(s"unexpected previous result $r")
+    }
+
+    // TODO Duplication: Something like this appears in several places.
+    def update[E >: Null <: Entity](
+        entity:               E,
+        newInstantiatedTypes: UIDSet[ReferenceType]
+    )(
+        eop: EOptionP[E, InstantiatedTypes]
+    ): Option[InterimEP[E, InstantiatedTypes]] = eop match {
+        case InterimUBP(ub: InstantiatedTypes) ⇒
+            val newUB = ub.updated(newInstantiatedTypes)
+            if (newUB.types.size > ub.types.size)
+                Some(InterimEUBP(entity, newUB))
+            else
+                None
+
+        case _: EPK[_, _] ⇒
+            val newUB = InstantiatedTypes.apply(newInstantiatedTypes)
+            Some(InterimEUBP(entity, newUB))
+
+        case r ⇒ throw new IllegalStateException(s"unexpected previous result $r")
+    }
+
+}
+
+class InstantiatedTypesAnalysisScheduler(
+        val setEntitySelector: SetEntitySelector
+) extends FPCFTriggeredAnalysisScheduler {
+
+    override type InitializationData = Null
+
+    override def uses: Set[PropertyBounds] = PropertyBounds.ubs(
+        InstantiatedTypes,
+        Callers
+    )
+
+    override def triggeredBy: PropertyKey[Callers] = Callers.key
+
+    override def derivesCollaboratively: Set[PropertyBounds] = PropertyBounds.ubs(InstantiatedTypes)
+
+    override def derivesEagerly: Set[PropertyBounds] = Set.empty
+
+    override def register(p: SomeProject, ps: PropertyStore, unused: Null): FPCFAnalysis = {
+        val analysis = new InstantiatedTypesAnalysis(p, setEntitySelector)
+        ps.registerTriggeredComputation(triggeredBy, analysis.analyze)
+        analysis
+    }
+
+    override def init(p: SomeProject, ps: PropertyStore): Null = {
+        //val initialInstantiatedTypes = UIDSet(p.get(InitialInstantiatedTypesKey).toSeq: _*)
+
+        // TODO AB what is this
+        //ps.preInitialize[SomeProject, InstantiatedTypes](p, InstantiatedTypes.key) {
+        //    case _: EPK[_, _] ⇒ InterimEUBP(p, org.opalj.br.fpcf.properties.cg.InstantiatedTypes(initialInstantiatedTypes))
+        //    case eps          ⇒ throw new IllegalStateException(s"unexpected property: $eps")
+        //}
+
+        null
+    }
+
+    override def beforeSchedule(p: SomeProject, ps: PropertyStore): Unit = {}
+
+    override def afterPhaseScheduling(ps: PropertyStore, analysis: FPCFAnalysis): Unit = {}
+
+    override def afterPhaseCompletion(
+        p:        SomeProject,
+        ps:       PropertyStore,
+        analysis: FPCFAnalysis
+    ): Unit = {}
+}
