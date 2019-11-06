5ae706d2abf1726a31450d946b7b6ff3649449c3
Tobias Roth <tobias_peter.roth@stud.tu-darmstadt.de>
2019-11-06 16:19:21 +0100

    Introduced Reference Immutability. A whole new lattice, the analysis which is partly taken from the field mutability analysis and mapped to the new lattice and the demo-class for running the reference immutability analysis.
    Former-commit-id: c8430bf1b770f0964bf58773755cd90cc1ec2685

---
+++ OPAL/br/src/main/scala/org/opalj/br/fpcf/properties/ReferenceImmutability.scala
@@ -0,0 +1,52 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.br.fpcf.properties
+
+import org.opalj.fpcf.Property
+import org.opalj.fpcf.PropertyKey
+import org.opalj.fpcf.PropertyMetaInformation
+
+sealed trait ReferenceImmutabilityPropertyMetaInformation extends PropertyMetaInformation {
+
+    type Self = ReferenceImmutability
+
+}
+
+/**
+ * Describes the reference immutability of org.opalj.br.Field.
+ * The analysis is used from the old implementation of the L2FieldMutabilityAnalysis.
+ *
+ * [[MutableReference]] A reference through which the object it points to could be changed.
+ *
+ * [[LazyInitializedReference]] A reference through which the object it points to is changed in a initial like phase
+ * and afterwards never changed through this reference
+ *
+ * [[ImmutableReference]] A reference through which the object it leads to is set in the constructor and afterwards
+ * never changed. And there is no possibility to change it afterwards through this reference..
+ *
+ * @author Tobias Peter Roth
+ */
+sealed trait ReferenceImmutability
+    extends Property
+    with ReferenceImmutabilityPropertyMetaInformation {
+    final def key: PropertyKey[ReferenceImmutability] = ReferenceImmutability.key
+}
+
+object ReferenceImmutability extends ReferenceImmutabilityPropertyMetaInformation {
+
+    final val PropertyKeyName = "opalj.ReferenceImmutability"
+
+    final val key: PropertyKey[ReferenceImmutability] = {
+        PropertyKey.create(
+            PropertyKeyName,
+            MutableReference
+        )
+    }
+}
+
+sealed trait RefImm extends ReferenceImmutability
+
+case object MutableReference extends RefImm
+
+case object LazyInitializedReference extends RefImm
+
+case object ImmutableReference extends RefImm
