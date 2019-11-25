5cbf0d80af461bfe3cf7c2af82e2dc63060c206f
Tobias Roth <tobias_peter.roth@stud.tu-darmstadt.de>
2019-11-25 14:59:47 +0100

    formatting respecting scalafmt
    Former-commit-id: 73bddf1b1f3b30d8a17f79a08448b0a0fb607f15

--- OPAL/si/src/main/scala/org/opalj/fpcf/ComputationSpecification.scala
+++ OPAL/si/src/main/scala/org/opalj/fpcf/ComputationSpecification.scala
@@ -17,160 +17,155 @@ case class SpecificationViolation(message: String) extends Exception(message)
  */
 trait ComputationSpecification[A] {
 
-    //
-    // PROPERTIES OF COMPUTATION SPECIFICATIONS
-    //
+  //
+  // PROPERTIES OF COMPUTATION SPECIFICATIONS
+  //
 
-    /**
-     * Identifies this computation specification; typically the name of the class
-     * which implements the underlying analysis.
-     *
-     * The default name is the name of `this` class.
-     *
-     * '''This method should be overridden.'''
-     */
-    def name: String = {
-        val nameCandidate = this.getClass.getSimpleName
-        if (nameCandidate.endsWith("$"))
-            nameCandidate.substring(0, nameCandidate.length() - 1)
-        else
-            nameCandidate
-    }
+  /**
+   * Identifies this computation specification; typically the name of the class
+   * which implements the underlying analysis.
+   *
+   * The default name is the name of `this` class.
+   *
+   * '''This method should be overridden.'''
+   */
+  def name: String = {
+    val nameCandidate = this.getClass.getSimpleName
+    if (nameCandidate.endsWith("$"))
+      nameCandidate.substring(0, nameCandidate.length() - 1)
+    else
+      nameCandidate
+  }
 
-    /**
-     * Returns the kinds of properties which are queried by this analysis.
-     *
-     * @note   This set consists only of property kinds which are directly used by the analysis.
-     *
-     * @note   Self usages should also be documented.
-     *
-     * @note   This method is called after
-     *         [[org.opalj.fpcf.ComputationSpecification#init(ps:org\.opalj\.fpcf\.PropertyStore)*]]
-     *         was called for all analyses belonging to an analysis scenario.
-     *         (E.g., it can be used to collect the set of used property bounds based on the
-     *         configuration choices made in other analyses.)
-     */
-    def uses(ps: PropertyStore): Set[PropertyBounds]
+  /**
+   * Returns the kinds of properties which are queried by this analysis.
+   *
+   * @note   This set consists only of property kinds which are directly used by the analysis.
+   *
+   * @note   Self usages should also be documented.
+   *
+   * @note   This method is called after
+   *         [[org.opalj.fpcf.ComputationSpecification#init(ps:org\.opalj\.fpcf\.PropertyStore)*]]
+   *         was called for all analyses belonging to an analysis scenario.
+   *         (E.g., it can be used to collect the set of used property bounds based on the
+   *         configuration choices made in other analyses.)
+   */
+  def uses(ps: PropertyStore): Set[PropertyBounds]
 
-    /**
-     * Returns the kind of the property that is lazily (on-demand) derived.
-     */
-    def derivesLazily: Option[PropertyBounds]
+  /**
+   * Returns the kind of the property that is lazily (on-demand) derived.
+   */
+  def derivesLazily: Option[PropertyBounds]
 
-    /**
-     * Returns the set of property kinds eagerly derived by the underlying analysis.
-     */
-    def derivesEagerly: Set[PropertyBounds]
+  /**
+   * Returns the set of property kinds eagerly derived by the underlying analysis.
+   */
+  def derivesEagerly: Set[PropertyBounds]
 
-    def derivesCollaboratively: Set[PropertyBounds]
+  def derivesCollaboratively: Set[PropertyBounds]
 
-    def derives: Iterator[PropertyBounds] = {
-        derivesEagerly.iterator ++ derivesCollaboratively.iterator ++ derivesLazily.iterator
-    }
+  def derives: Iterator[PropertyBounds] = {
+    derivesEagerly.iterator ++ derivesCollaboratively.iterator ++ derivesLazily.iterator
+  }
 
-    require(
-        (derivesCollaboratively intersect derivesEagerly).isEmpty,
-        "a property either has to be derived eagerly or collaboratively, but not both: "+
-            (derivesCollaboratively intersect derivesEagerly).mkString(", ")
-    )
+  require(
+    (derivesCollaboratively intersect derivesEagerly).isEmpty,
+    "a property either has to be derived eagerly or collaboratively, but not both: " +
+      (derivesCollaboratively intersect derivesEagerly).mkString(", ")
+  )
 
-    require(
-        derivesLazily.isDefined || derivesEagerly.nonEmpty || derivesCollaboratively.nonEmpty,
-        "the computation does not derive any information"
-    )
+  require(
+    derivesLazily.isDefined || derivesEagerly.nonEmpty || derivesCollaboratively.nonEmpty,
+    "the computation does not derive any information"
+  )
 
-    require(
-        derivesLazily.isEmpty || (derivesEagerly.isEmpty && derivesCollaboratively.isEmpty),
-        "a lazy analysis cannot also derive information eagerly and/or collaboratively "
-    )
+  require(
+    derivesLazily.isEmpty || (derivesEagerly.isEmpty && derivesCollaboratively.isEmpty),
+    "a lazy analysis cannot also derive information eagerly and/or collaboratively "
+  )
 
-    /**
-     * Specifies the kind of the computation that is performed. The kind restricts in which
-     * way the analysis is allowed to interact with the property store/other analyses.
-     */
-    def computationType: ComputationType
+  /**
+   * Specifies the kind of the computation that is performed. The kind restricts in which
+   * way the analysis is allowed to interact with the property store/other analyses.
+   */
+  def computationType: ComputationType
 
-    def toString(ps: PropertyStore): String = {
-        val uses = this.uses(ps).iterator.map(_.toSpecification).mkString("uses={", ", ", "}")
+  def toString(ps: PropertyStore): String = {
+    val uses = this.uses(ps).iterator.map(_.toSpecification).mkString("uses={", ", ", "}")
 
-        val derivesLazily =
-            this.derivesLazily.iterator.
-                map(_.toSpecification).
-                mkString("derivesLazily={", ", ", "}")
-        val derivesEagerly =
-            this.derivesEagerly.iterator.
-                map(_.toSpecification).
-                mkString("derivesEagerly={", ", ", "}")
-        val derivesCollaboratively =
-            this.derivesCollaboratively.iterator.
-                map(_.toSpecification).
-                mkString("derivesCollaboratively={", ", ", "}")
+    val derivesLazily =
+      this.derivesLazily.iterator.map(_.toSpecification).mkString("derivesLazily={", ", ", "}")
+    val derivesEagerly =
+      this.derivesEagerly.iterator.map(_.toSpecification).mkString("derivesEagerly={", ", ", "}")
+    val derivesCollaboratively =
+      this.derivesCollaboratively.iterator
+        .map(_.toSpecification)
+        .mkString("derivesCollaboratively={", ", ", "}")
 
-        s"ComputationSpecification(name=$name,type=$computationType,"+
-            s"$uses,$derivesLazily,$derivesEagerly,$derivesCollaboratively)"
+    s"ComputationSpecification(name=$name,type=$computationType," +
+      s"$uses,$derivesLazily,$derivesEagerly,$derivesCollaboratively)"
+  }
 
-    }
+  override def toString: String = {
+    s"ComputationSpecification(name=$name,type=$computationType)"
+  }
 
-    override def toString: String = {
-        s"ComputationSpecification(name=$name,type=$computationType)"
-    }
+  //
+  // LIFECYCLE RELATED METHODS
+  //
 
-    //
-    // LIFECYCLE RELATED METHODS
-    //
+  /**
+   * The type of the data used by the analysis at initialization time.
+   * For analyses without special initialization requirements this type is `Null`.
+   */
+  type InitializationData
 
-    /**
-     * The type of the data used by the analysis at initialization time.
-     * For analyses without special initialization requirements this type is `Null`.
-     */
-    type InitializationData
+  /**
+   * Called directly after the analysis is registered with an analysis scheduler; in particular
+   * before any analysis belonging to the same analysis scenario is scheduled –
+   * independent of the batch in which it will run.
+   *
+   * This enables further initialization of the computations that will eventually be executed.
+   * For example to initialize global configuration information.
+   *
+   * A computation specification does not have to call any methods of the property store that
+   * may trigger or schedule computations; i.e., it must – in particular – not call
+   * the methods `apply`, `schedule*`, `register*` or `waitOnPhaseCompletion`.
+   *
+   * @return The initialization data that is later on passed to schedule.
+   */
+  def init(ps: PropertyStore): InitializationData
 
-    /**
-     * Called directly after the analysis is registered with an analysis scheduler; in particular
-     * before any analysis belonging to the same analysis scenario is scheduled –
-     * independent of the batch in which it will run.
-     *
-     * This enables further initialization of the computations that will eventually be executed.
-     * For example to initialize global configuration information.
-     *
-     * A computation specification does not have to call any methods of the property store that
-     * may trigger or schedule computations; i.e., it must – in particular – not call
-     * the methods `apply`, `schedule*`, `register*` or `waitOnPhaseCompletion`.
-     *
-     * @return The initialization data that is later on passed to schedule.
-     */
-    def init(ps: PropertyStore): InitializationData
+  /**
+   * Called directly before the analyses belonging to a phase are effectively scheduled. I.e.,
+   * after phase setup, but potentially after other analyses' `beforeSchedule` method is called.
+   */
+  def beforeSchedule(ps: PropertyStore): Unit
 
-    /**
-     * Called directly before the analyses belonging to a phase are effectively scheduled. I.e.,
-     * after phase setup, but potentially after other analyses' `beforeSchedule` method is called.
-     */
-    def beforeSchedule(ps: PropertyStore): Unit
+  /**
+   * Called by the scheduler to let the analysis register itself or to start execution.
+   */
+  def schedule(ps: PropertyStore, i: InitializationData): A
 
-    /**
-     * Called by the scheduler to let the analysis register itself or to start execution.
-     */
-    def schedule(ps: PropertyStore, i: InitializationData): A
+  /**
+   * Called back after all analyses of a specific phase have been
+   *        schedule (i.e., before calling waitOnPhaseCompletion).
+   */
+  def afterPhaseScheduling(ps: PropertyStore, analysis: A): Unit
 
-    /**
-     * Called back after all analyses of a specific phase have been
-     *        schedule (i.e., before calling waitOnPhaseCompletion).
-     */
-    def afterPhaseScheduling(ps: PropertyStore, analysis: A): Unit
-
-    /**
-     * Called after phase completion.
-     */
-    def afterPhaseCompletion(ps: PropertyStore, analysis: A): Unit
+  /**
+   * Called after phase completion.
+   */
+  def afterPhaseCompletion(ps: PropertyStore, analysis: A): Unit
 
 }
 
 trait SimpleComputationSpecification[A] extends ComputationSpecification[A] {
 
-    final override type InitializationData = Null
-    final override def init(ps: PropertyStore): Null = null
-    final override def beforeSchedule(ps: PropertyStore): Unit = {}
-    final override def afterPhaseScheduling(ps: PropertyStore, analysis: A): Unit = {}
-    final override def afterPhaseCompletion(ps: PropertyStore, analysis: A): Unit = {}
+  final override type InitializationData = Null
+  final override def init(ps: PropertyStore): Null = null
+  final override def beforeSchedule(ps: PropertyStore): Unit = {}
+  final override def afterPhaseScheduling(ps: PropertyStore, analysis: A): Unit = {}
+  final override def afterPhaseCompletion(ps: PropertyStore, analysis: A): Unit = {}
 
 }
