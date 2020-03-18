3c0bb3a3ffa6bf3d1ddd2bce913b84d968974709
Tobias Roth <tobias_peter.roth@stud.tu-darmstadt.de>
2020-03-18 15:21:56 +0100

    New test-cases for fields with strings and arrays
    Former-commit-id: bfcd4da63d717f0abd7881956cafb16de6e1cbcc

---
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/immutability/field/ArrayAndString.java
@@ -0,0 +1,31 @@
+package org.opalj.fpcf.fixtures.immutability.field;
+
+import org.opalj.br.fpcf.properties.DependentImmutableField;
+import org.opalj.fpcf.properties.field_immutability.DeepImmutableFieldAnnotation;
+import org.opalj.fpcf.properties.field_immutability.DependentImmutableFieldAnnotation;
+import org.opalj.fpcf.properties.field_immutability.ShallowImmutableFieldAnnotation;
+
+public class ArrayAndString<T> {
+    @DeepImmutableFieldAnnotation("")
+    private String[] stringArray;
+    @DeepImmutableFieldAnnotation("")
+    private int[] intArray;
+    @DeepImmutableFieldAnnotation("")
+    private String string;
+    @DeepImmutableFieldAnnotation("")
+    private int i;
+    @ShallowImmutableFieldAnnotation("")
+    private TrivialMutableClass[] tmc;
+    @ShallowImmutableFieldAnnotation("")
+    private T[] tArray;
+
+    ArrayAndString(String[] stringArray, int[] intArray, String string, int i, TrivialMutableClass[] tmc, T[] tArray) {
+        this.stringArray = stringArray;
+        this.intArray = intArray;
+        this.string = string;
+        this.i = i;
+        this.tmc = tmc;
+        this.tArray = tArray;
+    }
+
+}
