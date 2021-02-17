8712d485814384c71541286790491a1d28cdba87
Dominik Helm <helm@cs.tu-darmstadt.de>
2021-02-17 14:10:09 +0100

    Fix semantics of calls w.r.t. private methods
    Former-commit-id: 1a5688007d1a1e771ad7ddd79fcff707634dd647

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/AbstractCallGraphAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/AbstractCallGraphAnalysis.scala
@@ -94,7 +94,7 @@ trait AbstractCallGraphAnalysis extends ReachableMethodAnalysis {
                     call.descriptor,
                     call.declaringClass,
                     stmt.pc,
-                    call.resolveCallTarget,
+                    call.resolveCallTarget(state.method.declaringClassType),
                     calls
                 )
 
@@ -105,7 +105,7 @@ trait AbstractCallGraphAnalysis extends ReachableMethodAnalysis {
                     call.descriptor,
                     call.declaringClass,
                     call.pc,
-                    call.resolveCallTarget,
+                    call.resolveCallTarget(state.method.declaringClassType),
                     calls
                 )
 
