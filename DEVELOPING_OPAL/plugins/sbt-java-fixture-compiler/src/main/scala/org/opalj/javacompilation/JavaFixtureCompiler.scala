0c15b48eef741907abc3799b4ec9254453f552e4
Dominik Helm <helm@cs.tu-darmstadt.de>
2021-02-19 15:20:35 +0100

    Bump fixture compiler version to latest
    Former-commit-id: 727348d82a15a71775b8e0eb2f7f10402cbe9205

--- DEVELOPING_OPAL/plugins/sbt-java-fixture-compiler/src/main/scala/org/opalj/javacompilation/JavaFixtureCompiler.scala
+++ DEVELOPING_OPAL/plugins/sbt-java-fixture-compiler/src/main/scala/org/opalj/javacompilation/JavaFixtureCompiler.scala
@@ -33,7 +33,7 @@ object JavaFixtureCompiler extends AutoPlugin {
 
     // tasks of the plugin
     val javaFixtureCompile = taskKey[Seq[JavaFixtureCompilationResult]](
-      "Compilation of Java fixture projects against Eclipse 4.6.1 JDT compiler."
+      "Compilation of Java fixture projects against Eclipse JDT 3.24.0 compiler."
     )
 
     val javaFixturePackage =
