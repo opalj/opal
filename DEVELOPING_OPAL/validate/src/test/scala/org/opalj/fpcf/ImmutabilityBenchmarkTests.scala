5d3e6104127cb306445ab254cd956a2e43e64b1b
tobias roth <roth@st.informatik.tu-darmstadt.de>
2021-03-12 16:08:40 +0100

    new immutability benchmark test runner
    Former-commit-id: 4bb5d4bb8f9beefa38f8814bec16d84831a75b1d

---
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/ImmutabilityBenchmarkTests.scala
@@ -0,0 +1,77 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.fpcf
+
+import org.opalj.br.fpcf.analyses.LazyUnsoundPrematurelyReadFieldsAnalysis
+
+import java.net.URL
+import org.opalj.ai.domain.l2
+import org.opalj.ai.fpcf.properties.AIDomainFactoryKey
+import org.opalj.br.analyses.Project
+import org.opalj.br.fpcf.analyses.LazyL0CompileTimeConstancyAnalysis
+import org.opalj.br.fpcf.analyses.LazyStaticDataUsageAnalysis
+import org.opalj.tac.cg.RTACallGraphKey
+import org.opalj.tac.fpcf.analyses.LazyFieldLocalityAnalysis
+import org.opalj.tac.fpcf.analyses.escape.LazyInterProceduralEscapeAnalysis
+import org.opalj.tac.fpcf.analyses.escape.LazyReturnValueFreshnessAnalysis
+import org.opalj.tac.fpcf.analyses.immutability.EagerL1ClassImmutabilityAnalysis
+import org.opalj.tac.fpcf.analyses.immutability.EagerL1TypeImmutabilityAnalysis
+import org.opalj.tac.fpcf.analyses.purity.LazyL2PurityAnalysis
+import org.opalj.tac.fpcf.analyses.immutability.EagerL3FieldImmutabilityAnalysis
+import org.opalj.tac.fpcf.analyses.immutability.fieldreference.EagerL0FieldReferenceImmutabilityAnalysis
+
+/**
+ * Tests if the properties specified in the test project (the classes in the (sub-)package of
+ * org.opalj.fpcf.fixture) and the computed ones match. The actual matching is delegated to
+ * PropertyMatchers to facilitate matching arbitrary complex property specifications.
+ *
+ * @author Tobias Roth
+ */
+class ImmutabilityBenchmarkTests extends PropertiesTest {
+
+    override def withRT = true
+
+    override def fixtureProjectPackage: List[String] = {
+        List("org/opalj/fpcf/fixtures/benchmark")
+    }
+
+    override def init(p: Project[URL]): Unit = {
+
+        p.updateProjectInformationKeyInitializationData(AIDomainFactoryKey) { _ ⇒
+            Set[Class[_ <: AnyRef]](classOf[l2.DefaultPerformInvocationsDomainWithCFGAndDefUse[URL]])
+        }
+
+        p.get(RTACallGraphKey)
+    }
+
+    describe("all immutability analyses are executed") {
+
+        val as = executeAnalyses(Set(
+            LazyUnsoundPrematurelyReadFieldsAnalysis,
+            LazyL2PurityAnalysis,
+            EagerL0FieldReferenceImmutabilityAnalysis,
+            EagerL3FieldImmutabilityAnalysis,
+            EagerL1TypeImmutabilityAnalysis,
+            EagerL1ClassImmutabilityAnalysis,
+            LazyStaticDataUsageAnalysis,
+            LazyL0CompileTimeConstancyAnalysis,
+            LazyInterProceduralEscapeAnalysis,
+            LazyReturnValueFreshnessAnalysis,
+            LazyFieldLocalityAnalysis
+        ))
+
+        as.propertyStore.shutdown()
+
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldReferenceImmutability"))
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldImmutability"))
+        validateProperties(
+            as,
+            classFilesWithAnnotations(as.project).map(tp ⇒ (tp._1.thisType, tp._2, tp._3)),
+            Set("ClassImmutability")
+        )
+        validateProperties(
+            as,
+            classFilesWithAnnotations(as.project).map(tp ⇒ (tp._1.thisType, tp._2, tp._3)),
+            Set("TypeImmutability")
+        )
+    }
+}
