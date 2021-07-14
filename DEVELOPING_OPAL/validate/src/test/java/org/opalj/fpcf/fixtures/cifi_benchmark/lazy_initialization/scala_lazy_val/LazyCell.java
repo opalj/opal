66c10ab8029a61b5c0fce307dba33f9c73e905f1
tobias roth <roth@st.informatik.tu-darmstadt.de>
2021-07-14 18:59:24 +0200

    added scala lazy val example

---
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/cifi_benchmark/lazy_initialization/scala_lazy_val/LazyCell.java
@@ -0,0 +1,38 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.fpcf.fixtures.cifi_benchmark.lazy_initialization.scala_lazy_val;
+
+import org.opalj.fpcf.properties.immutability.classes.TransitivelyImmutableClass;
+import org.opalj.fpcf.properties.immutability.field_assignability.LazilyInitializedField;
+import org.opalj.fpcf.properties.immutability.fields.TransitivelyImmutableField;
+import org.opalj.fpcf.properties.immutability.types.MutableType;
+
+/**
+*  This class represents the implementation of Scala lazy val from Scala 2.12.
+*  https://docs.scala-lang.org/sips/improved-lazy-val-initialization.html
+*
+*/
+@MutableType("non final class")
+@TransitivelyImmutableClass("Class has only transitive immutable fields.")
+public class LazyCell {
+
+@TransitivelyImmutableField("Lazy initialized field with primitive type")
+@LazilyInitializedField("The field is only set once in a synchronized way.")
+private volatile boolean bitmap_0 = false;
+
+@TransitivelyImmutableField("Lazy initialized field with primitive type")
+@LazilyInitializedField("The field is only set once in a synchronized way.")
+Integer value_0;
+
+private Integer value_lzycompute() {
+    synchronized (this){
+        if(value_0==0) {
+            value_0 = 42;
+            bitmap_0 = true;
+        }
+    }
+    return value_0;
+}
+public Integer getValue(){
+    return bitmap_0 ? value_0 : value_lzycompute();
+}
+}
