53fd8626888adc17de07ece06eb9833289b6efc8
Julius Naeumann <julius.naeumann@gmail.com>
2021-11-16 10:11:36 +0100

    Added PermittedSubclasses_attribute to DA

---
+++ OPAL/da/src/main/scala/org/opalj/da/PermittedSubclasses_attribute.scala
@@ -0,0 +1,29 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.da
+
+import scala.xml.{Node, NodeBuffer}
+
+/**
+ * Represents the ''PermittedSubclasses'' attribute (Java 17).
+ *
+ * @author Julius Naeumann
+ */
+case class PermittedSubclasses_attribute(
+        attribute_name_index: Constant_Pool_Index,
+        permittedSubclasses:        ClassesArray
+) extends Attribute {
+
+    override def attribute_length: Int = 2 + permittedSubclasses.size * 2
+
+    override def toXHTML(implicit cp: Constant_Pool): Node = {
+        <details class="attribute">
+            <summary>PermittedSubclasses</summary>
+            {
+                permittedSubclasses.map[String](p ⇒ cp(p).toString).sorted.map[NodeBuffer] { p ⇒
+                    <span>{ p }</span><br/>
+                }
+            }
+        </details>
+    }
+
+}
