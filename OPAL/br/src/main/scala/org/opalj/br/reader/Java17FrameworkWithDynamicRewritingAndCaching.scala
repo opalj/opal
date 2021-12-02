bd41f27863c64e9306b6effaae9d729421bc41a3
Dominik Helm <helm@cs.tu-darmstadt.de>
2021-12-02 11:19:10 +0100

    Improved documentation for Java Frameworks with Rewriting support

--- OPAL/br/src/main/scala/org/opalj/br/reader/Java17FrameworkWithDynamicRewritingAndCaching.scala
+++ OPAL/br/src/main/scala/org/opalj/br/reader/Java17FrameworkWithDynamicRewritingAndCaching.scala
@@ -1,10 +1,14 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
-package org.opalj.br.reader
+package org.opalj
+package br
+package reader
 
 /**
- * This configuration can be used to read in Java 17 (version 61) class files. All
- * standard information (as defined in the Java Virtual Machine Specification)
- * is represented. Instructions will be cached.
+ * This configuration can be used to read in Java 17 (version 61) class files with full
+ * support for rewriting `invokedynamic` instructions created by the JDK compiler for
+ * lambda and method reference expressions as well as opportunistic support for rewriting dynamic
+ * constants. All standard information (as defined in the Java Virtual Machine Specification) is
+ * represented. Instructions will be cached.
  *
  * @author Julius Naeumann
  */
