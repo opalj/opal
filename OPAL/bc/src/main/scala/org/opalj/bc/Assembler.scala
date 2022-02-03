0b29cb98e159e39dc55dbbb7364fc6956ec5aee9
Julius Naeumann <julius.naeumann@gmail.com>
2022-02-03 09:45:37 +0100

    added warning if atrictfp method modifier is written out on class file versions where it is ignored.

--- OPAL/bc/src/main/scala/org/opalj/bc/Assembler.scala
+++ OPAL/bc/src/main/scala/org/opalj/bc/Assembler.scala
@@ -3,13 +3,15 @@ package org.opalj
 package bc
 
 import scala.annotation.switch
-
 import java.io.ByteArrayOutputStream
 import java.io.DataOutputStream
 
+import org.opalj.bc.MethodFilter.logContext
+import org.opalj.bi.ACC_STRICT
 import org.opalj.bi.{ConstantPoolTags ⇒ CPTags}
 import org.opalj.da._
 import org.opalj.da.ClassFileReader.LineNumberTable_attribute
+import org.opalj.log.OPALLogger
 
 /**
  * Factory to create the binary representation (that is, an array of bytes) of a given class file.
@@ -752,6 +754,9 @@ object Assembler {
             writeShort(methods.size)
             methods foreach { m ⇒
                 serialize(m)
+                if  ((ACC_STRICT.mask & m.access_flags) != 0 && (classFile.major_version < 46) || (classFile.major_version > 60)) {
+                    OPALLogger.warn("assembler", s"Writing out ACC_STRICT flag for a method in a classfile of version ${classFile.major_version}, which is not interpreted in class files of version < 46 or > 60")
+                }
                 segmentInformation("Method: "+cp(m.name_index).toString, out.size)
             }
             segmentInformation("Methods", out.size)
