2199733fac11aa60dcea5c9c15f7f436eb35c58c
roterEmil <roth@st.informatik.tu-darmstadt.de>
2022-11-22 16:48:38 +0100

    reformatting

--- OPAL/bi/src/test/fixtures-java/projects/java16records/RecordClass.java
+++ OPAL/bi/src/test/fixtures-java/projects/java16records/RecordClass.java
@@ -1,5 +1,4 @@
-/*BSD 2-Clause License - see OPAL/LICENSE for details. */
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
 package java16records;
 
 public record RecordClass(int component1, Object component2) {}
-
