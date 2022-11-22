4b4fd4ffc1836dec64a2c0ee6900450275520b03
roterEmil <roth@st.informatik.tu-darmstadt.de>
2022-11-22 16:20:05 +0100

    removed comments

--- OPAL/bi/src/test/fixtures-java/projects/java16records/RecordClass.java
+++ OPAL/bi/src/test/fixtures-java/projects/java16records/RecordClass.java
@@ -1,6 +1,5 @@
-/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+BSD 2-Clause License - see OPAL/LICENSE for details. */
 /*package java16records;
 
 public record RecordClass(int component1, Object component2) {}
 
- */
