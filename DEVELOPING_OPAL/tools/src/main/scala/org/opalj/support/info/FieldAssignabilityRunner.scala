f477a2e4252c10a6616e4a82a48fd79b8dd0fde1
roterEmil <roth@st.informatik.tu-darmstadt.de>
2022-11-23 13:35:36 +0100

    optimization

--- DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/FieldAssignabilityRunner.scala
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/FieldAssignabilityRunner.scala
@@ -13,29 +13,23 @@ import org.opalj.br.fpcf.analyses.LazyL0CompileTimeConstancyAnalysis
 import org.opalj.br.fpcf.analyses.LazyStaticDataUsageAnalysis
 import org.opalj.br.fpcf.properties.immutability.Assignable
 import org.opalj.br.fpcf.properties.immutability.EffectivelyNonAssignable
-//import org.opalj.br.fpcf.properties.immutability.FieldImmutability
 import org.opalj.br.fpcf.properties.immutability.LazilyInitialized
 import org.opalj.br.fpcf.properties.immutability.NonAssignable
 import org.opalj.br.fpcf.properties.immutability.UnsafelyLazilyInitialized
 import org.opalj.tac.fpcf.analyses.escape.LazySimpleEscapeAnalysis
-import org.opalj.tac.fpcf.analyses.immutability.LazyClassImmutabilityAnalysis
-import org.opalj.tac.fpcf.analyses.immutability.LazyL0FieldImmutabilityAnalysis
-import org.opalj.tac.fpcf.analyses.immutability.LazyTypeImmutabilityAnalysis
 import org.opalj.tac.fpcf.analyses.immutability.field_assignability.LazyL2FieldAssignabilityAnalysis
 
 /**
- * Computes the field immutability; see [[FieldImmutability]] for details.
+ * Computes the field assignability.
  *
  * @author Dominik Helm
  * @author Tobias Roth
  */
-object FieldImmutabilityRunner extends ProjectAnalysisApplication {
+object FieldAssignabilityRunner extends ProjectAnalysisApplication {
 
     override def title: String = "Field immutability"
 
-    override def description: String = {
-        "Provides information about the immutability of fields."
-    }
+    override def description: String = {"Provides information about the immutability of fields." }
 
     override def doAnalyze(
         project:       Project[URL],
@@ -46,9 +40,6 @@ object FieldImmutabilityRunner extends ProjectAnalysisApplication {
         project.get(RTACallGraphKey)
         val (ps, _) = project.get(FPCFAnalysesManagerKey).runAll(
             LazyL2FieldAssignabilityAnalysis,
-            LazyL0FieldImmutabilityAnalysis,
-            LazyClassImmutabilityAnalysis,
-            LazyTypeImmutabilityAnalysis,
             LazyStaticDataUsageAnalysis,
             LazyL0CompileTimeConstancyAnalysis,
             LazySimpleEscapeAnalysis
