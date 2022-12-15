971c3a4e270ed8e8114e7f8c8a85f5713a94a611
Nicolas Gross <nicolas@fam-gross.org>
2022-12-15 10:47:10 +0100

    Remove duplicate sink handling

--- OPAL/ll/src/main/scala/org/opalj/ll/fpcf/analyses/ifds/taint/NativeBackwardTaintAnalysis.scala
+++ OPAL/ll/src/main/scala/org/opalj/ll/fpcf/analyses/ifds/taint/NativeBackwardTaintAnalysis.scala
@@ -40,10 +40,7 @@ class SimpleNativeBackwardTaintProblem(p: SomeProject) extends NativeBackwardTai
         // create flow facts if callee is source or sink
         val callInstr = call.instruction.asInstanceOf[Call]
         val callees = icfg.resolveCallee(callInstr)
-        if (callees.exists(_.name == "sink")) {
-            // taint variable that is put into sink
-            Some(NativeVariable(callInstr.argument(0).get))
-        } else if (callees.exists(_.name == "source")) in match {
+        if (callees.exists(_.name == "source")) in match {
             // create flow fact if source is reached with tainted value
             case NativeVariable(value) if value == call.instruction && !unbCallChain.contains(call.callable) =>
                 Some(NativeFlowFact(unbCallChain.prepended(call.callable)))
