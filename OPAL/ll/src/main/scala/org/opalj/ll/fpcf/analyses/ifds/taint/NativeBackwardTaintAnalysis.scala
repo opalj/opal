f886424987acc4df4b4649ff1efd96f40435aec1
Nicolas Gross <nicolas@fam-gross.org>
2022-12-15 15:05:01 +0100

    Rename SimpleCallGraphKey -> SimpleNativeCallGraphKey

--- OPAL/ll/src/main/scala/org/opalj/ll/fpcf/analyses/ifds/taint/NativeBackwardTaintAnalysis.scala
+++ OPAL/ll/src/main/scala/org/opalj/ll/fpcf/analyses/ifds/taint/NativeBackwardTaintAnalysis.scala
@@ -4,7 +4,7 @@ package org.opalj.ll.fpcf.analyses.ifds.taint
 import org.opalj.br.analyses.{ProjectInformationKeys, SomeProject}
 import org.opalj.fpcf.{PropertyBounds, PropertyKey, PropertyStore}
 import org.opalj.ifds.{Callable, IFDSFact, IFDSPropertyMetaInformation}
-import org.opalj.ll.fpcf.analyses.cg.SimpleCallGraphKey
+import org.opalj.ll.fpcf.analyses.cg.SimpleNativeCallGraphKey
 import org.opalj.ll.fpcf.analyses.ifds.{LLVMFunction, LLVMStatement, NativeFunction, NativeIFDSAnalysis, NativeIFDSAnalysisScheduler}
 import org.opalj.ll.fpcf.properties.NativeTaint
 import org.opalj.ll.llvm.value.Call
@@ -60,5 +60,5 @@ object NativeBackwardTaintAnalysisScheduler extends NativeIFDSAnalysisScheduler[
     override def init(p: SomeProject, ps: PropertyStore) = new SimpleNativeBackwardTaintAnalysis(p)
     override def property: IFDSPropertyMetaInformation[LLVMStatement, NativeTaintFact] = NativeTaint
     override val uses: Set[PropertyBounds] = Set()
-    override def requiredProjectInformation: ProjectInformationKeys = SimpleCallGraphKey +: super.requiredProjectInformation
+    override def requiredProjectInformation: ProjectInformationKeys = SimpleNativeCallGraphKey +: super.requiredProjectInformation
 }
