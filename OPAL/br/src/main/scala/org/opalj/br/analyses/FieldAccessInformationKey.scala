a85b7162558869dc23e8d8834a58a6319440d998
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-06-05 15:09:31 +0200

    Convert project information key to new analysis

--- OPAL/br/src/main/scala/org/opalj/br/analyses/FieldAccessInformationKey.scala
+++ OPAL/br/src/main/scala/org/opalj/br/analyses/FieldAccessInformationKey.scala
@@ -3,7 +3,12 @@ package org.opalj
 package br
 package analyses
 
-import org.opalj.concurrent.defaultIsInterrupted
+import org.opalj.br.fpcf.FPCFAnalysesManagerKey
+import org.opalj.br.fpcf.properties.{FieldAccessInformation => IndividualFieldAccessInformation}
+import org.opalj.br.fpcf.analyses.EagerFieldAccessInformationAnalysis
+import org.opalj.fpcf.FinalEP
+
+import scala.collection.mutable
 
 /**
  * The ''key'' object to get global field access information.
@@ -14,18 +19,27 @@ import org.opalj.concurrent.defaultIsInterrupted
  */
 object FieldAccessInformationKey extends ProjectInformationKey[FieldAccessInformation, Nothing] {
 
-    /**
-     * The [[FieldAccessInformationAnalysis]] has no special prerequisites.
-     *
-     * @return `Nil`.
-     */
-    override def requirements(project: SomeProject): Seq[ProjectInformationKey[Nothing, Nothing]] = Nil
+    override def requirements(project: SomeProject): ProjectInformationKeys =
+      EagerFieldAccessInformationAnalysis.requiredProjectInformation
 
     /**
      * Computes the field access information.
      */
     override def compute(project: SomeProject): FieldAccessInformation = {
-        FieldAccessInformationAnalysis.doAnalyze(project, defaultIsInterrupted)
+        val propertyStore = project.get(FPCFAnalysesManagerKey).runAll(EagerFieldAccessInformationAnalysis)._1
+
+        propertyStore.waitOnPhaseCompletion()
+
+        val allReadAccesses = mutable.AnyRefMap.empty[Field, Seq[(Method, PCs)]]
+        val allWriteAccesses = mutable.AnyRefMap.empty[Field, Seq[(Method, PCs)]]
+        propertyStore.entities(IndividualFieldAccessInformation.key) foreach {
+          case FinalEP(e, p) =>
+            allReadAccesses.put(e.asInstanceOf[Field], p.readAccesses.toSeq)
+            allWriteAccesses.put(e.asInstanceOf[Field], p.writeAccesses.toSeq)
+          case _ =>
+        }
+
+       new FieldAccessInformation(project, allReadAccesses, allWriteAccesses, unresolved = Vector())
     }
 }
 
