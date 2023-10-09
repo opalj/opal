05ae5be09fa9d7f306374954ac35b6a6dee84f51
Julius Naeumann <julius.naeumann@gmail.com>
2023-10-09 14:05:25 +0200

    Add Unit Test for ThreadRelatedCallsAnalysis: check for callgraph edge to runnable run(),  check that ThreadGroup.unhandledException() is marked as VMReachable

---
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/threads/CreateThreadGroup.java
@@ -0,0 +1,38 @@
+package org.opalj.fpcf.fixtures.threads;
+
+
+import org.opalj.fpcf.properties.callgraph.DirectCall;
+import org.opalj.fpcf.properties.callgraph.VMReachable;
+
+public class CreateThreadGroup {
+    @DirectCall(
+            name = "run",
+            line = 17,
+            resolvedTargets = {
+                    "Lorg/opalj/fpcf/fixtures/threads/MyRunnable;"})
+    public static void main(String[] args) {
+        ThreadGroup myThreadGroup = new MyThreadGroup();
+        Thread testThread = new Thread(myThreadGroup, new MyRunnable());
+        testThread.start();
+    }
+}
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello");
+    }
+}
+class MyThreadGroup extends ThreadGroup {
+
+
+    public MyThreadGroup() {
+        super("mythreadgroup");
+    }
+
+    @VMReachable
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        super.uncaughtException(t, e);
+    }
+}
