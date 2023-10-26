ea34e1d87db58e80caa415f6672723081c8ff923
roterEmil <roth@st.informatik.tu-darmstadt.de>
2023-10-26 09:46:26 +0200

    current status

--- OPAL/xl/src/main/scala/org/opalj/xl/Coordinator.scala
+++ OPAL/xl/src/main/scala/org/opalj/xl/Coordinator.scala
@@ -2,10 +2,15 @@
 package org.opalj.xl
 
 import java.net.URL
+
 import org.opalj.xl.javaanalyses.detector.scriptengine.AllocationSiteBasedApiScriptEngineDetectorScheduler
+import org.opalj.xl.utility.AnalysisResult
+import org.opalj.xl.utility.InterimAnalysisResult
 
 import org.opalj.log.GlobalLogContext
 import org.opalj.log.LogContext
+import org.opalj.fpcf.FinalEP
+import org.opalj.fpcf.FinalP
 import org.opalj.value.ValueInformation
 import org.opalj.br.analyses.AnalysisApplication
 import org.opalj.br.analyses.BasicReport
@@ -14,11 +19,17 @@ import org.opalj.br.analyses.Project
 import org.opalj.br.analyses.ReportableAnalysisResult
 import org.opalj.br.fpcf.FPCFAnalysesManagerKey
 import org.opalj.br.fpcf.FPCFAnalysisScheduler
+import org.opalj.br.fpcf.properties.pointsto.AllocationSitePointsToSet
+import org.opalj.br.ReferenceType
+import org.opalj.br.fpcf.properties.pointsto
 import org.opalj.tac.DUVar
 import org.opalj.tac.cg.AllocationSiteBasedPointsToCallGraphKey
 import org.opalj.tac.cg.TypeIteratorKey
+import org.opalj.tac.common.DefinitionSite
+import org.opalj.tac.common.DefinitionSitesKey
 import org.opalj.tac.fpcf.analyses.LazyTACAIProvider
 import org.opalj.tac.fpcf.analyses.cg.AllocationSitesPointsToTypeIterator
+import org.opalj.tac.ComputeTACAIKey
 
 object Coordinator extends AnalysisApplication with OneStepAnalysis[URL, ReportableAnalysisResult] {
 
@@ -44,36 +55,40 @@ object Coordinator extends AnalysisApplication with OneStepAnalysis[URL, Reporta
             AllocationSiteBasedTriggeredTajsConnectorScheduler
         )
 
-        val (_ /*propertyStore*/ , _) = project.get(FPCFAnalysesManagerKey).runAll(analyses)
-        /*
-        val defSites = project.get(DefinitionSitesKey).definitionSites.keySet()
+        val (propertyStore, _) = project.get(FPCFAnalysesManagerKey).runAll(analyses)
 
+        val tacaiKey = project.get(ComputeTACAIKey)
+
+        val defSites = project.get(DefinitionSitesKey).definitionSites.keySet()
+        println()
+        println("TAJS results:")
         project.allProjectClassFiles.flatMap(_.methods).foreach(method => {
             propertyStore(method, AnalysisResult.key) match {
-                case FinalP(FinalAnalysisResult(s)) =>
+                case FinalP(InterimAnalysisResult(s)) =>
                     println(s"TAJS result: $s")
-                case _ =>
+                case x => println(x)
             }
-        }) */
-
-        /*defSites.forEach(defSite => {
+        })
+        println()
+        println("OPAL results:")
+        defSites.forEach(defSite => {
             propertyStore(defSite, AllocationSitePointsToSet.key) match {
                 case FinalEP(DefinitionSite(method, pc), pointsToSet) =>
-                    if (method.name.startsWith("main") && pointsToSet.elements.size > 1)
+                    val taCode = tacaiKey(method)
+                    val stmts = taCode.stmts
+                    if (pointsToSet.elements.size > 0)
                         println(s"""
-                        | ===================================
-                        | OPAL results:
                         | method ${defSite.method} pc ${defSite.pc}
-                        | ${
+                        | DefSite: ${stmts(taCode.pcToIndex(defSite.pc)).asAssignment.targetVar}
+                        | PointsToSet (${pointsToSet.numElements}): ${
                             pointsToSet.elements.iterator
                                 .map(long => ReferenceType.lookup(pointsto.allocationSiteLongToTypeId(long)))
                                 .mkString("\n")
                         }
-                        | ===================================
-                        |""".stripMargin)
-                case _ =>
+                        | =================================== """.stripMargin)
+                case x => println(x)
             }
-        }) */
+        })
         BasicReport("")
     }
 }
