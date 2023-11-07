0dad13c99fbebab4e36079752ee48eeb7de5ebbb
Julius Naeumann <julius.naeumann@gmail.com>
2023-11-07 16:53:04 +0100

    print out all points-to-sets in XLJavaScriptTests for evaluation. add XLNoJavaScriptBenchmark test (XLJavaScriptTests without javascript analyses). todo: proper output, maybe to file?

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/xltest/XLJavaScriptTests.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/xltest/XLJavaScriptTests.scala
@@ -4,33 +4,25 @@ package fpcf
 package xltest
 
 import java.net.URL
-
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import org.opalj.xl.AllocationSiteBasedTriggeredTajsConnectorScheduler
 import org.opalj.xl.javaanalyses.detector.scriptengine.AllocationSiteBasedScriptEngineDetectorScheduler
-
 import org.opalj.fpcf.PropertiesTest
 import org.opalj.fpcf.PropertyStore
 import org.opalj.fpcf.SomeEPS
 import org.opalj.br.DefinedMethod
-import org.opalj.br.analyses.DeclaredMethodsKey
 import org.opalj.br.analyses.Project
 import org.opalj.br.fpcf.FPCFAnalysisScheduler
 import org.opalj.br.fpcf.properties.SimpleContext
-import org.opalj.br.fpcf.properties.SimpleContextsKey
-import org.opalj.br.fpcf.properties.pointsto.PointsToSetLike
+import org.opalj.br.fpcf.properties.pointsto.{AllocationSitePointsToSet, PointsToSetLike}
 import org.opalj.tac.cg.AllocationSiteBasedPointsToCallGraphKey
 import org.opalj.tac.cg.TypeIteratorKey
 import org.opalj.tac.common.DefinitionSite
-import org.opalj.tac.common.DefinitionSitesKey
 import org.opalj.tac.fpcf.analyses.LazyTACAIProvider
 import org.opalj.tac.fpcf.analyses.cg.AllocationSitesPointsToTypeIterator
-import org.opalj.tac.fpcf.analyses.cg.TypeIterator
 import org.opalj.tac.fpcf.analyses.cg.xta.TypePropagationAnalysisScheduler
 import org.opalj.tac.fpcf.analyses.cg.xta.XTASetEntitySelector
-import org.opalj.tac.fpcf.properties.TACAI
-import org.opalj.tac.fpcf.properties.cg.Callees
 
 /**
  * Tests XL interaction by validating Points-to-sets
@@ -53,6 +45,13 @@ class XLJavaScriptTests extends PropertiesTest {
             case _ => () => new AllocationSitesPointsToTypeIterator(p)
         }
     }
+    def addAnalyses():  Iterable[FPCFAnalysisScheduler] = {
+         Iterable(
+            AllocationSiteBasedScriptEngineDetectorScheduler,
+            AllocationSiteBasedTriggeredTajsConnectorScheduler,
+            new TypePropagationAnalysisScheduler(XTASetEntitySelector)
+        )
+    }
 
     describe("test JavaScript XL points-to-sets") {
         val statistics =
@@ -63,11 +62,7 @@ class XLJavaScriptTests extends PropertiesTest {
         info(statistics)
         var analyses: List[FPCFAnalysisScheduler] = List(LazyTACAIProvider)
         analyses ++= AllocationSiteBasedPointsToCallGraphKey.allCallGraphAnalyses(FixtureProject)
-        analyses ++= Iterable(
-            AllocationSiteBasedScriptEngineDetectorScheduler,
-            AllocationSiteBasedTriggeredTajsConnectorScheduler,
-            new TypePropagationAnalysisScheduler(XTASetEntitySelector)
-        )
+        analyses ++= addAnalyses()
         val as = executeAnalyses(
             analyses
         )
@@ -91,11 +86,6 @@ class XLJavaScriptTests extends PropertiesTest {
 
         val allContexts = allEntities.filter(_.isInstanceOf[SimpleContext]).map(_.asInstanceOf[SimpleContext])
         println((allEntities, definedMethods, allContexts))
-        val allocSites = as.propertyStore.properties(as.project.get(DefinitionSitesKey).getAllocationSites).toList
-        val contexts = as.propertyStore.properties(as.project.get(SimpleContextsKey)).toList
-        val methods = as.propertyStore.properties(as.project.allMethods).toList
-        val fields = as.propertyStore.properties(as.project.allFields).toList
-        val pts = allocSites.find(_.isInstanceOf[PointsToSetLike[_, _, _]])
 
         for (c <- allContexts) {
             val epss = as.propertyStore.properties(c).toIndexedSeq
@@ -107,52 +97,16 @@ class XLJavaScriptTests extends PropertiesTest {
             println(contextP)
         }
         val _ = as.project.allProjectClassFiles.flatMap(_.methods)
-        implicit val typeIterator: TypeIterator = as.project.get(TypeIteratorKey)
         implicit val ps: PropertyStore = as.propertyStore
 
         for (m <- definedMethods) {
-            val epss = as.propertyStore.properties(m).toIndexedSeq
-
-            val properties = epss.map(_.toFinalEP.p)
-            val calleesP = properties.find(_.isInstanceOf[Callees]).map(_.asInstanceOf[Callees])
-            val aitac = properties.find(_.isInstanceOf[TACAI])
-            if (aitac.nonEmpty) {
-                val tacs = aitac.get.asInstanceOf[TACAI]
-                val props = tacs.tac.get.stmts.map(as.propertyStore.properties).map(_.toList)
-                for (stmt <- tacs.tac.get.stmts) {
-                    val p = as.propertyStore.properties(stmt)
-                    println(p.toList)
-                }
-                println(props)
-            }
-
-            if (calleesP.nonEmpty) {
-                val callees = calleesP.get
-
-                val call = for {
-                    callerContext <- callees.callerContexts
-                    pc <- callees.callSitePCs(callerContext)
-                    calleeContext <- callees.callees(callerContext, pc)
-                } yield {
-                    val callee = calleeContext.method
-                    (
-                        callee.declaringClassType.toJVMTypeName,
-                        callee.name,
-                        callee.descriptor
-                    )
-                }
-                val resolvedCalls = call.toList
-                println(resolvedCalls)
-            }
-
-            print(calleesP)
-            println(aitac)
-
-            val codeEps = m.definedMethod.body.map(as.propertyStore.properties).map(_.toIndexedSeq)
-            println(codeEps)
+            val dm = m.definedMethod
+            val defsitesInMethod = ps.entities(propertyFilter = _.e.isInstanceOf[DefinitionSite]).map(_.asInstanceOf[DefinitionSite]).filter(_.method == dm).toSet
+            println(defsitesInMethod)
+            val pts = defsitesInMethod.map(defsite => (defsite, ps.properties(defsite).map(_.toFinalEP.p).find(_.isInstanceOf[AllocationSitePointsToSet]).map(_.asInstanceOf[AllocationSitePointsToSet])))
+            println("points to sets: ")
+            println(pts)
         }
-        as.project.get(DeclaredMethodsKey)
-        println((pts, contexts, methods, fields))
         validateProperties(as, methodsWithAnnotations(as.project), Set("PointsToSetIncludes"))
 
     }
