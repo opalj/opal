f8caf97578f04ed792cc1a3609ea8b7015426e58
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2023-11-23 15:49:09 +0100

    (MB) remove unnecessary imports

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/fieldassignability/L1FieldAssignabilityAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/fieldassignability/L1FieldAssignabilityAnalysis.scala
@@ -12,9 +12,6 @@ import org.opalj.br.fpcf.BasicFPCFLazyAnalysisScheduler
 import org.opalj.br.fpcf.FPCFAnalysis
 import org.opalj.fpcf.PropertyBounds
 import org.opalj.fpcf.PropertyStore
-import org.opalj.tac.TACMethodParameter
-import org.opalj.tac.TACode
-import org.opalj.tac.SelfReferenceParameter
 import org.opalj.br.Field
 import org.opalj.br.PC
 import org.opalj.br.fpcf.properties.cg.Callers
