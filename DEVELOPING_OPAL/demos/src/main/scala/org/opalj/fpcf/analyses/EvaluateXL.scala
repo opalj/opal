fcc13b26dd59ab8085a590712af86ac4e0f0527e
Julius Naeumann <julius.naeumann@gmail.com>
2023-11-27 12:12:56 +0100

    cleanup

--- DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/EvaluateXL.scala
+++ DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/EvaluateXL.scala
@@ -117,7 +117,7 @@ object GroundTruthParser {
 
     for (line <- Source.fromFile(groundTruthPath).getLines()) {
       methodPattern.findFirstMatchIn(line) match {
-        case Some(m) /* if !m.group(1).contains("$string_concat$") */ =>
+        case Some(m) =>
           val signature = m.group(1)
           val methodId = m.group(2).toInt
           dataset.addMethod(methodId, new Method(methodId, signature))
@@ -193,7 +193,6 @@ object GroundTruthParser {
         instanceIdToAllocSite.put(event.instanceId, Option.empty)
       }
     }
-    // expected: allocsite 175924209254439 <=> inst 44563007 (Vain.main, PC 35)
     (allocSiteToInstanceId.toMap, instanceIdToAllocSite.toMap)
 
   }
@@ -227,7 +226,6 @@ object GroundTruthParser {
       }
     }
     val (allocSiteToInstanceIds, instanceIdToAllocSite): (Map[Long, Set[Int]], Map[Int, Option[Long]]) = generateMapping(groundTruthData.toSet, testData.toSet)
-    //implicit val typeIterator: TypeIterator = project.get(TypeIteratorKey)
     val results = mutable.Map[Int, MethodResult]()
     for ((method, testPTS) <- testData.groupBy(_.defsiteMethodSignature).filter(m => !m._1.contains("$string_concat$"))) {
       val truePositiveData = mutable.Set.empty[TruePositive]
@@ -237,8 +235,6 @@ object GroundTruthParser {
       val instanceIdsToAllocSiteForMethod = mutable.Map[Set[Instance], Option[Long]]()
       val methodId = signatureToMethodId(method)
       val groundTruthMethod = groundTruth.methods(methodId)
-      //val instancesAndPossibleAllocSites = instances.map(instance => (instanceIdToAllocSite.getOrElse(instance.instanceId, Option.empty), instance.instanceId, normalizeType(instance.objClass)))
-      ///val possibleAllocSitesForInstanceIds = instancesAndPossibleAllocSites.groupBy(allocSiteInstIdType => (allocSiteInstIdType._1, allocSiteInstIdType._3)).view.mapValues(instances => instances.map(inst => new Instance(inst._2, inst._3))).toMap
 
       val testDataCoveredInstances = mutable.Set[(Int, Int)]()
       for (defsite <- testPTS) {
@@ -297,98 +293,6 @@ object GroundTruthParser {
     }
     results.toMap
   }
-    /*
-      all.get((Option(allocSiteId), normalizeType(allocSiteType))) match {
-        case Some(groundTruthInstances) =>
-          allocSiteToInstanceIdsForMethod.put(allocSiteId, groundTruthInstances)
-          //val groundTruthType = groundTruthInstanceIds(groundTruthIds.find(_ => true).get)
-          //val groundTruthTypeNormalized = normalizeType(groundTruthType)
-          testAllocsitesForPC add(allocSiteId, allocSiteType)
-          //if (normalizeType(allocSiteType) == groundTruthTypeNormalized) {
-          truePositiveData += TruePositive(method.signature, pc, groundTruthInstances, allocSiteId, pc, allocSiteType)
-        //} else {
-        //  falsePositiveData += FalsePositive(method.signature, defsitePC, allocSiteType, allocSiteId, pc)
-        //}
-        case _ => {
-          allocSiteToInstanceIdsForMethod.put(allocSiteId, Set.empty)
-          falsePositiveData += FalsePositive(method.signature, pc, allocSiteType, allocSiteId, pc)
-        }
-      }
-    }
-      /*
-    groundTruth.methods.foldLeft(Map.empty[Int, MethodResult]) { case (results, (methodId, method)) =>
-      val truePositiveData = mutable.Set.empty[TruePositive]
-      val falsePositiveData = mutable.Set.empty[FalsePositive]
-      val falseNegativeData = mutable.Set.empty[FalseNegative]
-      val testPTSforMethod = groupedWithoutByMethod.get(method.signature)
-      val allocSiteToInstanceIdsForMethod = mutable.Map[Long, Set[Instance]]()
-      val instanceIdsToAllocSiteForMethod = mutable.Map[Set[Instance], Option[Long]]()
-
-      for ((pc, instances) <- method.pcToInstances) {
-        //val groundTruthInstanceIds = instances.map(instance => instance.instanceId -> normalizeType(instance.objClass)).toMap
-        val instancesAndPossibleAllocSites = instances.map(instance => (instanceIdToAllocSite.getOrElse(instance.instanceId, Option.empty), instance.instanceId, normalizeType(instance.objClass)))
-        val possibleAllocSitesForInstanceIds = instancesAndPossibleAllocSites.groupBy(allocSiteInstIdType => (allocSiteInstIdType._1, allocSiteInstIdType._3)).view.mapValues(instances =>  instances.map(inst => new Instance(inst._2, inst._3))).toMap
-        val testPTSforPC = testPTSforMethod.map(pt => pt.filter(_._2.pc == pc))
-        val testAllocsitesForPC = mutable.Set[(Long, String)]()
-        val allocSites = testPTSforPC.getOrElse(Set.empty).flatMap(as => as._3)
-        for (as <- allocSites) {
-           for (allocSiteId <- as.elements.iterator) {
-              val (ctx, allocSitePC, typeId) = longToAllocationSite(allocSiteId)
-              val allocSiteType = normalizeType(ObjectType.lookup(typeId).toJava)
-              possibleAllocSitesForInstanceIds.get((Option(allocSiteId), normalizeType(allocSiteType))) match {
-                case Some(groundTruthInstances) =>
-                  allocSiteToInstanceIdsForMethod.put(allocSiteId, groundTruthInstances)
-                  //val groundTruthType = groundTruthInstanceIds(groundTruthIds.find(_ => true).get)
-                  //val groundTruthTypeNormalized = normalizeType(groundTruthType)
-                  testAllocsitesForPC add (allocSiteId , allocSiteType)
-                  //if (normalizeType(allocSiteType) == groundTruthTypeNormalized) {
-                    truePositiveData += TruePositive(method.signature, pc, groundTruthInstances, allocSiteId, pc, allocSiteType)
-                  //} else {
-                  //  falsePositiveData += FalsePositive(method.signature, defsitePC, allocSiteType, allocSiteId, pc)
-                  //}
-                case _ => {
-                  allocSiteToInstanceIdsForMethod.put(allocSiteId, Set.empty)
-                  falsePositiveData += FalsePositive(method.signature, pc, allocSiteType, allocSiteId, pc)
-                }
-              }
-            }
-        }
-        instances.map(inst => ())
-
-        for ((possibleAllocSite, groundTruthInstances) <- possibleAllocSitesForInstanceIds) {
-          instanceIdsToAllocSiteForMethod.put(groundTruthInstances, possibleAllocSite._1)
-          if ( possibleAllocSite._1.isEmpty || !testAllocsitesForPC.contains((possibleAllocSite._1.get, possibleAllocSite._2))) {
-            falseNegativeData += FalseNegative(method.signature, pc, groundTruthInstances)
-          } else {
-            print("")
-          }
-        }
-
-
-
-      }
-      val truePositive = truePositiveData.size
-      val falsePositive = falsePositiveData.size
-      val falseNegative = falseNegativeData.size
-      val precision = if (truePositive + falsePositive > 0) truePositive.toDouble / (truePositive + falsePositive) else 0
-      val recall = if (truePositive + falseNegative > 0) truePositive.toDouble / (truePositive + falseNegative) else 0
-
-      results + (methodId -> MethodResult(
-        method.signature,
-        precision,
-        recall,
-        truePositive,
-        falsePositive,
-        falseNegative,
-        truePositiveData.toSet,
-        falsePositiveData.filter(!ignoreFalsePositive(_)).toSet,
-        falseNegativeData.filter(!ignoreFalseNegative(_)).toSet,
-        allocSiteToInstanceIdsForMethod,
-        instanceIdsToAllocSiteForMethod
-      )) */
-
-    }
-  */
 
   def printTotalPrecisionRecall(results: Map[Int, MethodResult]): Unit = {
     val truePositive = results.values.map(_.truePositive).sum
@@ -411,30 +315,15 @@ object ComparePTS {
     val withTAJS = new PointsToAnalysisRunner()
     withoutTAJS.main(args)
 
-    //val groupedWithoutByMethod = withoutTAJS.pts.groupBy(_._1.toString)
-    /*for (elem <- groupedWithoutByMethod) {
-      println(elem._1)
-      val sorted = elem._2.toArray.sortBy(mdsas => mdsas._2.pc)
-      for (pts <- sorted) {
-        println("  " + pts._2.pc + "  " + pts._3.toString)
-
-      }
-    }*/
 
      val evalWithoutTAJS = GroundTruthParser.evaluate(groundTruth, withoutTAJS.pts, withoutTAJS.p)
     withTAJS.main(args ++ Iterable("RunTAJS"))
     val evalWithTAJS = GroundTruthParser.evaluate(groundTruth, withTAJS.pts, withTAJS.p)
 
-    //val groupedWithout = withoutTAJS.pts.groupBy(_._2.method.signature.toString)
-    //val groupedWith = withTAJS.pts.groupBy(_._2.method.signature.toString)
-    //val combinedMap = (groupedWithout.keySet ++ groupedWith.keySet).map(ds => (ds, groupedWithout.get(ds).map(_.map(_._3)), groupedWith.get(ds).map(_.map(_._3))))
-    //val differences = combinedMap.filter(comparison => comparison._2.toString != comparison._3.toString)
     println(s"without TAJS: ${withoutTAJS.pts.count(_._3.exists(_.types.nonEmpty))} non-empty pts")
     println(s"<pointstotrace id=\"withoutTAJS\" />")
-    //withoutTAJS.printPointsToSet()
     println(s"with TAJS: ${withTAJS.pts.count(_._3.exists(_.types.nonEmpty))} non-empty pts")
     println(s"<pointstotrace id=\"withTAJS\" />")
-    //withTAJS.printPointsToSet()
 
     for ((id) <-  evalWithoutTAJS.keys.toList.sorted) {
         val resultWithoutTAJS = evalWithoutTAJS(id)
@@ -457,9 +346,6 @@ object ComparePTS {
       }
     }
 
-
-
-
     println("without TAJS")
     GroundTruthParser.printTotalPrecisionRecall(evalWithoutTAJS)
     println("with TAJS")
@@ -474,10 +360,6 @@ class PointsToAnalysisRunner extends ProjectAnalysisApplication {
 
   override def checkAnalysisSpecificParameters(parameters: Seq[String]): Iterable[String] = {
     Iterable()
-  }
-
-  def printPointsToSet() = {
-
   }
 
   override def doAnalyze(project: Project[URL], parameters: Seq[String], isInterrupted: () => Boolean): BasicReport = {
@@ -508,7 +390,6 @@ class PointsToAnalysisRunner extends ProjectAnalysisApplication {
     val filter: SomeEPS => Boolean = _ => true
     val allEntities = ps1.entities(propertyFilter = filter).toList
 
-    //val defSites = allEntities.filter(_.isInstanceOf[DefinitionSite]).map(_.asInstanceOf[DefinitionSite])
     val definedMethods = allEntities.filter(_.isInstanceOf[DefinedMethod]).map(_.asInstanceOf[DefinedMethod])
 
     var results = ""
@@ -518,7 +399,6 @@ class PointsToAnalysisRunner extends ProjectAnalysisApplication {
       val ptsInMethod = defsitesInMethod.toArray.map(defsite => (m, defsite, ps1.properties(defsite).map(_.toFinalEP.p).find(_.isInstanceOf[AllocationSitePointsToSet]).map(_.asInstanceOf[AllocationSitePointsToSet])))
       pts ++= ptsInMethod
       results += m.name + "\n"
-      // results += "points to sets: " + pts
     }
 
     BasicReport(
