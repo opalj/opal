ae438e26646e8e732f2af2e84046b1aa67c16f35
Julius Naeumann <julius.naeumann@gmail.com>
2023-12-06 16:12:51 +0100

    fix test case line number

--- DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/xl/js/stateaccess/intraprocedural/unidirectional/JSAccessJava/JavaAllocationWriteFieldFromJS.java
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/xl/js/stateaccess/intraprocedural/unidirectional/JSAccessJava/JavaAllocationWriteFieldFromJS.java
@@ -14,7 +14,7 @@ import javax.script.ScriptException;
  *
  */
 public class JavaAllocationWriteFieldFromJS {
-    @PointsToSet(variableDefinition = 37,
+    @PointsToSet(variableDefinition = 36,
             expectedJavaAllocSites = {
                     @JavaMethodContextAllocSite(
                             cf = JavaAllocationWriteFieldFromJS.class,
