852ec2fbe0dd0573784fc447d0fd0033bebd36d7
LukasKlenner <klenner-lukas@t-online.de>
2024-03-19 15:37:01 +0100

    move AliasEntities to br; improve AliasSourceElement options

--- OPAL/br/src/main/scala/org/opalj/br/fpcf/properties/alias/AliasEntity.scala
+++ OPAL/br/src/main/scala/org/opalj/br/fpcf/properties/alias/AliasEntity.scala
@@ -1,50 +1,70 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
 package org.opalj
-package tac
+package br
 package fpcf
-package analyses
+package properties
 package alias
-
-import org.opalj.br.fpcf.properties.Context
 
 import scala.runtime.ScalaRunTime
 
+import org.opalj.br.fpcf.properties.Context
+
 /**
- * Represents a pair of [[AliasSourceElement]]s and a [[Context]] to which an alias relationship can be assigned.
+ * Represents a pair of [[AliasSourceElement]]s and a [[Context]] for each the elements to which an alias relationship can be assigned.
  * It is used to query and store the associated alias property in the property store.
  * The order of the elements is irrelevant, as the alias property is symmetric.
  * To ensure this, the given elements might be swapped internally.
  *
- * @param context The [[Context]] in which the alias relationship is valid.
  * @param e1 The first [[AliasSourceElement]] to which the alias relationship is assigned.
  * @param e2 The second [[AliasSourceElement]] to which the alias relationship is assigned.
+ * @param c1 The [[Context]] for the first element in which the alias relationship is valid.
+ * @param c2 The [[Context]] for the second element in which the alias relationship is valid.
  */
-class AliasEntity(val context: Context, private val e1: AliasSourceElement, private val e2: AliasSourceElement) {
+class AliasEntity(
+    private val c1: Context,
+    private val c2: Context,
+    private val e1: AliasSourceElement,
+    private val e2: AliasSourceElement
+) {
 
     /**
      * A copy of two elements of this [[AliasEntity]].
      * It is used to ensure that the order of the elements is irrelevant.
      */
-    private val (_element1, _element2) = (e1, e2) match {
-        case (e1: AliasReturnValue, e2) if !e2.isInstanceOf[AliasReturnValue] => (e1, e2)
-        case (e1, e2: AliasReturnValue) if !e1.isInstanceOf[AliasReturnValue] => (e2, e1)
-        case (e1: AliasNull, e2) => (e1, e2)
-        case (e1, e2: AliasNull) => (e2, e1)
+    private[this] val (_element1, _element2) = (e1, e2) match {
+        case (AliasNull, e2)                           => (AliasNull, e2)
+        case (e1, AliasNull)                           => (AliasNull, e1)
         case (e1, e2) if e1.hashCode() < e2.hashCode() => (e1, e2)
-        case (e1, e2) => (e2, e1)
+        case (e1, e2)                                  => (e2, e1)
     }
 
     /**
-     * Returns the first [[AliasSourceElement]] of this [[AliasEntity]].
-     * @return The first [[AliasSourceElement]] of this [[AliasEntity]].
+     * A copy of two contexts of this [[AliasEntity]]. It uses the same order as the elements.
+     */
+    private[this] val (_context1, _context2) = (c1, c2) match {
+        case (c1, c2) if e1 == _element1 => (c1, c2)
+        case (c1, c2)                    => (c2, c1)
+    }
+
+    /**
+     * @return the first [[AliasSourceElement]] of this [[AliasEntity]].
      */
     def element1: AliasSourceElement = _element1
 
     /**
-     * Returns the second [[AliasSourceElement]] of this [[AliasEntity]].
-     * @return The second [[AliasSourceElement]] of this [[AliasEntity]].
+     * @return the second [[AliasSourceElement]] of this [[AliasEntity]].
      */
     def element2: AliasSourceElement = _element2
+
+    /**
+     * @return the [[Context]] for the first [[AliasSourceElement]] of this [[AliasEntity]].
+     */
+    def context1: Context = _context1
+
+    /**
+     * @return the [[Context]] for the second [[AliasSourceElement]] of this [[AliasEntity]].
+     */
+    def context2: Context = _context2
 
     /**
      * Checks if the two elements of this [[AliasEntity]] are bound to a method.
@@ -66,18 +86,19 @@ class AliasEntity(val context: Context, private val e1: AliasSourceElement, priv
      */
     def elementsInSameMethod: Boolean = element1.method.eq(element2.method)
 
-    //we can't use a case class because the order of the order of the two elements
+    // we can't use a case class because the order of the two attributes is irrelevant
     override def equals(other: Any): Boolean = other match {
         case that: AliasEntity =>
             that.isInstanceOf[AliasEntity] &&
-                context == that.context &&
-                _element1 == that._element1 &&
-                _element2 == that._element2
+                context1 == that.context1 &&
+                context2 == that.context2 &&
+                element1 == that.element1 &&
+                element2 == that.element2
         case _ => false
     }
 
     override def hashCode(): Int = {
-        ScalaRunTime._hashCode((context, _element1, _element2))
+        ScalaRunTime._hashCode((context1, context2, element1, element2))
     }
 }
 
@@ -86,11 +107,13 @@ object AliasEntity {
     /**
      * Creates an [[AliasEntity]] that represents the given pair of [[AliasSourceElement]]s and the given [[Context]].
      *
-     * @param context The [[Context]] in which the alias relationship is valid.
+     * @param c1 The [[Context]] for the first element in which the alias relationship is valid.
+     * @param c2 The [[Context]] for the second element in which the alias relationship is valid.
      * @param e1 The first [[AliasSourceElement]] to which the alias relationship is assigned.
      * @param e2 The second [[AliasSourceElement]] to which the alias relationship is assigned.
      * @return An [[AliasEntity]] that represents the given pair of [[AliasSourceElement]]s and the given [[Context]].
      */
-    def apply(context: Context, e1: AliasSourceElement, e2: AliasSourceElement): AliasEntity = new AliasEntity(context, e1, e2)
+    def apply(c1: Context, c2: Context, e1: AliasSourceElement, e2: AliasSourceElement): AliasEntity =
+        new AliasEntity(c1, c2, e1, e2)
 
 }
