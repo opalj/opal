78305f96db1a73227e0b46b43055204f6112842b
klein648 <lukas.klein@in-serm.de>
2024-05-10 13:17:50 +0200

    Structure expanded

---
+++ TOOLS/ce/src/main/scala/org.opalj.ce/fileLocator.scala
@@ -0,0 +1,9 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj
+package ce
+
+class FileLocator(var filenames : String[]){
+    def LocateConfigurationFiles(): Unit = {
+
+    }
+}
