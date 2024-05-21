75de3db5d06961b206d21068409929d320c7d9f6
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2024-05-21 21:48:58 +0200

    Correctly interpret arguments of setLength

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/l0/interpretation/L0VirtualMethodCallInterpreter.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/l0/interpretation/L0VirtualMethodCallInterpreter.scala
@@ -7,8 +7,13 @@ package string
 package l0
 package interpretation
 
+import org.opalj.br.fpcf.properties.string.StringTreeConst
+import org.opalj.br.fpcf.properties.string.StringTreeNeutralElement
+import org.opalj.br.fpcf.properties.string.StringTreeNode
 import org.opalj.fpcf.ProperPropertyComputationResult
 import org.opalj.tac.fpcf.properties.string.StringFlowFunctionProperty
+import org.opalj.tac.fpcf.properties.string.StringTreeEnvironment
+import org.opalj.value.TheIntegerValue
 
 /**
  * @author Maximilian Rüsch
@@ -21,11 +26,39 @@ object L0VirtualMethodCallInterpreter extends StringInterpreter {
      * Currently, this function supports no method calls. However, it treats [[StringBuilder.setLength]] such that it
      * will return the lower bound for now.
      */
-    override def interpret(instr: T)(implicit state: InterpretationState): ProperPropertyComputationResult = {
-        instr.name match {
-            // IMPROVE interpret argument for setLength
-            case "setLength" => computeFinalLBFor(instr.receiver.asVar)
-            case _           => computeFinalResult(StringFlowFunctionProperty.identity)
+    override def interpret(call: T)(implicit state: InterpretationState): ProperPropertyComputationResult = {
+        val pReceiver = call.receiver.asVar.toPersistentForm(state.tac.stmts)
+
+        call.name match {
+            case "setLength" =>
+                call.params.head.asVar.value match {
+                    case TheIntegerValue(intVal) if intVal == 0 =>
+                        computeFinalResult(StringFlowFunctionProperty.constForVariableAt(
+                            state.pc,
+                            pReceiver,
+                            StringTreeNeutralElement
+                        ))
+
+                    case TheIntegerValue(intVal) =>
+                        computeFinalResult(
+                            PDUWeb(state.pc, pReceiver),
+                            (env: StringTreeEnvironment) => {
+                                env(state.pc, pReceiver) match {
+                                    case StringTreeConst(string) =>
+                                        val sb = new StringBuilder(string)
+                                        sb.setLength(intVal)
+                                        env.update(state.pc, pReceiver, StringTreeConst(sb.toString()))
+                                    case _ =>
+                                        env.update(state.pc, pReceiver, StringTreeNode.lb)
+                                }
+                            }
+                        )
+                    case _ =>
+                        computeFinalResult(StringFlowFunctionProperty.noFlow(state.pc, pReceiver))
+                }
+
+            case _ =>
+                computeFinalResult(StringFlowFunctionProperty.identity)
         }
     }
 }
