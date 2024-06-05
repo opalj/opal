ef57f722d31312df7065361dbcec6cfcc483301e
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2024-06-05 21:23:15 +0200

    Refactor out individual processing functions in data flow analysis

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/flowanalysis/DataFlowAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/flowanalysis/DataFlowAnalysis.scala
@@ -42,49 +42,104 @@ object DataFlowAnalysis {
         val childNodes = controlTree.get(node).diSuccessors.map(_.outer)
         val limitedFlowGraph = superFlowGraph.filter(n => childNodes.contains(n.outer))
 
-        node match {
-            case Statement(pc) if pc >= 0 => flowFunctionByPc(pc)(env)
-            case Statement(_)             => env
+        def processBlock(entry: FlowGraphNode): StringTreeEnvironment = {
+            var currentEnv = env
+            var currentNode = limitedFlowGraph.get(entry)
+            while (currentNode.diSuccessors.nonEmpty) {
+                currentEnv = pipe(currentNode.outer, currentEnv)
+                currentNode = currentNode.diSuccessors.head
+            }
 
-            case Region(Block, _, entry) =>
-                var currentEnv = env
-                var currentNode = limitedFlowGraph.get(entry)
-                while (currentNode.diSuccessors.nonEmpty) {
-                    currentEnv = pipe(currentNode.outer, currentEnv)
-                    currentNode = currentNode.diSuccessors.head
+            pipe(currentNode, currentEnv)
+        }
+
+        def processIfThenElse(entry: FlowGraphNode): StringTreeEnvironment = {
+            val entryNode = limitedFlowGraph.get(entry)
+            val successors = entryNode.diSuccessors.map(_.outer).toList.sorted
+            val branches = (successors.head, successors.tail.head)
+
+            val envAfterEntry = pipe(entry, env)
+            val envAfterBranches = (pipe(branches._1, envAfterEntry), pipe(branches._2, envAfterEntry))
+
+            envAfterBranches._1.join(envAfterBranches._2)
+        }
+
+        def processIfThen(entry: FlowGraphNode): StringTreeEnvironment = {
+            val entryNode = limitedFlowGraph.get(entry)
+            val (yesBranch, noBranch) = if (entryNode.diSuccessors.head.diSuccessors.nonEmpty) {
+                (entryNode.diSuccessors.head, entryNode.diSuccessors.tail.head)
+            } else {
+                (entryNode.diSuccessors.tail.head, entryNode.diSuccessors.head)
+            }
+
+            val envAfterEntry = pipe(entry, env)
+            val envAfterBranches = (
+                pipe(yesBranch.diSuccessors.head, pipe(yesBranch, envAfterEntry)),
+                pipe(noBranch, envAfterEntry)
+            )
+
+            envAfterBranches._1.join(envAfterBranches._2)
+        }
+
+        def processProper(entry: FlowGraphNode): StringTreeEnvironment = {
+            val entryNode = limitedFlowGraph.get(entry)
+
+            var sortedCurrentNodes = List(entryNode)
+            var currentNodeEnvs = Map((entryNode, pipe(entry, env)))
+            while (currentNodeEnvs.keys.exists(_.diSuccessors.nonEmpty)) {
+                val nextNodeEnvs = sortedCurrentNodes.flatMap { node =>
+                    if (node.diSuccessors.isEmpty) {
+                        Iterable((node, currentNodeEnvs(node)))
+                    } else {
+                        node.diSuccessors.toList.sortBy(_.outer).map { successor =>
+                            (successor, pipe(successor, currentNodeEnvs(node)))
+                        }
+                    }
                 }
+                sortedCurrentNodes = nextNodeEnvs.map(_._1).sortBy(_.outer)
+                currentNodeEnvs = nextNodeEnvs.groupMapReduce(_._1)(_._2) { (env, otherEnv) => env.join(otherEnv) }
+            }
 
-                pipe(currentNode, currentEnv)
+            sortedCurrentNodes.foldLeft(StringTreeEnvironment(Map.empty)) { (env, nextNode) =>
+                env.join(currentNodeEnvs(nextNode))
+            }
+        }
 
-            case Region(IfThenElse, _, entry) =>
-                val entryNode = limitedFlowGraph.get(entry)
-                val successors = entryNode.diSuccessors.map(_.outer).toList.sorted
-                val branches = (successors.head, successors.tail.head)
+        def processSelfLoop(entry: FlowGraphNode): StringTreeEnvironment = {
+            val resultEnv = pipe(entry, env)
+            // Looped operations that modify environment contents are not supported here
+            if (resultEnv != env) env.updateAll(StringTreeDynamicString)
+            else env
+        }
 
-                val envAfterEntry = pipe(entry, env)
-                val envAfterBranches = (pipe(branches._1, envAfterEntry), pipe(branches._2, envAfterEntry))
+        def processWhileLoop(entry: FlowGraphNode): StringTreeEnvironment = {
+            val entryNode = limitedFlowGraph.get(entry)
+            val envAfterEntry = pipe(entry, env)
 
-                envAfterBranches._1.join(envAfterBranches._2)
+            var resultEnv = envAfterEntry
+            var currentNode = entryNode.diSuccessors.head
+            while (currentNode != entryNode) {
+                resultEnv = pipe(currentNode.outer, resultEnv)
+                currentNode = currentNode.diSuccessors.head
+            }
 
-            case Region(IfThen, _, entry) =>
-                val entryNode = limitedFlowGraph.get(entry)
-                val (yesBranch, noBranch) = if (entryNode.diSuccessors.head.diSuccessors.nonEmpty) {
-                    (entryNode.diSuccessors.head, entryNode.diSuccessors.tail.head)
-                } else {
-                    (entryNode.diSuccessors.tail.head, entryNode.diSuccessors.head)
-                }
+            // Looped operations that modify environment contents are not supported here
+            if (resultEnv != envAfterEntry) envAfterEntry.updateAll(StringTreeDynamicString)
+            else envAfterEntry
+        }
 
-                val envAfterEntry = pipe(entry, env)
-                val envAfterBranches = (
-                    pipe(yesBranch.diSuccessors.head, pipe(yesBranch, envAfterEntry)),
-                    pipe(noBranch, envAfterEntry)
-                )
-
-                envAfterBranches._1.join(envAfterBranches._2)
-
-            case Region(Proper, _, entry) =>
-                val entryNode = limitedFlowGraph.get(entry)
-
+        def processNaturalLoop(entry: FlowGraphNode): StringTreeEnvironment = {
+            val entryPredecessors = limitedFlowGraph.get(entry).diPredecessors
+            val removedBackEdgesGraph = limitedFlowGraph.filterNot(
+                edgeP = edge =>
+                    edge.sources.toList.toSet.intersect(entryPredecessors).nonEmpty
+                        && edge.targets.contains(limitedFlowGraph.get(entry))
+            )
+            if (removedBackEdgesGraph.isCyclic) {
+                env.updateAll(StringTreeDynamicString)
+            } else {
+                // Handle resulting acyclic region
+                val entryNode = removedBackEdgesGraph.get(entry)
                 var sortedCurrentNodes = List(entryNode)
                 var currentNodeEnvs = Map((entryNode, pipe(entry, env)))
                 while (currentNodeEnvs.keys.exists(_.diSuccessors.nonEmpty)) {
@@ -101,69 +156,27 @@ object DataFlowAnalysis {
                     currentNodeEnvs = nextNodeEnvs.groupMapReduce(_._1)(_._2) { (env, otherEnv) => env.join(otherEnv) }
                 }
 
-                sortedCurrentNodes.foldLeft(StringTreeEnvironment(Map.empty)) { (env, nextNode) =>
+                val resultEnv = sortedCurrentNodes.foldLeft(StringTreeEnvironment(Map.empty)) { (env, nextNode) =>
                     env.join(currentNodeEnvs(nextNode))
                 }
 
-            case Region(SelfLoop, _, entry) =>
-                val resultEnv = pipe(entry, env)
-                // Looped operations that modify environment contents are not supported here
+                // Looped operations that modify string contents are not supported here
                 if (resultEnv != env) env.updateAll(StringTreeDynamicString)
                 else env
+            }
+        }
 
-            case Region(WhileLoop, _, entry) =>
-                val entryNode = limitedFlowGraph.get(entry)
-                val envAfterEntry = pipe(entry, env)
+        node match {
+            case Statement(pc) if pc >= 0 => flowFunctionByPc(pc)(env)
+            case Statement(_)             => env
 
-                var resultEnv = envAfterEntry
-                var currentNode = entryNode.diSuccessors.head
-                while (currentNode != entryNode) {
-                    resultEnv = pipe(currentNode.outer, resultEnv)
-                    currentNode = currentNode.diSuccessors.head
-                }
-
-                // Looped operations that modify environment contents are not supported here
-                if (resultEnv != envAfterEntry) envAfterEntry.updateAll(StringTreeDynamicString)
-                else envAfterEntry
-
-            case Region(NaturalLoop, _, entry) =>
-                val entryPredecessors = limitedFlowGraph.get(entry).diPredecessors
-                val removedBackEdgesGraph = limitedFlowGraph.filterNot(
-                    edgeP = edge =>
-                        edge.sources.toList.toSet.intersect(entryPredecessors).nonEmpty
-                            && edge.targets.contains(limitedFlowGraph.get(entry))
-                )
-                if (removedBackEdgesGraph.isCyclic) {
-                    env.updateAll(StringTreeDynamicString)
-                } else {
-                    // Handle resulting acyclic region
-                    val entryNode = removedBackEdgesGraph.get(entry)
-                    var sortedCurrentNodes = List(entryNode)
-                    var currentNodeEnvs = Map((entryNode, pipe(entry, env)))
-                    while (currentNodeEnvs.keys.exists(_.diSuccessors.nonEmpty)) {
-                        val nextNodeEnvs = sortedCurrentNodes.flatMap { node =>
-                            if (node.diSuccessors.isEmpty) {
-                                Iterable((node, currentNodeEnvs(node)))
-                            } else {
-                                node.diSuccessors.toList.sortBy(_.outer).map { successor =>
-                                    (successor, pipe(successor, currentNodeEnvs(node)))
-                                }
-                            }
-                        }
-                        sortedCurrentNodes = nextNodeEnvs.map(_._1).sortBy(_.outer)
-                        currentNodeEnvs = nextNodeEnvs.groupMapReduce(_._1)(_._2) { (env, otherEnv) =>
-                            env.join(otherEnv)
-                        }
-                    }
-
-                    val resultEnv = sortedCurrentNodes.foldLeft(StringTreeEnvironment(Map.empty)) { (env, nextNode) =>
-                        env.join(currentNodeEnvs(nextNode))
-                    }
-
-                    // Looped operations that modify string contents are not supported here
-                    if (resultEnv != env) env.updateAll(StringTreeDynamicString)
-                    else env
-                }
+            case Region(Block, _, entry)       => processBlock(entry)
+            case Region(IfThenElse, _, entry)  => processIfThenElse(entry)
+            case Region(IfThen, _, entry)      => processIfThen(entry)
+            case Region(Proper, _, entry)      => processProper(entry)
+            case Region(SelfLoop, _, entry)    => processSelfLoop(entry)
+            case Region(WhileLoop, _, entry)   => processWhileLoop(entry)
+            case Region(NaturalLoop, _, entry) => processNaturalLoop(entry)
 
             case _ => env
         }
