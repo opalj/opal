fd2a2bea9e82190d51ab90dfd926c75211de84d3
LukasKlenner <klenner-lukas@t-online.de>
2024-08-05 10:30:45 +0200

    move PDUVar to br package; apply further changes from upstream

--- OPAL/br/src/main/scala/org/opalj/br/PDUVar.scala
+++ OPAL/br/src/main/scala/org/opalj/br/PDUVar.scala
@@ -1,7 +1,6 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
-package org.opalj.tac
+package org.opalj.br
 
-import org.opalj.ai.PCs
 import org.opalj.collection.immutable.IntTrieSet
 import org.opalj.value.ValueInformation
 
@@ -31,13 +30,37 @@ abstract class PDUVar[+Value <: ValueInformation] {
      * For more information see [[DUVar.definedBy]]
      */
     def defPCs: PCs
+}
 
-    def toValueOriginForm(implicit pcToIndex: Array[Int]): DUVar[Value]
+class PDVar[+Value <: ValueInformation /*org.opalj.ai.ValuesDomain#DomainValue*/ ] private (
+    val value:  Value,
+    val usePCs: PCs
+) extends PDUVar[Value] {
+
+    def defPCs: Nothing = throw new UnsupportedOperationException
+
+    override def equals(other: Any): Boolean = {
+        other match {
+            case that: PDVar[_] => this.usePCs == that.usePCs
+            case _              => false
+        }
+    }
+
+    override def toString: String = {
+        s"PDVar(usePCs=${usePCs.mkString("{", ",", "}")},value=$value)"
+    }
+}
+
+object PDVar {
+
+    def apply[Value <: ValueInformation](value: Value, useSites: IntTrieSet): PDVar[Value] = new PDVar(value, useSites)
+
+    def unapply[Value <: ValueInformation](d: PDVar[Value]): Some[(Value, IntTrieSet)] = Some((d.value, d.usePCs))
 }
 
 class PUVar[+Value <: ValueInformation /*org.opalj.ai.ValuesDomain#DomainValue*/ ] private (
-        val value:                 Value,
-        val defPCs: PCs
+    val value:  Value,
+    val defPCs: PCs
 ) extends PDUVar[Value] {
 
     def usePCs: Nothing = throw new UnsupportedOperationException
@@ -50,24 +73,14 @@ class PUVar[+Value <: ValueInformation /*org.opalj.ai.ValuesDomain#DomainValue*/
     }
 
     override def toString: String = {
-        s"PUVar(defSites=${defPCs.mkString("{", ",", "}")},value=$value)"
+        s"PUVar(defPCs=${defPCs.mkString("{", ",", "}")},value=$value)"
     }
 
-    override def toValueOriginForm(
-        implicit pcToIndex: Array[Int]
-    ): UVar[Value] = UVar(value, valueOriginsOfPCs(defPCs, pcToIndex))
 }
 
 object PUVar {
 
-    def apply(d: org.opalj.ai.ValuesDomain)(
-        value:    d.DomainValue,
-        defPCs:   PCs
-    ): PUVar[d.DomainValue] = {
-        new PUVar[d.DomainValue](value, defPCs)
-    }
+    def apply[Value <: ValueInformation](value: Value, defPCs: IntTrieSet): PUVar[Value] = new PUVar(value, defPCs)
 
-    def apply[Value <: ValueInformation](value: Value, defSites: IntTrieSet): PUVar[Value] = new PUVar(value, defSites)
-
-    def unapply[Value <: ValueInformation](u: UVar[Value]): Some[(Value, IntTrieSet)] = Some((u.value, u.defSites))
+    def unapply[Value <: ValueInformation](u: PUVar[Value]): Some[(Value, IntTrieSet)] = Some((u.value, u.defPCs))
 }
