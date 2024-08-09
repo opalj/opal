b267e460249db8adb6a0f88a9afd379ec37e193d
roterEmil <roth@cs.tu-darmstadt.de>
2024-08-09 10:06:20 +0200

    added points-to interaction logger

--- OPAL/xl/src/main/scala/org/opalj/xl/logger/PointsToInteractionLogger.scala
+++ OPAL/xl/src/main/scala/org/opalj/xl/logger/PointsToInteractionLogger.scala
@@ -3,8 +3,17 @@ package org.opalj
 package xl
 package logger
 
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.Set
+
+import org.opalj.br.Field
+
 object PointsToInteractionLogger {
     val javaToNativeCalls = new scala.collection.mutable.HashMap[String, Array[Long]]()
 
     val nativeToJavaCalls = new scala.collection.mutable.HashMap[String, Array[Long]]()
+
+    val nativeToJavaFieldWrites = new HashMap[Field, Array[Long]]()
+
+    val nativeToJavaFieldReads = Set.empty[Field]
 }
