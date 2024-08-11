e7ae24eba2f616fdb85da1eb417d228652459b4b
S. Aranda-Simbron <sofia.arandasimbron@stud.tu-darmstadt.de>
2024-08-11 17:39:57 +0200

    deleted Hello programs

--- OPAL/tactobc/src/test/resources/javaFiles/HelloSofi.java
+++
@@ -1,8 +0,0 @@
-public class HelloSofi {
-    public static void main(String[] args) {
-        Object string = new Object();
-        System.out.println(string.toString());
-        string = String.valueOf(1.1d);
-        System.out.println("Successfully succeedded, amazing piece of code: " + string);
-    }
-}
