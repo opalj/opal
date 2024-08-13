74d5bdf57bf1f2c9c1c2e5ba09763eebccd1f728
Julius Naeumann <julius.naeumann@gmail.com>
2024-08-13 09:46:27 +0200

    removed old benchmarks

--- OPAL/xl/src/main/scala/org/opalj/xl/javaanalyses/benchmark/bidirectional/stateaccessing/Person.java
+++
@@ -1,6 +0,0 @@
-package org.opalj.xl.javaanalyses.benchmark.bidirectional.stateaccessing;
-
-public class Person {
-    public String name;
-    public int number;
-}
--- OPAL/xl/src/main/scala/org/opalj/xl/javaanalyses/benchmark/cyclic/executionTODO/Main.java
+++
@@ -1,9 +0,0 @@
-package org.opalj.xl.javaanalyses.benchmark.cyclic.executionTODO;
-
-import javax.script.ScriptException;
-/*
-public class Main {
-    public static void main(String args[]) throws ScriptException, NoSuchMethodException {
-        new Cyclic().f(10);
-    }
-} */
