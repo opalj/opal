b68c7656d800f4ad0efa13ef88f4592c737b41ec
klein648 <lukas.klein@in-serm.de>
2024-08-24 11:36:54 +0200

    added empty line on print message

--- TOOLS/ce/src/main/scala/org.opalj.ce/FileLocator.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/FileLocator.scala
@@ -1,49 +1,48 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
-package `org.opalj.ce`
+package org.opalj.ce
 
 import com.typesafe.config.Config
 import java.nio.file.attribute.BasicFileAttributes
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file._
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters._
 
 class FileLocator(var config: Config)  {
     println("FileLocator created. Initializing...")
-    def LocateConfigurationFiles(): Unit = {
-
-    }
 
     def getProjectRoot: String = {
-      val projectRoot = this.config.getString("user.dir")
-      println("Searching in the following directory: " + projectRoot)
-      projectRoot
+        val projectRoot = this.config.getString("user.dir")
+        println("Searching in the following directory: " + projectRoot)
+        projectRoot
     }
 
     def getConfigurationFilenames : mutable.Buffer[String] = {
-      val projectNames = this.config.getStringList("org.opalj.ce.configurationFilenames").asScala
+        val projectNames = this.config.getStringList("org.opalj.ce.configurationFilenames").asScala
 
-      println("Loaded the following Filenames: ")
-      for (filename <- projectNames) {
-        println(filename)
-      }
-      projectNames
+        println("Loaded the following Filenames: ")
+        for (filename <- projectNames) {
+            println(filename)
+        }
+        println()
+        projectNames
     }
 
-    def SearchFiles() : mutable.Buffer[Path] = {
-      val projectNames = this.getConfigurationFilenames
-      val projectRoot = Paths.get(this.getProjectRoot)
-      val foundFiles = ListBuffer[Path]()
+    def SearchFiles : mutable.Buffer[Path] = {
+        val projectNames = this.getConfigurationFilenames
+        val projectRoot = Paths.get(this.getProjectRoot)
+        val foundFiles = ListBuffer[Path]()
 
-      Files.walkFileTree(projectRoot, new java.nio.file.SimpleFileVisitor[Path]() {
-        override def visitFile(file: Path, attrs: BasicFileAttributes): java.nio.file.FileVisitResult = {
-          if (projectNames.contains(file.getFileName.toString)) {
-            foundFiles += file
-            println(s"Found file: ${file.toString}")
-          }
-          java.nio.file.FileVisitResult.CONTINUE
-        }
-      })
-      foundFiles
+        Files.walkFileTree(projectRoot, new java.nio.file.SimpleFileVisitor[Path]() {
+            override def visitFile(file: Path, attrs: BasicFileAttributes): java.nio.file.FileVisitResult = {
+                if (projectNames.contains(file.getFileName.toString)) {
+                    foundFiles += file
+                    println(s"Found file: ${file.toString}")
+                }
+            java.nio.file.FileVisitResult.CONTINUE
+            }
+        })
+    println()
+    foundFiles
     }
 }
