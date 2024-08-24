e5a8bb60baa681d0eb92828cc3276a5cda82f94c
klein648 <lukas.klein@in-serm.de>
2024-08-24 11:37:25 +0200

    Added test method for file locator and test method for comment parser

--- TOOLS/ce/src/main/scala/org.opalj.ce/ce.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/ce.scala
@@ -1,25 +1,41 @@
 /* BSD 2-Clause License - see OPAL/LICENSE for details. */
-package `org.opalj.ce`
+package org.opalj.ce
 
-import com.typesafe.config.{Config,ConfigFactory}
+import com.typesafe.config._
 
 /*
 *   Standalone configuration explorer
-*
+*   This is the main method that runs the configuration explorer
 */
 object ce {
     def main(args: Array[String]): Unit = {
-      println("Configuration Explorer Started")
+        println("Configuration Explorer Started")
 
-      // Load config with default filename for this application
-      val conf = this.LoadConfig()
+        // Load config with default filename for this application
+        val conf = this.LoadConfig()
 
-      val locator = new FileLocator(conf)
-      locator.SearchFiles()
+        println(conf.getObject("org.opalj"))
+
+        val locator = new FileLocator(conf)
+        val filepaths = locator.SearchFiles
+
+        // Bulk Imports all the configs
+        val CPW = new CommentParserWrapper
+        val configs = CPW.IterateConfigs(filepaths)
+        println(configs)
+
+        println()
+        println(conf.getObject("org.opalj"))
+
+        val SubstringTest: String = "\"This is a test\""
+        println(SubstringTest.substring(2))
     }
 
     def LoadConfig() : Config = {
-      println("Loading configuration")
-      ConfigFactory.load("ce")
+        println("Loading configuration")
+        val conf = ConfigFactory.load("ce")
+        println(conf.getClass)
+
+        conf
     }
 }
