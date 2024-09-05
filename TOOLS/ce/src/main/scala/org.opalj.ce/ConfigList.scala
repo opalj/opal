e6f5f533a3f39f99ca87cc48c3e31b2116c58ff9
klein648 <lukas.klein@in-serm.de>
2024-09-05 10:53:11 +0200

    FIX: ConfigList did not inherit from ConfigNode

--- TOOLS/ce/src/main/scala/org.opalj.ce/ConfigList.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/ConfigList.scala
@@ -2,5 +2,5 @@ package org.opalj.ce
 
 import scala.collection.mutable.ListBuffer
 
-case class ConfigList(entries: ListBuffer[ConfigNode], comment: Comment)
+case class ConfigList(entries: ListBuffer[ConfigNode], comment: Comment) extends ConfigNode
 
