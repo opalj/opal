b2d2b0b039b1dbe13e0ace1f53d27fc4c8439ccf
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2024-09-05 10:36:26 +0200

    Make excluded packages configurable

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/MethodStringFlowAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/MethodStringFlowAnalysis.scala
@@ -5,6 +5,8 @@ package fpcf
 package analyses
 package string
 
+import scala.jdk.CollectionConverters._
+
 import org.opalj.br.Method
 import org.opalj.br.analyses.DeclaredMethods
 import org.opalj.br.analyses.DeclaredMethodsKey
@@ -18,6 +20,9 @@ import org.opalj.fpcf.InterimResult
 import org.opalj.fpcf.ProperPropertyComputationResult
 import org.opalj.fpcf.Result
 import org.opalj.fpcf.SomeEPS
+import org.opalj.log.Error
+import org.opalj.log.Info
+import org.opalj.log.OPALLogger.logOnce
 import org.opalj.tac.fpcf.analyses.string.flowanalysis.DataFlowAnalysis
 import org.opalj.tac.fpcf.analyses.string.flowanalysis.FlowGraph
 import org.opalj.tac.fpcf.analyses.string.flowanalysis.Statement
@@ -31,12 +36,36 @@ import org.opalj.tac.fpcf.properties.string.StringFlowFunctionProperty
  */
 class MethodStringFlowAnalysis(override val project: SomeProject) extends FPCFAnalysis {
 
+    private final val ConfigLogCategory = "analysis configuration - method string flow analysis"
+
+    private val excludedPackages: Seq[String] = {
+        val packages =
+            try {
+                project.config.getStringList(MethodStringFlowAnalysis.ExcludedPackagesConfigKey).asScala
+            } catch {
+                case t: Throwable =>
+                    logOnce {
+                        Error(
+                            ConfigLogCategory,
+                            s"couldn't read: ${MethodStringFlowAnalysis.ExcludedPackagesConfigKey}",
+                            t
+                        )
+                    }
+                    Seq.empty[String]
+            }
+
+        logOnce(Info(ConfigLogCategory, s"${packages.size} packages are excluded from string flow analysis"))
+        packages.toSeq
+    }
+
     val declaredMethods: DeclaredMethods = project.get(DeclaredMethodsKey)
 
     def analyze(method: Method): ProperPropertyComputationResult = {
         val state = ComputationState(method, declaredMethods(method), ps(method, TACAI.key))
 
-        if (state.tacDependee.isRefinable) {
+        if (excludedPackages.exists(method.classFile.thisType.packageName.startsWith(_))) {
+            Result(state.entity, MethodStringFlow.lb)
+        } else if (state.tacDependee.isRefinable) {
             InterimResult.forUB(
                 state.entity,
                 MethodStringFlow.ub,
@@ -108,4 +137,9 @@ class MethodStringFlowAnalysis(override val project: SomeProject) extends FPCFAn
         val startEnv = state.getStartEnvAndReset
         MethodStringFlow(state.flowAnalysis.compute(state.getFlowFunctionsByPC)(startEnv))
     }
+}
+
+object MethodStringFlowAnalysis {
+
+    final val ExcludedPackagesConfigKey = "org.opalj.fpcf.analyses.string.MethodStringFlowAnalysis.excludedPackages"
 }
