d40b07b421aa0a68c5cd481e0000c2900f2c6dba
klein648 <lukas.klein@in-serm.de>
2024-09-12 13:13:40 +0200

    Implemented separated information for comments

--- TOOLS/ce/src/main/scala/org.opalj.ce/Comment.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/Comment.scala
@@ -4,6 +4,11 @@ import scala.collection.mutable.ListBuffer
 
 class Comment {
     val commentBuffer: ListBuffer[String] = ListBuffer[String]()
+    val constraints : ListBuffer[String] = ListBuffer[String]()
+    val description : ListBuffer[String] = ListBuffer[String]()
+    var label = ""
+    var brief = ""
+    var datatype = ""
 
     // Adds a comment to the comment buffer. If a comment has multiple lines, this method will be called multiple times
     def addComment(comment: String): Unit = {
@@ -12,19 +17,54 @@ class Comment {
 
     // Tells the Comment object that no more comments will be added. The comment object then begins parsing the comments into advanced objects
     def commitComments(): Unit = {
-
+        for(line <- commentBuffer){
+            if(line.trim.startsWith("@label")) {
+                this.label = line.trim.stripPrefix("@label").trim
+            } else if(line.trim.startsWith("@brief")) {
+                brief = line.trim.stripPrefix("@brief").trim
+            } else if(line.trim.startsWith("@constraint")){
+                constraints.addOne(line.trim.stripPrefix("@constraint").trim)
+            } else if(line.trim.startsWith("@type")){
+                datatype = line.trim.stripPrefix("@type").trim
+            } else {
+                description.addOne(line.trim.stripPrefix("@description").trim)
+            }
+        }
     }
 
     def toHTML(): String = {
-        var HTMLString = "<p>"
-        for(line <- commentBuffer) {
-            HTMLString += line + "<br>"
+        var HTMLString = ""
+        if(isEmpty() == false) {
+            HTMLString += "<p>"
+            if(description.length > 0) {
+                HTMLString += "<b> Description: </b> <br>"
+                for (line <- description) {
+                    HTMLString += line + "<br>"
+                }
+                HTMLString += "<br> <br>"
+            }
+            if(datatype.length > 0) HTMLString += "<b> Type: </b>" + datatype + "<br>"
+            if(constraints.length > 0) {
+                HTMLString += "<b> Constraints: </b><br>"
+                for (line <- constraints) {
+                    HTMLString += line + "<br>"
+                }
+            }
+            HTMLString += "</p>"
         }
-        HTMLString += "</p>"
         return HTMLString
     }
 
-    override def toString() : String = {
-        commentBuffer.toString()
+    def isEmpty(): Boolean = {
+        return commentBuffer.length == 0
+    }
+
+    def printObject() : Unit = {
+        println("CommentBuffer: " + commentBuffer.toString())
+        println("Constraints: " + constraints.toString())
+        println("Description: " + description.toString())
+        println("Label: " + label)
+        println("Brief: " + brief)
+        println("Type: " + datatype)
     }
 }
