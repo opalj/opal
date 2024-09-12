cd0df91f1bc0c990755b2b09d6118824704254a3
klein648 <lukas.klein@in-serm.de>
2024-09-12 13:14:20 +0200

    Implemented export to HTML

--- TOOLS/ce/src/main/scala/org.opalj.ce/ce.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/ce.scala
@@ -3,6 +3,9 @@ package org.opalj.ce
 
 import com.typesafe.config._
 
+import java.io.File
+import java.nio.file.Paths
+
 /*
 *   Standalone configuration explorer
 *   This is the main method that runs the configuration explorer
@@ -14,21 +17,17 @@ object ce {
         // Load config with default filename for this application
         val conf = this.LoadConfig()
 
-        println(conf.getObject("org.opalj"))
-
         val locator = new FileLocator(conf)
         val filepaths = locator.SearchFiles
 
         // Bulk Imports all the configs
         val CPW = new CommentParserWrapper
         val configs = CPW.IterateConfigs(filepaths)
-        println(configs)
 
-        println()
-        println(conf.getObject("org.opalj"))
+        val HE = new HTMLExporter(configs, Paths.get(conf.getString("user.dir") + conf.getString("org.opalj.ce.html.template")))
+        // Join File Paths for export
 
-        val SubstringTest: String = "\"This is a test\""
-        println(SubstringTest.substring(2))
+        HE.exportHTML(new File(conf.getString("user.dir") + conf.getString("org.opalj.ce.html.export")), conf.getString("org.opalj.ce.html.headline"),conf.getString("org.opalj.ce.html.content"))
     }
 
     def LoadConfig() : Config = {
