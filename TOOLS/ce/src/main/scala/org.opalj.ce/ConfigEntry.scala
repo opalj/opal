35debad6bff278f87c2d6e548be35da72125bb75
klein648 <lukas.klein@in-serm.de>
2024-09-12 15:09:31 +0200

    Fixed view for html elements that are present in the config files

--- TOOLS/ce/src/main/scala/org.opalj.ce/ConfigEntry.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/ConfigEntry.scala
@@ -4,6 +4,7 @@ case class ConfigEntry(value: String, comment: Comment) extends ConfigNode {
     override def toHTML(label: String, HTMLHeadline : String, HTMLContent : String): String = {
         var HTMLString = ""
         var head = label
+        var brief = this.comment.brief
         if(this.comment.label.isEmpty != true) head = this.comment.label
         else if (head == "") head = value
 
@@ -11,8 +12,13 @@ case class ConfigEntry(value: String, comment: Comment) extends ConfigNode {
         var content = "<b>Value: </b><code>" + value.replace("<","&lt").replace(">","&gt") + "</code><br>"
         content += this.comment.toHTML()
 
+        // If there is no brief preview, put the value into it
+        if(comment.isEmpty()){
+            brief = "<b>Value: </b><code>" + value.replace("<","&lt").replace(">","&gt") + "</code>"
+        }
+
         // Adds Header line with collapse + expand options
-        HTMLString += HTMLHeadline.replace("$label",head).replace("$brief",this.comment.brief)
+        HTMLString += HTMLHeadline.replace("$label",head).replace("$brief",brief)
 
         // Add content below
         HTMLString += HTMLContent.replace("$content", content)
