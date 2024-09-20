0d762436e4d76306ed217eeaedd61620768c4df1
roterEmil <23134918+roterEmil@users.noreply.github.com>
2024-09-20 14:13:43 +0200

    Update PrematurelyReadOfFinalField.java due to Dominiks review

--- DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/immutability/openworld/assignability/advanced_counter_examples/PrematurelyReadOfFinalField.java
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/fixtures/immutability/openworld/assignability/advanced_counter_examples/PrematurelyReadOfFinalField.java
@@ -13,23 +13,14 @@ public class PrematurelyReadOfFinalField {
     static int n = 5;
 
     public static void main(String[] args) {
-        System.out.println("Value A.X before constructor:" + PrematurelyReadOfFinalField.n);
         C c = new C();
-        System.out.println("Value A.X after constructor:" + PrematurelyReadOfFinalField.n);
-        System.out.println("Value C.x after constructor:" + c.x );
     }
 
 }
 class B {
-
     B() {
         PrematurelyReadOfFinalField.n = ((C) this).x;
     }
-
-    void b(C c) {
-        PrematurelyReadOfFinalField.n = c.x;
-    }
-
 }
 
 class C extends B{
@@ -39,7 +30,6 @@ class C extends B{
 
     C() {
         super();
-        //this.b(this);
         x = 3;
     }
 }
