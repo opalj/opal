db2daf09d1576fe9ed2ec755e5dad1ab0796eec8
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2024-09-23 08:55:12 +0200

    Improve equality check for supported types

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/l3/interpretation/L3FieldReadInterpreter.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/string/l3/interpretation/L3FieldReadInterpreter.scala
@@ -215,5 +215,5 @@ object L3FieldReadInterpreter {
      * Checks whether the given type is supported by the field read analysis, i.e. if it may contain values desirable
      * AND resolvable by the string analysis as a whole.
      */
-    private def isSupportedType(fieldType: FieldType): Boolean = fieldType.isBaseType || fieldType == ObjectType.String
+    private def isSupportedType(fieldType: FieldType): Boolean = fieldType.isBaseType || (fieldType eq ObjectType.String)
 }
