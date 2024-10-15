6afb6935e8b09461a7f5e82963b74550ffbe3e2d
Julius Naeumann <julius.naeumann@gmail.com>
2024-10-15 16:17:07 +0200

    Added AndroidAnalysisDemo

---
+++ DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/AndroidAnalysisDemo.scala
@@ -0,0 +1,101 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj
+package fpcf
+package analyses
+
+import org.opalj.ai.domain
+import org.opalj.ai.fpcf.properties.AIDomainFactoryKey
+import org.opalj.br.analyses.cg.InitialEntryPointsKey
+import org.opalj.br.analyses.{BasicReport, Project, ProjectAnalysisApplication}
+import org.opalj.br.fpcf.PropertyStoreKey
+import org.opalj.tac.cg.CFA_1_0_CallGraphKey
+import org.opalj.tac.cg.android.AndroidManifestKey
+import org.opalj.util.PerformanceEvaluation.time
+import org.opalj.util.Seconds
+
+import java.net.URL
+import java.nio.file.Paths
+import scala.xml.XML
+
+object AndroidAnalysisDemo extends ProjectAnalysisApplication {
+
+    override def title: String = "Generate Callgraph for Android App"
+
+    override def description: String = "Parses AndroidManifest.xml to precisely determine entry points"
+
+    override def doAnalyze(
+                            project:       Project[URL],
+                            parameters:    Seq[String],
+                            isInterrupted: () => Boolean
+                          ): BasicReport = {
+        val manifestPath = parameters.collectFirst {
+            case param if param.startsWith("-manifest=") => param.split("=")(1)
+        }
+        if (manifestPath.isEmpty)
+            throw new IllegalArgumentException("Pass path to (non-binary) manifest with -manifest=path/to/AndroidManifest.xml")
+        val result = analyze(project, manifestPath.get)
+        BasicReport(result)
+    }
+
+    override def checkAnalysisSpecificParameters(parameters: Seq[String]): Iterable[String] = {
+        val remainingParameters =
+            parameters.filter { p =>
+                  !p.startsWith("-manifest=")
+            }
+        super.checkAnalysisSpecificParameters(remainingParameters)
+    }
+
+    def analyze(project: Project[URL],
+                manifestPath: String): String = {
+        if (project.config.getString(InitialEntryPointsKey.ConfigKey) != "org.opalj.tac.cg.android.AndroidEntryPointsFinder"){
+            throw new IllegalArgumentException("Aborting. Set initial entry point finder to AndroidEntryPointsFinder to analyze android apps!")
+        }
+        var propertyStoreTime: Seconds = Seconds.None
+        var callGraphTime: Seconds = Seconds.None
+        project.updateProjectInformationKeyInitializationData(AIDomainFactoryKey) { _ =>
+            Set[Class[_ <: AnyRef]](classOf[domain.l2.DefaultPerformInvocationsDomainWithCFG[URL]])
+        }
+
+        project.updateProjectInformationKeyInitializationData(AndroidManifestKey) {
+            _ =>
+                val url = project.allMethodsWithBodyWithContext.map(_.source)(0)
+                val jarPath = url.getPath
+                val jarFilePath = jarPath.substring("file:".length, jarPath.indexOf("!"))
+                val jarDirectory = Paths.get(jarFilePath).getParent
+                val manifestPath = jarDirectory.resolve("../AndroidManifest.xml")
+                val xml = XML.loadFile(manifestPath.toString)
+                xml
+        }
+
+        implicit val ps: PropertyStore = time { project.get(PropertyStoreKey) } { t => propertyStoreTime = t.toSeconds }
+
+        val cg = time {
+            project.get(CFA_1_0_CallGraphKey)
+        } { t => callGraphTime = t.toSeconds }
+
+        try {
+            ps.shutdown()
+        } catch {
+            case t: Throwable =>
+                Console.err.println("PropertyStore shutdown failed: ")
+                t.printStackTrace()
+        }
+
+
+        val reachableContexts = cg.reachableMethods().to(Iterable)
+        val reachableMethods = reachableContexts.map(_.method).toSet
+
+        val numEdges = cg.numEdges
+
+        s"""
+           | Num Reachable Methods: ${reachableMethods.size}
+           | Num Edges: ${numEdges}
+           |
+           | CG Analysis took : $callGraphTime seconds
+           |
+           | level: ${project.getProjectInformationKeyInitializationData(AIDomainFactoryKey)}
+           | propertyStore: ${ps.getClass}
+           |
+           |""".stripMargin
+    }
+}
