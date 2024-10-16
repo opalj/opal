29e6e76ae27f3d1a7b24fc31204a8b7fcf7e327a
Julius Naeumann <julius.naeumann@gmail.com>
2024-10-16 09:50:34 +0200

    format

--- DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/AndroidAnalysisDemo.scala
+++ DEVELOPING_OPAL/demos/src/main/scala/org/opalj/fpcf/analyses/AndroidAnalysisDemo.scala
@@ -21,81 +21,83 @@ import scala.xml.XML
 
 object AndroidAnalysisDemo extends ProjectAnalysisApplication {
 
-    override def title: String = "Generate Callgraph for Android App"
+  override def title: String = "Generate Callgraph for Android App"
 
-    override def description: String = "Parses AndroidManifest.xml to determine entry points"
+  override def description: String = "Parses AndroidManifest.xml to determine entry points"
 
-    override def doAnalyze(
-                            project:       Project[URL],
-                            parameters:    Seq[String],
-                            isInterrupted: () => Boolean
-                          ): BasicReport = {
-        val manifestPath = parameters.collectFirst {
-            case param if param.startsWith("-manifest=") => param.split("=")(1)
-        }
-        if (manifestPath.isEmpty)
-            throw new IllegalArgumentException("Pass path to (non-binary) manifest with -manifest=path/to/AndroidManifest.xml")
-        var newConfig = project.config
-        newConfig = newConfig.withValue(
-            InitialEntryPointsKey.ConfigKey,
-            ConfigValueFactory.fromAnyRef("org.opalj.tac.cg.android.AndroidEntryPointsFinder")
-        )
-        val newProject = Project.recreate(project, newConfig)
-        val result = analyze(newProject, manifestPath.get)
-        BasicReport(result)
+  override def doAnalyze(
+                          project: Project[URL],
+                          parameters: Seq[String],
+                          isInterrupted: () => Boolean
+                        ): BasicReport = {
+    val manifestPath = parameters.collectFirst {
+      case param if param.startsWith("-manifest=") => param.split("=")(1)
+    }
+    if (manifestPath.isEmpty)
+      throw new IllegalArgumentException("Pass path to (non-binary) manifest with -manifest=path/to/AndroidManifest.xml")
+    var newConfig = project.config
+    newConfig = newConfig.withValue(
+      InitialEntryPointsKey.ConfigKey,
+      ConfigValueFactory.fromAnyRef("org.opalj.tac.cg.android.AndroidEntryPointsFinder")
+    )
+    val newProject = Project.recreate(project, newConfig)
+    val result = analyze(newProject, manifestPath.get)
+    BasicReport(result)
+  }
+
+  override def checkAnalysisSpecificParameters(parameters: Seq[String]): Iterable[String] = {
+    val remainingParameters =
+      parameters.filter { p =>
+        !p.startsWith("-manifest=")
+      }
+    super.checkAnalysisSpecificParameters(remainingParameters)
+  }
+
+  def analyze(project: Project[URL],
+              manifestPath: String): String = {
+
+
+    var propertyStoreTime: Seconds = Seconds.None
+    var callGraphTime: Seconds = Seconds.None
+    project.updateProjectInformationKeyInitializationData(AIDomainFactoryKey) { _ =>
+      Set[Class[_ <: AnyRef]](classOf[domain.l2.DefaultPerformInvocationsDomainWithCFG[URL]])
     }
 
-    override def checkAnalysisSpecificParameters(parameters: Seq[String]): Iterable[String] = {
-        val remainingParameters =
-            parameters.filter { p =>
-                  !p.startsWith("-manifest=")
-            }
-        super.checkAnalysisSpecificParameters(remainingParameters)
+    project.updateProjectInformationKeyInitializationData(AndroidManifestKey) {
+      _ => XML.loadFile(manifestPath)
     }
 
-    def analyze(project: Project[URL],
-                manifestPath: String): String = {
+    implicit val ps: PropertyStore = time {
+      project.get(PropertyStoreKey)
+    } { t => propertyStoreTime = t.toSeconds }
 
+    val cg = time {
+      project.get(CFA_1_0_CallGraphKey)
+    } { t => callGraphTime = t.toSeconds }
 
-        var propertyStoreTime: Seconds = Seconds.None
-        var callGraphTime: Seconds = Seconds.None
-        project.updateProjectInformationKeyInitializationData(AIDomainFactoryKey) { _ =>
-            Set[Class[_ <: AnyRef]](classOf[domain.l2.DefaultPerformInvocationsDomainWithCFG[URL]])
-        }
-
-        project.updateProjectInformationKeyInitializationData(AndroidManifestKey) {
-            _ => XML.loadFile(manifestPath)
-        }
-
-        implicit val ps: PropertyStore = time { project.get(PropertyStoreKey) } { t => propertyStoreTime = t.toSeconds }
-
-        val cg = time {
-            project.get(CFA_1_0_CallGraphKey)
-        } { t => callGraphTime = t.toSeconds }
-
-        try {
-            ps.shutdown()
-        } catch {
-            case t: Throwable =>
-                Console.err.println("PropertyStore shutdown failed: ")
-                t.printStackTrace()
-        }
-
-
-        val reachableContexts = cg.reachableMethods().to(Iterable)
-        val reachableMethods = reachableContexts.map(_.method).toSet
-
-        val numEdges = cg.numEdges
-
-        s"""
-           | Num Reachable Methods: ${reachableMethods.size}
-           | Num Edges: ${numEdges}
-           |
-           | CG Analysis took : $callGraphTime seconds
-           |
-           | level: ${project.getProjectInformationKeyInitializationData(AIDomainFactoryKey)}
-           | propertyStore: ${ps.getClass}
-           |
-           |""".stripMargin
+    try {
+      ps.shutdown()
+    } catch {
+      case t: Throwable =>
+        Console.err.println("PropertyStore shutdown failed: ")
+        t.printStackTrace()
     }
+
+
+    val reachableContexts = cg.reachableMethods().to(Iterable)
+    val reachableMethods = reachableContexts.map(_.method).toSet
+
+    val numEdges = cg.numEdges
+
+    s"""
+       | Num Reachable Methods: ${reachableMethods.size}
+       | Num Edges: ${numEdges}
+       |
+       | CG Analysis took : $callGraphTime seconds
+       |
+       | level: ${project.getProjectInformationKeyInitializationData(AIDomainFactoryKey)}
+       | propertyStore: ${ps.getClass}
+       |
+       |""".stripMargin
+  }
 }
