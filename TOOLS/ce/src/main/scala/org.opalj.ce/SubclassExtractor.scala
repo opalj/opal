480eda1c7a553f06de29696fbae8730571363f88
klein648 <lukas.klein@in-serm.de>
2024-10-18 17:14:12 +0200

    Added comments

--- TOOLS/ce/src/main/scala/org.opalj.ce/SubclassExtractor.scala
+++ TOOLS/ce/src/main/scala/org.opalj.ce/SubclassExtractor.scala
@@ -10,10 +10,20 @@ import org.opalj.br.ObjectType
 import org.opalj.br.ObjectType.unapply
 import org.opalj.br.analyses.Project
 
+/**
+ * The class subclassExtractor is a wrapper class around the bytecode representation project.
+ * It will fetch all subclasses from the opal project and prepares the bytecode hierarchies for querying
+ * @param f accepts any initialized FileLocator
+ */
 class SubclassExtractor(val f: FileLocator) {
     var classHierarchies: ListBuffer[ClassHierarchy] = new ListBuffer[ClassHierarchy]
     this.initialize()
 
+    /**
+     * This method queries the extracted class hierarchies for a class.
+     * @param root accepts a string class name in dot notation. E.g. "org.opalj.ce.ConfigNode"
+     * @return returns a set of the names of all subclasses of the root subclass
+     */
     def extractSubclasses(root: String): mutable.Set[String] = {
         val results = mutable.Set[String]()
         for (classHierarchy <- this.classHierarchies) {
@@ -27,6 +37,10 @@ class SubclassExtractor(val f: FileLocator) {
         results
     }
 
+    /**
+     * Method for fetching the class Hierarchies.
+     * It is run upon initialization and on demand for reloading the class hierarchies.
+     */
     def initialize(): Unit = {
         val files = f.FindJarArchives()
         for (file <- files) {
