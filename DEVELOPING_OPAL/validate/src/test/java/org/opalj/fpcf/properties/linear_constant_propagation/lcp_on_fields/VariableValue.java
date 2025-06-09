642366fccf44ac9e5ed607af8c89b66984f02891
Robin Körkemeier <robin.koerkemeier@gmx.de>
2025-06-09 11:41:35 +0200

    Adjust some documentation

--- DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/properties/linear_constant_propagation/lcp_on_fields/VariableValue.java
+++ DEVELOPING_OPAL/validate/src/test/java/org/opalj/fpcf/properties/linear_constant_propagation/lcp_on_fields/VariableValue.java
@@ -18,7 +18,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.CLASS)
 public @interface VariableValue {
     /**
-     * The PC of this variable in the TAC
+     * The PC of this variable in the bytecode
      */
     int pc();
 }
