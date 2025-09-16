9c0ff160e0dd71f641db82250cc7ce0483993750
Johannes Düsing <j.duesing13@googlemail.com>
2025-09-16 14:44:48 +0200

    Make rta configuration analysis a ReachableMethodAnalysis. Move duplicated code to package object

--- OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/xta/TypePropagationAnalysis.scala
+++ OPAL/tac/src/main/scala/org/opalj/tac/fpcf/analyses/cg/xta/TypePropagationAnalysis.scala
@@ -510,43 +510,6 @@ final class TypePropagationAnalysis private[analyses] (
         }
     }
 
-    private def candidateMatchesTypeFilter(candidateType: ReferenceType, filterType: ReferenceType): Boolean = {
-        val answer = classHierarchy.isASubtypeOf(candidateType, filterType)
-
-        if (answer.isYesOrNo) {
-            // Here, we know for sure that the candidate type is or is not a subtype of the filter type.
-            answer.isYes
-        } else {
-            // If the answer is Unknown, we don't know for sure whether the candidate is a subtype of the filter type.
-            // However, ClassHierarchy returns Unknown even for cases where it is very unlikely that this is the case.
-            // Therefore, we take some more features into account to make the filtering more precise.
-
-            // Important: This decision is a possible but unlikely cause of unsoundness in the call graph!
-
-            // If the filter type is not a project type (i.e., it is external), we assume that any candidate type
-            // is a subtype. This can be any external type or project types for which we have incomplete supertype
-            // information.
-            // If the filter type IS a project type, we consider the candidate type not to be a subtype since this is
-            // very likely to be not the case. For the candidate type, there are two options: Either it is an external
-            // type, in which case the candidate type could only be a subtype if project types are available in the
-            // external type's project at compile time. This is very unlikely since external types are almost always
-            // from libraries (like the JDK) which are not available in the analysis context, and which were almost
-            // certainly compiled separately ("Separate Compilation Assumption").
-            // The other option is that the candidate is also a project type, in which case we should have gotten a
-            // definitive Yes/No answer before. Since we didn't get one, the candidate type probably has a supertype
-            // which is not a project type. In that case, the above argument applies similarly.
-
-            val filterTypeIsProjectType = if (filterType.isClassType) {
-                project.isProjectType(filterType.asClassType)
-            } else {
-                val at = filterType.asArrayType
-                project.isProjectType(at.elementType.asClassType)
-            }
-
-            !filterTypeIsProjectType
-        }
-    }
-
     private def propagateTypes[E >: Null <: TypeSetEntity](
         targetSetEntity: E,
         newTypes:        UIDSet[ReferenceType],
