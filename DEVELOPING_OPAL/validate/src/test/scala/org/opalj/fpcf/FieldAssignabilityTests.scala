11da1b4d3e36e396f2ab6d65d52988fbf27521ae
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-09-16 18:58:45 +0200

    Revert minimizing tests

--- DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/FieldAssignabilityTests.scala
+++ DEVELOPING_OPAL/validate/src/test/scala/org/opalj/fpcf/FieldAssignabilityTests.scala
@@ -16,8 +16,12 @@ import org.opalj.tac.fpcf.analyses.LazyFieldLocalityAnalysis
 import org.opalj.tac.fpcf.analyses.escape.LazyInterProceduralEscapeAnalysis
 import org.opalj.tac.fpcf.analyses.escape.LazyReturnValueFreshnessAnalysis
 import org.opalj.tac.fpcf.analyses.fieldaccess.EagerFieldAccessInformationAnalysis
+import org.opalj.tac.fpcf.analyses.fieldassignability.EagerL0FieldAssignabilityAnalysis
+import org.opalj.tac.fpcf.analyses.fieldassignability.EagerL1FieldAssignabilityAnalysis
 import org.opalj.tac.fpcf.analyses.fieldassignability.EagerL2FieldAssignabilityAnalysis
+import org.opalj.tac.fpcf.analyses.purity.L2PurityAnalysis
 import org.opalj.tac.fpcf.analyses.purity.LazyL2PurityAnalysis
+import org.opalj.tac.fpcf.analyses.purity.SystemOutLoggingAllExceptionRater
 
 /**
  * Tests the field assignability analysis
@@ -45,6 +49,49 @@ class FieldAssignabilityTests extends PropertiesTest {
 
     }
 
+    describe("no analysis is scheduled") {
+        val as = executeAnalyses(Set.empty)
+        as.propertyStore.shutdown()
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldAssignability"))
+    }
+    L2PurityAnalysis.setRater(Some(SystemOutLoggingAllExceptionRater))
+
+    describe("the org.opalj.fpcf.analyses.L0FieldAssignability is executed") {
+
+        val as = executeAnalyses(
+            Set(
+                EagerL0FieldAssignabilityAnalysis,
+                LazyStaticDataUsageAnalysis,
+                LazyL2PurityAnalysis,
+                LazyL0CompileTimeConstancyAnalysis,
+                LazyInterProceduralEscapeAnalysis,
+                LazyReturnValueFreshnessAnalysis,
+                LazyFieldLocalityAnalysis,
+                EagerFieldAccessInformationAnalysis
+            )
+        )
+        as.propertyStore.shutdown()
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldAssignability"))
+    }
+
+    describe("the org.opalj.fpcf.analyses.L1FieldAssignability is executed") {
+
+        val as = executeAnalyses(
+            Set(
+                EagerL1FieldAssignabilityAnalysis,
+                LazyStaticDataUsageAnalysis,
+                LazyL2PurityAnalysis,
+                LazyL0CompileTimeConstancyAnalysis,
+                LazyInterProceduralEscapeAnalysis,
+                LazyReturnValueFreshnessAnalysis,
+                LazyFieldLocalityAnalysis,
+                EagerFieldAccessInformationAnalysis
+            )
+        )
+        as.propertyStore.shutdown()
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldAssignability"))
+    }
+
     describe("the org.opalj.fpcf.analyses.L2FieldAssignability is executed") {
 
         val as = executeAnalyses(
@@ -60,10 +107,6 @@ class FieldAssignabilityTests extends PropertiesTest {
             )
         )
         as.propertyStore.shutdown()
-        validateProperties(
-            as,
-            fieldsWithAnnotations(as.project).filter(t => t._1.toJava.contains("advanced_counter_examples")),
-            Set("FieldAssignability")
-        )
+        validateProperties(as, fieldsWithAnnotations(as.project), Set("FieldAssignability"))
     }
 }
