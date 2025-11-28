a1962943b3d5b3d5581a7e18b9c7786fed6f8aa2
leraroden <leraroden1403@gmail.com>
2025-11-28 16:34:47 +0100

    Remove unnecessary files

--- OPAL/tac2bc/src/test/resources/single/SourceCode.java
+++
@@ -1,9 +0,0 @@
-public class SourceCode {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        int d = a + 5;
-        System.out.println(c + d);
-    }
-}
