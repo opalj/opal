7c50e9c15aee8124f3c86de0b754541a0142cc37
Sven Keidel <svenkeidel@pm.me>
2026-05-12 13:06:09 +0200

    Accept EntryPointContainer "declaring-class" and "declaringClass" keys

--- OPAL/br/src/main/scala/org/opalj/br/analyses/cg/EntryPointFinder.scala
+++ OPAL/br/src/main/scala/org/opalj/br/analyses/cg/EntryPointFinder.scala
@@ -380,7 +380,8 @@ trait ConfigurationEntryPointsFinder extends EntryPointFinder {
 
     // Implement ConfigReader manually to avoid issue with scala-reflect: https://github.com/opalj/JCG/issues/17
     implicit private val entryPointContainerReader: ConfigReader[EntryPointContainer] =
-        ConfigReader.forProduct3("declaringClass", "name", "descriptor")(new EntryPointContainer(_,_,_))
+        ConfigReader.forProduct3("declaring-class", "name", "descriptor")(EntryPointContainer.apply)
+          .orElse(ConfigReader.forProduct3("declaringClass", "name", "descriptor")(EntryPointContainer.apply))
 }
 
 /**
